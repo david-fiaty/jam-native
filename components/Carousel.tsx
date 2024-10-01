6f68b7686a8733f90cf3652229b048cf9ab10c80
david-fiaty <dfiaty@gmail.com>
2024-10-01 16:34:06 +0000

    q

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -43,17 +43,20 @@ const styles = StyleSheet.create({
 const data = [
   {
     id: 1,
-    content: 'aaaa',
+    title: 'Create better, together',
+    content: 'Welcome to the Jam app. Jam is a place to explore and experience artists and creatives from different backgrounds in West Africa.',
     link: 'aaa link',
   },
   {
     id: 2,
-    content: 'bbb',
+    title: 'Everything you need',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     link: 'bbb link',
   },
   {
     id: 3,
-    content: 'aaaa',
+    title: 'The place to excel',
+    content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.',
     link: 'ccc link',
   },
 ];
--- components/CarouselItem.tsx
+++ components/CarouselItem.tsx
@@ -8,26 +8,6 @@ type Props = {
 }
 
 const {width} = Dimensions.get('screen');
-const data = [
-  {
-    id: 1,
-    title: 'Create better, together',
-    content: 'Welcome to the Jam app. Jam is a place to explore and experience artists and creatives from different backgrounds in West Africa.',
-    link: 'aaa link',
-  },
-  {
-    id: 2,
-    title: 'Everything you need',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    link: 'bbb link',
-  },
-  {
-    id: 3,
-    title: 'The place to excel',
-    content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.',
-    link: 'ccc link',
-  },
-];
 
 export default function CarouselItem({item, index, scrollX}: Props) {
   const itemAnimation = useAnimatedStyle(() => {
@@ -47,7 +27,7 @@ export default function CarouselItem({item, index, scrollX}: Props) {
 
   return (  
     <Animated.View style={[styles.itemContainer, itemAnimation]}>
-      <Text>{index}</Text>
+      <Text>{item.title}</Text>
       <Text>{item.content}</Text>
     </Animated.View>
   );
