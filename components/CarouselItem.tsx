c00ef297e294379ff507477cf18a9acc87d38eb0
david-fiaty <dfiaty@gmail.com>
2024-10-01 16:23:17 +0000

    q

--- components/CarouselItem.tsx
+++ components/CarouselItem.tsx
@@ -37,15 +37,7 @@ export default function CarouselItem({item, index, scrollX}: Props) {
             [-width * 0.25, 0 , width * 0.25],
             Extrapolation.CLAMP,
           ),
-        }, 
-        {
-          scale: interpolate(
-            scrollX.value,
-            [(index - 1) * width, index * width, (index + 1) * width],
-            [0.9, 1 , 0.9],
-            Extrapolation.CLAMP,
-          ),
-        }
+        },
       ],
     };
   });
