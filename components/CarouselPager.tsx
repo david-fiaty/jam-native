7f9e61b2f212176d30d2d7a1f4ede81f0c98dce0
david-fiaty <dfiaty@gmail.com>
2024-10-01 14:45:03 +0000

    Update CarouselPager.tsx

--- components/CarouselPager.tsx
+++ components/CarouselPager.tsx
@@ -2,7 +2,7 @@ import { StyleSheet, View } from 'react-native';
 import Animated, { SharedValue, useAnimatedStyle, interpolate, Extrapolation } from 'react-native-reanimated';
 
 type Props = {
-  data: object,
+  data: [],
   pagerIndex: number,
   scrollX: SharedValue<number>
 }
@@ -11,7 +11,11 @@ export default function CarouselPager({ data, pagerIndex, scrollX }: Props) {
 
   return (  
     <View style={styles.container}>
-    
+      {data.map((_, index) => {
+        return (
+          <View key={index} style={styles.dot}></View>
+        );
+      })}
     </View>
   );
 }
@@ -23,5 +27,12 @@ const styles = StyleSheet.create({
     height: 60,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  dot: {
+    backgroundColor: 'black',
+    height: 8,
+    width: 8,
+    marginHorizontal: 2,
+    borderRadius: 8,
   },
 });
