f8b7507727f2be907cc65bf267d21ec3547c49c5
david-fiaty <dfiaty@gmail.com>
2024-10-01 14:14:37 +0000

    Update Carousel.tsx

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -3,35 +3,6 @@ import Animated, { useAnimatedScrollHandler, useSharedValue } from 'react-native
 import CarouselItem from './CarouselItem';
 import CarouselPagination from './CarouselPagination';
 
-export default function Carousel() {
-  const scrollX = useSharedValue(0);
-  const onScrollHandler = useAnimatedScrollHandler({
-    onScroll: (e) => {
-      scrollX.value = e.contentOffset.x;
-    },
-  });
-
-  return (  
-    <View style={styles.container}>
-      <Animated.FlatList 
-        data={data} 
-        renderItem={({item, index}) => <CarouselItem item={item} index={index} scrollX={scrollX}/>} 
-        horizontal={true}
-        pagingEnabled={true}
-        showsHorizontalScrollIndicator={false}
-        onScroll={onScrollHandler}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'green',
-    width: '100%',
-    height: 200,
-  },
-});
 
 const data = [
   {
@@ -50,3 +21,34 @@ const data = [
     link: 'ccc link',
   },
 ];
+
+export default function Carousel() {
+  const scrollX = useSharedValue(0);
+  const onScrollHandler = useAnimatedScrollHandler({
+    onScroll: (e) => {
+      scrollX.value = e.contentOffset.x;
+    },
+  });
+
+  return (  
+    <View style={styles.container}>
+      <Animated.FlatList 
+        data={data} 
+        renderItem={({item, index}) => <CarouselItem item={item} index={index} scrollX={scrollX}/>} 
+        horizontal={true}
+        pagingEnabled={true}
+        showsHorizontalScrollIndicator={false}
+        onScroll={onScrollHandler}
+      />
+
+      <CarouselPagination />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
