f73e97a7a15b2dde4d5c5e5ba19c8b4c1bc461c4
david-fiaty <dfiaty@gmail.com>
2024-10-03 13:09:45 +0000

    Update authentication.tsx

--- app/authentication.tsx
+++ app/authentication.tsx
@@ -1,21 +1,12 @@
-import { StyleSheet, Image, Platform } from 'react-native';
-import { ThemedText } from '@/components/ThemedText';
+import { StyleSheet, Text } from 'react-native';
+
 
 export default function AuthenticationScreen() {
   return (
-    <ThemedText type="title">AuthenticationScreen</ThemedText>
+    <Text>AuthenticationScreen</Text>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
-  },
+
 });
