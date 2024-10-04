72230002270b98bf1fb2944bfddb6b92f7993d0f
david-fiaty <dfiaty@gmail.com>
2024-10-04 23:03:58 +0000

    q

--- components/navigation/TopToolbar.tsx
+++ components/navigation/TopToolbar.tsx
@@ -1,12 +1,14 @@
 import { View, StyleSheet } from 'react-native';
 import { SvgImage } from '@/components/SvgImage';
 import SettingsMenu from '../menus/SettingsMenu';
+import NotificationsMenu from '../menus/NotificationsMenu';
 
 const TopToolbar = () => {
   return (
     <View style={styles.container}>
       <SvgImage uri={require('@/assets/images/jam-logo.png')} width={45} height={45} />  
       <SettingsMenu />  
+      <NotificationsMenu />
     </View>
   );
 };
