7c5bda7c1a52cd945de4ce459b204e6a7519351d
david-fiaty <dfiaty@gmail.com>
2024-10-04 18:00:12 +0000

    Update BottomNavigation.tsx

--- components/navigation/BottomNavigation.tsx
+++ components/navigation/BottomNavigation.tsx
@@ -1,13 +1,38 @@
 import { View, StyleSheet } from 'react-native';
+import { useRouter } from 'expo-router';
+import { Button } from '@rneui/themed';
+import { GlobalStyles } from '@/constants/GlobalStyles';
 
 const BottomNavigation = () => {
+  const router = useRouter();
+
   return (
-    <View></View>
+    <View style={styles.container}>
+      <Button 
+        title="About" 
+        type="clear" 
+        onPress={() => router.push('/about')} 
+        titleStyle={GlobalStyles.text}
+      />
+      <Button 
+        title="Legal" 
+        type="clear" 
+        onPress={() => router.push('/legal')} 
+        titleStyle={styles.text}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  text: GlobalStyles.text,
 });
 
 export default BottomNavigation;
