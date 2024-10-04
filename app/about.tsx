dd5b5cb809b4772dade4abc669fa79eb4d399533
david-fiaty <dfiaty@gmail.com>
2024-10-04 04:42:24 +0000

    Update about.tsx

--- app/about.tsx
+++ app/about.tsx
@@ -1,5 +1,6 @@
 import { StyleSheet, View, Text} from 'react-native';
 import { Button } from '@rneui/themed';
+import AntDesign from '@expo/vector-icons/AntDesign';
 import { useNavigation } from '@react-navigation/native';
 import { GlobalStyles } from '@/constants/GlobalStyles';
 
@@ -8,12 +9,15 @@ const AboutScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Button 
-        title="About" 
-        type="clear" 
-        onPress={() => navigation.goBack()}
-        titleStyle={GlobalStyles.text}
-      />
+      <View style={styles.navbarContainer}>
+        <AntDesign name="left" size={20} color={GlobalStyles.text.color} />
+        <Button 
+          title="About" 
+          type="clear" 
+          onPress={() => navigation.goBack()}
+          titleStyle={GlobalStyles.text}
+        />
+      </View>
       <Text style={{color: GlobalStyles.text.color}}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </Text>
@@ -22,6 +26,11 @@ const AboutScreen = () => {
 };
 
 const styles = StyleSheet.create({
+  navbarContainer: {
+    display: 'flex',
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+  },
   container: {
     display: 'flex',
     flexDirection: 'column', 
