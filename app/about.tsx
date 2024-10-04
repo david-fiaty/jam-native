7834618e675f1623629e06acf22e70cde400fe98
david-fiaty <dfiaty@gmail.com>
2024-10-04 04:32:37 +0000

    Update about.tsx

--- app/about.tsx
+++ app/about.tsx
@@ -1,4 +1,5 @@
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text} from 'react-native';
+import { Button } from '@rneui/themed';
 import { useNavigation } from '@react-navigation/native';
 import { GlobalStyles } from '@/constants/GlobalStyles';
 
@@ -7,9 +8,11 @@ const AboutScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Button
-        title="Go Back"
+      <Button 
+        title="About" 
+        type="clear" 
         onPress={() => navigation.goBack()}
+        titleStyle={GlobalStyles.text}
       />
       <Text style={{color: GlobalStyles.text.color}}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
