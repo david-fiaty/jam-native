36ee98f3d2b94c559d1d0349b7a6cfaf175faec6
david-fiaty <dfiaty@gmail.com>
2024-10-04 18:18:14 +0000

    Create JamList.tsx

---
+++ components/JamList.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+const JamList = () => {
+  return (
+    <View style={styles.container}>
+      <Text>DDD</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'green',
+  },
+});
+
+export default JamList;
