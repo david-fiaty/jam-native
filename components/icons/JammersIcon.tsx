9769a3350fbf87367b8c198deff9d976807c0012
david-fiaty <dfiaty@gmail.com>
2024-10-04 22:06:38 +0000

    Update JammersIcon.tsx

--- components/icons/JammersIcon.tsx
+++ components/icons/JammersIcon.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import { GlobalStyles } from '@/constants/GlobalStyles';
 
