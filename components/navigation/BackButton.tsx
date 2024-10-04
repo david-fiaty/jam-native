d5613f59cffc1dba8334f7c071cf758aeec85016
david-fiaty <dfiaty@gmail.com>
2024-10-04 05:31:09 +0000

    q

---
+++ components/navigation/BackButton.tsx
@@ -0,0 +1,13 @@
+import { View, StyleSheet } from 'react-native';
+
+const BackButton = () => {
+  return (
+    <View></View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default BackButton;
