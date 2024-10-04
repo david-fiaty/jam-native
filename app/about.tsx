670abb2b97f904413eab9eef89582fc39f31d296
david-fiaty <dfiaty@gmail.com>
2024-10-04 18:05:40 +0000

    q

--- app/about.tsx
+++ app/about.tsx
@@ -2,6 +2,7 @@ import { StyleSheet, View, Text} from 'react-native';
 import { GlobalStyles } from '@/constants/GlobalStyles';
 import TopNavigation from '@/components/navigation/TopNavigation';
 import BackButton from '@/components/navigation/BackButton';
+import BottomNavigation from '@/components/navigation/BottomNavigation';
 
 const About = () => {
   return (
@@ -11,6 +12,7 @@ const About = () => {
       <Text style={styles.text}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </Text>
+      <BottomNavigation />
     </View>
   );
 };
