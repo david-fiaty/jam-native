fa0a4abfee4ed1eaea139260bbdf16e87d2a9264
david-fiaty <dfiaty@gmail.com>
2024-10-04 21:54:14 +0000

    Update ItemToolbar.tsx

--- components/jam/ItemToolbar.tsx
+++ components/jam/ItemToolbar.tsx
@@ -1,5 +1,6 @@
 import { StyleSheet, View } from 'react-native';
 import SaveIcon from '../icons/SaveIcon';
+import ShareIcon from '../icons/ShareIcon';
 
 type Props = {
   item: object,
@@ -10,6 +11,7 @@ const ItemToolbar = ({item, index}: Props) => {
   return (
     <View style={styles.container}>
       <SaveIcon />
+      <ShareIcon />
     </View>
   );
 };
