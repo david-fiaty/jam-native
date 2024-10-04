87a0e1989f63a79f809a82460aca1f04f994f61b
david-fiaty <dfiaty@gmail.com>
2024-10-04 04:16:41 +0000

    Update about.tsx

--- app/about.tsx
+++ app/about.tsx
@@ -1,9 +1,16 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { GlobalStyles } from '@/constants/GlobalStyles';
 
 const AboutScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <View style={GlobalStyles.container}>
+      <Button
+        title="Go Back"
+        onPress={() => navigation.goBack()}
+      />
       <Text>About Screen</Text>
     </View>
   );
