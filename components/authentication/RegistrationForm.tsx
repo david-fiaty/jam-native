677e795f17ac6f46b3e34bde754656f19c4d0dd6
david-fiaty <dfiaty@gmail.com>
2024-10-04 05:33:19 +0000

    Create RegistrationForm.tsx

---
+++ components/authentication/RegistrationForm.tsx
@@ -0,0 +1,13 @@
+import { View, StyleSheet } from 'react-native';
+
+const RegistrationForm = () => {
+  return (
+    <View></View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default RegistrationForm;
