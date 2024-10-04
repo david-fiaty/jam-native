4cbea48a6af599b629310d94a2d269d075fd5de1
david-fiaty <dfiaty@gmail.com>
2024-10-04 18:00:15 +0000

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,53 +1,30 @@
 import { View, StyleSheet } from 'react-native';
-import { useRouter } from 'expo-router';
-import { Button } from '@rneui/themed';
 import Carousel from '../components/carousel/Controller';
 import { GlobalStyles } from '@/constants/GlobalStyles';
 import { SvgImage } from '@/components/SvgImage';
 import SkipButton from '@/components/buttons/SkipButton';
 import LoginButton from '@/components/buttons/LoginButton';
+import BottomNavigation from '@/components/navigation/BottomNavigation';
 
 const Index = () => {
-  const router = useRouter();
-
   return (  
-    <View style={GlobalStyles.container}>
+    <View style={styles.container}>
       <SvgImage uri={require('@/assets/images/jam-logo.png')} width={110} height={110} />    
       <View style={styles.carousel}>
         <Carousel />
       </View>
       <LoginButton />
       <SkipButton />
-
-      <View style={styles.buttonGroup}>
-        <Button 
-          title="About" 
-          type="clear" 
-          onPress={() => router.push('/about')} 
-          titleStyle={GlobalStyles.text}
-        />
-        <Button 
-          title="Legal" 
-          type="clear" 
-          onPress={() => router.push('/legal')} 
-          titleStyle={GlobalStyles.text}
-        />
-      </View>
+      <BottomNavigation />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: GlobalStyles.container,
   carousel: {
     justifyContent: "center",
     alignItems: 'center',
-  },
-  buttonGroup: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignContent: 'center',
-    alignItems: 'center',
-    marginTop: 20,
   },
 });
 
