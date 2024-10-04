94431f26c33bdb775ffdcbd3abcaa4ea527a9698
david-fiaty <dfiaty@gmail.com>
2024-10-04 17:35:22 +0000

    q

--- components/navigation/TopNavigation.tsx
+++ components/navigation/TopNavigation.tsx
@@ -4,14 +4,19 @@ import { SvgImage } from '@/components/SvgImage';
 
 const TopNavigation = () => {
   return (
-    <View style={GlobalStyles.logo}>
-      <SvgImage uri={require('@/assets/images/jam-logo.png')} />    
+    <View style={styles.container}>
+      <SvgImage uri={require('@/assets/images/jam-logo.png')} width={45} height={45} />    
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    marginBottom: 20,
+  },
 });
 
 export default TopNavigation;
