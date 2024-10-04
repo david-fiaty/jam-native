fa6d2a1379a8533893c42bf8ae5678a37b021855
david-fiaty <dfiaty@gmail.com>
2024-10-04 05:25:00 +0000

    q

---
+++ components/navigation/BottomLinks.tsx
@@ -0,0 +1,13 @@
+import { View, StyleSheet } from 'react-native';
+
+const BottomLinks = () => {
+  return (
+    <View></View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default BottomLinks;
---
+++ components/navigation/Navbar.tsx
@@ -0,0 +1,13 @@
+import { View, StyleSheet } from 'react-native';
+
+const Navbar = () => {
+  return (
+    <View></View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default Navbar;
