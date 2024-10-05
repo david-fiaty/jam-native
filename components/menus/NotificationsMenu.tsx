67f3951162e18fb3544e210e890d983013c3b92e
david-fiaty <dfiaty@gmail.com>
2024-10-05 18:00:17 +0000

    Update NotificationsMenu.tsx

--- components/menus/NotificationsMenu.tsx
+++ components/menus/NotificationsMenu.tsx
@@ -1,23 +1,78 @@
-import { StyleSheet, View, Text } from 'react-native';
-import { Colors, GlobalStyles } from '@/constants/GlobalStyles';
+import { useState } from 'react';
+import { StyleSheet, Modal, Pressable, View, FlatList, TouchableWithoutFeedback } from 'react-native';
+import NotificationsIcon from '../icons/NotificationsIcon';
+import MenuItem from './MenuItem';
+import { GlobalStyles } from '@/constants/GlobalStyles';
+import BackButton from '../navigation/BackButton';
 
 const NotificationsMenu = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  
   return (
     <View style={styles.container}>        
-      <Text>+20</Text>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(!modalVisible)}
+      >
+        <TouchableWithoutFeedback onPress={() => setModalVisible(!modalVisible)}>
+          <View style={GlobalStyles.modal.container}>
+            <TouchableWithoutFeedback>
+              <View style={GlobalStyles.modal.view}>
+                <Pressable onPress={() => setModalVisible(!modalVisible)}>
+                  <BackButton title="Settings" style={styles.title} />
+                </Pressable>
+                <FlatList 
+                  data={data} 
+                  horizontal={false}  
+                  style={styles.list}
+                  renderItem={({item, index}) => <MenuItem item={item} index={index} />} 
+                />
+              </View>
+              </TouchableWithoutFeedback>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
+      <Pressable onPress={() => setModalVisible(true)}>
+        <NotificationsIcon />
+      </Pressable>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: Colors.tertiary,
-    borderRadius: 40,
-    paddingTop: 3,
-    paddingRight: 6,
-    paddingBottom: 3,
-    paddingLeft: 6,
+  },
+  list: {
+    width: '100%',
+  },
+  title: {
+    ...GlobalStyles.text,
+    ...{
+      fontSize: 16,
+      fontWeight: 'bold',
+    },
   },
 });
+
+const data = [
+  {
+    'label': 'Account information',
+    'path': '/jams',
+  },
+  {
+    'label': 'Change password',
+    'path': '/jams',
+  },
+  {
+    'label': 'Change user name',
+    'path': '/jams',
+  },
+  {
+    'label': 'Delete account',
+    'path': '/jams',
+  },
+];
 
 export default NotificationsMenu;
