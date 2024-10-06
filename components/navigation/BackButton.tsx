b6faa62ddfbe29c1ab6ea30cd512ec975a1f531d
david-fiaty <dfiaty@gmail.com>
2024-10-06 15:12:54 +0000

    Update BackButton.tsx

--- components/navigation/BackButton.tsx
+++ components/navigation/BackButton.tsx
@@ -1,24 +1,22 @@
 import { View, StyleSheet } from 'react-native';
 import { Button } from '@rneui/themed';
 import AntDesign from '@expo/vector-icons/AntDesign';
-import { useNavigation } from '@react-navigation/native';
 import { GlobalStyles } from '@/constants/GlobalStyles';
 
 type Props = {
   title: string,
   style?: {},
+  onPress: () => {},
 };
 
-const BackButton = ({title, style}: Props) => {
-  const navigation = useNavigation();
-
+const BackButton = ({title, style, onPress}: Props) => {
   return (
     <View style={styles.container}>
       <AntDesign name="left" size={20} color={GlobalStyles.icon.color} style={{marginTop: 8}} />
       <Button 
         title={title} 
         type="clear" 
-        onPress={() => navigation.goBack()}
+        onPress={onPress}
         titleStyle={[GlobalStyles.text, style]}
       />
     </View>
