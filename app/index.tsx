5da2735fd20e6e6cf0711942fec6aae5c085088d
david-fiaty <dfiaty@gmail.com>
2024-10-06 18:34:14 +0000

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,15 @@
 import { View, StyleSheet } from 'react-native';
 import Carousel from '../components/carousel/Controller';
 import { GlobalStyles } from '@/constants/GlobalStyles';
+import { SvgImage } from '@/components/SvgImage';
 import SkipButton from '@/components/buttons/SkipButton';
 import LoginButton from '@/components/buttons/LoginButton';
 import BottomLinks from '@/components/navigation/BottomLinks';
-import JamLogo from '@/components/JamLogo';
 
 const Index = () => {
   return (  
     <View style={styles.container}>
-      <JamLogo width={110} height={110} />
+      <SvgImage uri={require('@/assets/images/jam-logo.png')} width={110} height={110} />    
       <Carousel />
       <LoginButton />
       <SkipButton />
