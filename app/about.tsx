d30d08c5e45bb65db261d8c2851c7a6febb2828e
david-fiaty <dfiaty@gmail.com>
2024-10-06 15:12:47 +0000

    Update about.tsx

--- app/about.tsx
+++ app/about.tsx
@@ -1,14 +1,17 @@
 import { StyleSheet, View, Text} from 'react-native';
+import { useRouter } from 'expo-router';
 import { GlobalStyles } from '@/constants/GlobalStyles';
 import TopToolbar from '@/components/navigation/TopToolbar';
 import BackButton from '@/components/navigation/BackButton';
 import BottomLinks from '@/components/navigation/BottomLinks';
 
 const About = () => {
+  const router = useRouter();
+  
   return (
     <View style={styles.container}>
       <TopToolbar />
-      <BackButton title="About" />
+      <BackButton title="About" onPress={() => router.push('/')}/>
       <Text style={styles.text}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </Text>
