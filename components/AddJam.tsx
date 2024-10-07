502d71f02ed1e670c2a5437a72589d3ccf6594cd
david-fiaty <dfiaty@gmail.com>
2024-10-07 16:58:23 +0000

    Create AddJam.tsx

---
+++ components/AddJam.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+const AddJam = () => {
+  return (
+    <View>
+      <Text>Add jam</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default AddJam;
