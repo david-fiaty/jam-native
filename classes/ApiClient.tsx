5d3c872c57c5817254fbb5b9834d8df712942d88
david-fiaty <dfiaty@gmail.com>
2024-10-07 13:22:34 +0000

    Update ApiClient.tsx

--- classes/ApiClient.tsx
+++ classes/ApiClient.tsx
@@ -1,13 +1,19 @@
+import DataProvider from "./DataProvider";
+
 const ApiClient = class ApiClient {
   constructor() {
 
+  }
+
+  getApiData(dataKey: string) {
+    
   }
 
   async sendRequest() {
     try {
       const response = await fetch('https://reactnative.dev/movies.json');
       const json = await response.json();
-      
+
       return json.movies;
     } catch (error) {
       console.error(error);
