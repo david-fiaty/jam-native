5a234455259264abedba2e77f3136267619435c0
david-fiaty <dfiaty@gmail.com>
2024-10-07 23:53:02 +0000

    Update SaveJam.tsx

--- components/jam/SaveJam.tsx
+++ components/jam/SaveJam.tsx
@@ -1,36 +1,18 @@
-import { useState } from 'react';
-import { StyleSheet, Modal, Pressable, View, TouchableWithoutFeedback } from 'react-native';
-import { GlobalStyles } from '@/constants/GlobalStyles';
-import BackButton from '../navigation/BackButton';
-import SaveIcon from '../icons/SaveIcon';
+import { StyleSheet, View, Text} from 'react-native';
+import ShareIcon from '../icons/ShareIcon';
+import ModalWindow from '../ModalWindow';
 
 const SaveJam = () => {
-  const [modalVisible, setModalVisible] = useState(false);
-  
   return (
     <View style={styles.container}>        
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => setModalVisible(!modalVisible)}
-      >
-        <TouchableWithoutFeedback onPress={() => setModalVisible(!modalVisible)}>
-          <View style={GlobalStyles.modal.container}>
-            <TouchableWithoutFeedback>
-              <View style={GlobalStyles.modal.view}>
-                <Pressable onPress={() => setModalVisible(!modalVisible)}>
-                  <BackButton title="Jam is now saved to your Jams" style={styles.title} />
-                </Pressable>
-                
-              </View>
-              </TouchableWithoutFeedback>
-          </View>
-        </TouchableWithoutFeedback>
-      </Modal>
-      <Pressable onPress={() => setModalVisible(true)}>
-        <SaveIcon />
-      </Pressable>
+      <ModalWindow 
+        label={<ShareIcon />}
+        title="Save Jam" 
+        content={
+          <Text>SAVE JAM</Text>
+        }
+        animation="slide"
+      />   
     </View>
   );
 };
@@ -38,44 +20,6 @@ const SaveJam = () => {
 const styles = StyleSheet.create({
   container: {
   },
-  list: {
-    width: '100%',
-  },
-  title: {
-    ...GlobalStyles.text,
-    ...{
-      fontSize: 16,
-      fontWeight: 'bold',
-    },
-  },
-  text: GlobalStyles.text,
-  row: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingTop: 8,
-    paddingBottom: 8,
-    gap: 16,
-  }
 });
-
-const data = [
-  {
-    'label': 'Account information',
-    'path': '/jams',
-  },
-  {
-    'label': 'Change password',
-    'path': '/jams',
-  },
-  {
-    'label': 'Change user name',
-    'path': '/jams',
-  },
-  {
-    'label': 'Delete account',
-    'path': '/jams',
-  },
-];
 
 export default SaveJam;
