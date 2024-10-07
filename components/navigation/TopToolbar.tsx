eb7a34f613d6a3390c10392a5d2376a2ed1ca3eb
david-fiaty <dfiaty@gmail.com>
2024-10-07 22:34:30 +0000

    Update TopToolbar.tsx

--- components/navigation/TopToolbar.tsx
+++ components/navigation/TopToolbar.tsx
@@ -1,14 +1,21 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import { StaticImage } from '@/components/StaticImage';
 import SettingsMenu from '../menus/SettingsMenu';
 import NotificationsMenu from '../menus/NotificationsMenu';
 import SearchIcon from '../icons/SearchIcon';
+import ModalWindow from '../ModalWindow';
 
 const TopToolbar = () => {
   return (
     <View style={styles.container}>
       <StaticImage uri={require('@/assets/images/jam-logo.png')} width={45} height={45} /> 
       <View style={styles.buttons}> 
+        <ModalWindow 
+          label="OOPP"
+          title="test window" 
+          animation="slide" 
+          component={<Text>My modal component</Text>}
+        />
         <SettingsMenu />  
         <NotificationsMenu />
         <SearchIcon />
