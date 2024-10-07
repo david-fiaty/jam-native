102faf4b3bc7fff4f8c73b481126ace3671f7ee8
david-fiaty <dfiaty@gmail.com>
2024-10-07 16:58:28 +0000

    Update BottomTabs.tsx

--- components/navigation/BottomTabs.tsx
+++ components/navigation/BottomTabs.tsx
@@ -1,9 +1,13 @@
-import { Text } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 const BottomTabs = () => {
-
   return (
-    <Text>yo</Text>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
   );
 }
 
