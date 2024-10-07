cfc0c955333f359b891d08ba6eec09c3cfe3ec59
david-fiaty <dfiaty@gmail.com>
2024-10-07 22:05:12 +0000

    Update JamsList.tsx

--- components/jams/JamsList.tsx
+++ components/jams/JamsList.tsx
@@ -18,21 +18,18 @@ const styles = StyleSheet.create({
 const data = [
   {
     id: 1,
-    title: 'Create better, together',
+    host_count: 3,
     content: 'Welcome to the Jam app. Jam is a place to explore and experience artists and creatives from different backgrounds in West Africa.',
-    link: 'aaa link',
   },
   {
     id: 2,
-    title: 'Everything you need',
+    host_count: 7,
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    link: 'bbb link',
   },
   {
     id: 3,
-    title: 'The place to excel',
+    host_count: 5,
     content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.',
-    link: 'ccc link',
   },
 ];
 
