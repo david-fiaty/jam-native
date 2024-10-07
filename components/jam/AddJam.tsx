e150606833704b2d0e4db0a89c8b0a2670380a04
david-fiaty <dfiaty@gmail.com>
2024-10-07 23:22:02 +0000

    Update AddJam.tsx

--- components/jam/AddJam.tsx
+++ components/jam/AddJam.tsx
@@ -1,52 +1,23 @@
-import { useState } from 'react';
-import { StyleSheet, Modal, Pressable, Text, View, TouchableWithoutFeedback } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { GlobalStyles, Colors } from '@/constants/GlobalStyles';
+import { Colors } from '@/constants/GlobalStyles';
+import ModalWindow from '../ModalWindow';
 
 const AddJam = () => {
-  const [modalVisible, setModalVisible] = useState(false);
-  
   return (
-    <View style={styles.container}>        
-      <Modal
-        animationType="none"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => setModalVisible(!modalVisible)}
-      >
-        <TouchableWithoutFeedback onPress={() => setModalVisible(!modalVisible)}>
-          <View style={GlobalStyles.modal.container}>
-            <TouchableWithoutFeedback>
-              <View style={styles.view}>
-                <Text>ADD JAM</Text>
-              </View>
-            </TouchableWithoutFeedback>
-          </View>
-        </TouchableWithoutFeedback>
-      </Modal>
-      <Pressable onPress={() => setModalVisible(true)}>
-        <Ionicons name="add" size={26} color={Colors.primary} />
-      </Pressable>
+    <View style={styles.container}>     
+      <ModalWindow 
+        label={<Ionicons name="add" size={26} color={Colors.primary} />}
+        title="Add Jam" 
+        content={<Text>JAM FORM</Text>}
+        animation="none"
+      />   
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-  },
-  view: {
-    ...GlobalStyles.modal.view,
-    ...{
-      backgroundColor: Colors.tertiary,
-      height: '92.3%',
-    }
-  },
-  title: {
-    ...GlobalStyles.text,
-    ...{
-      fontSize: 16,
-      fontWeight: 'bold',
-    },
   },
 });
 
