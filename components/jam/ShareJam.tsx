35ca0c061af7ef25a28e681f62a3666b4d1c4f3c
david-fiaty <dfiaty@gmail.com>
2024-10-09 01:16:53 +0000

    Update ShareJam.tsx

--- components/jam/ShareJam.tsx
+++ components/jam/ShareJam.tsx
@@ -1,5 +1,7 @@
 import { StyleSheet, View, Text} from 'react-native';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import ModalView from '../ModalView';
 import { GlobalStyles, Colors } from '@/constants/GlobalStyles';
 
@@ -13,7 +15,36 @@ const ShareJam = () => {
         />}
         title="Share Jam" 
         content={
-          <Text>SHARE JAM</Text>
+          <View style={GlobalStyles.modal.wrapper}>
+            <View style={styles.row}>
+              <MaterialIcons 
+                name="content-copy" 
+                size={20} style={styles.icon} 
+              />
+              <Text style={GlobalStyles.text}>Copy link</Text>
+            </View>
+            <View style={styles.row}>
+              <MaterialIcons 
+                name="email" 
+                size={20} style={styles.icon} 
+              />
+              <Text style={GlobalStyles.text}>Send email</Text>
+            </View>
+            <View style={styles.row}>
+              <MaterialCommunityIcons 
+                name="twitter" 
+                size={20} style={styles.icon} 
+              />
+              <Text style={GlobalStyles.text}>Twitter</Text>
+            </View>
+            <View style={styles.row}>
+              <MaterialCommunityIcons 
+                name="facebook" 
+                size={20} style={styles.icon} 
+              />
+              <Text style={GlobalStyles.text}>Facebook</Text>
+            </View>
+          </View>
         }
         animation="slide"
       />   
@@ -23,6 +54,12 @@ const ShareJam = () => {
 
 const styles = StyleSheet.create({
   container: {
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 15,
   },
   icon: {
     ...GlobalStyles.icon,
