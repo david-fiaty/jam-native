0ab1730d656f7145e76f836581c0cf537fd8a1b4
david-fiaty <dfiaty@gmail.com>
2024-10-10 13:52:33 +0000

    Create SettingsMenuItems.ts

---
+++ constants/SettingsMenuItems.ts
@@ -0,0 +1,20 @@
+const SettingsMenuItems = [
+  {
+    'label': 'Account information',
+    'path': '/jams',
+  },
+  {
+    'label': 'Change password',
+    'path': '/jams',
+  },
+  {
+    'label': 'Change user name',
+    'path': '/jams',
+  },
+  {
+    'label': 'Delete account',
+    'path': '/jams',
+  },
+];
+
+export default SettingsMenuItems;
