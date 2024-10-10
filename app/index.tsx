c5131c485adface1b8f4a818587b9970c422961d
david-fiaty <dfiaty@gmail.com>
2024-10-10 16:00:44 +0000

    Updated the location class

--- app/index.tsx
+++ app/index.tsx
@@ -5,14 +5,17 @@ import Slideshow from '../components/Slideshow';
 import { GlobalStyles } from '@/constants/GlobalStyles';
 import { StaticImage } from '@/components/StaticImage';
 import BottomLinks from '@/components/navigation/BottomLinks';
+//import UserLocation from '@/components/user/UserLocation';
 import UserLocation from '@/classes/UserLocation';
 
 const Index = () => {
   const router = useRouter();
 
+  console.log(UserLocation.get());
+  
+
   return (  
     <View style={styles.container}>
-      <UserLocation />
       <StaticImage uri={require('@/assets/images/jam-logo.png')} width={110} height={110} />    
       <Slideshow />
       <Button 
