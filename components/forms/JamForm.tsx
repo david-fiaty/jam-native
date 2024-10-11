fc3c21f80cd87b660e1da03d7cec896284f73693
david-fiaty <dfiaty@gmail.com>
2024-10-11 23:30:02 +0000

    Update JamForm.tsx

--- components/forms/JamForm.tsx
+++ components/forms/JamForm.tsx
@@ -1,12 +1,31 @@
-import { StyleSheet, View, Text, TextInput, Pressable } from 'react-native';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
 import { GlobalStyles, Colors } from '@/constants/GlobalStyles';
 import MegaphoneIcon from '../icons/MegaphoneIcon';
+import ApiClient from '@/classes/ApiClient';
 
 const JamForm = () => {
+  const data = ApiClient.get('jams');
+
   return (
     <View style={styles.container}>    
       <Text style={GlobalStyles.text}>What kind of Jam is it?</Text>
       <MegaphoneIcon size={32} />
+      <View style={styles.list}>
+        <FlatList 
+          data={data} 
+          numColumns={3}
+          contentContainerStyle={{gap: GlobalStyles.gap}}
+          columnWrapperStyle={{gap: GlobalStyles.gap}}
+          scrollEnabled={false}
+          renderItem={({item, index}) => {
+            return (
+              <View style={styles.item}>
+                <Text style={GlobalStyles.text}>{item.id}</Text>   
+              </View>
+            );
+          }}
+        />
+      </View>
     </View>
   );
 };
@@ -16,23 +35,18 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'flex-start',
-    width: '80%',
-  },
-  continue: {
     width: '100%',
   },
-  signup: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginTop: 10,
+  list: {
   },
-  button: {
-    height: 45,
-    backgroundColor: Colors.primary,
+  item: {
+    backgroundColor: Colors.tertiary,
+    padding: 10,
     borderWidth: 1,
     borderRadius: 8,
+    borderColor: Colors.tertiary,
+    width: 99,
+    height: 99,
   },
 });
 
