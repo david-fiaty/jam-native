e3958f79982ff36b76ae8d69643391e43a29158b
david-fiaty <dfiaty@gmail.com>
2024-10-11 19:26:02 +0000

    Updated scroll

--- components/profile/ProfileScreen.tsx
+++ components/profile/ProfileScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View, ScrollView } from 'react-native';
+import { StyleSheet, View, ScrollView, Dimensions } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { Colors } from '@/constants/GlobalStyles';
 import ModalView from '../ModalView';
@@ -6,6 +6,7 @@ import ProfileForm from '@/components/profile/ProfileForm';
 import ProfileImage from '@/components/profile/ProfileImage';
 import ProfileJams from '@/components/profile/ProfileJams';
 import ProfileProjects from '@/components/profile/ProfileProjects';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const ProfileScreen = () => {
   return (
@@ -14,14 +15,14 @@ const ProfileScreen = () => {
         title="Your profile" 
         label={<Ionicons name="person-circle" size={26} color={Colors.primary} />}
         content={
-          <View style={styles.wrapper}>  
-            <ScrollView contentContainerStyle={styles.scroll}>
+          <SafeAreaView style={styles.wrapper}>  
+            <ScrollView style={styles.scroll}>
               <ProfileImage />
               <ProfileForm />
               <ProfileProjects />
-              <ProfileJams/>
+              <ProfileJams />
             </ScrollView>  
-          </View>
+          </SafeAreaView>
         }
         animation="slide"
       />    
@@ -31,13 +32,15 @@ const ProfileScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
+    height: '100%',
   },
   wrapper: {
     width: '100%',
-    paddingBottom: 40,
+    flex: 1,
+    height: Dimensions.get('screen').height,
   },
   scroll: {
-    display: 'flex',
+
   },  
 });
 
