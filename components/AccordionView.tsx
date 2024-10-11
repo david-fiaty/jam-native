2326c317a0f741ca40f5fd9efacccad7e2c92c49
david-fiaty <dfiaty@gmail.com>
2024-10-11 22:17:42 +0000

    Update AccordionView.tsx

--- components/AccordionView.tsx
+++ components/AccordionView.tsx
@@ -52,96 +52,23 @@ const SELECTORS = [
 ];
 
 export default function AccordionView() {
-  const [activeSections, setActiveSections] = useState<number[]>([]);
   const [collapsed, setCollapsed] = useState(true);
-  const [multipleSelect, setMultipleSelect] = useState(false);
 
   return (
     <View style={styles.container}>
-      <ScrollView contentContainerStyle={{ paddingTop: 30 }}>
-        <View style={styles.multipleToggle}>
-          <Text style={styles.multipleToggle__title}>Multiple Select?</Text>
-          <Switch
-            value={multipleSelect}
-            onValueChange={(a) => setMultipleSelect(a)}
-          />
+      <TouchableOpacity onPress={() => setCollapsed((prev) => !prev)}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Single Collapsible</Text>
         </View>
-
-        <View style={styles.selectors}>
-          <Text style={styles.selectTitle}>Select:</Text>
-
-          {SELECTORS.map((selector) => (
-            <TouchableOpacity
-              key={selector.title}
-              onPress={() =>
-                setActiveSections(
-                  typeof selector.value === 'number' ? [selector.value] : []
-                )
-              }
-            >
-              <View style={styles.selector}>
-                <Text
-                  style={
-                    selector.value === activeSections[0] &&
-                    styles.activeSelector
-                  }
-                >
-                  {selector.title}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          ))}
+      </TouchableOpacity>
+      <Collapsible collapsed={collapsed} align="center">
+        <View style={styles.content}>
+          <Text>
+            Bacon ipsum dolor amet chuck turducken landjaeger tongue spare
+            ribs
+          </Text>
         </View>
-
-        <TouchableOpacity onPress={() => setCollapsed((prev) => !prev)}>
-          <View style={styles.header}>
-            <Text style={styles.headerText}>Single Collapsible</Text>
-          </View>
-        </TouchableOpacity>
-        <Collapsible collapsed={collapsed} align="center">
-          <View style={styles.content}>
-            <Text>
-              Bacon ipsum dolor amet chuck turducken landjaeger tongue spare
-              ribs
-            </Text>
-          </View>
-        </Collapsible>
-        <Accordion
-          activeSections={activeSections}
-          sections={CONTENT}
-          touchableComponent={TouchableOpacity}
-          expandMultiple={multipleSelect}
-          renderHeader={(section, _, isActive) => (
-            <Animatable.View
-              duration={400}
-              style={[
-                styles.header,
-                isActive ? styles.active : styles.inactive,
-              ]}
-              transition="backgroundColor"
-            >
-              <Text style={styles.headerText}>{section.title}</Text>
-            </Animatable.View>
-          )}
-          renderContent={(section, _, isActive) => (
-            <Animatable.View
-              duration={400}
-              style={[
-                styles.content,
-                isActive ? styles.active : styles.inactive,
-              ]}
-              transition="backgroundColor"
-            >
-              <Animatable.Text animation={isActive ? 'bounceIn' : undefined}>
-                {section.content}
-              </Animatable.Text>
-            </Animatable.View>
-          )}
-          duration={400}
-          onChange={setActiveSections}
-          renderAsFlatList={false}
-        />
-      </ScrollView>
+      </Collapsible>
     </View>
   );
 }
@@ -150,12 +77,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F5FCFF',
-  },
-  title: {
-    textAlign: 'center',
-    fontSize: 22,
-    fontWeight: '300',
-    marginBottom: 20,
+    marginTop: 100,
   },
   header: {
     backgroundColor: '#F5FCFF',
@@ -169,38 +91,12 @@ const styles = StyleSheet.create({
   content: {
     padding: 20,
     backgroundColor: '#fff',
+    height: 200,
   },
   active: {
     backgroundColor: 'rgba(255,255,255,1)',
   },
   inactive: {
     backgroundColor: 'rgba(245,252,255,1)',
-  },
-  selectors: {
-    marginBottom: 10,
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  selector: {
-    backgroundColor: '#F5FCFF',
-    padding: 10,
-  },
-  activeSelector: {
-    fontWeight: 'bold',
-  },
-  selectTitle: {
-    fontSize: 14,
-    fontWeight: '500',
-    padding: 10,
-  },
-  multipleToggle: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginVertical: 30,
-    alignItems: 'center',
-  },
-  multipleToggle__title: {
-    fontSize: 16,
-    marginRight: 8,
   },
 });
