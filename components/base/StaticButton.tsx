a455861964f67f8132486f67eda75041cc799af7
david-fiaty <dfiaty@gmail.com>
2024-10-12 18:53:10 +0000

    Button test

--- components/base/StaticButton.tsx
+++ components/base/StaticButton.tsx
@@ -1,21 +1,28 @@
 
 import { StyleSheet, View, Pressable } from 'react-native';
 import { Button } from '@rneui/themed';
+import { GlobalStyles } from '@/constants/GlobalStyles';
 
 type Props = {
-  props: {},
+  title: string,
+  type: string,
+  buttonStyle: object,
+  titleStyle: object,
+  onPress: () => void,
 };
 
-export function StaticButton({props}: Props) {
+export function StaticButton({...props}: Props) {
+  console.log(props);
+
   return (
     <Pressable>
       <View style={styles.container}>
         <Button 
           title={props.title} 
-          type={props.title}
-          buttonStyle={props.buttonStyle} 
-          titleStyle={props.titleStyle}
-          onPress={props.onPress} 
+          type={props.type}
+          buttonStyle={[styles.button, props.buttonStyle]} 
+          titleStyle={[styles.title, props.titleStyle]}
+          onPress={props.onPress}
         />
       </View>
     </Pressable>
@@ -26,4 +33,15 @@ const styles = StyleSheet.create({
   container: {
     alignSelf: 'center',
   },
+  button: {
+    marginTop: 40,
+    borderRadius: 30,
+    overflow: 'hidden',
+    minWidth: '35%',
+    maxWidth: '40%',
+    alignSelf: 'center',
+  },
+  title: {
+    ...GlobalStyles.text,
+  }
 });
