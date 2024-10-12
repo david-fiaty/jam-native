aa1d53136f143b1950268d4d74e7ab32c7ab8592
david-fiaty <dfiaty@gmail.com>
2024-10-12 21:16:50 +0000

    Update SignupScreen.tsx

--- components/screens/SignupScreen.tsx
+++ components/screens/SignupScreen.tsx
@@ -1,38 +1,23 @@
-import { useRouter } from 'expo-router';
 import { StyleSheet, View } from 'react-native';
-import { Colors } from '@/constants/GlobalStyles';
 import LoginForm from '@/components/forms/LoginForm';
 import TextBlock from '../base/TextBlock';
 import JamLogo from '../images/JamLogo';
+import ScreenView from '../base/ScreenView';
 
 const SignupScreen = () => {
-  const router = useRouter();
-
   return (
     <View style={styles.container}>
-      <JamLogo width={110} height={110} />  
-      <TextBlock>Welcome back</TextBlock>
-      <LoginForm />
+      <ScreenView>
+        <JamLogo width={110} height={110} />  
+        <TextBlock>Welcome back</TextBlock>
+        <LoginForm />
+      </ScreenView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center', 
-    alignContent: 'center',
-    backgroundColor: Colors.background,
-    height: '100%',
-  },
-  title: {
-    textTransform: 'uppercase',
-    marginBottom: 20,
-    marginTop: 20,
-    textAlign: 'center',
-  },
+  container: {},
 });
 
 export default SignupScreen;
