aa3e0c79b7fb1e6125b256a18f5a1ff27de52864
david-fiaty <dfiaty@gmail.com>
2024-10-12 21:48:17 +0000

    Update JamItem.tsx

--- components/jam/JamItem.tsx
+++ components/jam/JamItem.tsx
@@ -26,7 +26,7 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     paddingBottom: GlobalStyles.gap,
     borderWidth: 1,
-    borderRadius: 5,
+    borderRadius: 8,
     borderColor: GlobalStyles.border.color,
   },
 });
