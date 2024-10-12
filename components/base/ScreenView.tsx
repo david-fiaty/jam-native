02e6512d36beb63ccd020751982d1e0377c74fe7
david-fiaty <dfiaty@gmail.com>
2024-10-12 21:14:51 +0000

    Create ScreenView.tsx

---
+++ components/base/ScreenView.tsx
@@ -0,0 +1,30 @@
+
+import { ReactNode } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Colors } from '@/constants/GlobalStyles';
+
+type Props = {
+  style?: {},
+  children?: ReactNode,
+};
+
+const ScreenView = ({style, children}: Props) => {
+  return (
+    <View style={styles.container}>{children}</View>   
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center', 
+    alignContent: 'center',
+    backgroundColor: Colors.background,
+    height: '100%',
+  },
+});
+
+export default ScreenView;
