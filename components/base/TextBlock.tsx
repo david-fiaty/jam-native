b02a3a3f6130fcdb3774ad322f9cef28f2627620
david-fiaty <dfiaty@gmail.com>
2024-10-12 17:22:23 +0000

    q

--- components/base/TextBlock.tsx
+++ components/base/TextBlock.tsx
@@ -3,20 +3,14 @@ import { GlobalStyles } from '@/constants/GlobalStyles';
 import React, { ReactNode } from 'react';
 
 type Props = {
-  fontSize?: number,
-  textTransform?: boolean,
-  textDecoration?: boolean,
-  children: ReactNode,
+  styles?: {},
+  children?: ReactNode,
 };
 
-const TextBlock = ({fontSize, textTransform, textDecoration, children}: Props) => {
+const TextBlock = ({styles, children}: Props) => {
   return (
-    <Text style={styles.text}>{children}</Text>   
+    <Text style={[styles, GlobalStyles.text]}>{children}</Text>   
   );
 };
-
-const styles = StyleSheet.create({
-  text: GlobalStyles.text,
-});
 
 export default TextBlock;
