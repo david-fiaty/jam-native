66f05712d064dda8092ba241e8cf6d78c970e9b8
david-fiaty <dfiaty@gmail.com>
2024-10-13 17:20:05 +0000

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
 import { View, StyleSheet } from 'react-native';
 import WelcomeScreen from '@/components/screens/WelcomeScreen';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import TopToolbar from '@/components/navigation/TopToolbar';
 
 const Index = () => {
   return (  
     <SafeAreaView style={styles.container}>
+      <TopToolbar />
       <WelcomeScreen />
     </SafeAreaView>
   );
