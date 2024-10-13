80fd79cbae22b4ff0fb138b086f7ee0a8b19399a
david-fiaty <dfiaty@gmail.com>
2024-10-13 23:35:11 +0000

    Update ProfileImage.tsx

--- components/profile/ProfileImage.tsx
+++ components/profile/ProfileImage.tsx
@@ -1,14 +1,13 @@
 import { StyleSheet, View } from 'react-native';
 import { Button } from '@rneui/themed';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { GlobalStyles } from '@/constants/GlobalStyles';
 import TextBlock from '@/components/base/TextBlock';
 
 const ProfileImage = () => {
   return (
     <View style={styles.container}>
-      <FontAwesome name="user-circle" size={86} color={GlobalStyles.icon.color} />
+      <Ionicons name="person-circle" size={86} color={GlobalStyles.icon.color} />
       <Button 
         type="outline" 
         buttonStyle={styles.button} 
