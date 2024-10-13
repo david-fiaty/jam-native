c585befe273638038a6c2f7aac43afad9dd25b95
david-fiaty <dfiaty@gmail.com>
2024-10-13 18:52:14 +0000

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,10 @@
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import WelcomeScreen from '@/components/screens/WelcomeScreen';
-import PrimaryIcon from '@/components/icons/PrimaryIcon';
 
 const Index = () => {
   return (  
     <SafeAreaView style={styles.container}>
-      <PrimaryIcon name="user" />
       <WelcomeScreen />
     </SafeAreaView>
   );
