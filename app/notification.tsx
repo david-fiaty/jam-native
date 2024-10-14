770bc842c4e841b4560e06cd7061b47e0f072e84
david-fiaty <dfiaty@gmail.com>
2024-10-14 02:38:46 +0000

    Create notification.tsx

---
+++ app/notification.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import TopToolbar from '@/components/navigation/TopToolbar';
+
+const Notification = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <TopToolbar />
+      <Text>NOTIFICATION</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column', 
+    marginTop: 30,
+    padding: 25,
+  },
+});
+
+export default Notification;
