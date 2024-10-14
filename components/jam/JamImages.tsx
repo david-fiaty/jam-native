759f1a0da57d1dd232e2c6a2502f9987cf22dc12
david-fiaty <dfiaty@gmail.com>
2024-10-14 00:17:01 +0000

    Update JamImages.tsx

--- components/jam/JamImages.tsx
+++ components/jam/JamImages.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet, View } from 'react-native';
+import { StaticImage } from '../base/StaticImage';
 import { Colors } from '@/constants/GlobalStyles';
 
 type Props = {
@@ -7,8 +8,15 @@ type Props = {
 };
 
 const JamImages = ({item, index}: Props) => {
+  const source = require(`@/assets/images/${item.image}`); 
+
   return (
     <View style={styles.container}>
+      <StaticImage 
+        source={source} 
+        width={200}
+        height={200}
+      />   
     </View>
   );
 };
