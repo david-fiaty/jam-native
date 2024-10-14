bcdda2a3633b1ae3970347237f14bff3d9ff410b
david-fiaty <dfiaty@gmail.com>
2024-10-14 23:24:46 +0000

    Added screens

---
+++ components/screens/PasswordScreen.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, View} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import BackButton from '@/components/navigation/BackButton';
+import BottomLinks from '@/components/navigation/BottomLinks';
+import TextBlock from '@/components/base/TextBlock';
+
+const PasswordScreen = () => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <BackButton title="Account title" onPress={() => navigation.popToTop()}/>
+      <TextBlock>Account content</TextBlock>
+      <BottomLinks />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+  },
+});
+
+export default PasswordScreen;
