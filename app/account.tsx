93b5e65fd71ba29b70630322d2313d63062c344c
david-fiaty <dfiaty@gmail.com>
2024-10-14 22:33:32 +0000

    Update account.tsx

--- app/account.tsx
+++ app/account.tsx
@@ -1,16 +1,18 @@
 import { StyleSheet, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { useRouter } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import TopToolbar from '@/components/navigation/TopToolbar';
 import BackButton from '@/components/navigation/BackButton';
 
 const Account = () => {
   const navigation = useNavigation();
+  const router = useRouter();
 
   return (
     <SafeAreaView style={styles.container}>
       <TopToolbar />
-      <BackButton title="Account information" onPress={() => navigation.goBack()}/>
+      <BackButton title="Account information" onPress={() => router.replace('/jams')}/>
       <Text>ACCOUNT</Text>
     </SafeAreaView>
   );
