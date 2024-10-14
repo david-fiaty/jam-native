5e70ce54f2d9d184065c38730c450c796ba5cc55
david-fiaty <dfiaty@gmail.com>
2024-10-14 23:40:19 +0000

    Added settings screens

--- components/screens/PasswordScreen.tsx
+++ components/screens/PasswordScreen.tsx
@@ -1,24 +1,43 @@
-import { StyleSheet, View} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import BackButton from '@/components/navigation/BackButton';
-import BottomLinks from '@/components/navigation/BottomLinks';
-import TextBlock from '@/components/base/TextBlock';
+import { StyleSheet, View, ScrollView, Pressable } from 'react-native';
+import ClearIcon from '../icons/ClearIcon';
+import ModalView from '@/components/base/ModalView';
+import ProfileForm from '@/components/profile/ProfileForm';
+import ProfileImage from '@/components/profile/ProfileImage';
+import ProfileJams from '@/components/profile/ProfileJams';
+import ProfileProjects from '@/components/profile/ProfileProjects';
+
+import { GlobalStyles } from '@/constants/GlobalStyles';
 
 const PasswordScreen = () => {
-  const navigation = useNavigation();
-
   return (
-    <View style={styles.container}>
-      <BackButton title="Password title" onPress={() => navigation.popToTop()}/>
-      <TextBlock>Password content</TextBlock>
-      <BottomLinks />
+    <View style={styles.container}>    
+      <ModalView 
+        title="Your profile" 
+        animation="slide"
+        label={<ClearIcon name="user" size={GlobalStyles.tabsbar.icon.size} />}
+        content={
+          <ScrollView 
+            nestedScrollEnabled={true}
+            contentContainerStyle={{ flexGrow: 1 }}
+          >
+            <Pressable>
+              <ProfileImage />
+              <ProfileForm />
+              <ProfileProjects />
+              <ProfileJams />
+            </Pressable>
+          </ScrollView>  
+        }
+      />    
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-  },
+  container: {},
+  scroll: {
+    flexGrow: 1,
+  },  
 });
 
 export default PasswordScreen;
