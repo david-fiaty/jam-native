70cb00e8571b52a1810772b436211d0615b45516
david-fiaty <dfiaty@gmail.com>
2024-10-14 16:13:25 +0000

    Create RadioGroup.tsx

---
+++ components/fields/RadioGroup.tsx
@@ -0,0 +1,44 @@
+import RadioGroup from 'react-native-radio-buttons-group';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+
+const RadioGroup = () => {
+  const [radioButtons, setRadioButtons] = useState([
+    {
+      id: '1',
+      label: 'Option 1',
+      value: 'option1',
+      color: '#f39c12',
+      selected: false,
+      size: 24, // Size of the square
+      borderColor: '#34495e', // Border color for square
+      shape: 'square', // Custom shape
+    },
+    {
+      id: '2',
+      label: 'Option 2',
+      value: 'option2',
+      color: '#e74c3c',
+      selected: false,
+      size: 24,
+      borderColor: '#34495e',
+      shape: 'square',
+    },
+  ]);
+
+  function onPressRadioButton(radioButtonsArray) {
+    setRadioButtons(radioButtonsArray);
+  }
+
+  return (
+    <View>
+      <RadioGroup
+        radioButtons={radioButtons}
+        onPress={onPressRadioButton}
+        layout="row" // Horizontal layout
+      />
+    </View>
+  );
+}
+
+export default RadioGroup;
