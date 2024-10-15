f5c925a5f2fb224580f5801c02bd22ac1cb25256
david-fiaty <dfiaty@gmail.com>
2024-10-15 16:49:58 +0000

    Update CheckboxListField.tsx

--- components/fields/CheckboxListField.tsx
+++ components/fields/CheckboxListField.tsx
@@ -1,7 +1,10 @@
 import { StyleSheet, View, ScrollView, FlatList, TouchableOpacity } from 'react-native';
+import { CheckBox } from '@rneui/themed';
 import ModalView from '@/components/base/ModalView';
 import TextBlock from '../base/TextBlock';
 import { GlobalStyles, Colors } from '@/constants/GlobalStyles';
+import ClearIcon from '../icons/ClearIcon';
+import SecondaryIcon from '../icons/SecondaryIcon';
 
 type Props = {
   label: JSX.Element,
@@ -25,15 +28,26 @@ const CheckboxListField = ({label, title, data}: Props) => {
           >
             <FlatList 
               data={data} 
+              horizontal={false}  
               numColumns={1}
+              style={styles.list}
               contentContainerStyle={{gap: GlobalStyles.space}}
-              columnWrapperStyle={{gap: GlobalStyles.space}}
               scrollEnabled={false}
               renderItem={({item, index}) => {
                 return (
                   <TouchableOpacity onPress={() => console.log('clicked')}>
-                    <View style={styles.container}>
-                      <TextBlock>{item.name}</TextBlock>   
+                    <View style={styles.item}>
+                      <View style={styles.left}>
+                        <SecondaryIcon name="user" />
+                        <TextBlock>{item.name}</TextBlock>
+                      </View>
+                      <View style={styles.right}>
+                        <CheckBox 
+                          iconRight={true}
+                          containerStyle={styles.checkbox} 
+                          checked={true} 
+                        />
+                      </View>
                     </View>
                   </TouchableOpacity>
                 );
@@ -47,11 +61,39 @@ const CheckboxListField = ({label, title, data}: Props) => {
 };
 
 const styles = StyleSheet.create({
-  container: {},
-  item: {},
+  container: {
+
+  },
+  list: {
+    width: '100%',
+  },
+  item: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: GlobalStyles.space,
+  },
+  left: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    gap: GlobalStyles.space,
+  },
+  right: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
   scroll: {
     flexGrow: 1,
   },  
+  checkbox: {
+    padding: 0,
+    margin: 0,
+  },
 });
 
 export default CheckboxListField;
