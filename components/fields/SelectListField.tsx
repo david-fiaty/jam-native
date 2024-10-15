2896ba21f6c5bdef23a6705e01b8495ec9454f7e
david-fiaty <dfiaty@gmail.com>
2024-10-15 23:13:05 +0000

    Update SelectListField.tsx

--- components/fields/SelectListField.tsx
+++ components/fields/SelectListField.tsx
@@ -1,19 +1,11 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Dropdown } from "react-native-element-dropdown";
-import AntDesign from "@expo/vector-icons/AntDesign";
 import { GlobalStyles } from "@/constants/GlobalStyles";
-import TextBlock from "../base/TextBlock";
 
 const data = [
   { label: "Item 1", value: "1" },
   { label: "Item 2", value: "2" },
-  { label: "Item 3", value: "3" },
-  { label: "Item 4", value: "4" },
-  { label: "Item 5", value: "5" },
-  { label: "Item 6", value: "6" },
-  { label: "Item 7", value: "7" },
-  { label: "Item 8", value: "8" },
 ];
 
 const SelectListField = () => {
@@ -41,7 +33,7 @@ const SelectListField = () => {
         maxHeight={300}
         labelField="label"
         valueField="value"
-        placeholder={!isFocus ? "Select item" : "..."}
+        placeholder={!isFocus ? "Status" : "..."}
         searchPlaceholder="Search..."
         value={value}
         onFocus={() => setIsFocus(true)}
@@ -50,14 +42,6 @@ const SelectListField = () => {
           setValue(item.value);
           setIsFocus(false);
         }}
-        renderLeftIcon={() => (
-          <AntDesign
-            style={styles.icon}
-            color={isFocus ? "blue" : "black"}
-            name="Safety"
-            size={20}
-          />
-        )}
       />
     </View>
   );
