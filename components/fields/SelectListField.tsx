033ea6da0c5c04eac02cbcae825fcf9f1dda25b1
david-fiaty <dfiaty@gmail.com>
2024-10-15 18:59:59 +0000

    Update SelectListField.tsx

--- components/fields/SelectListField.tsx
+++ components/fields/SelectListField.tsx
@@ -1,20 +1,63 @@
-import { StyleSheet, View, TextInput } from 'react-native';
-import { GlobalStyles } from '@/constants/GlobalStyles';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { GlobalStyles } from "@/constants/GlobalStyles";
+import TextBlock from "../base/TextBlock";
 
-type Props = {
-  placeholder?: string,
-  style?: object,
-};
+const data = [
+  { label: "Item 1", value: "1" },
+  { label: "Item 2", value: "2" },
+  { label: "Item 3", value: "3" },
+  { label: "Item 4", value: "4" },
+  { label: "Item 5", value: "5" },
+  { label: "Item 6", value: "6" },
+  { label: "Item 7", value: "7" },
+  { label: "Item 8", value: "8" },
+];
 
-const SelectListField = ({placeholder, style}: Props) => {
+const SelectListField = () => {
+  const [value, setValue] = useState(null);
+  const [isFocus, setIsFocus] = useState(false);
+
+  const renderLabel = () => {
+    if (value || isFocus) {
+      return (<></>);
+    }
+    return null;
+  };
+
   return (
     <View style={styles.container}>
-      <TextInput
-        style={[styles.field, style]}
-        placeholder={placeholder}
-        placeholderTextColor={GlobalStyles.text.color}
-        multiline={true}
-        textAlignVertical="top"
+      {renderLabel()}
+      <Dropdown
+        style={[styles.dropdown, isFocus && { borderColor: "blue" }]}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        inputSearchStyle={styles.inputSearchStyle}
+        iconStyle={styles.iconStyle}
+        data={data}
+        search
+        maxHeight={300}
+        labelField="label"
+        valueField="value"
+        placeholder={!isFocus ? "Select item" : "..."}
+        searchPlaceholder="Search..."
+        value={value}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
+        onChange={(item) => {
+          setValue(item.value);
+          setIsFocus(false);
+        }}
+        renderLeftIcon={() => (
+          <AntDesign
+            style={styles.icon}
+            color={isFocus ? "blue" : "black"}
+            name="Safety"
+            size={20}
+          />
+        )}
       />
     </View>
   );
@@ -22,16 +65,36 @@ const SelectListField = ({placeholder, style}: Props) => {
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
+    backgroundColor: "white",
+    width: '100%',
   },
-  field: {
+  dropdown: {
     ...GlobalStyles.field,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  label: {
+    ...GlobalStyles.text,
     ...{
-      paddingTop: GlobalStyles.space,
-      height: GlobalStyles.space*10,
+      position: "absolute",
+      backgroundColor: "white",
+      left: 22,
+      top: 8,
+      zIndex: 999,
+      paddingHorizontal: 8,
+      fontSize: 14,
     },
+  },
+  placeholderStyle: GlobalStyles.text,
+  selectedTextStyle: GlobalStyles.text,
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
   },
 });
 
