76fdfbc67a9e7fc871cdb15fff707382debe0eb0
david-fiaty <dfiaty@gmail.com>
2024-10-15 14:08:21 +0000

    Create NotificationScreen.tsx

---
+++ components/screens/NotificationScreen.tsx
@@ -0,0 +1,41 @@
+import { StyleSheet, View, ScrollView, Pressable } from 'react-native';
+import ModalView from '@/components/base/ModalView';
+import ProfileForm from '@/components/profile/ProfileForm';
+import ProfileImage from '@/components/profile/ProfileImage';
+import ProfileJams from '@/components/profile/ProfileJams';
+import ProfileProjects from '@/components/profile/ProfileProjects';
+import MenuItem from '../base/MenuItem';
+import TextBlock from '../base/TextBlock';
+
+type Props = {
+  menuItem: object,
+};
+
+const NotificationScreen = ({menuItem}: Props) => {
+  return (
+    <View style={styles.container}>    
+      <ModalView 
+        title={<TextBlock>{menuItem.label}</TextBlock>} 
+        animation="fade"
+        label={<MenuItem label={menuItem.label} />}
+        content={
+          <ScrollView 
+            nestedScrollEnabled={true}
+            contentContainerStyle={{ flexGrow: 1 }}
+          >
+            <TextBlock>{menuItem.content}</TextBlock>
+          </ScrollView>  
+        }
+      />    
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+  scroll: {
+    flexGrow: 1,
+  },  
+});
+
+export default NotificationScreen;
