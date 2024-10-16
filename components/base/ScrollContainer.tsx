5a04cc6904dda80e96e9a01b97eff3a276a960f4
david-fiaty <dfiaty@gmail.com>
2024-10-16 18:54:15 +0000

    Update ScrollContainer.tsx

--- components/base/ScrollContainer.tsx
+++ components/base/ScrollContainer.tsx
@@ -20,7 +20,7 @@ const ScrollContainer = ({style, children}: Props) => {
 
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,
+    //flexGrow: 1, // Todo - Enable or remove
   },
 });
 
