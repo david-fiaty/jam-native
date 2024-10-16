5e7059cac2df7b7ddb429d91fcf1ca20705d1637
david-fiaty <dfiaty@gmail.com>
2024-10-16 22:53:31 +0000

    Update JamsScreen.tsx

--- components/screens/JamsScreen.tsx
+++ components/screens/JamsScreen.tsx
@@ -1,4 +1,4 @@
-import { FlatList } from 'react-native';
+import { View, FlatList } from 'react-native';
 import JamItem from '@/components/jam/JamItem';
 import ApiClient from '@/classes/ApiClient';
 import { GlobalStyles } from '@/constants/GlobalStyles';
@@ -7,16 +7,20 @@ const JamsScreen = () => {
   const data = ApiClient.get('jams');
 
   return (
-    <FlatList 
-      data={data} 
-      horizontal={false}  
-      renderItem={({item, index}) => <JamItem item={item} index={index} />} 
-    />
+    <View style={styles.container}>
+      <FlatList 
+        data={data} 
+        horizontal={false}  
+        renderItem={({item, index}) => <JamItem item={item} index={index} />} 
+      />
+    </View>
   );
 };
 
 const styles = {
-  paddingHorizontal: GlobalStyles.space,
+  container: {
+    paddingHorizontal: GlobalStyles.space,
+  },
 };
 
 export default JamsScreen;
