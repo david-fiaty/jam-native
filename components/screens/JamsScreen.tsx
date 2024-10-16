3c5c1a66cd36b6a6b71ab4a281df3139d3af8aa7
david-fiaty <dfiaty@gmail.com>
2024-10-16 23:04:29 +0000

    Update JamsScreen.tsx

--- components/screens/JamsScreen.tsx
+++ components/screens/JamsScreen.tsx
@@ -1,7 +1,26 @@
 import { View, FlatList } from 'react-native';
-import JamItem from '@/components/jam/JamItem';
 import ApiClient from '@/classes/ApiClient';
 import { GlobalStyles } from '@/constants/GlobalStyles';
+import JamHeader from '@/components/jam/JamHeader';
+import JamImages from '@/components/jam/JamImages';
+import JamToolbar from '@/components/jam/JamToolbar';
+import JamContent from '@/components/jam/JamContent';
+
+type ItemProps = {
+  item: object,
+  index: number,
+};
+
+const Item = ({item, index}: ItemProps) => {
+  return (
+    <View style={styles.container}>
+      <JamHeader item={item} index={index} />
+      <JamImages item={item} index={index} />
+      <JamToolbar item={item} index={index} />
+      <JamContent item={item} index={index} />
+    </View>
+  );
+};
 
 const JamsScreen = () => {
   const data = ApiClient.get('jams');
@@ -11,7 +30,7 @@ const JamsScreen = () => {
       <FlatList 
         data={data} 
         horizontal={false}  
-        renderItem={({item, index}) => <JamItem item={item} index={index} />} 
+        renderItem={({item, index}) => <Item item={item} index={index} />} 
       />
     </View>
   );
