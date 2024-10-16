d1afc948d50730e9c89f740784b7ae1f50b89d12
david-fiaty <dfiaty@gmail.com>
2024-10-16 15:54:00 +0000

    Update DeviceManager.tsx

--- classes/DeviceManager.tsx
+++ classes/DeviceManager.tsx
@@ -1,5 +1,13 @@
+import { Dimensions, ScaledSize } from "react-native";
 
 class DeviceManager {
+  screen: ScaledSize;
+  window: ScaledSize;
+
+  constructor() {
+    this.screen = Dimensions.get('screen');
+    this.window = Dimensions.get('window');
+  }
 };
 
 export default (new DeviceManager());
