ce98c9749a9022a5d4eaf8c7df9ab2da0db4be75
david-fiaty <dfiaty@gmail.com>
2024-10-16 22:03:52 +0000

    Update header bar

--- app/language.tsx
+++ app/language.tsx
@@ -1,11 +1,9 @@
 import { Text } from 'react-native';
-import HeaderBar from '@/components/navigation/HeaderBar';
 import ViewportContainer from '@/components/base/ViewportContainer';
 
 const Language = () => {
   return (
     <ViewportContainer>
-      <HeaderBar />
       <Text>LANGUAGE</Text>
     </ViewportContainer>
   );
--- app/legal.tsx
+++ app/legal.tsx
@@ -1,12 +1,9 @@
-import { StyleSheet } from 'react-native';
-import HeaderBar from '@/components/navigation/HeaderBar';
 import LegalScreen from '@/components/screens/LegalScreen';
 import ViewportContainer from '@/components/base/ViewportContainer';
 
 const Legal = () => {
   return (
     <ViewportContainer>
-      <HeaderBar />
       <LegalScreen />
     </ViewportContainer>
   );
