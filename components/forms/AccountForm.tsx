e9c57c74b8e1549915978b7a29a9cd6bdaad253c
david-fiaty <dfiaty@gmail.com>
2024-10-16 00:05:09 +0000

    Create AccountForm.tsx

---
+++ components/forms/AccountForm.tsx
@@ -0,0 +1,44 @@
+import { StyleSheet, View } from 'react-native';
+import { GlobalStyles, Colors } from '@/constants/GlobalStyles';
+import InputTextField from '../fields/InputTextField';
+
+const AccountForm = () => {
+  return (
+    <View style={styles.container}>    
+      <InputTextField placeholder="Fullname" style={GlobalStyles.field} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+    gap: GlobalStyles.space,
+  },
+  buttonContainer: {
+    width: '100%',
+  },
+  signup: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: GlobalStyles.space,
+  },
+  button: {
+    height: 45,
+    backgroundColor: Colors.primary,
+    borderWidth: 1,
+    borderRadius: 8,
+  },
+  field: {
+    backgroundColor: '#FFFFFF',
+    height: 45,
+    borderColor: Colors.primary, 
+  },
+});
+
+export default AccountForm;
