b38904af8e4a7234ad0c6240547100da9c3e0bc0
david-fiaty <dfiaty@gmail.com>
2024-10-17 01:02:16 +0000

    Create FooterBar - Copy.tsx

---
+++ components/navigation/FooterBar - Copy.tsx
@@ -0,0 +1,68 @@
+import { StyleSheet, View, Text } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MapScreen from '@/components/screens/MapScreen';
+import AddJamScreen from '@/components/screens/AddJamScreen';
+import ProfileScreen from '@/components/screens/ProfileScreen';
+import { GlobalStyles, Colors } from '@/constants/GlobalStyles';
+
+const FooterBar = () => {
+  const Tab = createBottomTabNavigator();
+
+  return (
+    <View style={styles.container}>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarStyle: styles.tabs,
+        }}
+      >
+        <Tab.Screen 
+          name="map" 
+          component={Text}
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: ({ color, size }) => (
+              <MapScreen />
+            ),
+          }}
+        />
+        <Tab.Screen 
+          name="add" 
+          component={Text}
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: ({ color, size }) => (
+              <AddJamScreen />
+            ),
+          }}
+        />
+        <Tab.Screen 
+          name="account" 
+          component={Text}
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: ({ color, size }) => (
+              <ProfileScreen />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+    borderTopWidth: 1,
+  },
+  tabs: {
+    flex: 1,
+    padding: GlobalStyles.space.base,
+    backgroundColor: Colors.background,
+  },
+});
+
+export default FooterBar;
