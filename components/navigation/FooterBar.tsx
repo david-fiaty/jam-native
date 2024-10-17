533c7fd5f22a1e8769a7e0a293400a59b5da9d83
david-fiaty <dfiaty@gmail.com>
2024-10-17 19:57:15 +0000

    Update FooterBar.tsx

--- components/navigation/FooterBar.tsx
+++ components/navigation/FooterBar.tsx
@@ -20,11 +20,12 @@ const FooterBar = () => {
       <TestModal />
       <TestScreen />
       <TouchableWithoutFeedback>
-      <TouchableOpacity onPress={() => {
-        setIsAddJamTabActive(!isAddJamTabActive);
-        console.log(isAddJamTabActive);
-      }}>
-      
+      <TouchableOpacity 
+        onPress={() => {
+          setIsAddJamTabActive(!isAddJamTabActive);
+          console.log(isAddJamTabActive);
+        }}
+      >
         <ClearIcon name="plus" size={GlobalStyles.footer.icon.size} />
       </TouchableOpacity>
       </TouchableWithoutFeedback>
