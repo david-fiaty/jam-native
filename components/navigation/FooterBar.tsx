08e67a6d14a5b0d84bf8f9d6f90d920b0d173904
david-fiaty <dfiaty@gmail.com>
2024-10-17 20:47:43 +0000

    Update FooterBar.tsx

--- components/navigation/FooterBar.tsx
+++ components/navigation/FooterBar.tsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { GlobalStyles, Colors } from '@/constants/GlobalStyles';
-import TestModal from '../TestModal';
+
 import ClearIcon from '../icons/ClearIcon';
 import TabElement from '../base/TabElement';
 import AddJamScreen from '../screens/AddJamScreen';
@@ -14,7 +14,6 @@ const FooterBar = () => {
 
   return (
     <View style={styles.container}>
-      <TestModal />
       <TabElement 
         label={<ClearIcon name="plus" size={GlobalStyles.footer.icon.size} />} 
         content={<AddJamScreen />} 
@@ -32,7 +31,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-around',
     width: '100%',
-    padding: GlobalStyles.space.base,
+    //padding: GlobalStyles.space.base,
     backgroundColor: Colors.background,
     borderTopWidth: 1,
     borderTopColor: Colors.primary,
