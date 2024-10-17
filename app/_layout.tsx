dac9cef670ca47fb08d201c901e3fc2caea24edc
david-fiaty <dfiaty@gmail.com>
2024-10-17 17:30:23 +0000

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={DefaultTheme}>
       <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{...headerOptions, ...fadeAnimationOptions }} />
         <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="about" options={{ headerShown: false }} />
         <Stack.Screen name="legal" options={{ headerShown: false }} />
