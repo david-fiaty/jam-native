2eb93099cc174aa02137da1a854dced6422866bd
david-fiaty <dfiaty@gmail.com>
2024-10-17 21:46:21 +0000

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,33 @@
 import WelcomeScreen from '@/components/screens/WelcomeScreen';
-import ViewportContainer from '@/components/base/ViewportContainer';
 import FooterBar from '@/components/navigation/FooterBar';
 import JamsScreen from '@/components/screens/JamsScreen';
+
+import { StyleSheet, View } from 'react-native';
+import ClearIcon from '@/components/icons/ClearIcon';
+import TabElement from '@/components/base/TabElement';
+import AddJamScreen from '@/components/screens/AddJamScreen';
+import { GlobalStyles, Colors } from '@/constants/GlobalStyles';
+import ViewportContainer from '@/components/base/ViewportContainer';
 
 const Index = () => {
   return (  
     <ViewportContainer>
-      <FooterBar />
+      <View style={styles.container}>
+        <TabElement 
+          label={<ClearIcon name="plus" size={GlobalStyles.footer.icon.size} />} 
+          content={<AddJamScreen />} 
+        />
+      </View>
     </ViewportContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'yellow',
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default Index;
