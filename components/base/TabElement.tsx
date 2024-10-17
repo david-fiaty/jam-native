86e55f1e0db106b6ed2967f590960fa0ee52dcd6
david-fiaty <dfiaty@gmail.com>
2024-10-17 23:47:53 +0000

    Update TabElement.tsx

--- components/base/TabElement.tsx
+++ components/base/TabElement.tsx
@@ -56,10 +56,11 @@ const styles = StyleSheet.create({
     top: 0,
     left: 0,
     right: 0,
+    bottom: 0,
     backgroundColor: 'rgba(0,0,0,0.5)',
     justifyContent: 'center',
     alignItems: 'center',
-    height: DeviceManager.modal.height - GlobalStyles.header.height,
+    //height: DeviceManager.modal.height - GlobalStyles.header.height,
   },
   modalContent: {
     backgroundColor: 'white',
