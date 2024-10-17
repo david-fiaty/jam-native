569d0860853ba8bbc416ad62205fd866193feb8a
david-fiaty <dfiaty@gmail.com>
2024-10-17 04:26:42 +0000

    Update ProfileScreen.tsx

--- components/screens/ProfileScreen.tsx
+++ components/screens/ProfileScreen.tsx
@@ -1,18 +1,28 @@
+import { View } from 'react-native';
 import ProfileForm from '@/components/profile/ProfileForm';
 import ProfileImage from '@/components/profile/ProfileImage';
 import ProfileJams from '@/components/profile/ProfileJams';
 import ProfileProjects from '@/components/profile/ProfileProjects';
 import ScrollContainer from '../base/ScrollContainer';
+import { GlobalStyles } from '@/constants/GlobalStyles';
 
 const ProfileScreen = () => {
   return (
-    <ScrollContainer>
-      <ProfileImage />
-      <ProfileForm />
-      <ProfileProjects />
-      <ProfileJams />
-    </ScrollContainer>   
+    <View style={styles.container}>
+      <ScrollContainer>
+        <ProfileImage />
+        <ProfileForm />
+        <ProfileProjects />
+        <ProfileJams />
+      </ScrollContainer>   
+    </View>
   );
+};
+
+const styles = {
+  container: {
+    paddingHorizontal: GlobalStyles.space.container,
+  },
 };
 
 export default ProfileScreen;
