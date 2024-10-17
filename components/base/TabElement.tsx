128196e527bb4d04de641fe615d0cdc63d743e9a
david-fiaty <dfiaty@gmail.com>
2024-10-17 23:08:30 +0000

    Update TabElement.tsx

--- components/base/TabElement.tsx
+++ components/base/TabElement.tsx
@@ -53,6 +53,7 @@ const styles = StyleSheet.create({
     height: '100%',
     flexDirection: 'column',
     justifyContent: 'flex-end',
+    alignItems: 'center',
   },
   label: {
     //position: 'absolute',
