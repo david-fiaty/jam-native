3288f35acc888300c72003b2c1f50ac26cb53b06
david-fiaty <dfiaty@gmail.com>
2024-10-18 06:55:19 +0000

    Update JamForm.tsx

--- components/forms/JamForm.tsx
+++ components/forms/JamForm.tsx
@@ -47,6 +47,28 @@ const JamForm = () => {
           { label: "Private", value: 0 },
         ]} 
       />
+      <SelectListField 
+        placeholder="Indutries"
+        data={[
+          { label: "Public", value: 1 },
+          { label: "Private", value: 0 },
+        ]} 
+      />
+      <SelectListField 
+        placeholder="Sub industries"
+        data={[
+          { label: "Public", value: 1 },
+          { label: "Private", value: 0 },
+        ]} 
+      />
+      <SelectListField 
+        placeholder="Sub industries"
+        data={[
+          { label: "Public", value: 1 },
+          { label: "Private", value: 0 },
+        ]} 
+      />
+
       { /* <MultiselectDropdownField /> */ }
       <View style={styles.buttonContainer}>
         <Button 
