f316ff6bf9cb8ce9ac94e1ce29bc73f7bf6595ef
david-fiaty <dfiaty@gmail.com>
2024-10-18 04:05:19 +0000

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,9 @@
+import { useEffect } from 'react';
 import 'react-native-reanimated';
 import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
 import { Colors } from '@/constants/GlobalStyles';
 import HeaderBar from '@/components/navigation/HeaderBar';
 
