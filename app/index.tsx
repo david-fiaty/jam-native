696bc78e8cf59997f082601cf5e3c6a1415d37e3
david-fiaty <dfiaty@gmail.com>
2024-10-18 12:08:59 +0000

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
-import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, View, TouchableOpacity, AppState } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import ClearIcon from '@/components/icons/ClearIcon';
 import DeviceManager from '@/classes/DeviceManager';
 import AddJamScreen from '@/components/screens/AddJamScreen';
@@ -19,6 +20,9 @@ const Index = () => {
   const [addTabActive, setAddTabActive] = useState(false);
   const [profileTabActive, setProfileTabActive] = useState(false);
   
+  const [isComponentVisible, setIsComponentVisible] = useState(false);
+  const [appState, setAppState] = useState(AppState.currentState);
+
   const WelcomeScreen = () => {
     return (
       <View style={styles.container}>
@@ -30,6 +34,45 @@ const Index = () => {
         </FullScreenView>
       </View>
     );
+  };
+
+
+  useEffect(() => {
+    // Check AsyncStorage when the app is launched to decide if the component should be shown
+    const checkIfFirstLaunch = async () => {
+      const alreadyShown = await AsyncStorage.getItem('componentShown');
+      if (!alreadyShown) {
+        setIsComponentVisible(true);
+        // Mark the component as shown for this session
+        await AsyncStorage.setItem('componentShown', 'true');
+      }
+    };
+
+    checkIfFirstLaunch();
+
+    // Listener to detect when the app comes to the foreground
+    const appStateListener = AppState.addEventListener('change', handleAppStateChange);
+
+    return () => {
+      // Cleanup the listener when component unmounts
+      appStateListener.remove();
+    };
+  }, []);
+
+  const handleAppStateChange = (nextAppState: any) => {
+    if (appState.match(/inactive|background/) && nextAppState === 'active') {
+      // App has been relaunched, check the component visibility again
+      checkIfComponentShouldBeVisible();
+    }
+    setAppState(nextAppState);
+  };
+
+  const checkIfComponentShouldBeVisible = async () => {
+    const alreadyShown = await AsyncStorage.getItem('componentShown');
+    if (!alreadyShown) {
+      setIsComponentVisible(true);
+      await AsyncStorage.setItem('componentShown', 'true');
+    }
   };
 
   return (  
@@ -73,7 +116,7 @@ const Index = () => {
           </TouchableOpacity>
         </View>
         <View style={styles.modal}>
-          <WelcomeScreen />
+          {isComponentVisible && <WelcomeScreen />}
           {!mapTabActive && !addTabActive && !profileTabActive && (<JamsScreen />)}
           {mapTabActive && (<MapScreen />)}
           {addTabActive && (<AddJamScreen />)}
