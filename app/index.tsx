f5fbde4585bc131afbfca32a25e331606bcea6d3
david-fiaty <dfiaty@gmail.com>
2024-10-18 12:17:32 +0000

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,5 @@
-import { useState, useEffect } from 'react';
-import { StyleSheet, View, TouchableOpacity, AppState } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState } from 'react';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
 import ClearIcon from '@/components/icons/ClearIcon';
 import DeviceManager from '@/classes/DeviceManager';
 import AddJamScreen from '@/components/screens/AddJamScreen';
@@ -9,72 +8,13 @@ import ViewportContainer from '@/components/base/ViewportContainer';
 import MapScreen from '@/components/screens/MapScreen';
 import ProfileScreen from '@/components/screens/ProfileScreen';
 import JamsScreen from '@/components/screens/JamsScreen';
-import Slideshow from '@/components/slideshow/Slideshow';
-import BottomLinks from '@/components/navigation/BottomLinks';
-import JamLogo from '@/components/images/JamLogo';
-import LoginSignupButton from '@/components/buttons/LoginSignupButton';
-import FullScreenView from '@/components/base/FullScreenView';
+import WelcomeScreen from '@/components/screens/WelcomeScreen';
 
 const Index = () => {
   const [mapTabActive, setMapTabActive] = useState(false);
   const [addTabActive, setAddTabActive] = useState(false);
   const [profileTabActive, setProfileTabActive] = useState(false);
   
-  const [isComponentVisible, setIsComponentVisible] = useState(false);
-  const [appState, setAppState] = useState(AppState.currentState);
-
-  const WelcomeScreen = () => {
-    return (
-      <View style={styles.container}>
-        <FullScreenView>
-          <JamLogo width={110} height={110} />    
-          <Slideshow />
-          <LoginSignupButton />
-          <BottomLinks />
-        </FullScreenView>
-      </View>
-    );
-  };
-
-
-  useEffect(() => {
-    // Check AsyncStorage when the app is launched to decide if the component should be shown
-    const checkIfFirstLaunch = async () => {
-      const alreadyShown = await AsyncStorage.getItem('componentShown');
-      if (!alreadyShown) {
-        setIsComponentVisible(true);
-        // Mark the component as shown for this session
-        await AsyncStorage.setItem('componentShown', 'true');
-      }
-    };
-
-    checkIfFirstLaunch();
-
-    // Listener to detect when the app comes to the foreground
-    const appStateListener = AppState.addEventListener('change', handleAppStateChange);
-
-    return () => {
-      // Cleanup the listener when component unmounts
-      appStateListener.remove();
-    };
-  }, []);
-
-  const handleAppStateChange = (nextAppState: any) => {
-    if (appState.match(/inactive|background/) && nextAppState === 'active') {
-      // App has been relaunched, check the component visibility again
-      checkIfComponentShouldBeVisible();
-    }
-    setAppState(nextAppState);
-  };
-
-  const checkIfComponentShouldBeVisible = async () => {
-    const alreadyShown = await AsyncStorage.getItem('componentShown');
-    if (!alreadyShown) {
-      setIsComponentVisible(true);
-      await AsyncStorage.setItem('componentShown', 'true');
-    }
-  };
-
   return (  
     <ViewportContainer>
       <View style={styles.container}>
@@ -116,7 +56,7 @@ const Index = () => {
           </TouchableOpacity>
         </View>
         <View style={styles.modal}>
-          {isComponentVisible && <WelcomeScreen />}
+          <WelcomeScreen />
           {!mapTabActive && !addTabActive && !profileTabActive && (<JamsScreen />)}
           {mapTabActive && (<MapScreen />)}
           {addTabActive && (<AddJamScreen />)}
