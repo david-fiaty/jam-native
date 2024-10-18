b239d117b878acc70b5b5c7f43e73c3acd064b77
david-fiaty <dfiaty@gmail.com>
2024-10-18 09:36:02 +0000

    Update TestScreen.tsx

--- components/TestScreen.tsx
+++ components/TestScreen.tsx
@@ -1,35 +1,15 @@
-import * as React from 'react';
-import { View, useWindowDimensions } from 'react-native';
-import { TabView, SceneMap } from 'react-native-pager-view';
+import { useState } from 'react';
+import { Tab } from '@rneui/themed';
 
-const FirstRoute = () => (
-  <View style={{ flex: 1, backgroundColor: '#ff4081' }} />
-);
-
-const SecondRoute = () => (
-  <View style={{ flex: 1, backgroundColor: '#673ab7' }} />
-);
-
-const renderScene = SceneMap({
-  first: FirstRoute,
-  second: SecondRoute,
-});
-
-export default function TestScreen() {
-  const layout = useWindowDimensions();
-
-  const [index, setIndex] = React.useState(0);
-  const [routes] = React.useState([
-    { key: 'first', title: 'First' },
-    { key: 'second', title: 'Second' },
-  ]);
-
+const TestScreen = () => {
+  const [index, setIndex] = useState(0);
   return (
-    <TabView
-      navigationState={{ index, routes }}
-      renderScene={renderScene}
-      onIndexChange={setIndex}
-      initialLayout={{ width: layout.width }}
-    />
+    <>
+      <Tab value={index} onChange={setIndex} dense>
+        <Tab.Item>Tab</Tab.Item>
+        <Tab.Item>Tab</Tab.Item>
+      </Tab>
+    </>
   );
-}
+
+};
