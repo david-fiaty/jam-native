a0aa3222f83d6bda9d53fcc6fb01cd153dfd4f8e
david-fiaty <dfiaty@gmail.com>
2024-10-18 09:20:11 +0000

    Update TestScreen.tsx

--- components/TestScreen.tsx
+++ components/TestScreen.tsx
@@ -1,67 +1,35 @@
-import React, { useRef, useEffect } from 'react';
-import { View, Button, Animated, StyleSheet } from 'react-native';
-import { GlobalStyles, Colors } from '@/constants/GlobalStyles';
-import DeviceManager from '@/classes/DeviceManager';
+import * as React from 'react';
+import { View, useWindowDimensions } from 'react-native';
+import { TabView, SceneMap } from 'react-native-tab-view';
 
+const FirstRoute = () => (
+  <View style={{ flex: 1, backgroundColor: '#ff4081' }} />
+);
 
+const SecondRoute = () => (
+  <View style={{ flex: 1, backgroundColor: '#673ab7' }} />
+);
 
-const TestScreen = () => {
-  const { height } = DeviceManager.window; 
-  const slideAnim = useRef(new Animated.Value(height)).current; 
-  
-  const slideIn = () => {
-    Animated.timing(slideAnim, {
-      toValue: -height, 
-      duration: 300, 
-      useNativeDriver: true, 
-    }).start();
-  };
-
-  const slideOut = () => {
-    Animated.timing(slideAnim, {
-      toValue: height,
-      duration: 300,
-      useNativeDriver: true,
-    }).start();
-  };
-
-  return (
-    <View style={styles.container}>
-      <Button title="Slide Up" onPress={slideIn} />
-
-      <Animated.View
-        style={[
-          styles.animatedView,
-          { transform: [{ translateY: slideAnim }] }, 
-        ]}
-      >
-        <View style={styles.content}>
-          <Button title="Slide Down" onPress={slideOut} />
-        </View>
-      </Animated.View>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-  animatedView: {
-    position: 'absolute',
-    width: DeviceManager.window.width,
-    left: 0,
-    backgroundColor: 'lightblue',
-    top: DeviceManager.window.height,
-    height: DeviceManager.modal.height - GlobalStyles.header.height + GlobalStyles.space.base,
-  },
-  content: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+const renderScene = SceneMap({
+  first: FirstRoute,
+  second: SecondRoute,
 });
 
-export default TestScreen;
+export default function TestSceen() {
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'first', title: 'First' },
+    { key: 'second', title: 'Second' },
+  ]);
+
+  return (
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{ width: layout.width }}
+    />
+  );
+}
