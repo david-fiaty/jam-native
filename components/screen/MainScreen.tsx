d16b81994fe0b557eaba194ef7c0105972c8f5ed
david-fiaty <dfiaty@gmail.com>
2024-10-19 21:26:07 +0000

    Update MainScreen.tsx

--- components/screen/MainScreen.tsx
+++ components/screen/MainScreen.tsx
@@ -1,17 +1,13 @@
-
-import { useTranslation, initReactI18next } from "react-i18next";
 import { Text, Button } from '@rneui/themed';
-import  t from '@/translation/i18n'; 
+import i18n from '@/translation/i18n'; 
 
 
 export default () => {  
-  const { t } = useTranslation();
-  
   return (
     <>
       <Text>Text block</Text>
       <Button title="My Button" />
-      <Text>{t('welcome')}</Text>
+      <Text>{i18n.t('welcome')}</Text>
     </>
   );
 }
