e119791b9ca5b57858f88f35c7afa515b7295a9b
david-fiaty <dfiaty@gmail.com>
2024-10-19 17:58:22 +0000

    Update languages.ts

--- translation/languages.ts
+++ translation/languages.ts
@@ -1,6 +1,5 @@
 import { initReactI18next } from "react-i18next";
 import i18n from "i18next";
-
 import english from '@/translation/resources/english';
 import french from '@/translation/resources/french';
 
