15e53d35750e6038d768d009e59ac117bc931965
david-fiaty <dfiaty@gmail.com>
2024-10-19 18:15:53 +0000

    Create i18n.ts

---
+++ translation/i18n.ts
@@ -0,0 +1,20 @@
+import { initReactI18next } from "react-i18next";
+import i18n from "i18next";
+import english from '@/translation/resources/english';
+import french from '@/translation/resources/french';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    lng: 'en',
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {
+      en: { translation: english },
+      fr: { translation: french },
+    },
+  });
+
+export default i18n;
