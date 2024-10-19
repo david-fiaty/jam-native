492f7a7156be01ba6dd6bf1f0da889ebc4f179e2
david-fiaty <dfiaty@gmail.com>
2024-10-19 19:14:40 +0000

    Update AppLogo.tsx

--- components/image/AppLogo.tsx
+++ components/image/AppLogo.tsx
@@ -9,11 +9,7 @@ const source = require('@/assets/images/jam-logo.png');
 
 export default ({width, height}: Props) => {
   return (
-    <StaticImage 
-      source={source} 
-      width={width}
-      height={height}
-    />   
+    <StaticImage source={source} width={width} height={height} />   
   );
 };
 
