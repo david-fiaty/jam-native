9dd015daf40b99cbd20e900767068949cefdfd2f
david-fiaty <dfiaty@gmail.com>
2024-10-19 14:48:13 +0000

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
+import { ThemeProvider, createTheme } from '@rneui/themed';
 import ViewportContainer from "@/components/base/ViewportContainer";
-import SplashScreen from "@/components/screens/SplashScreen";
+import SplashScreen from "@/components/screen/SplashScreen";
 
 const Index = () => {
   return (  
