ce414b1df7cbf73d1cbafd624d3536974308be58
david-fiaty <dfiaty@gmail.com>
2024-10-19 21:40:39 +0000

    Update ScreenView.tsx

--- components/view/ScreenView.tsx
+++ components/view/ScreenView.tsx
@@ -1,4 +1,5 @@
-import { Colors, GlobalStyles } from '@/constants/GlobalStyles';
+import { Colors } from '@/constants/Colors';
+import { GlobalStyles } from '@/constants/GlobalStyles';
 import { ReactNode } from 'react';
 import { StyleSheet, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -21,7 +22,7 @@ export default ({style, children}: Props) => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    //flex: 1,
     backgroundColor: Colors.background,
     paddingTop: GlobalStyles.space.container,
   },
