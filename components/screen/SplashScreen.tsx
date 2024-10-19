9f798835e71b38d3354e7615fb0f0fc6cb8bbb72
david-fiaty <dfiaty@gmail.com>
2024-10-19 16:45:19 +0000

    Update SplashScreen.tsx

--- components/screen/SplashScreen.tsx
+++ components/screen/SplashScreen.tsx
@@ -1,10 +1,7 @@
 import { Text } from 'react-native';
-import ViewportContainer from '../base/ViewportContainer';
 
 export default function SplashScreen() {
   return (
-    <ViewportContainer>
-      <Text>ExpoSplashScreen Demo! 👋</Text>
-    </ViewportContainer>
+    <Text>ExpoSplashScreen Demo! 👋</Text>
   );
 }
