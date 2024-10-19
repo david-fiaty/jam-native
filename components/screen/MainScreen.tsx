066aeea42c52a974e6638425b5f87f41adf77ca9
david-fiaty <dfiaty@gmail.com>
2024-10-19 18:05:19 +0000

    Update MainScreen.tsx

--- components/screen/MainScreen.tsx
+++ components/screen/MainScreen.tsx
@@ -1,10 +1,22 @@
+import i18n from '@/translation/languages'; 
+import { useTranslation } from 'react-i18next';
 import { Text, Button } from '@rneui/themed';
 
 export default function MainScreen() {
+  const { t } = useTranslation();
+  
+  const changeLanguage = (lang) => {
+    i18n.changeLanguage(lang); 
+  };
+
   return (
     <>
-    <Text>Text block</Text>
-    <Button title="My Button" />
+      <Text>Text block</Text>
+      <Button title="My Button" />
+      <Text>{t('welcome')}</Text>
+
+      <Button title="french" onPress={() => changeLanguage('fr')} />
+      <Button title="english" onPress={() => changeLanguage('en')} />
     </>
   );
 }
