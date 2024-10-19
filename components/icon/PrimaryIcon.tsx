58bc187f3f42b8b8e28cc60a5e9c8ff0387f8b9d
david-fiaty <dfiaty@gmail.com>
2024-10-19 23:38:58 +0000

    Create PrimaryIcon.tsx

---
+++ components/icon/PrimaryIcon.tsx
@@ -0,0 +1,54 @@
+import { StyleSheet } from 'react-native';
+import StaticIcon from '../base/IconBase';
+import { Layout } from '@/constants/Layout';
+
+type Props = {
+  name: string,
+  size?: number, 
+};
+
+const PrimaryIcon = ({name, size}: Props) => {
+
+  return (
+    <StaticIcon 
+      name={name} 
+      iconStyle={styles.iconStyle} 
+      containerStyle={styles.containerStyle}
+      size={size} 
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  /*
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: GlobalStyles.space.base*3,
+    height: GlobalStyles.space.base*3,
+    borderRadius: GlobalStyles.space.base*1.5,
+    backgroundColor: Colors.primary,
+    borderColor: Colors.primary,
+  },
+  */
+  containerStyle: {
+    //width: 40,
+    //height: 40,
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'flex-start',
+    justifyContent: 'center',
+    borderRadius: 8,
+    borderWidth: 1,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  iconStyle: {
+    color: 'red',
+    backgroundColor: 'orange',
+    padding: Layout.space.base,
+  },
+});
+
+export default PrimaryIcon;
