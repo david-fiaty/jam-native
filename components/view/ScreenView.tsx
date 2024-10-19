8ad1b1a928cfa48145c2f26a71829a0cb87c729e
david-fiaty <dfiaty@gmail.com>
2024-10-19 17:03:11 +0000

    Create ScreenView.tsx

---
+++ components/view/ScreenView.tsx
@@ -0,0 +1,30 @@
+import { Colors, GlobalStyles } from '@/constants/GlobalStyles';
+import { ReactNode } from 'react';
+import { StyleSheet, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+type Props = {
+  style?: object,
+  children?: ReactNode,
+};
+
+const ScreenView = ({style, children}: Props) => {
+  return (
+    <SafeAreaView 
+      edges={['left', 'right', 'bottom']} 
+      style={[styles.container, style]}
+    >
+      {children}
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.background,
+    paddingTop: GlobalStyles.space.container,
+  },
+});
+
+export default ScreenView;
