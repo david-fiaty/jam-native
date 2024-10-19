af0839196569b996b4822e13b7710df3767174eb
david-fiaty <dfiaty@gmail.com>
2024-10-19 16:05:34 +0000

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,54 @@
+import { useCallback, useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 import { Button } from '@rneui/themed';
 import { ThemeProvider } from '@rneui/themed';
+import Entypo from '@expo/vector-icons/Entypo';
 import ViewportContainer from "@/components/base/ViewportContainer";
-import SplashScreen from "@/components/screen/SplashScreen";
 import BaseTheme from "@/constants/BaseTheme";
 
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
 export default () => {
-  return (  
+  const [appIsReady, setSplashScreenIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync(Entypo.font);
+        await new Promise(resolve => setTimeout(resolve, 3000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setSplashScreenIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutReady = useCallback(async () => {
+    if (appIsReady) await SplashScreen.hideAsync();
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return (
+      <View onLayout={onLayoutReady}>
+  
+        <Text>SplashScreen Demo! 👋</Text>
+        
+      </View>
+    );
+  }
+
+  return (
     <ThemeProvider theme={BaseTheme}>
       <ViewportContainer>
-        <SplashScreen />
-        <Button title="My Button" />
+        <Button title="My Button" titleStyle={{ color: 'pink' }} />
       </ViewportContainer>
     </ThemeProvider>
   );
-};
+}
