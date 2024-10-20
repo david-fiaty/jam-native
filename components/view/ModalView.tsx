3d6054ab2f41c4e2ab2ffa5c0a5ad56328a4f684
david-fiaty <dfiaty@gmail.com>
2024-10-20 13:28:40 +0000

    Update ModalView.tsx

--- components/view/ModalView.tsx
+++ components/view/ModalView.tsx
@@ -1,6 +1,11 @@
-import { StyleSheet } from 'react-native';
+import React, { useRef, useEffect } from 'react';
+import { View, Button, Animated, StyleSheet, Dimensions } from 'react-native';
 import BoxView from './BoxView';
 import { BaseProps } from '@/constants/Types';
+import DeviceManager from '@/classes/DeviceManager';
+
+const { height } = DeviceManager.window.height;
+
 
 export default ({style, children}: BaseProps) => {
   return (
