85abb2c49abff7b07cf3a992d0ce76cdaab8800d
david-fiaty <dfiaty@gmail.com>
2024-10-20 00:52:22 +0000

    Create HorizontalView.tsx

---
+++ components/view/HorizontalView.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Layout } from '@/constants/Layout';
+
+type Props = {
+  style?: object,
+  children?: ReactNode,
+};
+
+export default ({style, children}: Props) => {
+  return (
+    <View style={[styles.container, style]}>
+      {children}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    gap: Layout.space.base,
+  },
+});
