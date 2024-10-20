409c6e51702b13dae13a11ad857321dfdb3c64dd
david-fiaty <dfiaty@gmail.com>
2024-10-20 01:38:54 +0000

    Update ScreenView.tsx

--- components/view/ScreenView.tsx
+++ components/view/ScreenView.tsx
@@ -1,6 +1,6 @@
 import { Colors } from '@/constants/Colors';
 import { ReactNode } from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 type Props = {
