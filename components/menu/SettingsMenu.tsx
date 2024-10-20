a54e09e4905be903c56b822bf3d113c71b47a6b0
david-fiaty <dfiaty@gmail.com>
2024-10-20 15:39:38 +0000

    Update SettingsMenu.tsx

--- components/menu/SettingsMenu.tsx
+++ components/menu/SettingsMenu.tsx
@@ -1,10 +1,51 @@
+import { StyleSheet, FlatList } from 'react-native';
 import TextBase from '../base/TextBase';
 import { BaseProps } from '@/constants/Types';
+import TextView from '../view/TextView';
+
+const items = [
+  {
+    'label': 'Account information',
+    'path': '/account',
+  },
+  {
+    'label': 'Change password',
+    'path': '/password',
+  },
+  {
+    'label': 'Language',
+    'path': '/language',
+  },
+];
 
 export default ({style, children}: BaseProps) => {
   return (
-    <TextBase style={style}>
-      {children}
-    </TextBase>
+    <TextView>Settings menu</TextView>
   );
+
+  /*
+  return (
+    <FlatList 
+      data={items} 
+      horizontal={false}  
+      style={styles.list}
+      renderItem={({item, index}) => {
+        return (
+          <TextView>{item.label}</TextView>
+        );
+      }} 
+    />
+  );
+  */
 };
+
+const styles = StyleSheet.create({
+  container: {
+  },
+  list: {
+    width: '100%',
+  },
+  item: {
+
+  },
+});
