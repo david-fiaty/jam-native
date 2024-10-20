09895d4ac26954d7d71b2753e67d11164632f727
david-fiaty <dfiaty@gmail.com>
2024-10-20 21:15:38 +0000

    Update ProfileForm.tsx

--- components/form/ProfileForm.tsx
+++ components/form/ProfileForm.tsx
@@ -1,11 +1,12 @@
-import { StyleSheet, View, FlatList } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { BaseProps } from '@/constants/Types';
 import TextView from '../view/TextView';
+import ListView from '../view/ListView';
 
 export default ({style, children}: BaseProps) => {
   return (
     <View style={styles.container}>
-      <TextView>Profile form</TextView>
+      <ListView />
     </View>
   );
 };
