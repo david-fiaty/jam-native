aa8a4e4cbea78d06f73d7c007b992817b7851fcc
david-fiaty <dfiaty@gmail.com>
2024-10-20 22:29:55 +0000

    Update ListView.tsx

--- components/view/ListView.tsx
+++ components/view/ListView.tsx
@@ -7,20 +7,21 @@ import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 type Props = BaseProps & {
   data: object,
+  labelField: string, 
   style?: object,
 };
 
-export default ({data, style}: Props) => {
+export default ({data, labelField, style}: Props) => {
   return (
     <FlatList 
       data={data} 
       horizontal={false}  
-      contentContainerStyle={styles.list}
+      contentContainerStyle={[style, styles.list]}
       renderItem={({item, index}) => {
         return (
           <TouchableOpacity>
             <View style={styles.item}>
-              <TextView>{item.name}</TextView>
+              <TextView>{item[labelField]}</TextView>
             </View>
           </TouchableOpacity>
         );
