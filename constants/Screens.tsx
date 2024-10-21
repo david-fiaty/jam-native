bd20d05f6b27a6b8d451dcefdd5eb480ed30f78d
david-fiaty <dfiaty@gmail.com>
2024-10-21 01:53:09 +0000

    Update Screens.tsx

--- constants/Screens.tsx
+++ constants/Screens.tsx
@@ -8,26 +8,32 @@ import ProfileForm from '@/components/form/ProfileForm';
 export const Screens = {
   settingsMenu: {
     effect: 'fade',
+    axis: null,
     component: () => <SettingsMenu />,
   },
   notificationsMenu: {
     effect: 'fade',
+    axis: null,
     component: () => <NotificationsMenu />,
   }, 
   searchMenu: {
     effect: 'slide',
+    axis: 'y',
     component: () => <SearchMenu />,
   },
   mapView: {
     effect: 'fade',
+    axis: null,
     component: () => <MapView />,
   },
   addJamForm: {
     effect: 'fade',
+    axis: null,
     component: () => <AddJamForm />,
   },
   profileForm: {
     effect: 'fade',
+    axis: null,
     component: () => <ProfileForm />,
   },
 };
