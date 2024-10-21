83632aa70bead581bf368b2ee6c9c3943384864c
david-fiaty <dfiaty@gmail.com>
2024-10-21 17:01:39 +0000

    Update ScreenAnimation.tsx

--- classes/ScreenAnimation.tsx
+++ classes/ScreenAnimation.tsx
@@ -2,6 +2,57 @@ import { Animated } from 'react-native';
 import DeviceManager from '@/classes/DeviceManager';
 
 class ScreenAnimation {
+  slideIn(reference: Animated.Value) {
+    return Animated.timing(reference, {
+      toValue: 0, 
+      duration: 300, 
+      useNativeDriver: true, 
+    });
+  }
+
+  slideOut(reference: Animated.Value) {
+    return Animated.timing(reference, {
+      toValue: DeviceManager.window.height, 
+      duration: 300,
+      useNativeDriver: true,
+    });
+  }
+
+  fadeIn(reference: Animated.Value) {
+    return Animated.timing(reference, {
+      toValue: 1, 
+      duration: 300, 
+      useNativeDriver: true, 
+    });
+  }
+
+
+  fadeOut(reference: Animated.Value) {
+    return Animated.timing(reference, {
+      toValue: 0, 
+      duration: 300, 
+      useNativeDriver: true, 
+    });
+  }
+
+  pushIn(reference: Animated.Value) {
+    return Animated.timing(reference, {
+      toValue: 0, 
+      duration: 300, 
+      useNativeDriver: true, 
+    });
+  }
+
+
+  pushOut(reference: Animated.Value) {
+    return Animated.timing(reference, {
+      toValue: DeviceManager.window.width, 
+      duration: 300, 
+      useNativeDriver: true, 
+    });
+  }
+
+
   slide(effect: Animated.Value) {
     return {
       in: (axis?: string) => {
