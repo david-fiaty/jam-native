de1423ebb542ac73b6c6a794175f0067cfb84db6
david-fiaty <dfiaty@gmail.com>
2024-10-21 21:04:20 +0000

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,22 @@
 import { Stack } from 'expo-router';
+import HeaderView from '@/components/view/HeaderView';
+import { Colors } from '@/constants/Colors';
+
+const screenOptions = { 
+  header: (props: object) => <HeaderView />,    
+  headerShown: true,
+  statusBarColor: Colors.background,
+  statusBarStyle: 'dark',
+  headerTintColor: Colors.background,    
+  headerStyle: {
+    backgroundColor: Colors.background, 
+  },
+};
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={screenOptions} />
     </Stack>
   );
 }
