d6fd148c75294255b4aa783c53aba5a68fae59ad
david-fiaty <dfiaty@gmail.com>
2024-10-21 17:05:58 +0000

    Update ScreenAnimation.tsx

--- classes/ScreenAnimation.tsx
+++ classes/ScreenAnimation.tsx
@@ -43,52 +43,12 @@ class ScreenAnimation {
     });
   }
 
-
   pushOut(reference: Animated.Value) {
     return Animated.timing(reference, {
       toValue: DeviceManager.window.width, 
       duration: 300, 
       useNativeDriver: true, 
     });
-  }
-
-
-  slide(effect: Animated.Value) {
-    return {
-      in: (axis?: string) => {
-        return Animated.timing(effect, {
-          toValue: 0, 
-          duration: 300, 
-          useNativeDriver: true, 
-        });
-      },
-      out: (axis?: string) => {
-        return Animated.timing(effect, {
-          toValue: DeviceManager.window.height, 
-          duration: 300,
-          useNativeDriver: true,
-        });
-      },
-    };
-  }
-
-  fade(effect: Animated.Value, axis?: string) {
-    return {
-      in: (axis?: string) => {
-        return Animated.timing(effect, {
-          toValue: 1, 
-          duration: 300, 
-          useNativeDriver: true, 
-        });
-      },
-      out: (axis?: string) => {
-        return Animated.timing(effect, {
-          toValue: 0, 
-          duration: 300, 
-          useNativeDriver: true, 
-        });
-      },
-    };
   }
 };
 
