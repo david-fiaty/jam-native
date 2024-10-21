3322565a19ef68c7e7f9bf9ca2787caff86c71f0
david-fiaty <dfiaty@gmail.com>
2024-10-21 23:21:52 +0000

    Update AddJamForm.tsx

--- components/form/AddJamForm.tsx
+++ components/form/AddJamForm.tsx
@@ -1,16 +1,19 @@
-import { StyleSheet, View, FlatList } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { BaseProps } from '@/constants/Types';
 import TextView from '../view/TextView';
+import i18n from '@/translation/i18n';
+import BoxView from '../view/BoxView';
 
 export default ({style, children}: BaseProps) => {
   return (
-    <View style={styles.container}>
-      <TextView>Add Jam form</TextView>
-    </View>
+    <BoxView align="flex-start" justify="flex-start">
+      <TextView>{i18n.t('What kind of Jam is it?')}</TextView>
+    </BoxView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    //justifyContent
   },
 });
