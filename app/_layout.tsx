aa5e3498395b99f6b425256ab1bcce4c8094b1f5
david-fiaty <dfiaty@gmail.com>
2024-10-21 00:00:33 +0000

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,6 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="notification" options={{ headerShown: false }} />
     </Stack>
   );
 }
