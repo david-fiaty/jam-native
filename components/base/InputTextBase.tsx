3dc9cb23992ee1282319ef5c3b4e60d477ef1bda
david-fiaty <dfiaty@gmail.com>
2024-10-22 17:26:17 +0000

    Create InputTextBase.tsx

---
+++ components/base/InputTextBase.tsx
@@ -0,0 +1,40 @@
+import { StyleSheet, TextInput } from 'react-native';
+import { BaseProps } from '@/constants/Types';
+import { Layout } from '@/constants/Layout';
+import { Colors } from '@/constants/Colors';
+import BoxView from '../view/BoxView';
+
+type Props = BaseProps & {
+  placeholder?: string,
+  containerStyle?: object,
+};
+
+const InputTextBase = ({placeholder, containerStyle}: Props) => {
+  return (
+    <BoxView style={[styles.container, containerStyle]}>
+      <TextInput
+        style={styles.element}
+        placeholder={placeholder}
+        placeholderTextColor={Colors.primary}
+        multiline={true}
+        textAlignVertical="center"
+        numberOfLines={1}
+      />
+    </BoxView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+  element: {
+    backgroundColor: Colors.secondary,
+    width: '100%',
+    borderWidth: 1, 
+    borderColor: Colors.secondary, 
+    borderRadius: Layout.radius.round,
+    paddingHorizontal: Layout.space.base, 
+    justifyContent: 'center',
+  },
+});
+
+export default InputTextBase;
