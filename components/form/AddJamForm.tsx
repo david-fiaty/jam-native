32757859523239c5ed88a8bbb3f3d0f07310a870
david-fiaty <dfiaty@gmail.com>
2024-10-22 15:47:58 +0000

    Renamed a component

--- components/form/AddJamForm.tsx
+++ components/form/AddJamForm.tsx
@@ -5,7 +5,7 @@ import TextView from "../view/TextView";
 import i18n from "@/translation/i18n";
 import BoxView from "../view/BoxView";
 import BackButton from "../button/BackButton";
-import SquareOptionsField from "../field/SquareOptionsField";
+import JamCategoriesField from "../field/JamCategoriesField";
 import AddMediaField from "../field/AddMediaField";
 
 const AddJamForm = ({ style, children }: BaseProps) => {
@@ -16,7 +16,7 @@ const AddJamForm = ({ style, children }: BaseProps) => {
         onPress={() => console.log('clicked')}
       />
       <TextView>{i18n.t('What kind of Jam is it?')}</TextView>
-      <SquareOptionsField data={JamCategoriesData} />
+      <JamCategoriesField data={JamCategoriesData} />
       <AddMediaField />
     </BoxView>
   );
