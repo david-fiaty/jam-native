c00014e76d9e168e5f1ab99c82497e9c84233965
david-fiaty <dfiaty@gmail.com>
2024-10-22 13:22:33 +0000

    Create DropdownField.tsx

---
+++ components/field/DropdownField.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text } from 'react-native';
+import { BaseProps } from '@/constants/Types';
+import { Colors } from '@/constants/Colors';
+
+const DropdownField = ({style, children}: BaseProps) => {
+  return (
+    <Text style={[styles.content, style]}>
+      {children}
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  content: {
+    color: Colors.primary,
+    //fontFamily: 'BaseFont', // Todo - Enable font
+  },
+});
+
+export default DropdownField;
