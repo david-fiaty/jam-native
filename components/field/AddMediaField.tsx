89debe5aa6a89cd28704b7a499b03a4ee0a3b9d7
david-fiaty <dfiaty@gmail.com>
2024-10-22 15:39:08 +0000

    Update AddMediaField.tsx

--- components/field/AddMediaField.tsx
+++ components/field/AddMediaField.tsx
@@ -1,5 +1,6 @@
 import { StyleSheet } from "react-native";
 import MediaPickerBase from "../base/MediaPickerBase";
+import i18n from "@/translation/i18n";
 import BoxView from "../view/BoxView";
 import TextView from "../view/TextView";
 import IconView from "../view/IconView";
@@ -14,7 +15,7 @@ const AddMediaField = () => {
       label={
         <BoxView direction="row" align="center" style={styles.container}>
           <IconView name="plus" theme="secondary" size={22} radius="round" />
-          <TextView>Add media</TextView>
+          <TextView>{i18n.t('Add media')}</TextView>
         </BoxView>
       }
     />
@@ -23,7 +24,7 @@ const AddMediaField = () => {
 
 const styles = StyleSheet.create({
   container: {
-    width: "100%",
+    width: '100%',
   },
 });
 
