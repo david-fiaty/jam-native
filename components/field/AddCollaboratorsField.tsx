e8f23a0af768db2d87caf6a03a5f2a3fc6f90c5b
david-fiaty <dfiaty@gmail.com>
2024-10-22 15:55:02 +0000

    Create AddCollaboratorsField.tsx

---
+++ components/field/AddCollaboratorsField.tsx
@@ -0,0 +1,37 @@
+
+import { StyleSheet, View, FlatList, TouchableOpacity } from 'react-native';
+import CheckboxSelectBase from '../base/CheckboxSelectBase';
+
+const AddCollaboratorsField = () => {  
+  return (
+    <CheckboxSelectBase />
+  );
+}
+
+const styles = StyleSheet.create({
+/*
+
+  container: {
+    flexDirection: 'column',
+    gap: Layout.space.small,
+  },
+  square: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Colors.secondary,
+    padding: Layout.space.base,
+    borderWidth: 1,
+    borderRadius: Layout.radius.round,
+    borderColor: Colors.secondary,
+    width: Layout.space.base*7,
+    height: Layout.space.base*7,
+  },
+  selected: {
+    borderColor: Colors.primary,
+  },
+  */
+
+});
+
+export default AddCollaboratorsField;
