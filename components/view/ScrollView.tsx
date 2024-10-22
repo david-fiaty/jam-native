8f3c25a0bc7ae1b8bc02560bb3e233a40071d875
david-fiaty <dfiaty@gmail.com>
2024-10-22 20:37:27 +0000

    Create ScrollView.tsx

---
+++ components/view/ScrollView.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react';
+import { Pressable, StyleSheet, ScrollView } from 'react-native';
+
+type Props = {
+  style?: object,
+  children?: ReactNode,
+};
+
+const ScrollContainer = ({style, children}: Props) => {
+  return (
+    <ScrollView 
+      style={[styles.container, style]}
+      nestedScrollEnabled={true}
+      contentContainerStyle={styles.content}
+    >
+      <Pressable>{children}</Pressable>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+  content: {},
+});
+
+export default ScrollContainer;
