2a40b277e11f64b91dddfe3e87ba0a2ec856d485
david-fiaty <dfiaty@gmail.com>
2024-10-22 13:33:22 +0000

    Update AddJamForm.tsx

--- components/form/AddJamForm.tsx
+++ components/form/AddJamForm.tsx
@@ -1,43 +1,47 @@
-import { StyleSheet } from 'react-native';
-import { BaseProps } from '@/constants/Types';
-import TextView from '../view/TextView';
-import i18n from '@/translation/i18n';
-import BoxView from '../view/BoxView';
-import BackButton from '../button/BackButton';
-import SquareOptionsField from '../field/SquareOptionsField';
-import MediaPickerField from '../field/MediaPickerField';
+import { StyleSheet } from "react-native";
+import { BaseProps } from "@/constants/Types";
+import TextView from "../view/TextView";
+import i18n from "@/translation/i18n";
+import BoxView from "../view/BoxView";
+import BackButton from "../button/BackButton";
+import SquareOptionsField from "../field/SquareOptionsField";
+import MediaPickerField from "../field/MediaPickerField";
+import DropdownField from "../field/DropdownField";
 
 const jamCategories = [
   {
-    id: 'calls',
-    label: i18n.t('Calls'),
-    icon: 'megaphone',
+    id: "calls",
+    label: i18n.t("Calls"),
+    icon: "megaphone",
   },
   {
-    id: 'looking',
-    label: i18n.t('Looking'),
-    icon: 'link',
+    id: "looking",
+    label: i18n.t("Looking"),
+    icon: "link",
   },
   {
-    id: 'events',
-    label: i18n.t('Events'),
-    icon: 'users',
+    id: "events",
+    label: i18n.t("Events"),
+    icon: "users",
   },
   {
-    id: 'random',
-    label: i18n.t('Random'),
-    icon: 'infinite',
+    id: "random",
+    label: i18n.t("Random"),
+    icon: "infinite",
   },
 ];
 
-const AddJamForm = ({style, children}: BaseProps) => {
+const AddJamForm = ({ style, children }: BaseProps) => {
   return (
     <BoxView align="flex-start" justify="flex-start">
-      <BackButton title={i18n.t('Add new Jam')} onPress={() => console.log('clicked')} />
-      <TextView>{i18n.t('What kind of Jam is it?')}</TextView>
+      <BackButton
+        title={i18n.t("Add new Jam")}
+        onPress={() => console.log("clicked")}
+      />
+      <TextView>{i18n.t("What kind of Jam is it?")}</TextView>
       <SquareOptionsField data={jamCategories} />
       <MediaPickerField />
-      
+      <DropdownField />
     </BoxView>
   );
 };
