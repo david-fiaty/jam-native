635a3e423b905f1fb11108d9efa1472c53537d23
david-fiaty <dfiaty@gmail.com>
2024-10-22 14:13:58 +0000

    Update AddJamForm.tsx

--- components/form/AddJamForm.tsx
+++ components/form/AddJamForm.tsx
@@ -1,13 +1,14 @@
 import { StyleSheet } from "react-native";
 import { BaseProps } from "@/constants/Types";
-import TextView from "../view/TextView";
+import { JamCategoriesData } from "@/constants/Data";
 import i18n from "@/translation/i18n";
-import BoxView from "../view/BoxView";
 import BackButton from "../button/BackButton";
 import SquareOptionsField from "../field/SquareOptionsField";
 import MediaPickerField from "../field/MediaPickerField";
 import DropdownField from "../field/DropdownField";
-import { JamCategoriesData } from "@/constants/Data";
+import TextView from '../view/TextView';
+import IconView from '../view/IconView';
+import BoxView from '../view/BoxView';
 
 const AddJamForm = ({ style, children }: BaseProps) => {
   return (
@@ -18,7 +19,12 @@ const AddJamForm = ({ style, children }: BaseProps) => {
       />
       <TextView>{i18n.t('What kind of Jam is it?')}</TextView>
       <SquareOptionsField data={JamCategoriesData} />
-      <MediaPickerField />
+      <MediaPickerField label={
+        <BoxView direction="row" align="center" style={styles.container}>
+          <IconView name="plus" theme="secondary" size={22} />
+          <TextView>Add media</TextView> 
+        </BoxView>      
+      } />
       <DropdownField />
     </BoxView>
   );
