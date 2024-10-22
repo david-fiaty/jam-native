ef24cd05dce57b36421f4a7bf0ed447b84d4e97b
david-fiaty <dfiaty@gmail.com>
2024-10-22 19:37:16 +0000

    Update Data.ts

--- constants/Data.ts
+++ constants/Data.ts
@@ -22,3 +22,134 @@ export const JamCategoriesData = [
     icon: "infinite",
   },
 ];
+
+export const StatusOptionsData = [
+  { 
+    label: i18n.t('Enabled'), 
+    value: 1 
+  },
+  { 
+    label: i18n.t('Disabled'), 
+    value: 0 
+  },
+];
+
+export const IndustriesData = [
+  {
+    label: 'Books and Press / Publishing',
+    value: 'Books and Press / Publishing',
+  },
+  {
+    label: 'Audiovisual',
+    value: 'Audiovisual',
+  },
+  {
+    label: 'Performing Arts',
+    value: 'Performing Arts',
+  },
+  {
+    label: 'Visual Arts and Crafts',
+    value: 'Visual Arts and Crafts',
+  },
+  {
+    label: 'Design and Architecture',
+    value: 'Design and Architecture',
+  },
+  {
+    label: 'Creative Services',
+    value: 'Creative Services',
+  },
+  {
+    label: 'Publishing Professions',
+    value: 'Publishing Professions',
+  },
+  {
+    label: 'Audiovisual Professions',
+    value: 'Audiovisual Professions',
+  },
+  {
+    label: 'Performing Arts Professions',
+    value: 'Performing Arts Professions',
+  },
+  {
+    label: 'Visual Arts Professions',
+    value: 'Visual Arts Professions',
+  },
+  {
+    label: 'Design Professions',
+    value: 'Design Professions',
+  },
+  {
+    label: 'Service Professions',
+    value: 'Service Professions',
+  },
+  {
+    label: 'Places / Venues',
+    value: 'Places / Venues',
+  },
+  {
+    label: 'Cultural Activities',
+    value: 'Cultural Activities',
+  },
+];
+
+
+export const SubIndustriesData = [
+  {
+    label: 'Books and Press / Publishing',
+    value: 'Books and Press / Publishing',
+  },
+  {
+    label: 'Audiovisual',
+    value: 'Audiovisual',
+  },
+  {
+    label: 'Performing Arts',
+    value: 'Performing Arts',
+  },
+  {
+    label: 'Visual Arts and Crafts',
+    value: 'Visual Arts and Crafts',
+  },
+  {
+    label: 'Design and Architecture',
+    value: 'Design and Architecture',
+  },
+  {
+    label: 'Creative Services',
+    value: 'Creative Services',
+  },
+  {
+    label: 'Publishing Professions',
+    value: 'Publishing Professions',
+  },
+  {
+    label: 'Audiovisual Professions',
+    value: 'Audiovisual Professions',
+  },
+  {
+    label: 'Performing Arts Professions',
+    value: 'Performing Arts Professions',
+  },
+  {
+    label: 'Visual Arts Professions',
+    value: 'Visual Arts Professions',
+  },
+  {
+    label: 'Design Professions',
+    value: 'Design Professions',
+  },
+  {
+    label: 'Service Professions',
+    value: 'Service Professions',
+  },
+  {
+    label: 'Places / Venues',
+    value: 'Places / Venues',
+  },
+  {
+    label: 'Cultural Activities',
+    value: 'Cultural Activities',
+  },
+];
+
