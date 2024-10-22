6542232d98e0e3c66747d77fcbc559c0081c3d58
david-fiaty <dfiaty@gmail.com>
2024-10-22 21:37:59 +0000

    Update ProfileForm.tsx

--- components/form/ProfileForm.tsx
+++ components/form/ProfileForm.tsx
@@ -1,15 +1,42 @@
 import { StyleSheet, View } from 'react-native';
 import { BaseProps } from '@/constants/Types';
-import ListView from '../view/ListView';
-import ApiClient from '@/classes/ApiClient';
+import TextView from '../view/TextView';
+import i18n from "@/translation/i18n";
+import BoxView from "../view/BoxView";
+import BackButton from "../button/BackButton";
+import JamCategoriesField from "../field/JamCategoriesField";
+import AddMediaField from "../field/AddMediaField";
+import AddCollaboratorsField from "../field/AddCollaboratorsField";
+import AddCaptionField from "../field/AddCaptionField";
+import UserLocationField from "../field/UserLocationField";
+import StatusField from "../field/StatusField";
+import IndustryField from "../field/IndustryField";
+import PostButton from "../button/PostButton";
+import DividerView from "../view/DividerView";
 
 const ProfileForm = ({style, children}: BaseProps) => {
-  const data = ApiClient.get('jammers');
 
   return (
-    <View style={styles.container}>
-      <ListView data={data} />
-    </View>
+    <BoxView align="flex-start" justify="flex-start" scroll={true}>
+      <BackButton
+        title={i18n.t('Add new Jam')}
+        onPress={() => console.log('clicked')}
+      />
+      <TextView>{i18n.t('What kind of Jam is it?')}</TextView>
+
+      <DividerView />
+      <AddMediaField />
+      <AddCollaboratorsField />
+
+      <DividerView />
+      <AddCaptionField />
+
+      <DividerView />
+      <UserLocationField />
+      <StatusField />
+      <IndustryField />
+      <PostButton />
+    </BoxView>
   );
 };
 
