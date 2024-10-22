f8203572354c736a453875520f46ebdafccca9a9
david-fiaty <dfiaty@gmail.com>
2024-10-22 22:04:14 +0000

    Create CreativeOrganizationField.tsx

---
+++ components/field/CreativeOrganizationField.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+import { StatusOptionsData } from '@/constants/Data';
+import i18n from '@/translation/i18n';
+import BoxView from '../view/BoxView';
+import SelectListBase from '../base/SelectListBase';
+
+const CreativeOrganizationField = () => {
+  return (
+    <BoxView direction="row" align="space-between" style={styles.container}>
+      <SelectListBase data={StatusOptionsData} placeholder={i18n.t('Status')} />
+    </BoxView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+  },
+});
+
+export default CreativeOrganizationField;
