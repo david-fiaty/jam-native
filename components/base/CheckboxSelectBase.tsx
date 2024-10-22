2f1579c1d85d779abfeecb901f8c9ec9afa36404
david-fiaty <dfiaty@gmail.com>
2024-10-22 16:15:53 +0000

    Update CheckboxSelectBase.tsx

--- components/base/CheckboxSelectBase.tsx
+++ components/base/CheckboxSelectBase.tsx
@@ -19,42 +19,6 @@ const CheckboxSelectBase = ({label, title, data}: Props) => {
 
 const styles = StyleSheet.create({
   container: {},
-
-  
-  /*
-  list: {
-    width: '100%',
-    marginTop: GlobalStyles.space.base*2,
-    borderWidth: 1,
-    borderColor: Colors.primary,
-    borderRadius: GlobalStyles.space.base,
-    padding: GlobalStyles.space.base,
-  },
-  item: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: GlobalStyles.space.base,
-  },
-  left: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    gap: GlobalStyles.space.base,
-  },
-  right: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-  },
-  scroll: {
-    flexGrow: 1,
-  },  
-  checkbox: {
-    padding: 0,
-    margin: 0,
-  },
-  */
 });
 
 export default CheckboxSelectBase;
