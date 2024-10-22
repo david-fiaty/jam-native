9032ebc7535cead91fe980188d8c921d2e876ad3
david-fiaty <dfiaty@gmail.com>
2024-10-22 15:36:42 +0000

    Update AddMediaField.tsx

--- components/field/AddMediaField.tsx
+++ components/field/AddMediaField.tsx
@@ -1,27 +1,29 @@
-import { StyleSheet } from 'react-native';
-import MediaPickerBase from '../base/MediaPickerBase';
-import BoxView from '../view/BoxView';
-import TextView from '../view/TextView';
-import IconView from '../view/IconView';
+import { StyleSheet } from "react-native";
+import MediaPickerBase from "../base/MediaPickerBase";
+import BoxView from "../view/BoxView";
+import TextView from "../view/TextView";
+import IconView from "../view/IconView";
 
 type ImagePreviewProps = {
   selectedImage?: string;
 };
 
-const AddMediaField = () => {  
+const AddMediaField = () => {
   return (
-      <MediaPickerBase label={
+    <MediaPickerBase
+      label={
         <BoxView direction="row" align="center" style={styles.container}>
           <IconView name="plus" theme="secondary" size={22} radius="round" />
-          <TextView>Add media</TextView> 
+          <TextView>Add media</TextView>
         </BoxView>
-      } />
+      }
+    />
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
+    width: "100%",
   },
 });
 
