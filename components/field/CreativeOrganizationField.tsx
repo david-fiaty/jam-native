34c6eda2d3adb89a566b4284d4a841fcaaee84df
david-fiaty <dfiaty@gmail.com>
2024-10-22 22:08:21 +0000

    Update CreativeOrganizationField.tsx

--- components/field/CreativeOrganizationField.tsx
+++ components/field/CreativeOrganizationField.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet } from 'react-native';
-import { StatusOptionsData } from '@/constants/Data';
+import { CreativeOrganizationOptionsData } from '@/constants/Data';
 import i18n from '@/translation/i18n';
 import BoxView from '../view/BoxView';
 import SelectListBase from '../base/SelectListBase';
@@ -7,7 +7,7 @@ import SelectListBase from '../base/SelectListBase';
 const CreativeOrganizationField = () => {
   return (
     <BoxView direction="row" align="space-between" style={styles.container}>
-      <SelectListBase data={StatusOptionsData} placeholder={i18n.t('Status')} />
+      <SelectListBase data={CreativeOrganizationOptionsData} placeholder={i18n.t('Creative organization')} />
     </BoxView>
   );
 };
