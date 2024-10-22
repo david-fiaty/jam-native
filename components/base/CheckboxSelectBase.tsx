7df7687f35e75c8aa561c26c4bafa0634ee84bea
david-fiaty <dfiaty@gmail.com>
2024-10-22 16:40:53 +0000

    Update CheckboxSelectBase.tsx

--- components/base/CheckboxSelectBase.tsx
+++ components/base/CheckboxSelectBase.tsx
@@ -1,5 +1,6 @@
 import { StyleSheet, TouchableOpacity } from 'react-native';
 import { CheckBox } from '@rneui/themed';
+import ModalView from '../view/ModalView';
 
 type Props = {
   label: JSX.Element,
