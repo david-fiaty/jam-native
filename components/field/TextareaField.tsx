6dd451755bb1a8e9a01b1e3f8db03924a605be26
david-fiaty <dfiaty@gmail.com>
2024-10-22 16:51:40 +0000

    Update TextareaField.tsx

--- components/field/TextareaField.tsx
+++ components/field/TextareaField.tsx
@@ -30,7 +30,7 @@ const styles = StyleSheet.create({
   element: {
     backgroundColor: Colors.secondary,
     width: '100%',
-    height: 36,
+    //height: 36,
     borderWidth: 1, 
     borderColor: Colors.secondary, 
     borderRadius: Layout.radius.round,
