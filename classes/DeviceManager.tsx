1018eb1b21ec7cd03d10466cef766007b8b0b65d
david-fiaty <dfiaty@gmail.com>
2024-10-23 14:26:28 +0000

    Update DeviceManager.tsx

--- classes/DeviceManager.tsx
+++ classes/DeviceManager.tsx
@@ -1,26 +1,15 @@
 import { Dimensions, ScaledSize, StatusBar } from 'react-native';
-import { GlobalStyles } from '@/constants/GlobalStyles';
-import { Layout } from '@/constants/Layout';
 
 class DeviceManager {
   screen: ScaledSize;
   window: ScaledSize;
-  modalView: object;
   statusBar: object;
 
   constructor() {
     this.screen = Dimensions.get('screen');
     this.window = Dimensions.get('window');
-    this.modalView = this.getModalViewSize();
     this.statusBar = this.getStatusBarSize();
   }
-
-  getModalViewSize() {
-    return {
-      width: this.window.width,
-      height: this.window.height - StatusBar.currentHeight - Layout.header.height - Layout.footer.height,
-    };
-  } 
 
   getStatusBarSize() {
     return {
