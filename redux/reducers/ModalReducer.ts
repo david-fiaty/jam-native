170cb41c8aa4d63a6b6e8bb0a2b195eeb5d46c7f
david-fiaty <dfiaty@gmail.com>
2024-10-23 16:05:54 +0000

    Update ModalReducer.ts

--- redux/reducers/ModalReducer.ts
+++ redux/reducers/ModalReducer.ts
@@ -1,5 +1,18 @@
 
-const ModalReducer = {};
+const initialState = {
+  modal: [],
+};
 
+const ModalReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SET_ACTIVE':
+      return {
+        ...state,
+        tab: [...state.modal, action.payload],
+      };
+    default:
+      return state;
+  }
+};
 
 export default ModalReducer;
