57f007e32f83887acaae7077cc58c3ac007e2638
david-fiaty <dfiaty@gmail.com>
2024-10-23 22:50:34 +0000

    Update Screens.tsx

--- constants/Screens.tsx
+++ constants/Screens.tsx
@@ -9,30 +9,37 @@ import JamsList from "@/components/list/JamsList";
 export const Screens = {
   jamsList: {
     effect: 'push',
+    active: false,
     component: () => <JamsList />,
   },
   settingsMenu: {
     effect: 'push',
+    active: false,
     component: () => <SettingsMenu />,
   },
   notificationsMenu: {
     effect: 'fade',
+    active: false,
     component: () => <NotificationsMenu />,
   },
   searchMenu: {
     effect: 'slide',
+    active: false,
     component: () => <SearchMenu />,
   },
   mapView: {
     effect: 'slide',
+    active: false,
     component: () => <MapView />,
   },
   addJamForm: {
     effect: 'fade',
+    active: false,
     component: () => <AddJamForm />,
   },
   profileForm: {
     effect: 'fade',
+    active: false,
     component: () => <ProfileForm />,
   },
 };
