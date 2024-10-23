f83f7c46c8c5cd2f10eca4113f91f393fd07a6d7
david-fiaty <dfiaty@gmail.com>
2024-10-23 15:16:05 +0000

    Update Slices.ts

--- redux/Slices.ts
+++ redux/Slices.ts
@@ -7,11 +7,9 @@ const cartSlice = createSlice({
   },
   reducers: {
     addProduct: (state, action) => {
-      // Add product to cart
       state.items.push(action.payload);
     },
     updateProduct: (state, action) => {
-      // Find the product and update its quantity
       const { id, quantity } = action.payload;
       const existingProduct = state.items.find(item => item.id === id);
       if (existingProduct) {
@@ -19,7 +17,6 @@ const cartSlice = createSlice({
       }
     },
     deleteProduct: (state, action) => {
-      // Filter out the product to delete it from cart
       const productId = action.payload;
       state.items = state.items.filter(item => item.id !== productId);
     },
