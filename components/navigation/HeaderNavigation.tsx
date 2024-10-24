11260e97778048db9ffe90c37cc6094341fafebe
david-fiaty <dfiaty@gmail.com>
2024-10-24 17:42:39 +0000

    Update HeaderNavigation.tsx

--- components/navigation/HeaderNavigation.tsx
+++ components/navigation/HeaderNavigation.tsx
@@ -1,10 +1,15 @@
 import { TouchableOpacity } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
 import { Layout } from '@/constants/Layout';
 import IconView from "../view/IconView";
 import BoxView from "../view/BoxView";
 import LogoView from '../view/LogoView';
+import { setTabActive } from "@/redux/slices/TabSlice";
 
 const HeaderNavigation = () => {
+  const dispatch = useDispatch();
+  const tabState = useSelector((state) => state.tab);
+  
   return (
     <BoxView direction="row" align="center" justify="space-between" style={Layout.header}>
       <BoxView direction="row" align="center" style={Layout.headerRight}>
@@ -14,7 +19,7 @@ const HeaderNavigation = () => {
       </BoxView>
       <BoxView direction="row" align="center" justify="space-between">
         <BoxView direction="row" align="center" style={Layout.headerRight}> 
-          <IconView name="menu" theme="secondary" size={22} onPress={() => {}} />
+          <IconView name="menu" theme="secondary" size={22} onPress={() => dispatch(setTabActive('AddJamForm'))} />
           <IconView label="15+" theme="secondary" size={13} onPress={() => {}} />
           <IconView name="search" theme="clear" size={22} onPress={() => {}} />
         </BoxView>
