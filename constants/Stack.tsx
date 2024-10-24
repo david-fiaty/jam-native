79f6d66d8b27843945083f8a5376d0df59d6dc3e
david-fiaty <dfiaty@gmail.com>
2024-10-24 21:13:51 +0000

    Create Stack.tsx

---
+++ constants/Stack.tsx
@@ -0,0 +1,42 @@
+export const Stack = [
+  {
+    name: 'MapView', 
+    effect: 'slide',
+    active: false,
+  },
+  {
+    name: 'AddJamForm', 
+    effect: 'push',
+    active: false,
+  },
+  {
+    name: 'ProfileForm', 
+    effect: 'fade',
+    active: false,
+  },
+  {
+    name: 'JamsList', 
+    effect: 'push',
+    active: false,
+  },
+  {
+    name: 'SettingsMenu', 
+    effect: 'push',
+    active: false,
+  },
+  {
+    name: 'NotificationsMenu', 
+    effect: 'push',
+    active: false,
+  },
+  {
+    name: 'SearchScreen', 
+    effect: 'push',
+    active: false,
+  },
+  {
+    name: 'JammersList', 
+    effect: 'slide',
+    active: false,
+  },
+];
