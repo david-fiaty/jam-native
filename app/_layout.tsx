dbe758de3fe8dcfd50781c7327ddfdeba90d4938
david-fiaty <dfiaty@gmail.com>
2024-10-24 18:24:53 +0000

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,11 +2,17 @@ import { Stack } from 'expo-router';
 import { Provider } from 'react-redux';
 import Store from "@/redux/Store";
 import { Colors } from '@/constants/Colors';
+import HeaderNavigation from '@/components/navigation/HeaderNavigation';
 
 const screenOptions = { 
-  headerShown: false,
+  header: (props: object) => <HeaderNavigation />,    
+  headerShown: true,
   statusBarColor: Colors.background,
   statusBarStyle: 'dark',
+  headerTintColor: Colors.background,    
+  headerStyle: {
+    backgroundColor: Colors.background, 
+  },
 };
 
 export default function RootLayout() {
@@ -14,6 +20,7 @@ export default function RootLayout() {
     <Provider store={Store}>
       <Stack>
         <Stack.Screen name="index" options={screenOptions} />
+        <Stack.Screen name="notification" options={screenOptions} />
       </Stack>
     </Provider>
   );
