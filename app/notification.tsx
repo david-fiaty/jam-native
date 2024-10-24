ba1fc3cfa9c408580225c8e88b48d026dd5762ee
david-fiaty <dfiaty@gmail.com>
2024-10-24 19:02:25 +0000

    Update notification.tsx

--- app/notification.tsx
+++ app/notification.tsx
@@ -1,14 +1,19 @@
+import { useEffect, useState } from "react";
+import { useLocalSearchParams } from "expo-router";
 import { ThemeProvider } from '@rneui/themed';
 import ScreenView from '@/components/view/ScreenView';
-import TextView from "@/components/view/TextView";
 import BaseTheme from "@/constants/BaseTheme";
+import NotificationScreen from '@/components/screen/NotificationScreen';
 
 export default () => {
+  const { params } = useLocalSearchParams();
+  
+  console.log(params);
 
   return ( 
     <ThemeProvider theme={BaseTheme}>
       <ScreenView>
-        <TextView>Notification</TextView>
+        <NotificationScreen item={} />
       </ScreenView>
     </ThemeProvider>
   );
