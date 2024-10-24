b3c7a48883ea6737ca0ec3d4442c26c797da2832
david-fiaty <dfiaty@gmail.com>
2024-10-24 21:13:49 +0000

    Update Screens.tsx

--- constants/Screens.tsx
+++ constants/Screens.tsx
@@ -7,53 +7,13 @@ import ProfileForm from "@/components/form/ProfileForm";
 import JamsList from "@/components/list/JamsList";
 import JammersList from "@/components/list/JammersList";
 
-export const Screens = [
-  {
-    name: 'MapView', 
-    effect: 'slide',
-    active: false,
-    component: () => <MapView />,
-  },
-  {
-    name: 'AddJamForm', 
-    effect: 'push',
-    active: false,
-    component: () => <AddJamForm />,
-  },
-  {
-    name: 'ProfileForm', 
-    effect: 'fade',
-    active: false,
-    component: () => <ProfileForm />,
-  },
-  {
-    name: 'JamsList', 
-    effect: 'push',
-    active: false,
-    component: () => <JamsList />,
-  },
-  {
-    name: 'SettingsMenu', 
-    effect: 'push',
-    active: false,
-    component: () => <SettingsMenu />,
-  },
-  {
-    name: 'NotificationsMenu', 
-    effect: 'push',
-    active: false,
-    component: () => <NotificationsMenu />,
-  },
-  {
-    name: 'SearchScreen', 
-    effect: 'push',
-    active: false,
-    component: () => <SearchScreen />,
-  },
-  {
-    name: 'JammersList', 
-    effect: 'slide',
-    active: false,
-    component: () => <JammersList />,
-  },
-];
+export const Screens = {
+  MapView: () => <MapView />,
+  AddJamForm: () => <AddJamForm />,
+  ProfileForm: () => <ProfileForm />,
+  JamsList: () => <JamsList />,
+  SettingsMenu: () => <SettingsMenu />,
+  NotificationsMenu: () => <NotificationsMenu />,
+  SearchScreen: () => <SearchScreen />,
+  JammersList: () => <JammersList />,
+};
