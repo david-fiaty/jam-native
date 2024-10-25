72e80907c26b9aff77d4a597eb02e6c6b4132846
david-fiaty <dfiaty@gmail.com>
2024-10-25 17:54:17 +0000

    Update NotificationScreen.tsx

--- components/screen/NotificationScreen.tsx
+++ components/screen/NotificationScreen.tsx
@@ -1,19 +1,39 @@
-import { StyleSheet, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import { useRouter } from 'expo-router';
+import { useDispatch } from 'react-redux';
+import { setTabActive } from "@/redux/slices/TabSlice";
+import { Layout } from '@/constants/Layout';
 import { BaseProps } from '@/constants/Types';
-import TextView from '../view/TextView';
+import BoxView from "../view/BoxView";
+import BackButton from "../button/BackButton";
+import SpinnerView from '../view/SpinnerView';
+import i18n from '@/translation/i18n';
 
 type Props = BaseProps & {
   item?: object,
 };
 
 const NotificationScreen = ({item}: Props) => {
-  
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setIsLoaded(true), Layout.animation.duration);
+  });
+
+  if (!isLoaded) return <SpinnerView />;
+
   console.log(item);
 
   return (
-    <View style={styles.container}>
-      <TextView>Search menu</TextView>
-    </View>
+    <BoxView align="flex-start" justify="flex-start" scroll={true}>
+      <BackButton
+        title={i18n.t('Your profile')}
+        onPress={() => router.back() }
+      />
+    </BoxView>
   );
 };
 
