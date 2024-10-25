343a98f35e9928a10c95905dc2b60f473103aa31
david-fiaty <dfiaty@gmail.com>
2024-10-25 01:13:06 +0000

    Update NotificationsMenu.tsx

--- components/menu/NotificationsMenu.tsx
+++ components/menu/NotificationsMenu.tsx
@@ -1,18 +1,25 @@
 import { StyleSheet, View, TouchableOpacity } from 'react-native';
 import { useRouter } from 'expo-router';
-import { BaseProps } from '@/constants/Types';
-import ListView from '../view/ListView';
-import TextView from '../view/TextView';
+import { useDispatch } from 'react-redux';
+import { setTabActive } from "@/redux/slices/TabSlice";
 import { Layout } from '@/constants/Layout';
 import { ListItemProps } from '@/constants/Types';
+import ListView from '../view/ListView';
+import TextView from '../view/TextView';
 import ApiClient from '@/classes/ApiClient';
+import BackButton from '../button/BackButton';
 
-const NotificationsMenu = ({style, children}: BaseProps) => {
+const NotificationsMenu = () => {
   const router = useRouter();
   const items = ApiClient.get('notifications');
+  const dispatch = useDispatch();
 
   return (
     <View style={styles.container}>
+      <BackButton
+        title={i18n.t('Settings')}
+        onPress={() => dispatch(setTabActive('SettingsMenu'))}
+      />
       <ListView 
         data={items} 
         renderItem={({item, index}: ListItemProps) => {
