726f7da1e627918f433c115555ab2fbfa96caf52
david-fiaty <dfiaty@gmail.com>
2024-10-25 16:22:49 +0000

    Update ShareButton.tsx

--- components/button/ShareButton.tsx
+++ components/button/ShareButton.tsx
@@ -1,18 +1,24 @@
-import { StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import IconView from '../view/IconView';
 import i18n from '@/translation/i18n';
+import BoxView from '../view/BoxView';
+import TextView from '../view/TextView';
 
-
-const ShareButton = () => {
-  return (       
-    <IconView name="user" size={22} theme="tertiary" />
-  );
+type Props = {
+  title?: string,
 };
 
-const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-  },
-});
+const ShareButton = ({title}: Props) => {
+  const buttonTitle = title ? i18n.t(title) : i18n.t('Share');
+
+  return (       
+    <TouchableOpacity onPress={() => {}}>
+      <BoxView direction="row" align="center" justify="flex-start">
+        <IconView name="user" size={22} theme="tertiary" />
+        <TextView>{buttonTitle}</TextView>
+      </BoxView>
+    </TouchableOpacity>
+  );
+};
 
 export default ShareButton;
