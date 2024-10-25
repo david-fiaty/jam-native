24e31385c696d5ee8801413b8c02ba0a4bf2d929
david-fiaty <dfiaty@gmail.com>
2024-10-25 02:24:08 +0000

    Update Stack.tsx

--- constants/Stack.tsx
+++ constants/Stack.tsx
@@ -6,12 +6,12 @@ export const Stack = [
   },
   {
     name: 'MapView', 
-    effect: 'slide',
+    effect: 'fade',
     active: false,
   },
   {
     name: 'AddJamForm', 
-    effect: 'push',
+    effect: 'fade',
     active: false,
   },
   {
@@ -21,17 +21,17 @@ export const Stack = [
   },
   {
     name: 'SettingsMenu', 
-    effect: 'push',
+    effect: 'fade',
     active: false,
   },
   {
     name: 'NotificationsMenu', 
-    effect: 'push',
+    effect: 'fade',
     active: false,
   },
   {
     name: 'SearchView', 
-    effect: 'push',
+    effect: 'fade',
     active: false,
   },
   {
