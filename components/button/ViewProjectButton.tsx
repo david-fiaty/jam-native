5d4573c33320f4313f17cc378aa57c0bf1f97ab7
david-fiaty <dfiaty@gmail.com>
2024-10-25 16:52:13 +0000

    Update ViewProjectButton.tsx

--- components/button/ViewProjectButton.tsx
+++ components/button/ViewProjectButton.tsx
@@ -15,7 +15,7 @@ const ViewProjectButton = ({title, style}: Props) => {
   return (       
     <TouchableOpacity onPress={() => {}}>
       <BoxView direction="row" align="center" justify="flex-start" style={style}>
-        <IconView name="plus" size={22} theme="tertiary" />
+        <IconView name="view" size={22} theme="tertiary" />
         <TextView>{buttonTitle}</TextView>
       </BoxView>
     </TouchableOpacity>
