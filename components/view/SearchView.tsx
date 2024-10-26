e92be9bd71bd07b7b2d672ed4e2bcf66a21596c6
david-fiaty <dfiaty@gmail.com>
2024-10-26 17:44:35 +0000

    Update SearchView.tsx

--- components/view/SearchView.tsx
+++ components/view/SearchView.tsx
@@ -6,6 +6,8 @@ import { Colors } from "@/constants/Colors";
 import ApiClient from '@/classes/ApiClient';
 import ImageView from "./ImageView";
 import { Layout } from "@/constants/Layout";
+import BoxView from "./BoxView";
+import TextView from "./TextView";
 
 const SearchView = () => {
   const [index, setIndex] = React.useState(0);
@@ -13,152 +15,17 @@ const SearchView = () => {
   const projectsData = ApiClient.get('projects');
 
   return (
-    <View style={styles.container}>
-      <Tab
-        value={index}
-        dense={true}
-        onChange={(e) => setIndex(e)}
-        indicatorStyle={{
-          backgroundColor: Colors.primary,
-          height: 1,
-        }}
-      >
-        <Tab.Item
-          title="All"
-          titleStyle={styles.title}
-          buttonStyle={styles.button}
-        />
-        <Tab.Item
-          title="Calls"
-          titleStyle={styles.title}
-          buttonStyle={styles.button}
-        />
-        <Tab.Item
-          title="Jams"
-          titleStyle={styles.title}
-          buttonStyle={styles.button}
-        />
-        <Tab.Item
-          title="Projects"
-          titleStyle={styles.title}
-          buttonStyle={styles.button}
-        />
-      </Tab>
-
-      <TabView 
-        value={index} 
-        onChange={setIndex} 
-        animationType="timing" 
-        disableTransition={true}
-      >
-        <TabView.Item style={styles.tab}>
-          <FlatList 
-            data={jamsData} 
-            numColumns={3}
-            contentContainerStyle={{gap: GlobalStyles.space.base}}
-            columnWrapperStyle={{gap: GlobalStyles.space.base}}
-            scrollEnabled={false}
-            renderItem={({item, index}) => {
-              return (
-                <TouchableOpacity>
-                  <View style={styles.item}>
-                    <ImageView 
-                      source={item.image} 
-                      width="100%"
-                      height="100%"
-                      resizeMode="cover"
-                      style={styles.image}
-                    />
-                  </View>
-                </TouchableOpacity>
-              );
-            }}
-          />
-        </TabView.Item>
-
-        <TabView.Item style={styles.tab}>
-          <FlatList 
-            data={projectsData} 
-            numColumns={3}
-            contentContainerStyle={{gap: GlobalStyles.space.base}}
-            columnWrapperStyle={{gap: GlobalStyles.space.base}}
-            scrollEnabled={false}
-            renderItem={({item, index}) => {
-              return (
-                <TouchableOpacity>
-                  <View style={styles.item}>
-                    <ImageView 
-                      source={item.image} 
-                      width="100%"
-                      height="100%"
-                      resizeMode="cover"
-                      style={styles.image}
-                    />
-                  </View>
-                </TouchableOpacity>
-              );
-            }}
-          />
-        </TabView.Item>
-
-        <TabView.Item style={styles.tab}>
-          <FlatList 
-            data={jamsData} 
-            numColumns={3}
-            contentContainerStyle={{gap: GlobalStyles.space.base}}
-            columnWrapperStyle={{gap: GlobalStyles.space.base}}
-            scrollEnabled={false}
-            renderItem={({item, index}) => {
-              return (
-                <TouchableOpacity>
-                  <View style={styles.item}>
-                    <ImageView 
-                      source={item.image} 
-                      width="100%"
-                      height="100%"
-                      resizeMode="cover"
-                      style={styles.image}
-                    />
-                  </View>
-                </TouchableOpacity>
-              );
-            }}
-          />
-        </TabView.Item>
-
-        <TabView.Item style={styles.tab}>
-          <FlatList 
-            data={projectsData} 
-            numColumns={3}
-            contentContainerStyle={{gap: GlobalStyles.space.base}}
-            columnWrapperStyle={{gap: GlobalStyles.space.base}}
-            scrollEnabled={false}
-            renderItem={({item, index}) => {
-              return (
-                <TouchableOpacity>
-                  <View style={styles.item}>
-                    <ImageView 
-                      source={item.image} 
-                      width="100%"
-                      height="100%"
-                      resizeMode="cover"
-                      style={styles.image}
-                    />
-                  </View>
-                </TouchableOpacity>
-              );
-            }}
-          />
-        </TabView.Item>
-      </TabView>
-    </View>
+    <BoxView direction="column" align="flex-start" justify="flex-start" scroll={true} style={[Layout.screenContent, styles.container]}>
+      <TextView>SearchView screen</TextView>
+    </BoxView>
   );
 };
 
 const styles = {
   container: {
-    flexDirection: 'column',
     height: '100%',
+    width: '100%',
+    backgroundColor: 'red',
   },
   tab: {
     width: '100%',
