14adf6cb4ca8e8e98ca378ac2ebad39c4974f608
david-fiaty <dfiaty@gmail.com>
2024-10-26 21:01:52 +0000

    Update SearchField.tsx

--- components/field/SearchField.tsx
+++ components/field/SearchField.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { StyleSheet } from 'react-native';
 import { Input } from '@rneui/themed';
 import BoxView from '../view/BoxView';
@@ -5,13 +6,25 @@ import IconView from '../view/IconView';
 import { Layout } from '@/constants/Layout';
 
 const SearchField = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const expandedStyle = {
+
+  };
+
   return (
     <BoxView direction="row" align="space-between" style={styles.container}>
       <Input 
         inputContainerStyle={styles.inputContainerStyle}
         inputStyle={styles.inputStyle}
         rightIconContainerStyle={styles.rightIconContainerStyle}
-        rightIcon={<IconView name="search" theme="clear" onPress={() => {/*dispatch(setActiveScreen('SearchView')) */}} />}
+        rightIcon={
+          <IconView 
+            name="search" 
+            theme="clear" 
+            onPress={() => setIsExpanded(!isExpanded)}
+          />
+        }
       />
     </BoxView>
   );
@@ -19,7 +32,7 @@ const SearchField = () => {
 
 const styles = StyleSheet.create({
   container: {
-    width: Layout.header.width/4,
+    width: Layout.header.width/6,
   },
   inputContainerStyle: {
     borderBottomWidth: 0,
@@ -30,7 +43,8 @@ const styles = StyleSheet.create({
     flexGrow: 1,
   },
   rightIconContainerStyle: {
-    backgroundColor: 'blue',
+    //backgroundColor: 'blue',
+    marginRight: -Layout.space.base,
   },
 });
 
