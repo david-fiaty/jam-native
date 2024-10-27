1b713a95de4a5b01e7dcdde495569d93136290bd
david-fiaty <dfiaty@gmail.com>
2024-10-27 17:10:57 +0000

    Update DataManager.tsx

--- classes/DataManager.tsx
+++ classes/DataManager.tsx
@@ -1,8 +1,11 @@
 import ApiEndpoints from '@/constants/Endpoints';
+import ApiClient from './ApiClient';
 
 class DataManager {
-  get(key: keyof typeof ApiEndpoints) {
-    
+  get(key: keyof typeof ApiEndpoints, filter?: boolean) {
+    const data = ApiClient.get(key);
+
+    return data;
   }
 };
 
