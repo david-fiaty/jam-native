61bd3054bcf64661b50c67bed562e76f84198c97
david-fiaty <dfiaty@gmail.com>
2024-10-27 19:15:31 +0000

    Update CopyJamLinkButton.tsx

--- components/button/CopyJamLinkButton.tsx
+++ components/button/CopyJamLinkButton.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
+import * as Clipboard from 'expo-clipboard';
 import IconView from '../view/IconView';
 import i18n from '@/translation/i18n';
 import BoxView from '../view/BoxView';
@@ -12,8 +14,22 @@ type Props = {
 const CopyJamLinkButton = ({title, style}: Props) => {
   const buttonTitle = title ? i18n.t(title) : i18n.t('Copy link');
 
+  const [copiedText, setCopiedText] = useState('');
+
+  const copyToClipboard = async () => {
+    await Clipboard.setStringAsync('hello world');
+  };
+
+  const getFromClipboard = async () => {
+    const text = await Clipboard.getStringAsync();
+    setCopiedText(text);
+  };
+
+
+  console.log(copiedText);
+
   return (       
-    <TouchableOpacity onPress={() => {}}>
+    <TouchableOpacity onPress={copyToClipboard}>
       <BoxView direction="row" align="center" justify="flex-start" style={style}>
         <IconView name="copy" theme="tertiary" />
         <TextView>{buttonTitle}</TextView>
