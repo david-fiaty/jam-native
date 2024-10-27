61e0bbf28f81fb6098316b2147f55cc280726ee3
david-fiaty <dfiaty@gmail.com>
2024-10-27 16:31:22 +0000

    Update SearchView.tsx

--- components/view/SearchView.tsx
+++ components/view/SearchView.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { View, FlatList, TouchableOpacity } from "react-native";
 import { useDispatch, useSelector } from 'react-redux';
+import { setSearchFilter } from "@/redux/slices/SearchSlice";
 import { Layout } from "@/constants/Layout";
 import ApiClient from "@/classes/ApiClient";
 import ImageView from "./ImageView";
@@ -57,16 +58,20 @@ const tabs = [
 ];
 
 const SearchView = () => {
-  const numColumns = 3;
+  const dispatch = useDispatch();
   const searchState = useSelector((state) => state.search);
 
+  const numColumns = 3;
+
   const renderTab = (item, index) => (
-    <TouchableOpacity>
+    <TouchableOpacity onPress={() => dispatch(setSearchFilter(item.id))}>
       <View style={styles.tabItem}>
         <TextView>{item.label}</TextView>
       </View>
     </TouchableOpacity>
   );
+
+  console.log(searchState);
 
   return (
     <BoxView
