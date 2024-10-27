1f77db7975526096008a2b497a6a0f0a90d7443c
david-fiaty <dfiaty@gmail.com>
2024-10-27 21:02:17 +0000

    Update AboutButton.tsx

--- components/button/AboutButton.tsx
+++ components/button/AboutButton.tsx
@@ -1,13 +1,14 @@
 import { StyleSheet } from 'react-native';
 import { useRouter } from 'expo-router';
 import ButtonBase from '../base/ButtonBase';
+import i18n from '@/translation/i18n';
 
 const AboutButton = () => {
   const router = useRouter();
 
   return (
     <ButtonBase
-      title="About" 
+      title={i18n.t('About')} 
       onPress={() => router.push('/about')} 
       //containerStyle={styles.container}
       //labelStyle={styles.label}
