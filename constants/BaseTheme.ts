1aade32270769aa25b85181b10dba4ab2ee6db0d
david-fiaty <dfiaty@gmail.com>
2024-10-27 12:36:44 +0000

    Update BaseTheme.ts

--- constants/BaseTheme.ts
+++ constants/BaseTheme.ts
@@ -16,20 +16,6 @@ const BaseTheme = createTheme({
         borderRadius: Layout.radius.round,
       },
     },
-    Input: {
-      placeholderTextColor: Colors.primary,
-      containerStyle: Layout.formField,
-      inputContainerStyle: {
-        borderBottomWidth: 0,
-        flexGrow: 1,
-      },
-      inputStyle: {
-        padding: 0,
-        margin: 0,
-        color: Colors.primary,
-        fontSize: Layout.fontSize.base,
-      },
-    },
   },
 });
 
