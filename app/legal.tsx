96b8eeca0accca465f35317b2a6b80168a536b43
david-fiaty <dfiaty@gmail.com>
2024-10-27 20:12:15 +0000

    Create legal.tsx

---
+++ app/legal.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from '@rneui/themed';
+import ScreenView from '@/components/view/ScreenView';
+import BaseTheme from "@/constants/BaseTheme";
+import TextView from '@/components/view/TextView';
+
+export default () => {
+  return ( 
+    <ThemeProvider theme={BaseTheme}>
+      <ScreenView>
+        <TextView>Legal</TextView>
+      </ScreenView>
+    </ThemeProvider>
+  );
+}
