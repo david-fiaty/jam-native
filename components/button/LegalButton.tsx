883a1b22b1255f11decd5a2b8349c742b3089900
david-fiaty <dfiaty@gmail.com>
2024-10-27 21:02:18 +0000

    Update LegalButton.tsx

--- components/button/LegalButton.tsx
+++ components/button/LegalButton.tsx
@@ -1,13 +1,14 @@
 import { StyleSheet } from 'react-native';
 import { useRouter } from 'expo-router';
 import ButtonBase from '../base/ButtonBase';
+import i18n from '@/translation/i18n';
 
 const LegalButton = () => {
   const router = useRouter();
 
   return (
     <ButtonBase
-      tite="Legal" 
+      title={i18n.t('Legal')} 
       onPress={() => router.push('/legal')} 
       //containerStyle={styles.container}
       //labelStyle={styles.label}
