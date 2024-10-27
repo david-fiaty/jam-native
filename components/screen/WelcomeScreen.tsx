5b9d9b9b921d3fd2e5b53a5a4083f6f90610cb40
david-fiaty <dfiaty@gmail.com>
2024-10-27 20:50:12 +0000

    Update WelcomeScreen.tsx

--- components/screen/WelcomeScreen.tsx
+++ components/screen/WelcomeScreen.tsx
@@ -9,7 +9,6 @@ const WelcomeScreen = () => {
   return (
     <BoxView direction="column" align="center" justify="center" style={styles.container}>
         <LogoView size={{ width: 110, height: 110 }} />    
-        
         <LoginSignupButton />
         <BottomLinks />
     </BoxView>
