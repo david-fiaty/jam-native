5de6392714aad5988cbb517f20fc1ff0f4552a26
david-fiaty <dfiaty@gmail.com>
2024-10-28 21:12:16 +0000

    Update EntityManager.tsx

--- classes/EntityManager.tsx
+++ classes/EntityManager.tsx
@@ -3,8 +3,17 @@ import Fields from "@/constants/Fields";
 
 class EntityManager {
   create(key: string, data: object) {
-    console.log(key);
-    console.log('create called');
+    try {
+      let entity = {};
+      for (const [sourceField, targetField] of Object.entries(Fields[key])) {
+        entity[sourceField] = data[targetField]; 
+      }
+
+      return entity;
+    }
+    catch (error) {
+      console.log(error);
+    }
   }
 };
 
