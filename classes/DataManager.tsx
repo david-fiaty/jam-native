ae7390686dc22d9a436bd275ccd54b206ce1e91f
david-fiaty <dfiaty@gmail.com>
2024-10-28 19:56:24 +0000

    Update DataManager.tsx

--- classes/DataManager.tsx
+++ classes/DataManager.tsx
@@ -5,15 +5,17 @@ import Fields from '@/constants/Fields';
 
 class DataManager {
   async get(key: keyof typeof ApiEndpoints, options: object) {
+    // Variables
     const searchState = Store.getState().search;
-
     let data = await this.getData(key);
     let results: object = [];
 
+    // Apply search
     if (options?.filter !== false && searchState.filter.length) {
       results = data.filter(item => item?.active === true); // Todo - Apply filter fields
     }
 
+    // Apply filters
     if (options?.search !== false && searchState.value.length) {
       results = results.length ? results : data;
       results.filter(item => item?.active === true); // Todo - Apply filter fields
@@ -26,9 +28,9 @@ class DataManager {
     let data = await ApiClient.get(key);
 
 
+    console.log(typeof data);
 
 
-    
     return data;
   }
 };
