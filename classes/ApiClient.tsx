6943570692946443ed646798dc4090a4701a60c3
david-fiaty <dfiaty@gmail.com>
2024-10-28 19:52:39 +0000

    Update ApiClient.tsx

--- classes/ApiClient.tsx
+++ classes/ApiClient.tsx
@@ -8,7 +8,8 @@ class ApiClient {
   async get(key: keyof typeof Endpoints) {
     try {
       return await this.sendRequest(Endpoints[key]);
-    } catch (error) {
+    } 
+    catch (error) {
       console.log(error);
     }
   }
@@ -23,7 +24,8 @@ class ApiClient {
         });
 
         return this.processResponse(await response.json());
-      } catch (error) {
+      } 
+      catch (error) {
         console.error(error);
       }
     }
