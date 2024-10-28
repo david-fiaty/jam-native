a5ae92fa57e53edb9fe66f34ae1129f2085a0487
david-fiaty <dfiaty@gmail.com>
2024-10-28 00:58:18 +0000

    Update HeaderNavigation.tsx

--- components/navigation/HeaderNavigation.tsx
+++ components/navigation/HeaderNavigation.tsx
@@ -12,8 +12,6 @@ const HeaderNavigation = () => {
   const dispatch = useDispatch();
   const activeScreen = ScreenManager.getActiveScreen();
 
-  console.log(activeScreen);
-
   return (
     <BoxView direction="row" align="center" justify="space-between" style={Layout.header}>
       <BoxView direction="row" align="center" style={Layout.headerRight}>
@@ -21,13 +19,16 @@ const HeaderNavigation = () => {
           <LogoView size={Layout.logo} />
         </TouchableOpacity>
       </BoxView>
-      <BoxView direction="row" align="center" justify="space-between">
-        <BoxView direction="row" align="center" style={Layout.headerRight}> 
-          <IconView name="menu" theme="secondary" onPress={() => dispatch(setActiveScreen('SettingsMenu'))} />
-          <IconView label="15+" theme="secondary" size={13} onPress={() => dispatch(setActiveScreen('NotificationsMenu'))} />
-          <SearchField />
+      { activeScreen?.headerNavigation &&
+        <BoxView direction="row" align="center" justify="space-between">
+          <BoxView direction="row" align="center" style={Layout.headerRight}> 
+            <IconView name="menu" theme="secondary" onPress={() => dispatch(setActiveScreen('SettingsMenu'))} />
+            <IconView label="15+" theme="secondary" size={13} onPress={() => dispatch(setActiveScreen('NotificationsMenu'))} />
+            <SearchField />
+          </BoxView>
         </BoxView>
-      </BoxView>
+      }
+      
     </BoxView>
   );
 };
