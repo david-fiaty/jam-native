5084f1e61c5e4a6f94c3b9a58a9d968fb2d58681
david-fiaty <dfiaty@gmail.com>
2024-10-28 01:21:37 +0000

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,19 @@
 import { ThemeProvider } from '@rneui/themed';
+import { CacheManager } from '@georstat/react-native-image-cache';
+import { Dirs } from 'react-native-file-access';
 import BaseTheme from "@/constants/BaseTheme";
 import ScreenView from '@/components/view/ScreenView';
 import WelcomeScreen from '@/components/screen/WelcomeScreen';
+
+CacheManager.config = {
+  baseDir: `${Dirs.CacheDir}/images_cache/`,
+  blurRadius: 15,
+  cacheLimit: 0,
+  maxRetries: 3 ,
+  retryDelay: 3000,
+  sourceAnimationDuration: 1000,
+  thumbnailAnimationDuration: 1000,
+};
 
 export default () => {
   return ( 
