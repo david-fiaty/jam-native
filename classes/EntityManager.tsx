432b86ae977c4fe4f3fa775572e582e3c1f80c9d
david-fiaty <dfiaty@gmail.com>
2024-10-29 00:47:30 +0000

    Update EntityManager.tsx

--- classes/EntityManager.tsx
+++ classes/EntityManager.tsx
@@ -4,20 +4,30 @@ import Entities from "@/constants/Entities";
 class EntityManager {
   create(key: string, data: object) {
     try {
-      let entity = {};
-      let definition = Entities.find(item => item.type == key);
-
-      for (const [sourceField, targetField] of Object.entries(definition.fields)) {
-        if (data?.[targetField]) {
-          entity[sourceField] = data?.[targetField];       
-        }
-      }
+      let entity = this.buildEntity(key, data);
 
       return entity;
     }
     catch (error) {
       console.log(error);
     }
+  }
+
+  buildEntity(key: string, data: object) {
+    let entity = {};
+    let definition = this.getFieldDefinition(key);
+
+    for (const [sourceField, targetField] of Object.entries(definition.fields)) {
+      if (data?.[targetField]) {
+        entity[sourceField] = data?.[targetField];       
+      }
+    }
+
+    return entity;
+  }
+
+  getFieldDefinition(key: string) {
+    return Entities.find(item => item.type == key);
   }
 };
 
