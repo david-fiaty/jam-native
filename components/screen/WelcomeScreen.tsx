2599b99394b263cd618d2f76ff5464c962ba987c
david-fiaty <dfiaty@gmail.com>
2024-10-29 19:01:40 +0000

    Update WelcomeScreen.tsx

--- components/screen/WelcomeScreen.tsx
+++ components/screen/WelcomeScreen.tsx
@@ -1,19 +1,17 @@
-import { StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import Slideshow from '@/components/slideshow/Slideshow';
 import BottomLinks from '@/components/navigation/BottomLinks';
 import LoginSignupButton from '../button/LoginSignupButton';
 import LogoView from '../view/LogoView';
 import BoxView from '../view/BoxView';
 import { Divider } from '@rneui/base';
 import WelcomeSlideshow from '../welcome-slideshow/WelcomeSlideshow';
-import SlideshowView from '../view/SlideshowView';
 
 const WelcomeScreen = () => {
   return (
     <BoxView direction="column" align="center" justify="center" style={styles.container}>
       <LogoView size={{ width: 110, height: 110 }} />    
       
-<SlideshowView />
-
       <Divider />
       <WelcomeSlideshow />
 
