8b1d1a0a0c671f8a2b254ba7c73c177e20e409dd
david-fiaty <dfiaty@gmail.com>
2024-10-29 18:04:21 +0000

    Update WelcomeScreen.tsx

--- components/screen/WelcomeScreen.tsx
+++ components/screen/WelcomeScreen.tsx
@@ -7,10 +7,44 @@ import BoxView from '../view/BoxView';
 import { Divider } from '@rneui/base';
 import WelcomeSlideshow from '../welcome-slideshow/WelcomeSlideshow';
 
+
+import { Dimensions, Text } from 'react-native';
+import Carousel from 'react-native-reanimated-carousel';
+
+
 const WelcomeScreen = () => {
+
+  const width = Dimensions.get('window').width;
+  
   return (
     <BoxView direction="column" align="center" justify="center" style={styles.container}>
       <LogoView size={{ width: 110, height: 110 }} />    
+
+
+      <View  style={{ flex: 1 }}>
+            <Carousel
+                loop
+                width={width}
+                height={width / 2}
+                autoPlay={true}
+                data={[...new Array(6).keys()]}
+                scrollAnimationDuration={1000}
+                onSnapToItem={(index) => console.log('current index:', index)}
+                renderItem={({ index }) => (
+                    <View
+                        style={{
+                            flex: 1,
+                            borderWidth: 1,
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <Text style={{ textAlign: 'center', fontSize: 30 }}>
+                            {index}
+                        </Text>
+                    </View>
+                )}
+            />
+        </View>
       
       <Divider />
       <WelcomeSlideshow />
