4c46e6b6e260c81d3020c8e09e032c53c3c5b9b8
david-fiaty <dfiaty@gmail.com>
2024-10-29 19:24:20 +0000

    Update SlideshowView.tsx

--- components/view/SlideshowView.tsx
+++ components/view/SlideshowView.tsx
@@ -1,51 +1,58 @@
-import * as React from "react";
-import { Dimensions, Text, View } from "react-native";
-import { useSharedValue } from "react-native-reanimated";
-import Carousel, {
-  ICarouselInstance,
-} from "react-native-reanimated-carousel";
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
  
-const data = [...new Array(6).keys()];
-const width = Dimensions.get("window").width;
- 
+import Slick from 'react-native-slick';
+
 const SlideshowView = () => {
-  const ref = React.useRef<ICarouselInstance>(null);
-  const progress = useSharedValue<number>(0);
-  
-  const onPressPagination = (index: number) => {
-    ref.current?.scrollTo({
-      /**
-       * Calculate the difference between the current index and the target index
-       * to ensure that the carousel scrolls to the nearest index
-       */
-      count: index - progress.value,
-      animated: true,
-    });
-  };
+    return (
+      <Slick style={styles.wrapper} showsButtons={true}>
+        <View style={styles.slide1}>
+          <Text style={styles.text}>Hello Slick</Text>
+        </View>
+        <View style={styles.slide2}>
+          <Text style={styles.text}>Beautiful</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.text}>And simple</Text>
+        </View>
+      </Slick>
+    );
+};
+
  
-  return (
-    <View style={{ flex: 1 }}>
-      <Carousel
-        ref={ref}
-        width={width}
-        height={width / 2}
-        data={data}
-        onProgressChange={progress}
-        renderItem={({ index }) => (
-          <View
-            style={{
-              flex: 1,
-              borderWidth: 1,
-              justifyContent: "center",
-            }}
-          >
-            <Text style={{ textAlign: "center", fontSize: 30 }}>{index}</Text>
-          </View>
-        )}
-      />
- 
-    </View>
-  );
-}
+const styles = StyleSheet.create({
+  wrapper: {
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
+});
 
 export default SlideshowView;
+ 
+ 
