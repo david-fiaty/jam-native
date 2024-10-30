724cc08447fd0c1a70ba5cac131d29f0027a1e5b
david-fiaty <dfiaty@gmail.com>
2024-10-30 14:55:56 +0000

    Update TextSlideshow.tsx

--- components/slideshow/TextSlideshow.tsx
+++ components/slideshow/TextSlideshow.tsx
@@ -55,7 +55,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'yellow',
   },
   pagination: {
-    //top: 122, 
+    bottom: 0, 
     left: 0,
     right: 0,
     height: Layout.space.base,
