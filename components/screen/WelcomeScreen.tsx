771d7373a984dc9d2b38b3fe65c9ee92b053e297
david-fiaty <dfiaty@gmail.com>
2024-10-30 15:07:12 +0000

    Removed text slideshow

--- components/screen/WelcomeScreen.tsx
+++ components/screen/WelcomeScreen.tsx
@@ -1,11 +1,9 @@
 import { View, StyleSheet } from 'react-native';
-import Slideshow from '@/components/slideshow/Slideshow';
 import BottomLinks from '@/components/navigation/BottomLinks';
 import LoginSignupButton from '../button/LoginSignupButton';
 import LogoView from '../view/LogoView';
 import BoxView from '../view/BoxView';
 import { Divider } from '@rneui/base';
-import WelcomeSlideshow from '../welcome-slideshow/WelcomeSlideshow';
 import TextSlideshow from '../slideshow/TextSlideshow';
 
 const textSlideshowData = [
--- components/slideshow/TextSlideshow.tsx
+++ components/slideshow/TextSlideshow.tsx
@@ -34,7 +34,8 @@ const TextSlideshow = ({data}: Props) => {
 const styles = StyleSheet.create({
   container: { 
     height: 122,
-    marginVertical: Layout.space.base*2,
+    marginTop: Layout.space.base,
+    marginBottom: Layout.space.base*2,
   },
   item: {
     flex: 1,
