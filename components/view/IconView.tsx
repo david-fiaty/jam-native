da523ec101331b1fbf6481bdb25f8aa94594f22c
david-fiaty <dfiaty@gmail.com>
2024-10-30 18:34:00 +0000

    Update IconView.tsx

--- components/view/IconView.tsx
+++ components/view/IconView.tsx
@@ -64,7 +64,7 @@ const styles = StyleSheet.create({
   iconStyle: {
     color: Colors.white,
     backgroundColor: Colors.primary,
-    padding: Layout.space.base*0.65,
+    padding: Layout.space.base*0.75,
     borderRadius: Layout.radius.circle,
   },
   primary: {
