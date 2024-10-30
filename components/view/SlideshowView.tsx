b8db28f09d577f0a0bbf74a0efd7f0a91b011846
david-fiaty <dfiaty@gmail.com>
2024-10-30 13:48:53 +0000

    Update SlideshowView.tsx

--- components/view/SlideshowView.tsx
+++ components/view/SlideshowView.tsx
@@ -1,65 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native';
-import Slick from 'react-native-slick';
-
+import { StyleSheet, Text, View } from "react-native";
+import Slick from "react-native-slick";
 
 type Props = {
-  data?: [],
-  type?: string,
+  data?: [];
+  type?: string;
 };
 
-const SlideshowView = ({data, type}: Props) => {
-    const SlideshowItem = data?.map(item => {
-      if (type == 'text') {
-        return (
-          <View style={styles.slide1}>
-            <Text style={styles.text}>{item.title}</Text>
-          </View>
-        );
-      }
-      else if (type == 'image') {
-        return (
-          <View style={styles.slide1}>
-            <Text style={styles.text}>{item.title}</Text>
-          </View>
-        );
-      }
-    });
+const SlideshowView = ({ data, type }: Props) => {
+  const SlideshowItem = data?.map((item) => {
+    if (type == "text") {
+      return (
+        <View style={styles.item}>
+          <Text style={styles.text}>{item.title}</Text>
+        </View>
+      );
+    } else if (type == "image") {
+      return (
+        <View style={styles.item}>
+          <Text style={styles.text}>{item.title}</Text>
+        </View>
+      );
+    }
+  });
 
-    return (
-      <Slick style={styles.wrapper} showsButtons={true}>
-        {SlideshowItem}
-      </Slick>
-    );
+  return (
+    <Slick style={styles.container} showsButtons={true}>
+      {SlideshowItem}
+    </Slick>
+  );
 };
- 
+
 const styles = StyleSheet.create({
-  wrapper: {
-  },
-  slide1: {
+  container: {},
+  item: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#9DD6EB',
-  },
-  slide2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#97CAE5',
-  },
-  slide3: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#92BBD9',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#9DD6EB",
   },
   text: {
     color: '#fff',
     fontSize: 30,
     fontWeight: 'bold',
-  }
+  },
 });
 
 export default SlideshowView;
- 
- 
