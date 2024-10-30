3f09d1fe75e84182b2712da860609b7974f0b2d6
david-fiaty <dfiaty@gmail.com>
2024-10-30 14:11:51 +0000

    Update WelcomeScreen.tsx

--- components/screen/WelcomeScreen.tsx
+++ components/screen/WelcomeScreen.tsx
@@ -11,15 +11,21 @@ import TextSlideshow from '../slideshow/TextSlideshow';
 const textSlideshowData = [
   {
     id: 1,
-    title: 'A',
+    title: 'Create better, together',
+    content: 'Welcome to the Jam app. Jam is a place to explore and experience artists and creatives from different backgrounds in West Africa.',
+    link: 'aaa link',
   },
   {
     id: 2,
-    title: 'B',
+    title: 'Everything you need',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    link: 'bbb link',
   },
   {
     id: 3,
-    title: 'C',
+    title: 'The place to excel',
+    content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.',
+    link: 'ccc link',
   },
 ];
 
@@ -27,8 +33,7 @@ const WelcomeScreen = () => {
   return (
     <BoxView direction="column" align="center" justify="center" style={styles.container}>
       <LogoView size={{ width: 110, height: 110 }} />    
-      
-
+    
       <TextSlideshow data={textSlideshowData} />
 
       <Divider />
