bcad5052c5150a39d122c5bca00f4ec38cdeeaee
david-fiaty <dfiaty@gmail.com>
2024-10-31 18:37:21 +0000

    Update HeaderNavigation.tsx

--- components/navigation/HeaderNavigation.tsx
+++ components/navigation/HeaderNavigation.tsx
@@ -24,7 +24,7 @@ const HeaderNavigation = () => {
           <LogoView size={Layout.logo} />
         </TouchableOpacity>
       </BoxView>
-      { (route.name == 'main' || activeScreen?.headerNavigation) && UserManager.isLoggedIn() === true &&
+      { (route.name == 'main' || activeScreen?.headerNavigation) &&
         <BoxView direction="row" align="center" justify="space-between">
           <BoxView direction="row" align="center" style={Layout.headerRight}> 
             <IconView name="menu" theme="secondary" onPress={() => dispatch(setActiveScreen({
@@ -34,7 +34,7 @@ const HeaderNavigation = () => {
               name: 'NotificationsMenu',
             }))} />
             
-            <SearchField />
+            { UserManager.isLoggedIn() === true && <SearchField /> }
             
           </BoxView>
         </BoxView>
