91985b74fa1c10b2cba3579881008c06d5cfd614
david-fiaty <dfiaty@gmail.com>
2024-10-31 14:20:06 +0000

    Update FacebookLoginButton.tsx

--- components/button/FacebookLoginButton.tsx
+++ components/button/FacebookLoginButton.tsx
@@ -4,37 +4,33 @@ import ButtonBase from '../base/ButtonBase';
 import BoxView from '../view/BoxView';
 import i18n from '@/translation/i18n';
 import { Layout } from '@/constants/Layout';
+import TextView from '../view/TextView';
+import ImageView from '../view/ImageView';
 
-const FacebookLoginButton = () => {
+const source = require('@/assets/images/google-logo.png'); 
+
+const GoogleLoginButton = () => {
   return (
-    <BoxView direction="column" align="center" justify="space-between" style={{width: '100%'}}>
-      <ButtonBase
-        title={i18n.t('Continue with Facebook')} 
-        titleStyle={styles.titleStyle}
-        buttonStyle={styles.buttonStyle}
-        containerStyle={styles.containerStyle}
-        onPress={() => console.log('clicked')} 
+    <BoxView direction="row" align="center" justify="flex-start" style={styles.container}>
+      <ImageView 
+        source={source} 
+        width={32}
+        height={32}
+        resizeMode="cover"
       />
+      <TextView>{i18n.t('Continue with Facebook')}</TextView>
     </BoxView>
   );
 };
 
 const styles = StyleSheet.create({
-  containerStyle: {
+  container: {
+    width: '100%',
     borderWidth: 0.5, 
     borderColor: Colors.primary,
     borderRadius: Layout.radius.round,
-    width: '100%',
-    backgroundColor: 'red',
-  },
-  buttonStyle: {
-    backgroundColor: Colors.white,
-    width: '100%',
-    justifyContent: 'flex-start',
-  },
-  titleStyle: {
-    color: Colors.primary,
+    padding: Layout.space.base/2,
   },
 });
 
-export default FacebookLoginButton;
+export default GoogleLoginButton;
