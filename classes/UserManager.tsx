ef55d86c6112ae5ca532645e4be1e6c87de497e7
david-fiaty <dfiaty@gmail.com>
2024-10-31 15:22:30 +0000

    Update UserManager.tsx

--- classes/UserManager.tsx
+++ classes/UserManager.tsx
@@ -2,7 +2,6 @@ import Store from '@/redux/Store';
 import DataManager from './DataManager';
 
 class UserManager {
-
   login(email: string, password: string) {
     console.log(email);
     console.log(password);
