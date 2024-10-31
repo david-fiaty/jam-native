73015fd2a98282493eb1f55cd49715a3b7daec81
david-fiaty <dfiaty@gmail.com>
2024-10-31 19:53:35 +0000

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,14 @@
 import { ThemeProvider } from '@rneui/themed';
-import Toast from 'react-native-toast-message';
 import BaseTheme from "@/constants/BaseTheme";
 import ScreenView from '@/components/view/ScreenView';
 import WelcomeScreen from '@/components/screen/WelcomeScreen';
 
 export default () => {
   return ( 
-    <>
-      <ThemeProvider theme={BaseTheme}>
-        <ScreenView>
-          <WelcomeScreen />
-        </ScreenView>
-      </ThemeProvider>
-      <Toast />
-    </>
+    <ThemeProvider theme={BaseTheme}>
+      <ScreenView>
+        <WelcomeScreen />
+      </ScreenView>
+    </ThemeProvider>
   );
 }
