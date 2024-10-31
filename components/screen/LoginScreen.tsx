f9772b9238fc88871a3e37b7eaa8f6b83e17075e
david-fiaty <dfiaty@gmail.com>
2024-10-31 17:44:00 +0000

    Update LoginScreen.tsx

--- components/screen/LoginScreen.tsx
+++ components/screen/LoginScreen.tsx
@@ -36,10 +36,10 @@ const LoginScreen = () => {
 
       router.push('/main');
     }
-  }
-  
+  }  
 
   console.log(userState);
+
   return (
     <BoxView direction="column" align="center" justify="center" style={Layout.screenContent}>
       <LogoView size={{ width: 80, height: 80 }} />    
