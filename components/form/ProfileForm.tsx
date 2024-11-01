4b1a707460a0ba566e5f22844a46495063bfef12
david-fiaty <dfiaty@gmail.com>
2024-11-01 20:42:20 +0000

    Update ProfileForm.tsx

--- components/form/ProfileForm.tsx
+++ components/form/ProfileForm.tsx
@@ -52,7 +52,10 @@ const ProfileForm = () => {
       <UserProfileImageField />
 
       <DividerView />
+
       <InputTextField placeholder={i18n.t('Public name')} />
+
+      
       <InputTextField placeholder={i18n.t('IG handle')} />
       <InputTextField placeholder={i18n.t('Email address')} />
       <InputTextField placeholder={i18n.t('Phone number')} />
