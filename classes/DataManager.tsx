12e5caa9a1d7410c8be1767162cf89d3cb4ff500
david-fiaty <dfiaty@gmail.com>
2024-11-01 13:46:29 +0000

    Update DataManager.tsx

--- classes/DataManager.tsx
+++ classes/DataManager.tsx
@@ -26,7 +26,11 @@ class DataManager {
       }); 
     }
 
-    return results?.length ? results : data;
+    // Process results
+    results = results?.length > 0 ? results : data;
+    //results = results.filter(item => JSON.stringify(item) !== '{}');
+    
+    return results;
   }
 
   async post(key: keyof typeof ApiEndpoints, data: object, options?: object) {
