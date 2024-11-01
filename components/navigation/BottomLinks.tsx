1668b13df928e334d2bed2d1e57a7a87f057202e
david-fiaty <dfiaty@gmail.com>
2024-11-01 19:07:08 +0000

    Update BottomLinks.tsx

--- components/navigation/BottomLinks.tsx
+++ components/navigation/BottomLinks.tsx
@@ -1,20 +1,23 @@
 import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import { useRouter } from 'expo-router';
 import { Layout } from '@/constants/Layout';
 import BoxView from '../view/BoxView';
 import TextView from '../view/TextView';
 import i18n from '@/translation/i18n';
 
 const BottomLinks = () => {
+  const router = useRouter();
+
   return (
     <BoxView direction="row" align="center" justify="space-around" style={styles.container}>
 
-    <TouchableOpacity onPress={() => {}}>
-      <TextView>{i18n.t('About')}</TextView>
-    </TouchableOpacity>
+      <TouchableOpacity onPress={() => router.push('/about')}>
+        <TextView>{i18n.t('About')}</TextView>
+      </TouchableOpacity>
 
-    <TouchableOpacity onPress={() => {}}>
-      <TextView>{i18n.t('Legal')}</TextView>
-    </TouchableOpacity>
+      <TouchableOpacity onPress={() => router.push('/legal')}>
+        <TextView>{i18n.t('Legal')}</TextView>
+      </TouchableOpacity>
 
     </BoxView>
   );
