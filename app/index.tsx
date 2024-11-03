0c72d111de5aa5b58ea74504dedf23146111e296
david-fiaty <dfiaty@gmail.com>
2024-11-03 17:54:56 +0000

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -2,15 +2,21 @@ import { useEffect } from 'react';
 import { ThemeProvider } from '@rneui/themed';
 import { useDispatch } from 'react-redux';
 import { setScreenState } from '@/redux/slices/ScreenSlice';
+import { Screens } from '@/constants/Screens';
 import BaseTheme from "@/constants/BaseTheme";
 import ScreenView from '@/components/view/ScreenView';
 import WelcomeScreen from '@/components/screen/WelcomeScreen';
 
 export default () => {
   const dispatch = useDispatch(); 
+  const screens = Screens.map((item: any) => {
+    delete item['component'];
+    return item;
+  });
 
   useEffect(() => {
-    //dispatch(setScreenState());
+    console.log(screens);
+    dispatch(setScreenState(screens));
   }, []);
 
   return ( 
