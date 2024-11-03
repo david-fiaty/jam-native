4d1e6beeb0e342533e62d4596310d10677c423bf
david-fiaty <dfiaty@gmail.com>
2024-11-03 20:03:13 +0000

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -127,13 +127,7 @@ const RootLayout = () => {
   return (
     <Provider store={Store}>
       <Stack>
-        {
-          routes.map((item: any) => {
-            return (
-              <Stack.Screen name={item.name} options={item.options} />
-            );
-          })
-        }
+        {routes.map((item: any) => <Stack.Screen name={item.name} options={item.options} />)}
       </Stack>
     </Provider>
   );
