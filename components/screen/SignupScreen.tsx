3e79936929a30ed02745d80e7e831bfb88726bc5
david-fiaty <dfiaty@gmail.com>
2024-11-03 15:00:00 +0000

    Create SignupScreen.tsx

---
+++ components/screen/SignupScreen.tsx
@@ -0,0 +1,90 @@
+import { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { setAccessToken, setIsLoggedIn, setAccountData } from '@/redux/slices/UserSlice';
+import { useRouter } from 'expo-router';
+import { Divider } from '@rneui/base';
+import { Layout } from '@/constants/Layout';
+import { Colors } from '@/constants/Colors';
+import LogoView from '../view/LogoView';
+import BoxView from '../view/BoxView';
+import TextView from '../view/TextView';
+import i18n from '@/translation/i18n';
+import InputTextField from '../field/InputTextField';
+import ContinueButton from '../button/ContinueButton';
+import SkipButton from '../button/SkipButton';
+import GoogleLoginButton from '../button/GoogleLoginButton';
+import FacebookLoginButton from '../button/FacebookLoginButton';
+import InstagramLoginButton from '../button/InstagramLoginButton';
+import UserManager from '@/classes/UserManager';
+
+const SignupScreen = () => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const userState = useSelector((state: any) => state.user);
+
+  const signup = async () => {
+    // Todo - Connect username and password
+    let response = await UserManager.login(username, password);
+    //let response = await UserManager.login('mitsiomotu@yopmail.com', 'Password1234');
+
+    if (response?.tokens?.access_token?.length) {
+      dispatch(setAccessToken(JSON.stringify(response.tokens)));
+      dispatch(setAccountData(JSON.stringify(response.user)));
+      dispatch(setIsLoggedIn(true));
+
+      router.replace('/main');
+    }
+  }  
+
+  return (
+    <BoxView direction="column" align="center" justify="center" style={Layout.screenContent}>
+      <LogoView size={{ width: 80, height: 80 }} />    
+      <TextView style={styles.wecomeMessage}>{i18n.t('Create an account')}</TextView> 
+
+      <Divider /><Divider />
+      <InputTextField 
+        containerStyle={styles.inputTextFieldContainer}
+        placeholder={i18n.t('Email address')} 
+        onChangeText={(text: string) => setUsername(text)}
+      />
+      <InputTextField 
+        containerStyle={styles.inputTextFieldContainer}
+        placeholder={i18n.t('Password')} 
+        secureTextEntry={true} 
+        autoCapitalize="none"
+        spellCheck={false}
+        onChangeText={(text: string) => setPassword(text)}
+      />
+
+      <ContinueButton onPress={login} />
+
+      <BoxView direction="row" align="center" justify="space-between" style={{width: '100%'}}>
+        <TextView>{i18n.t('Already have an account? Signup')}</TextView>
+        <SkipButton onPress={async () => router.push('/main')} />
+      </BoxView>
+
+      <Divider /><Divider />
+      <GoogleLoginButton />
+      <FacebookLoginButton />
+      <InstagramLoginButton />
+    </BoxView>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputTextFieldContainer: {
+    backgroundColor: Colors.white,
+    borderWidth: 0.5,
+    borderRadius: Layout.radius.round,
+    borderColor: Colors.primary,
+  },
+  wecomeMessage: {
+    textTransform: 'uppercase',
+    fontSize: Layout.fontSize.base*1.1,
+  }
+});
+
+export default SignupScreen;
