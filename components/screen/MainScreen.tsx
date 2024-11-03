41416f9fec4aa396da6adcbc6f5b3b79d4a6eed4
david-fiaty <dfiaty@gmail.com>
2024-11-03 19:30:41 +0000

    Update MainScreen.tsx

--- components/screen/MainScreen.tsx
+++ components/screen/MainScreen.tsx
@@ -5,13 +5,38 @@ import { useSelector } from 'react-redux';
 import { Layout } from "@/constants/Layout";
 import { Colors } from "@/constants/Colors";
 import { Modals } from "@/constants/Modals";
-import { Screens } from "@/constants/Screens";
 import ScreenView from "../view/ScreenView";
 import BoxView from "../view/BoxView";
 import DeviceManager from "@/classes/DeviceManager";
 import FooterNavigation from "../navigation/FooterNavigation";
 import JamsList from "../list/JamsList";
 import ScreenManager from "@/classes/ScreenManager";
+import SettingsMenu from "@/components/menu/SettingsMenu";
+import NotificationsMenu from "@/components/menu/NotificationsMenu";
+import SearchView from "@/components/view/SearchView";
+import MapView from "@/components/view/MapView";
+import AddJamForm from "@/components/form/AddJamForm";
+import ProfileForm from "@/components/form/ProfileForm";
+import JammersList from "@/components/list/JammersList";
+import HostsList from "@/components/list/HostsList";
+import SaveJamView from "@/components/view/SaveJamView";
+import ShareJamView from "@/components/view/ShareJamView";
+import MoreJamView from "@/components/view/MoreJamView";
+
+const Screens = {
+  JamsList: <JamsList />,
+  MapView: <MapView />,
+  AddJamForm: <AddJamForm />,
+  ProfileForm: <ProfileForm />,
+  SettingsMenu: <SettingsMenu />,
+  NotificationsMenu: <NotificationsMenu />,
+  SearchView: <SearchView />,
+  JammersList: <JammersList />,
+  HostsList: <HostsList />,
+  SaveJamView: <SaveJamView />,
+  ShareJamView: <ShareJamView />,
+  MoreJamView: <MoreJamView />,
+};
 
 const MainScreen = () => {
   // Parameters
