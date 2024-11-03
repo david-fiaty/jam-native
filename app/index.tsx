8d5cd4f2a4728b36489beb856fa5c051443a9819
david-fiaty <dfiaty@gmail.com>
2024-11-03 17:37:37 +0000

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,15 @@
 import { ThemeProvider } from '@rneui/themed';
+import { useDispatch } from 'react-redux';
+import { setScreenState } from '@/redux/slices/ScreenSlice';
 import BaseTheme from "@/constants/BaseTheme";
 import ScreenView from '@/components/view/ScreenView';
 import WelcomeScreen from '@/components/screen/WelcomeScreen';
 
 export default () => {
+  const dispatch = useDispatch(); 
+
+  //dispatch(setScreenState());
+
   return ( 
     <ThemeProvider theme={BaseTheme}>
       <ScreenView>
