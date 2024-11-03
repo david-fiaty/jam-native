ea318e9e0d8f24d729eaf790a925aec98da09076
david-fiaty <dfiaty@gmail.com>
2024-11-03 15:51:50 +0000

    Update LoginScreen.tsx

--- components/screen/LoginScreen.tsx
+++ components/screen/LoginScreen.tsx
@@ -64,7 +64,7 @@ const LoginScreen = () => {
 
       <BoxView direction="row" align="center" justify="space-between" style={{width: '100%'}}>
         <BoxView direction="row" align="center" justify="flex-start">
-          <TextView>{i18n.t('New user?')}</TextView>
+          <TextView>{i18n.t('Don\'t have an account?')}</TextView>
           <LinkView onPress={async () => router.push('/signup')}>
             {i18n.t('Sign up')}
           </LinkView>
