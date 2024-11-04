aebaa02153f4a22452b594486d2dd4e1336f8eaa
david-fiaty <dfiaty@gmail.com>
2024-11-04 12:57:41 +0000

    Update MapView.tsx

--- components/view/MapView.tsx
+++ components/view/MapView.tsx
@@ -54,22 +54,20 @@ const MapView = ({ style, children }: BaseProps) => {
             longitudeDelta: 3,
           }}
         >
-          {
-            data.map(item => {
-              if (item?.longitude && item?.latitude) {
-                return (
-                  <Marker
-                    key={item.id}
-                    coordinate={{ latitude: parseFloat(item?.latitude), longitude: parseFloat(item?.longitude) }}
-                    title={item?.description?.substring(0, 20) + '...'}
-                    description={item?.description}
-                  />
-                );
-              }
+          { data.map((item: any) => {
+            if (item?.longitude && item?.latitude) {
+              return (
+                <Marker
+                  key={item.id}
+                  coordinate={{ latitude: parseFloat(item?.latitude), longitude: parseFloat(item?.longitude) }}
+                  title={item?.description?.substring(0, 20) + '...'}
+                  description={item?.description}
+                />
+              );
+            }
 
-              return null;
-            })
-          }
+            return null;
+          }) }
 
         </RNMapView>
       </View>
