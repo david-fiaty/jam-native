6cab546a0603bfdb1a846a3a541cc1302ba68305
david-fiaty <dfiaty@gmail.com>
2024-11-07 14:33:20 +0000

    Update MessageView.tsx

--- components/view/MessageView.tsx
+++ components/view/MessageView.tsx
@@ -2,6 +2,9 @@ import { StyleSheet, Text, View } from 'react-native';
 import { useSelector } from 'react-redux';
 import { Colors } from '@/constants/Colors';
 import { Layout } from '@/constants/Layout';
+import DeviceManager from '@/classes/DeviceManager';
+
+const statusBarHeight = DeviceManager.getStatusBarSize().height;
 
 const MessageView = () => {
   const messageState = useSelector((state: any) => state.message);
@@ -25,6 +28,7 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.primary,
     color: Colors.white,
     padding: Layout.space.base,
+    marginTop: Layout.space.base + statusBarHeight,
   },
   content: {
     color: 'white',
