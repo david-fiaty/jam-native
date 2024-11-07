ccf55b2a71ace7d5f01bea2ab69ed8cdc31f06c9
david-fiaty <dfiaty@gmail.com>
2024-11-07 12:58:42 +0000

    Update MessageView.tsx

--- components/view/MessageView.tsx
+++ components/view/MessageView.tsx
@@ -1,21 +1,34 @@
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { BaseProps } from '@/constants/Types';
 import { Colors } from '@/constants/Colors';
 import { Layout } from '@/constants/Layout';
+import BoxView from './BoxView';
 
-const MessageView = ({style, children}: BaseProps) => {
-  
+type Props = BaseProps & {
+  content: string,
+};
+
+const MessageView = ({content}: Props) => {
   return (
-    <Text style={[styles.content, style]}>
-      {children}
-    </Text>
+    <View style={styles.container}>
+      <Text style={styles.content}>
+        {content}
+      </Text>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Colors.primary,
+    color: Colors.white,
+    padding: Layout.space.base,
+  },
   content: {
-    color: Colors.primary,
-    fontSize: Layout.fontSize.base,
+    color: 'white',
   },
 });
 
