7ecb8923a64acf324ac6f7a3997239f69142c5eb
david-fiaty <dfiaty@gmail.com>
2024-11-07 15:10:39 +0000

    Update MessageView.tsx

--- components/view/MessageView.tsx
+++ components/view/MessageView.tsx
@@ -1,8 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { useSelector } from 'react-redux';
 import { Colors } from '@/constants/Colors';
 import { Layout } from '@/constants/Layout';
 import DeviceManager from '@/classes/DeviceManager';
+import IconView from './IconView';
+import BoxView from './BoxView';
 
 const statusBarHeight = DeviceManager.getStatusBarSize().height;
 
@@ -12,23 +14,25 @@ const MessageView = () => {
   if (!messageState?.text.length) return <></>;
 
   return (
-    <View style={styles.container}>
+    <BoxView direction="row" align="center" justify="space-between" style={styles.container}>
       <Text style={styles.content}>
         {messageState.text}
       </Text>
-    </View>
+      <TouchableOpacity onPress={() => console.log('clicked')}>
+        <IconView name="delete" theme="primary" />
+      </TouchableOpacity>
+    </BoxView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
     backgroundColor: Colors.primary,
     color: Colors.white,
     padding: Layout.space.base,
     marginTop: Layout.space.base + statusBarHeight,
+    marginHorizontal: Layout.space.base*1.5,
+    borderRadius: Layout.radius.round,
     position: 'absolute', 
     top: 0,      
     left: 0, 
