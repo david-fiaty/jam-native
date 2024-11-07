c85c8d0a081511ad941d3711d8d06f3f741b877c
david-fiaty <dfiaty@gmail.com>
2024-11-07 23:11:20 +0000

    Update LoginScreen.tsx

--- components/screen/LoginScreen.tsx
+++ components/screen/LoginScreen.tsx
@@ -29,14 +29,18 @@ const LoginScreen = () => {
 
   const login = async () => {
     // Todo - Connect username and password
-    //let success = await UserManager.login(username, password);
-    let success = await UserManager.login('mitsiomotu@yopmail.com', 'Password1234');
-    setIsProcessing(false);
+    //let response = await UserManager.login(username, password);
+    let response = await UserManager.login('mitsiomotu@yopmail.com', 'Password1234');
 
-    if (success) {
+    if (response?.tokens?.access_token?.length) {
+      dispatch(setAccessToken(JSON.stringify(response.tokens)));
+      dispatch(setAccountData(JSON.stringify(response.user)));
+      dispatch(setIsLoggedIn(true));
+      setIsProcessing(false);
       router.replace('/jams');
     }
     else {
+      setIsProcessing(false);
       ScreenManager.showMessage(i18n.t('Invalid user name or password.'));
     }
   }  
