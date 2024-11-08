8a35b6493817a29ed6293d6179f91db2d07cd8ea
david-fiaty <dfiaty@gmail.com>
2024-11-08 15:43:06 +0000

    Update ImageView.tsx

--- components/view/ImageView.tsx
+++ components/view/ImageView.tsx
@@ -1,26 +1,32 @@
 import { Image, StyleSheet } from 'react-native';
 import CachedImage from 'expo-cached-image'
 import { BaseProps } from '@/constants/Types';
+import SpinnerView from './SpinnerView';
 
 type Props = BaseProps & {
   path?: any,
   uri?: any,
   width?: string | number,
   height?: string | number,
-  resizeMethod?: string,
-  resizeMode?: string,
+  resizeMethod?: any,
+  resizeMode?: any,
   style?: object, 
 };
 
 const ImageView = ({path, uri, width, height, resizeMethod, resizeMode, style}: Props) => {
   const source = path ? path : {uri: uri};
+  const cacheKey = 'image-' + Math.random().toString(16).slice(2);
+  resizeMethod = resizeMethod || 'scale';
+  resizeMode = resizeMode || 'contain';
 
   return (
-    <Image 
+    <CachedImage
       source={source} 
-      resizeMethod={resizeMethod || 'scale'}
-      resizeMode={resizeMode || 'contain'}
+      cacheKey={cacheKey}
+      resizeMethod={resizeMethod}
+      resizeMode={resizeMode}
       style={[styles.image, style, {width: width, height: height}]} 
+      placeholderContent={<SpinnerView />}
     />   
   );
 };
