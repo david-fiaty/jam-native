d861d89200194904ec998e934f9b15452e23a3bf
david-fiaty <dfiaty@gmail.com>
2024-11-18 17:45:45 +0000

    Update LoginScreen.tsx

--- components/screen/LoginScreen.tsx
+++ components/screen/LoginScreen.tsx
@@ -30,12 +30,8 @@ const LoginScreen = () => {
   const submitForm = async () => {
     // Todo - Connect username and password
     //let response = await UserManager.login(username, password);
-    let response = await UserManager.login('mitsiomotu@yopmail.com', 'Password1234');
-
-    if (response?.tokens?.access_token?.length) {
-      dispatch(setAccessToken(JSON.stringify(response.tokens)));
-      dispatch(setAccountData(JSON.stringify(response.user)));
-      dispatch(setIsLoggedIn(true));
+    const success: boolean = await UserManager.login('mitsiomotu@yopmail.com', 'Password1234');
+    if (success === true) {
       setIsProcessing(false);
       router.replace('/jams');
     }
