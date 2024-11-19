f0a616afc81912fc572e387fac29c6fffc6f6a36
david-fiaty <dfiaty@gmail.com>
2024-11-19 21:00:25 +0000

    Update Endpoints.ts

--- constants/Endpoints.ts
+++ constants/Endpoints.ts
@@ -3,32 +3,39 @@ const Endpoints = {
     url: '/jams/',
     method: 'GET',
     cacheable: true,
+    searcheable: true,
   },
   projects: {
     url: '/projects/',
     method: 'GET',
     cacheable: true,
+    searcheable: true,
   },
   jammers: {
     cacheable: false,
+    searcheable: true,
   },
   notifications: {
     cacheable: false,
+    searcheable: false,
   },
   login: {
     url: '/login/',
     method: 'POST',
     cacheable: false,
+    searcheable: false,
   },
   signup: {
     url: '/register/',
     method: 'POST',
     cacheable: false,
+    searcheable: false,
   },
   currentUser: {
     url: '/get-current-user-info/',
     method: 'GET',
     cacheable: false,
+    searcheable: false,
   }
 };
 
