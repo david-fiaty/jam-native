77fbd173ff0a86fa2618938d30dc56a2ba94ec3e
david-fiaty <dfiaty@gmail.com>
2024-11-19 21:39:42 +0000

    Update MapView.tsx

--- components/view/MapView.tsx
+++ components/view/MapView.tsx
@@ -55,11 +55,16 @@ const MapView = ({ style, children }: BaseProps) => {
           }}
         >
           { data.map((item: any) => {
-            if (item?.longitude && item?.latitude) {
+            if (item?.geolocation_longitude && item?.geolocation_latitude) {
+              let coordinate = { 
+                latitude: parseFloat(item?.geolocation_latitude), 
+                longitude: parseFloat(item?.geolocation_longitude),
+              };
+
               return (
                 <Marker
                   key={item.id}
-                  coordinate={{ latitude: parseFloat(item?.latitude), longitude: parseFloat(item?.longitude) }}
+                  coordinate={coordinate}
                   title={item?.caption?.substring(0, 20) + '...'}
                   description={item?.caption}
                 />
