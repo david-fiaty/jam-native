5b9ee34a3fcd61c8e3b99938373be7460ca26cd2
david-fiaty <dfiaty@gmail.com>
2024-11-19 21:03:41 +0000

    Update DataManager.tsx

--- classes/DataManager.tsx
+++ classes/DataManager.tsx
@@ -1,29 +1,31 @@
 import Store from '@/redux/Store';
-import ApiEndpoints from '@/constants/Endpoints';
+import Endpoints from '@/constants/Endpoints';
 import ApiManager from './ApiManager';
 import EntityManager from './EntityManager';
 
 class DataManager {
-  async get(key: keyof typeof ApiEndpoints, options: any) {
+  async get(key: keyof typeof Endpoints, options: any) {
     // Variables
-    const searchState = Store.getState().search;
     let data: any = await this.getData(key);
     let results: any = [];
 
-    // Apply search
-    if (options?.filter !== false && searchState.filter?.length) {
-      results = data.filter((item: any)  => item?.type == searchState.filter); 
-    }
+    // Apply search and filter
+    if (Endpoints[key].searcheable === true) {
+      const searchState = Store.getState().search;
+      
+      if (options?.filter !== false && searchState.filter?.length) {
+        results = data.filter((item: any)  => item?.type == searchState.filter); 
+      }
 
-    // Apply filters
-    if (options?.search !== false && searchState.value?.length) {
-      results = results.length ? results : data;
-      results = data.filter((item: any) => {
-        let haystack = item?.description?.toLowerCase();
-        let needle = searchState.value.toLowerCase();
+      if (options?.search !== false && searchState.value?.length) {
+        results = results.length ? results : data;
+        results = data.filter((item: any) => {
+          let haystack = item?.description?.toLowerCase();
+          let needle = searchState.value.toLowerCase();
 
-        return haystack.includes(needle);
-      }); 
+          return haystack.includes(needle);
+        }); 
+      }
     }
 
     // Process results
@@ -33,11 +35,11 @@ class DataManager {
     return results;
   }
 
-  async post(key: keyof typeof ApiEndpoints, data: any, options?: any) {
+  async post(key: keyof typeof Endpoints, data: any, options?: any) {
     return await ApiManager.post(key, data);
   }
 
-  async getData(key: keyof typeof ApiEndpoints) {
+  async getData(key: keyof typeof Endpoints) {
     let data: any = await ApiManager.get(key);
     let result: any = [];
 
@@ -50,7 +52,7 @@ class DataManager {
     return result;
   }
 
-  async postData(key: keyof typeof ApiEndpoints, data: object, options: object) {
+  async postData(key: keyof typeof Endpoints, data: object, options: object) {
 
   }
 };
