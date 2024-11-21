4efbaa9779b9e0b464dbeca8120dfec939948fcf
david-fiaty <dfiaty@gmail.com>
2024-11-21 15:55:48 +0000

    Update CountryField.tsx

--- components/field/CountryField.tsx
+++ components/field/CountryField.tsx
@@ -1,13 +1,21 @@
 import { StyleSheet } from 'react-native';
 import { CreativeOrganizationOptionsData } from '@/constants/Data';
+import { BaseProps } from '@/constants/Types';
 import i18n from '@/translation/i18n';
 import BoxView from '../view/BoxView';
 import SelectListBase from '../base/SelectListBase';
 
-const CountryField = () => {
+type Props = BaseProps & {
+  selected?: any,
+};
+
+const CountryField = ({selected}: Props) => {
   return (
     <BoxView direction="row" align="space-between" style={styles.container}>
-      <SelectListBase data={CreativeOrganizationOptionsData} placeholder={i18n.t('Creative organization')} />
+      <SelectListBase 
+        data={CreativeOrganizationOptionsData} 
+        placeholder={i18n.t('Country')} 
+      />
     </BoxView>
   );
 };
