24d058a49e3c2eb190e8887972c496993adfc67f
david-fiaty <dfiaty@gmail.com>
2024-11-21 17:31:00 +0000

    Update JamCategoriesField.tsx

--- components/field/JamCategoriesField.tsx
+++ components/field/JamCategoriesField.tsx
@@ -11,13 +11,13 @@ const JamCategoriesField = () => {
   const [selectedOption, setSelectedOption] = useState(null);
   const data = Data.jamCategories;
 
-  const renderItem = (item, index) => (
-    <TouchableOpacity onPress={() => setSelectedOption(item.id)}>
+  const renderItem = (row: any) => (
+    <TouchableOpacity onPress={() => setSelectedOption(row.item.id)}>
       <View style={styles.container}>
-        <View style={[styles.square, selectedOption == item.id ? styles.selected : {}]}>
-          <IconView name={item.icon} theme="secondary" />
+        <View style={[styles.square, selectedOption == row.item.id ? styles.selected : {}]}>
+          <IconView name={row.item.icon} theme="secondary" />
         </View>
-        <TextView>{item.label}</TextView>   
+        <TextView>{row.item.label}</TextView>   
       </View>
     </TouchableOpacity>
   );
@@ -28,10 +28,9 @@ const JamCategoriesField = () => {
       numColumns={4}
       horizontal={false}
       scrollEnabled={false}
-      conten
-      tContainerStyle={Layout.listContainer}
+      contentContainerStyle={Layout.listContainer}
       columnWrapperStyle={Layout.listColumnWrapper}
-      renderItem={({item, index}) => renderItem(item, index)}
+      renderItem={(item: any) => renderItem(item)}
     />
   );
 }
