a763083a913cffa2618ab84cb080e58a15452d0b
david-fiaty <dfiaty@gmail.com>
2024-11-21 16:09:59 +0000

    Update ProfileForm.tsx

--- components/form/ProfileForm.tsx
+++ components/form/ProfileForm.tsx
@@ -21,7 +21,7 @@ const ProfileForm = () => {
   const [userData, setUserData] = useState<any>(null);
   const [profileData, setProfileData] = useState<any>(null);
 
-  const updateValue = (key: string, value: string) => {
+  const updateField = (key: string, value: string) => {
     setProfileData({...profileData, ...{ [key]: value }});
   };
 
@@ -53,25 +53,25 @@ const ProfileForm = () => {
       <InputTextField
         placeholder={i18n.t('Email address')}
         value={profileData?.email}
-        onChangeText={(value: string) => updateValue('email', value)}
+        onChangeText={(value: string) => updateField('email', value)}
       />
       
       <InputTextField
         placeholder={i18n.t('Profile name')}
         value={profileData?.profile_name}
-        onChangeText={(value: string) => updateValue('profile_name', value)}
+        onChangeText={(value: string) => updateField('profile_name', value)}
       />
 
       <InputTextField
         placeholder={i18n.t('Phone number')}
         value={profileData?.phone_number}
-        onChangeText={(value: string) => updateValue('phone_number', value)}
+        onChangeText={(value: string) => updateField('phone_number', value)}
       />
 
       <InputTextareaField
         placeholder={i18n.t('Description')}
         value={profileData?.profile_description}
-        onChangeText={(value: string) => updateValue('profile_description', value)}
+        onChangeText={(value: string) => updateField('profile_description', value)}
       />
 
       <UserLocationField 
