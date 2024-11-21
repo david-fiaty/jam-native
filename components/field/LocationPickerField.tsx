06a92120d14b2a0bc2aae9738eff33ee3b9d7fd7
david-fiaty <dfiaty@gmail.com>
2024-11-21 19:14:09 +0000

    Update LocationPickerField.tsx

--- components/field/LocationPickerField.tsx
+++ components/field/LocationPickerField.tsx
@@ -1,4 +1,3 @@
-import { StyleSheet } from 'react-native';
 import { BaseProps } from '@/constants/Types';
 import i18n from '@/translation/i18n';
 import InputTextField from '../field/InputTextField';
@@ -12,22 +11,14 @@ type Props = BaseProps & {
 
 const LocationPickerField = ({latitude, longitude}: Props) => {
   return (
-    <BoxView direction="row" align="space-between" style={styles.container}>
+    <BoxView direction="row" align="space-between">
       <InputTextField 
         disabled={true}
         placeholder={i18n.t('Location')} 
-        containerStyle={styles.containerStyle} 
         rightIcon={<IconView name="location" theme="transparent" />}
       />
     </BoxView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-  },
-  containerStyle: {
-  }
-});
 
 export default LocationPickerField;
