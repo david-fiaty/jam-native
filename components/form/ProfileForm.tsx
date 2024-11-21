c1123cdaa72144b0076d8569a0f237b377d0c4e9
david-fiaty <dfiaty@gmail.com>
2024-11-21 14:59:35 +0000

    Update ProfileForm.tsx

--- components/form/ProfileForm.tsx
+++ components/form/ProfileForm.tsx
@@ -29,9 +29,9 @@ const ProfileForm = () => {
   const [userData, setUserData] = useState(null);
   const [profileData, setProfileData] = useState(null);
 
-  UserManager.getUserData().then((currentUserData: any) => {
-    if (!userData) setUserData(Object.assign({}, currentUserData));
-    if (!profileData) setProfileData(Object.assign({}, currentUserData.account.profiles[0]));
+  UserManager.getUserData().then((data: any) => {
+    if (!userData) setUserData(Object.assign({}, data));
+    if (!profileData) setProfileData(Object.assign({}, data?.account?.profiles?.[0]));
 
     setIsLoaded(true);
   });
