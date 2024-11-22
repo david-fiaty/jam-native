8bc4117e30f1d03cf8ddbc0498ab4306da7cd018
david-fiaty <dfiaty@gmail.com>
2024-11-22 15:22:17 +0000

    Update ObjectHelper.ts

--- helper/ObjectHelper.ts
+++ helper/ObjectHelper.ts
@@ -1,23 +1,18 @@
 const ObjectHelper = {
   update(target: any, keyPath: string, newValue: any) {
     const keys = keyPath.split('.'); 
-    if (keys.length === 1) {
-      target[keys[0]] = newValue;
-      return;
-    }
+    let current = Object.assign({}, target); 
   
-    let current = target; 
     for (let i = 0; i < keys.length - 1; i++) {
       const key = keys[i];
       if (!current[key] || typeof current[key] !== 'object') {
         current[key] = {};
       }
-
       current = current[key]; 
     }
-  
-    current[keys[keys.length - 1]] = newValue;
 
+    current[keys[keys.length - 1]] = newValue;
+  
     return current;
   }  
 };
