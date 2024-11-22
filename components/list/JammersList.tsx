6ab8385115455d0f27fdcbd63ead39f80465e64a
david-fiaty <dfiaty@gmail.com>
2024-11-22 17:49:54 +0000

    Update JammersList.tsx

--- components/list/JammersList.tsx
+++ components/list/JammersList.tsx
@@ -18,9 +18,8 @@ const JammersList = () => {
   const profilesData: any = [];
   const entityId = ScreenManager.getActiveScreen()?.entityId;
   
-  DataManager.find('jams', 'id', entityId).then((item: any) => {
+  DataManager.find('jams', 'id', [entityId]).then((item: any) => {
     if (!entity) setEntity(item);
-    
     console.log(entity?.jammers);
 
     setIsLoaded(true);
