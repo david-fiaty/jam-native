9bded17c646a9a0d915d046e379ca0b98ebe78b7
david-fiaty <dfiaty@gmail.com>
2024-11-22 14:32:25 +0000

    Update DatePickerField.tsx

--- components/field/DatePickerField.tsx
+++ components/field/DatePickerField.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Platform, TouchableOpacity } from 'react-native';
+import { StyleSheet, Platform, TouchableOpacity } from 'react-native';
 import { BaseProps } from '@/constants/Types';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import InputTextField from '../field/InputTextField';
@@ -25,26 +25,32 @@ const DatePickerField = ({placeholder, value, callback}: Props) => {
   const display = Platform.OS === 'ios' ? 'spinner' : 'default';
 
   return (
-    <BoxView direction="row" align="space-between">
-      <TouchableOpacity onPress={() => setShow(true)}>
-        <InputTextField 
-          readOnly={true}
-          placeholder={placeholder} 
-          rightIcon={<IconView name="calendar" theme="transparent" />}
-          value={date.toLocaleDateString()}
-        />
-      </TouchableOpacity>
+      <BoxView direction="row" align="space-between">
+        <TouchableOpacity onPress={() => setShow(true)} style={styles.fieldContainer}>
+          <InputTextField 
+            readOnly={true}
+            placeholder={placeholder} 
+            rightIcon={<IconView name="calendar" theme="transparent" />}
+            value={date.toLocaleDateString()}
+          />
+        </TouchableOpacity>
 
-      {show && (
-        <DateTimePicker
-          value={date} 
-          mode="date"
-          display={display} 
-          onChange={onChange}
-        />
-      )}
-    </BoxView>
+        {show && (
+          <DateTimePicker
+            value={date} 
+            mode="date"
+            display={display} 
+            onChange={onChange}
+          />
+        )}
+      </BoxView>
   );
 };
+
+const styles = StyleSheet.create({
+  fieldContainer: {
+    width: '100%'
+  },
+});
 
 export default DatePickerField;
