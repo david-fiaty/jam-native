5294129160d98a159c077bb5669d3fdd226f5537
david-fiaty <dfiaty@gmail.com>
2024-11-23 08:45:18 +0000

    Update DataManager.tsx

--- manager/DataManager.tsx
+++ manager/DataManager.tsx
@@ -8,6 +8,11 @@ class DataManager {
     let data: any = await ApiManager.get(key);
     let results: any = [];
 
+    // Extract data with key
+    if (Endpoints[key].dataKey !== null) {
+      console.log(data[Endpoints[key].dataKey]);
+    }
+
     // Apply search and filter
     if (Endpoints[key].searcheable === true) {
       const searchState = Store.getState().search;
