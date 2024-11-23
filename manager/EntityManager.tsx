e17f19e85728a1d5eb7b2fb76d56167ed0e1faad
david-fiaty <dfiaty@gmail.com>
2024-11-23 11:01:37 +0000

    Update EntityManager.tsx

--- manager/EntityManager.tsx
+++ manager/EntityManager.tsx
@@ -1,5 +1,6 @@
 import Endpoints from '@/constants/Endpoints';
 import DataManager from './DataManager';
+import UserManager from './UserManager';
 
 class EntityManager {
   async find(key: keyof typeof Endpoints, idValues: any) {
@@ -12,6 +13,16 @@ class EntityManager {
 
   async findJam(entityId: any) {
     return await DataManager.find('jams', 'id', entityId);
+  }
+
+  async saveJam(entityId: any) {
+    let profileId = await UserManager.getProfileId();
+    let response = await DataManager.post('saveJam', {
+      profile_id: profileId,
+      save_items_ids: [entityId],
+    });
+
+    return !!response;
   }
 
   async getProfiles() {
