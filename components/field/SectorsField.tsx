9e93b9104b520d2304a8cbc269622e48974edf3b
david-fiaty <dfiaty@gmail.com>
2024-11-23 16:06:32 +0000

    Update SectorsField.tsx

--- components/field/SectorsField.tsx
+++ components/field/SectorsField.tsx
@@ -4,6 +4,7 @@ import i18n from '@/translation/i18n';
 import BoxView from '../view/BoxView';
 import SelectListBase from '../base/SelectListBase';
 import Data from '@/constants/StaticData';
+import EntityManager from '@/manager/EntityManager';
 
 type Props = BaseProps & {
   value?: any,
@@ -12,7 +13,11 @@ type Props = BaseProps & {
 const SectorsField = ({value}: Props) => {
   const mainIndustries = Data.mainIndustries;
   const subIndustries = Data.subIndustries;
-  
+
+  EntityManager.getSectors().then((data: any) => {
+    console.log(data);
+  });
+
   return (
     <BoxView direction="column" align="center" style={styles.container}>
       <SelectListBase data={mainIndustries} placeholder={i18n.t('Industries')} />
