c914d8f69a084337d39d32ec87edea7eb5892ea3
david-fiaty <dfiaty@gmail.com>
2024-11-23 17:47:08 +0000

    Update CountryField.tsx

--- components/field/CountryField.tsx
+++ components/field/CountryField.tsx
@@ -3,19 +3,32 @@ import { BaseProps } from '@/constants/Types';
 import i18n from '@/translation/i18n';
 import BoxView from '../view/BoxView';
 import SelectListBase from '../base/SelectListBase';
+import StaticData from '@/constants/StaticData';
 
 type Props = BaseProps & {
   value?: any,
+  onChangeValue?: (option: any) => void,
 };
 
-const CountryField = ({value}: Props) => {
-  const data: any = [];
+const CountryField = ({value, onChangeValue}: Props) => {
+  const countriesData: any = StaticData.countries;
+
+  const buildOptions = (optionsData: any) => {
+    return optionsData.map((item: any) => {
+      return {
+        value: item?.code?.toLowerCase(),
+        label: item?.name,
+      }
+    });
+  };
 
   return (
     <BoxView direction="row" align="space-between" style={styles.container}>
       <SelectListBase 
-        data={data} 
+        value={value}
+        data={buildOptions(countriesData)} 
         placeholder={i18n.t('Country')} 
+        onChangeValue={onChangeValue}
       />
     </BoxView>
   );
