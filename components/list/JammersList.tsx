41288621be4356ece236dfacd010172ff091b82c
david-fiaty <dfiaty@gmail.com>
2024-11-23 09:23:19 +0000

    Update JammersList.tsx

--- components/list/JammersList.tsx
+++ components/list/JammersList.tsx
@@ -34,7 +34,7 @@ const JammersList = () => {
     <TouchableOpacity onPress={() => console.log('clicked')}>
       <BoxView direction="row" align="center" justify="flex-start" style={Layout.listItem}>
         <IconView name="user" theme="tertiary" />
-        <TextView>{row.item.name}</TextView>
+        <TextView>{row.item.profile_name}</TextView>
       </BoxView>
     </TouchableOpacity>
   );
