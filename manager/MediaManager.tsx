ada146c9183e1320abc87a32379fc21fc1bc284e
david-fiaty <dfiaty@gmail.com>
2024-11-25 16:02:57 +0000

    Revert "Update MediaManager.tsx"
    This reverts commit 7a119a23e0c236262ec1395e399b68cce988b1a3.

--- manager/MediaManager.tsx
+++ manager/MediaManager.tsx
@@ -3,7 +3,11 @@ import FileSystem from "react-native-fs";
 class MediaManager {
   async getBinaryData(uri: string) {
     try {
-      return await FileSystem.readFile(uri, 'base64');
+      const platformFilePath = uri.startsWith('file://') ? uri.replace('file://', '') : uri;
+      const base64String = await FileSystem.readFile(platformFilePath, 'base64');
+      const binaryData = Uint8Array.from(atob(base64String), (c) => c.charCodeAt(0));
+
+      return binaryData; 
     } 
     catch (error) {
       console.error('Error reading file:', error);
