8c87fc9c79afb4cdee52fb9081a2b0d4491dbd73
david-fiaty <dfiaty@gmail.com>
2024-11-25 14:04:31 +0000

    Update LoginScreen.tsx

--- components/screen/LoginScreen.tsx
+++ components/screen/LoginScreen.tsx
@@ -42,7 +42,7 @@ const LoginScreen = () => {
   }  
 
   return (
-    <BoxView direction="column" align="center" justify="center" style={[Layout.screenContent]}>
+    <BoxView direction="column" align="center" justify="center" style={Layout.screenContent}>
       <LogoView size={{ width: 80, height: 80 }} />    
       <TextView style={styles.wecomeMessage}>{i18n.t('Welcome back')}</TextView> 
 
