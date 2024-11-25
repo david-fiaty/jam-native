7a119a23e0c236262ec1395e399b68cce988b1a3
david-fiaty <dfiaty@gmail.com>
2024-11-25 16:01:27 +0000

    Update MediaManager.tsx

--- manager/MediaManager.tsx
+++ manager/MediaManager.tsx
@@ -3,11 +3,7 @@ import FileSystem from "react-native-fs";
 class MediaManager {
   async getBinaryData(uri: string) {
     try {
-      const platformFilePath = uri.startsWith('file://') ? uri.replace('file://', '') : uri;
-      const base64String = await FileSystem.readFile(platformFilePath, 'base64');
-      const binaryData = Uint8Array.from(atob(base64String), (c) => c.charCodeAt(0));
-
-      return binaryData; 
+      return await FileSystem.readFile(uri, 'base64');
     } 
     catch (error) {
       console.error('Error reading file:', error);
