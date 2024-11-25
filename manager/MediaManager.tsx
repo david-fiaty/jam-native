a945651d391c878d5fc886f81eec9e23fce1f38b
david-fiaty <dfiaty@gmail.com>
2024-11-25 15:38:00 +0000

    Update MediaManager.tsx

--- manager/MediaManager.tsx
+++ manager/MediaManager.tsx
@@ -1,8 +1,19 @@
-
+import FileSystem from "react-native-fs";
 
 class MediaManager {
   async getBinaryData(uri: string) {
+    try {
+      const platformFilePath = uri.startsWith('file://') ? uri.replace('file://', '') : uri;
+      const base64String = await FileSystem.readFile(platformFilePath, 'base64');
+      const binaryData = Uint8Array.from(atob(base64String), (c) => c.charCodeAt(0));
+
+      return binaryData; 
+    } 
+    catch (error) {
+      console.error('Error reading file:', error);
+      return null;
+    }
   }
 }
 
-export default (new MediaManager());
+export default new MediaManager();
