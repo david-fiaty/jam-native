63ee33fc3f9ac9ccbad8bc324823d663dadd106d
david-fiaty <dfiaty@gmail.com>
2024-11-26 17:04:22 +0000

    Update Config.ts

--- constants/Config.ts
+++ constants/Config.ts
@@ -1,6 +1,6 @@
 export const Config = {
   dataCacheEnabled: false,
-  imageCacheEnabled: true,
+  imageCacheEnabled: false,
   apiEnabled: false,
   apiUrl: 'https://the.jammm.app/backend/api/v1',
   imageUrl: 'https://the.jammm.app/backend',
