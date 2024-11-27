08fcbc33521a9b2fd57c0e8d4ba343335e687531
david-fiaty <dfiaty@gmail.com>
2024-11-27 00:42:09 +0000

    Update AddCollaboratorsField.tsx

--- components/field/AddCollaboratorsField.tsx
+++ components/field/AddCollaboratorsField.tsx
@@ -1,31 +1,26 @@
-
-import { StyleSheet } from 'react-native';
-import CheckboxSelectBase from '../base/CheckboxSelectBase';
-import i18n from '@/translation/i18n';
+import { StyleSheet } from "react-native";
+import { BaseProps } from "@/constants/Types";
+import CheckboxSelectBase from "../base/CheckboxSelectBase";
+import i18n from "@/translation/i18n";
 import BoxView from "../view/BoxView";
 import TextView from "../view/TextView";
 import IconView from "../view/IconView";
-import DataManager from '@/manager/DataManager';
 
-const AddCollaboratorsField = () => {  
-  const data = DataManager.get('jammers');
+type Props = BaseProps & {
+  onPressEvent?: () => void;
+};
 
+const AddCollaboratorsField = ({ onPressEvent }: Props) => {
   return (
-    <CheckboxSelectBase 
-      data={data}
-      label={
-        <BoxView direction="row" align="center" style={styles.container}>
-          <IconView name="plus" theme="secondary" radius="round" />
-          <TextView>{i18n.t('Add collaborators')}</TextView>
-        </BoxView>  
-      } 
-    />
+    <BoxView direction="row" align="center" style={styles.container} onPress={onPressEvent}>
+      <IconView name="plus" theme="secondary" radius="round" />
+      <TextView>{i18n.t("Add collaborators")}</TextView>
+    </BoxView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-  },
+  container: {},
 });
 
 export default AddCollaboratorsField;
