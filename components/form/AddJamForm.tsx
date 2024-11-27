4bced811845f0ca63562fb6865d8aa8de7253d23
david-fiaty <dfiaty@gmail.com>
2024-11-27 19:05:18 +0000

    Update AddJamForm.tsx

--- components/form/AddJamForm.tsx
+++ components/form/AddJamForm.tsx
@@ -149,8 +149,8 @@ const AddJamForm = () => {
             <TextView>{i18n.t('Add media')}</TextView>
           </BoxView>
         }
-        onSelectMedia={(mediaList: any) => updateField('upload_medias', mediaList)} 
-        onDeleteMedia={(mediaList: any) => updateField('upload_medias', mediaList)}
+        onSelectItem={(mediaList: any) => updateField('upload_medias', mediaList)} 
+        onDeleteItem={(mediaList: any) => updateField('upload_medias', mediaList)}
       />
       
       <AddCollaboratorsField 
