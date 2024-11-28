4bf155204726870676effd693565811ade893a0b
david-fiaty <dfiaty@gmail.com>
2024-11-28 00:40:50 +0000

    Update CollaboratorsList.tsx

--- components/list/CollaboratorsList.tsx
+++ components/list/CollaboratorsList.tsx
@@ -82,7 +82,7 @@ const CollaboratorsList = () => {
         <IconView name="user" theme="tertiary" />
         <TextView>{row.item.profile_name}</TextView>
         { selectedProfiles.includes(row.item.id) &&
-          <IconView name="checkmark" theme="clear" size={18}/>
+          <IconView name="checkmark" theme="clear" size={18} />
         }
       </BoxView>
     </TouchableOpacity>
@@ -96,6 +96,7 @@ const CollaboratorsList = () => {
       />
       
       <InputTextField 
+        value={searchValue}
         containerStyle={styles.inputTextFieldContainer}
         placeholder={i18n.t('Search...')} 
         onChangeText={(text: string) => setSearchValue(text)}
