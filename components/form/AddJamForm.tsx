259184a326ff35d6c32820a801b613fce6941900
david-fiaty <dfiaty@gmail.com>
2024-11-28 01:52:40 +0000

    Update AddJamForm.tsx

--- components/form/AddJamForm.tsx
+++ components/form/AddJamForm.tsx
@@ -37,11 +37,6 @@ const AddJamForm = () => {
   const [profileId, setProfileId] = useState<number>(0);
   const jamCategoriesData = StaticData.jamCategories;
 
-  
-  EntityManager.getSectors().then(x => {
-    console.log(x);
-  });
-
   const updateField = (key: string, value: any) => {
     dispatch(setJamData<any>({key: key, value: value, profile_id: profileId}));
   };
