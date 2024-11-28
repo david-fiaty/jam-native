403bc72d4cb2d48c1b430ef7cb970ed6754a74db
david-fiaty <dfiaty@gmail.com>
2024-11-28 19:28:49 +0000

    Update AddJamForm.tsx

--- components/form/AddJamForm.tsx
+++ components/form/AddJamForm.tsx
@@ -184,6 +184,7 @@ const AddJamForm = () => {
         }
       />
 
+      <DividerView />
       <CollaboratorsField
         onPressEvent={() => ScreenManager.toggleModal('CollaboratorsList')}
       />
