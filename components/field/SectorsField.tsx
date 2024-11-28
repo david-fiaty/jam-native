d2ca549ba5948bc098de34a6b9ec786a795e324c
david-fiaty <dfiaty@gmail.com>
2024-11-28 17:42:36 +0000

    Update SectorsField.tsx

--- components/field/SectorsField.tsx
+++ components/field/SectorsField.tsx
@@ -32,17 +32,12 @@ const SectorsField = ({value, onChangeListValue, onChangeSublistValue}: Props) =
 
   const onChangeList = (option: any) => {
 
-    //console.log(JSON.stringify(listData, 0, 2));
-   console.log(option);
+    let sublistOpions = buildOptions(listData.find((item: any) => item?.value == option?.value)?.children);
+    setSublistData(sublistOpions);
 
-    //setSublistData(buildOptions(listData.find((item: any) => item?.value == option?.value)?.sub_sectors));
-
-    /*
-    let x = listData.filter((item: any) => item?.id == option?.value)?.sub_sectors;
-    console.log(x);
-    */
-
-    //if (onChangeListValue) onChangeListValue(option);
+    console.log(sublistOpions);
+    
+    if (onChangeListValue) onChangeListValue(option);
   };
 
   const onChangeSublist = (option: any) => {
