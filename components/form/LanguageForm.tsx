08b97152ca73aeb1d4c9bfb7d110c94b5aaf1cd7
david-fiaty <dfiaty@gmail.com>
2024-11-29 21:23:54 +0000

    Update LanguageForm.tsx

--- components/form/LanguageForm.tsx
+++ components/form/LanguageForm.tsx
@@ -1,24 +1,15 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useRouter } from 'expo-router';
 import { Layout } from '@/constants/Layout';
 import i18n from "@/translation/i18n";
 import BoxView from "../view/BoxView";
 import BackButton from "../button/BackButton";
-import InputTextField from '../field/InputTextField';
-import SpinnerView from '../view/SpinnerView';
 import SelectListBase from '../base/SelectListBase';
 import StaticData from '@/constants/StaticData';
 
 const LanguageForm = () => {
   const router = useRouter();
-  const [isLoaded, setIsLoaded] = useState(false);
   const data = StaticData.languages;
-
-  useEffect(() => {
-    setTimeout(() => setIsLoaded(true), Layout.animation.duration);
-  });
-
-  if (!isLoaded) return <SpinnerView />;
 
   return (
     <BoxView align="flex-start" justify="flex-start" scroll={true} style={Layout.screenContent}>
