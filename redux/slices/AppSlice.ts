79231a6d361910b655ae326c63574c036a5bf7ba
david-fiaty <dfiaty@gmail.com>
2024-11-29 23:28:24 +0000

    Update AppSlice.ts

--- redux/slices/AppSlice.ts
+++ redux/slices/AppSlice.ts
@@ -5,17 +5,13 @@ const AppSlice = createSlice({
   name: 'app',
   initialState: {
     isStarted: false,
-    language: Config.defaultLanguage,
   },
   reducers: {
     setIsStarted: (state, action) => {
       state.isStarted = action.payload;
     },
-    setLanguage: (state, action) => {
-      state.language = action.payload;
-    },
   },
 });
 
-export const { setIsStarted, setLanguage } = AppSlice.actions;
+export const { setIsStarted } = AppSlice.actions;
 export default AppSlice.reducer;
