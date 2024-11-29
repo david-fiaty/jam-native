dab639228ecac556ac9c2b2b5da4bb9a87f9af6b
david-fiaty <dfiaty@gmail.com>
2024-11-29 23:37:48 +0000

    Update LanguageForm.tsx

--- components/form/LanguageForm.tsx
+++ components/form/LanguageForm.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'expo-router';
+import { useState } from 'react';
 import { Layout } from '@/constants/Layout';
 import i18n from "@/translation/i18n";
 import i18next from 'i18next';
@@ -11,10 +12,11 @@ import UserManager from '@/manager/UserManager';
 const LanguageForm = () => {
   const router = useRouter();
   const data = StaticData.languages;
+  const [currentLanguage, setCurrentLanguage] = useState('');
 
   const changeLanguage = (language: any) => {
     i18next.changeLanguage(language.value);
-    UserManager.setLanguage(language);
+    UserManager.setLanguage(language.value);
   };
 
   return (
