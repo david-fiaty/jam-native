ac2157a892e3b1df8866133e2587aa89fd2e30e5
david-fiaty <dfiaty@gmail.com>
2024-11-29 23:13:53 +0000

    Update language manager

--- components/form/LanguageForm.tsx
+++ components/form/LanguageForm.tsx
@@ -1,6 +1,5 @@
-import { useEffect } from 'react';
 import { useRouter } from 'expo-router';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setLanguage } from '@/redux/slices/AppSlice';
 import { Layout } from '@/constants/Layout';
 import i18n from "@/translation/i18n";
@@ -9,14 +8,12 @@ import BoxView from "../view/BoxView";
 import BackButton from "../button/BackButton";
 import SelectListBase from '../base/SelectListBase';
 import StaticData from '@/constants/StaticData';
+import UserManager from '@/manager/UserManager';
 
 const LanguageForm = () => {
   const dispatch = useDispatch();
-  const appState = useSelector((state: any) => state.app);
   const router = useRouter();
   const data = StaticData.languages;
-
-  console.log(appState.language);
 
   const changeLanguage = (language: any) => {
     dispatch(setLanguage(language.value));
@@ -29,7 +26,8 @@ const LanguageForm = () => {
         title={i18n.t('Language')}
         onPress={() => router.back()}
       />
-      <SelectListBase 
+      <SelectListBase
+        value={UserManager.getLanguage()}
         data={data} 
         placeholder={i18n.t('English')} 
         onChangeValue={((option: any) => changeLanguage(option))}
