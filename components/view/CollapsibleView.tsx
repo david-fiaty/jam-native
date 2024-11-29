b483be035d6c219bd441b6c514b7e16be5b785eb
david-fiaty <dfiaty@gmail.com>
2024-11-29 20:17:05 +0000

    Update CollapsibleView.tsx

--- components/view/CollapsibleView.tsx
+++ components/view/CollapsibleView.tsx
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, TouchableOpacity } from 'react-native';
 import Collapsible from 'react-native-collapsible';
-import TextView from './TextView';
 import { Layout } from '@/constants/Layout';
-import i18n from '@/translation/i18n';
 
 type Props = {
   label?: any,
-  openedLabel?:any,
+  openedLabel?: any,
   content?: any,
+  headerStyle?: any,
 };
 
-const CollapsibleView = ({label, content, openedLabel}: Props) => {
+const CollapsibleView = ({label, openedLabel, content, headerStyle}: Props) => {
   const [collapsed, setCollapsed] = useState(true);
   let buttonLabel = label;
   
@@ -22,7 +21,7 @@ const CollapsibleView = ({label, content, openedLabel}: Props) => {
   return (
     <View>
       <TouchableOpacity onPress={() => setCollapsed((prev) => !prev)}>
-        {buttonLabel}
+        <View style={headerStyle}>{buttonLabel}</View>
       </TouchableOpacity>
       <Collapsible 
         collapsed={collapsed} 
