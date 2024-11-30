b5d07d53342993f0e292b0ac33406936a7572ac0
david-fiaty <dfiaty@gmail.com>
2024-11-30 19:59:58 +0000

    Revert "Update Store.ts"
    This reverts commit eaa7c2019defd9af9d73f89f58f919ba739ac1a0.

--- redux/Store.ts
+++ redux/Store.ts
@@ -5,6 +5,7 @@ import UserReducer from './slices/UserSlice';
 import MessageReducer from './slices/MessageSlice';
 import AppReducer from './slices/AppSlice';
 import JamFormReducer from './slices/JamFormSlice';
+import ProfileFormReducer from './slices/ProfileFormSlice';
 
 const Store = configureStore({
   reducer: {
@@ -14,6 +15,7 @@ const Store = configureStore({
     message: MessageReducer,
     app: AppReducer,
     jamForm: JamFormReducer, 
+    profileForm: ProfileFormReducer,
   },
   middleware: getDefaultMiddleware =>
     process.env.NODE_ENV === 'production' ? getDefaultMiddleware() : getDefaultMiddleware({
