a1faaecfb43129ffbc16de98d47310df309ce9fa
david-fiaty <dfiaty@gmail.com>
2024-11-30 16:31:21 +0000

    Update ProfileForm.tsx

--- components/form/ProfileForm.tsx
+++ components/form/ProfileForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 import { Layout } from "@/constants/Layout";
 import { Config } from "@/constants/Config";
 import i18n from "@/translation/i18n";
@@ -18,8 +18,8 @@ import InputTextareaField from "../field/InputTextareaField";
 import UserManager from "@/manager/UserManager";
 import ScreenManager from "@/manager/ScreenManager";
 import MediaPickerBase from "../base/MediaPickerBase";
-import ImageView from '../view/ImageView';
-import IconView from '../view/IconView';
+import ImageView from "../view/ImageView";
+import IconView from "../view/IconView";
 
 const ProfileForm = () => {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
@@ -48,8 +48,8 @@ const ProfileForm = () => {
       style={Layout.screenContent}
     >
       <BackButton
-        title={i18n.t('Your profile')}
-        onPress={() => ScreenManager.toggleModal('ProfileForm')}
+        title={i18n.t("Your profile")}
+        onPress={() => ScreenManager.toggleModal("ProfileForm")}
       />
 
       <MediaPickerBase
@@ -68,7 +68,7 @@ const ProfileForm = () => {
                   radius="circle"
                 />
                 <TextView>
-                  {i18n.t('Change your Jammer user profile image.')}
+                  {i18n.t("Change your Jammer user profile image.")}
                 </TextView>
                 <IconView name="next" theme="clear" size={60} />
               </BoxView>
@@ -88,7 +88,7 @@ const ProfileForm = () => {
                   resizeMode="cover"
                 />
                 <TextView>
-                  {i18n.t('Upload your Jammer user profile image.')}
+                  {i18n.t("Upload your Jammer user profile image.")}
                 </TextView>
                 <IconView name="next" theme="clear" size={60} />
               </BoxView>
@@ -100,28 +100,28 @@ const ProfileForm = () => {
       <DividerView />
 
       <InputTextField
-        placeholder={i18n.t('Email address')}
+        placeholder={i18n.t("Email address")}
         value={profileData?.email}
-        onChangeText={(value: string) => updateField('email', value)}
+        onChangeText={(value: string) => updateField("email", value)}
       />
 
       <InputTextField
-        placeholder={i18n.t('Profile name')}
+        placeholder={i18n.t("Profile name")}
         value={profileData?.profile_name}
-        onChangeText={(value: string) => updateField('profile_name', value)}
+        onChangeText={(value: string) => updateField("profile_name", value)}
       />
 
       <InputTextField
-        placeholder={i18n.t('Phone number')}
+        placeholder={i18n.t("Phone number")}
         value={profileData?.phone_number}
-        onChangeText={(value: string) => updateField('phone_number', value)}
+        onChangeText={(value: string) => updateField("phone_number", value)}
       />
 
       <InputTextareaField
-        placeholder={i18n.t('Description')}
+        placeholder={i18n.t("Description")}
         value={profileData?.profile_description}
         onChangeText={(value: string) =>
-          updateField('profile_description', value)
+          updateField("profile_description", value)
         }
       />
 
@@ -131,25 +131,29 @@ const ProfileForm = () => {
       />
 
       <InputTextField
-        placeholder={i18n.t('Region')}
-        value={profileData?.region}
-        onChangeText={(value: string) => updateField('region', value)}
+        placeholder={i18n.t("Address")}
+        value={profileData?.address}
+        onChangeText={(value: string) => updateField("address", value)}
       />
-
 
       <InputTextField
-        placeholder={i18n.t('City')}
+        placeholder={i18n.t("City")}
         value={profileData?.town_or_locality}
-        onChangeText={(value: string) => updateField('city', value)}
+        onChangeText={(value: string) => updateField("city", value)}
       />
 
+      <InputTextField
+        placeholder={i18n.t("Region")}
+        value={profileData?.region}
+        onChangeText={(value: string) => updateField("region", value)}
+      />
 
       <CountryField value={profileData?.country} />
 
       <DividerView theme="secondary" />
       <SectorsField
-        label={<TextView>{i18n.t('Industries')}</TextView>}
-        onPressEvent={() => ScreenManager.toggleModal('SectorsList')}
+        label={<TextView>{i18n.t("Industries")}</TextView>}
+        onPressEvent={() => ScreenManager.toggleModal("SectorsList")}
       />
 
       <DividerView theme="secondary" />
