41ff95dfd2b5804109eb2d2ff5747ddfe084350c
david-fiaty <dfiaty@gmail.com>
2024-11-30 17:12:10 +0000

    Update LocationMapView.tsx

--- components/view/LocationMapView.tsx
+++ components/view/LocationMapView.tsx
@@ -28,9 +28,6 @@ const LocationMapView = ({ style, children }: BaseProps) => {
 
   if (!jamsData?.length) {
     EntityManager.getJams().then((data: any) => {
-
-        console.log(data);
-
       setJamsData(data);
       setIsLoaded(true);
     });
