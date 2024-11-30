1da2372be67d8c84a40f5b2da899bc20162359b6
david-fiaty <dfiaty@gmail.com>
2024-11-30 19:42:19 +0000

    Update LocationMapView.tsx

--- components/view/LocationMapView.tsx
+++ components/view/LocationMapView.tsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from "react";
 import { StyleSheet, View, TouchableWithoutFeedback } from "react-native";
-import RNMapView, { Marker, MapPressEvent } from 'react-native-maps';
+import RNMapView, { Marker, MapPressEvent } from "react-native-maps";
 import { BaseProps } from "@/constants/Types";
 import { Layout } from "@/constants/Layout";
 import { Colors } from "@/constants/Colors";
 import SpinnerView from "./SpinnerView";
 import DeviceManager from "@/manager/DeviceManager";
+import ScreenManager from "@/manager/ScreenManager";
 import i18n from "@/translation/i18n";
+import BackButton from "../button/BackButton";
+import BoxView from "./BoxView";
 
 const LocationMapView = ({ style, children }: BaseProps) => {
   const [selectedLocation, setSelectedLocation] = useState<any>(null);
@@ -23,7 +26,7 @@ const LocationMapView = ({ style, children }: BaseProps) => {
         setSelectedLocation({
           latitude: currentLocation?.coords?.latitude,
           longitude: currentLocation?.coords?.longitude,
-        })
+        });
       }
 
       setIsLoaded(true);
@@ -33,40 +36,56 @@ const LocationMapView = ({ style, children }: BaseProps) => {
   if (!isLoaded) return <SpinnerView />;
 
   return (
-    <TouchableWithoutFeedback>
-      <View style={[Layout.screenContent, styles.container]}>
-        <RNMapView
-          style={styles.map}
-          provider="google"
-          initialRegion={{
-            latitude: 8.6195,
-            longitude: 0.8248,
-            latitudeDelta: 3,
-            longitudeDelta: 3,
-          }}
-          onPress={onMapPress}
-        >
-          {selectedLocation && (
-            <Marker
-              pinColor={Colors.secondary}
-              title={i18n.t("Your Location")}
-              description={i18n.t("This is where you are currently")}
-              coordinate={{
-                latitude: parseFloat(selectedLocation?.latitude),
-                longitude: parseFloat(selectedLocation?.longitude),
-              }}
-            />
-          )}
-        </RNMapView>
-      </View>
-    </TouchableWithoutFeedback>
+    <BoxView 
+      direction="column" 
+      align="flex-start" 
+      justify="flex-start" 
+      style={[Layout.screenContent, styles.screenContent]}
+    >
+      <BackButton
+        title={i18n.t("Add location")}
+        onPress={() => ScreenManager.toggleModal("ProfileForm")}
+      />
+      <TouchableWithoutFeedback>
+        <View style={styles.container}>
+          <RNMapView
+            style={styles.map}
+            provider="google"
+            initialRegion={{
+              latitude: 8.6195,
+              longitude: 0.8248,
+              latitudeDelta: 3,
+              longitudeDelta: 3,
+            }}
+            onPress={onMapPress}
+          >
+            {selectedLocation && (
+              <Marker
+                pinColor={Colors.secondary}
+                title={i18n.t("Your Location")}
+                description={i18n.t("This is where you are currently")}
+                coordinate={{
+                  latitude: parseFloat(selectedLocation?.latitude),
+                  longitude: parseFloat(selectedLocation?.longitude),
+                }}
+              />
+            )}
+          </RNMapView>
+        </View>
+      </TouchableWithoutFeedback>
+    </BoxView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  screenContent: {
     padding: 0,
-    paddingTop: Layout.space.base * 2,
+    paddingTop: Layout.space.base*1.5,
+  },
+  container: {
+    width: '100%',
+    height: '100%',
+    flexGrow: 1,
   },
   map: {
     flex: 1,
