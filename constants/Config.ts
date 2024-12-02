dc0d301d70ac1fe6a6ac1ea2476710496cd8c80c
david-fiaty <dfiaty@gmail.com>
2024-12-02 14:03:36 +0000

    Update Config.ts

--- constants/Config.ts
+++ constants/Config.ts
@@ -4,6 +4,8 @@ export const Config = {
   apiEnabled: false,
   apiUrl: 'https://the.jammm.app/backend/api/v1',
   imageUrl: 'https://the.jammm.app/backend',
+  mapApiKey: 'AIzaSyB91uv-OuWw6iNMjVZmuHJsRVDHP9baCq0',
+  geocodeUrl: 'https://maps.googleapis.com/maps/api/geocode/json',
   defaultLanguage: 'en',
   fallbackLanguage: 'en',
   appNamespace: 'jam-native',
