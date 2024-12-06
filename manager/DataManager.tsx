ec916c0e88a83e54dd3d6cd870f6baad58b7f994
david-fiaty <dfiaty@gmail.com>
2024-12-06 17:10:40 +0000

    Revert "Update DataManager.tsx"
    This reverts commit c337a50dd5feed4bb6e9b74e550f083b1b4f10ba.

--- manager/DataManager.tsx
+++ manager/DataManager.tsx
@@ -42,10 +42,6 @@ class DataManager {
     return results;
   }
 
-  async get(key: keyof typeof Endpoints, idArray: any , options?: any) {
-    return await ApiManager.get(key, idArray, options);
-  }
-
   async post(key: keyof typeof Endpoints, data: any, options?: any) {
     return await ApiManager.post(key, data);
   }
