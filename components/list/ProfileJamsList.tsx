b4aeb01d66485912da51e40810d79dc5b6e949f2
david-fiaty <dfiaty@gmail.com>
2024-12-08 23:07:48 +0000

    Update ProfileJamsList.tsx

--- components/list/ProfileJamsList.tsx
+++ components/list/ProfileJamsList.tsx
@@ -14,10 +14,12 @@ import AddItemButton from "../button/AddItemButton";
 import NoImageView from "../view/NoImageView";
 import BoxView from "../view/BoxView";
 import { Colors } from "@/constants/Colors";
+import DeviceManager from "@/manager/DeviceManager";
 
 type Props = {
   idArray?: any;
 };
+
 
 const ProfileJamsList = ({ idArray }: Props) => {
   const numColumns = 3;
@@ -25,14 +27,25 @@ const ProfileJamsList = ({ idArray }: Props) => {
   const [profileJams, setProfileJams] = useState<any>([]);
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
+  const getImageSize = () => {
+    let windowWidth: any = DeviceManager.window.width;
+    let imageDim: number = (windowWidth/3) - Layout.space.base*1.7;
+
+    return {
+      width: imageDim,
+      height: imageDim,
+    };
+  };
+
   const renderItemImage = (url: any) => {
     if (!url) return <NoImageView imageSize={48} />;
 
+    let imageSize = getImageSize();
     return (
       <ImageView
         uri={Config.imageUrl + url}
-        width={96.7}
-        height={96.7}
+        width={imageSize.width}
+        height={imageSize.height}
         resizeMode="cover"
         style={[styles.image, ScreenManager.getGridCellSize(numColumns)]}
       />
@@ -96,7 +109,7 @@ const ProfileJamsList = ({ idArray }: Props) => {
         <ListView
           data={profileJams}
           numColumns={numColumns}
-          contentContainerStyle={{ gap: Layout.space.base }}
+          contentContainerStyle={{ gap: Layout.space.base, backgroundColor: 'red' }}
           columnWrapperStyle={{ gap: Layout.space.base }}
           scrollEnabled={false}
           renderItem={(row: any) => renderItem(row)}
