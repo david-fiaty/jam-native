48c75ea0474a11758756542f15842d0a05840df7
david-fiaty <dfiaty@gmail.com>
2024-12-11 19:14:59 +0000

    Update ProfileProjectsList.tsx

--- components/list/ProfileProjectsList.tsx
+++ components/list/ProfileProjectsList.tsx
@@ -26,6 +26,7 @@ const ProfileProjectsList = ({ title, idArray, addButton }: Props) => {
   const router = useRouter();
   const [profileProjects, setProfileProjects] = useState<any>([]);
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [projectImages, addProjectImage] = useState<any>([]);
 
   const renderItem = (row: any) => {
     let imageSize = MediaManager.getThumbnailSize();
@@ -83,10 +84,11 @@ const ProfileProjectsList = ({ title, idArray, addButton }: Props) => {
   }
 
   if (profileProjects?.length > 0 ) {
-    profileProjects.map((item:any) => {
-      EntityManager.getProjectImageUrl(profileProjects[1]).then((value: any) => {
+    profileProjects.map((item: any) => {
+      EntityManager.getProjectImageUrl(item).then((value: any) => {
         if (value) {
-          
+          //console.log(value);
+          //addProjectImage({...{item?.id: value}});
         }
       });
     });  
