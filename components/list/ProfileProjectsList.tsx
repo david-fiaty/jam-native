816bf3fb50505d847697cceec48c959df709c15d
david-fiaty <dfiaty@gmail.com>
2024-12-11 19:04:56 +0000

    Update ProfileProjectsList.tsx

--- components/list/ProfileProjectsList.tsx
+++ components/list/ProfileProjectsList.tsx
@@ -30,12 +30,6 @@ const ProfileProjectsList = ({ title, idArray, addButton }: Props) => {
   const renderItem = (row: any) => {
     let imageSize = MediaManager.getThumbnailSize();
     let output = <></>;
-    
-    EntityManager.getProjectImageUrl(row?.item).then((value: any) => {
-      console.log(value);
-    });
-
-    //console.log(url);
 
     if (row?.item?.id == "addItem") {
       output = <AddItemButton
@@ -85,6 +79,14 @@ const ProfileProjectsList = ({ title, idArray, addButton }: Props) => {
       //if (addButton === true) data.push({ id: "addItem" });
       setProfileProjects(data);
       setIsLoaded(true);
+    });
+  }
+
+  if (profileProjects?.length > 0 ) {
+    EntityManager.getProjectImageUrl(profileProjects[1]).then((value: any) => {
+      if (value) {
+        console.log(value);
+      }
     });
   }
 
