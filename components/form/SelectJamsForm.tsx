53700796002196ba3ef617811be7672ae9bd878d
david-fiaty <dfiaty@gmail.com>
2024-12-12 17:05:08 +0000

    Update SelectJamsForm.tsx

--- components/form/SelectJamsForm.tsx
+++ components/form/SelectJamsForm.tsx
@@ -3,21 +3,20 @@ import { useState } from "react";
 import { useRouter } from "expo-router";
 import { Layout } from "@/constants/Layout";
 import { Colors } from "@/constants/Colors";
-import TextView from "../view/TextView";
 import i18n from "@/translation/i18n";
 import ImageView from "../view/ImageView";
 import ScreenManager from "@/manager/ScreenManager";
 import ListView from "../view/ListView";
 import EntityManager from "@/manager/EntityManager";
 import SpinnerView from "../view/SpinnerView";
-import AddItemButton from "../button/AddItemButton";
 import NoImageView from "../view/NoImageView";
 import BoxView from "../view/BoxView";
 import MediaManager from "@/manager/MediaManager";
 import BackButton from "../button/BackButton";
 import UserManager from "@/manager/UserManager";
+import IconView from "../view/IconView";
 
-const SelectJamsForm = () => {
+const toggleItemsForm = () => {
   const numColumns = 3;
   const router = useRouter();
   const [profileData, setProfileData] = useState<any>([]);
@@ -28,19 +27,31 @@ const SelectJamsForm = () => {
   // Todo - Connect to profile jams
   const idArray = [20, 46, 39, 49, 18, 33, 50];
 
-  const selectJam = (row: any) => {
+  const toggleItem = (row: any) => {
     let selectedJamsList = [...selectedJams];
-    let index: number = selectedJamsList.findIndex((id: any) => id == row.item.id);
+    let index: number = selectedJamsList.findIndex(
+      (id: any) => id == row.item.id
+    );
 
     if (index === -1) selectedJamsList.push(row.item.id);
     else delete selectedJamsList[index];
 
-    setSelectedJams(selectedJamsList.filter(n => n));
+    setSelectedJams(selectedJamsList.filter((n) => n));
+  };
+
+  const deleteItem = (row: any) => {
+    console.log('delete pressed');
   };
 
   const renderItem = (row: any) => {
-    let imageSize = MediaManager.getThumbnailSize();
-    let output = null;
+    let imageSize: any = MediaManager.getThumbnailSize();
+    let output: any = null;
+    let isSelected: boolean = true;
+
+    let imageStyle = {
+      ...styles.selectedItem,
+      ...(isSelected ? styles.selectedItem : {}),
+    };
 
     if (!row?.item?.medias?.[0]?.url) {
       output = (
@@ -58,18 +69,28 @@ const SelectJamsForm = () => {
             width={imageSize.width}
             height={imageSize.height}
             resizeMode="cover"
-            style={[styles.image, ScreenManager.getGridCellSize(numColumns)]}
+            style={[
+              styles.image,
+              ScreenManager.getGridCellSize(numColumns),
+              imageStyle,
+            ]}
           />
+
+          {isSelected && (
+            <TouchableOpacity
+              style={styles.deleteItem}
+              onPress={() => deleteItem(row)}
+            >
+              <IconView name="delete" theme="primary" size={8} />
+            </TouchableOpacity>
+          )}
         </View>
       );
     }
 
     if (parseInt(row?.item?.id) > 0) {
       output = (
-        <TouchableOpacity
-          key={row.item.id}
-          onPress={() => selectJam(row)}
-        >
+        <TouchableOpacity key={row.item.id} onPress={() => toggleItem(row)}>
           {output}
         </TouchableOpacity>
       );
@@ -92,9 +113,6 @@ const SelectJamsForm = () => {
   }
 
   if (!isLoaded) return <SpinnerView />;
-
-
-  console.log(selectedJams);
 
   return (
     <BoxView
@@ -131,17 +149,23 @@ const styles = StyleSheet.create({
     marginBottom: Layout.space.base,
     flex: 1,
   },
-  link: {
-    borderBottomWidth: 1,
-    borderBottomColor: Colors.primary,
-  },
   item: {
     flexDirection: "column",
     gap: Layout.space.small,
+  },
+  selectedItem: {
+    opacity: 0.7,
+    borderWidth: 1,
+    borderColor: Colors.primary,
+  },
+  deleteItem: {
+    position: "absolute",
+    top: 5,
+    right: 5,
   },
   image: {
     borderRadius: Layout.space.base,
   },
 });
 
-export default SelectJamsForm;
+export default toggleItemsForm;
