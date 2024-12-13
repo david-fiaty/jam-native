90dc73ca55b4f9f59b214a8eb865cdb4979da9cd
david-fiaty <dfiaty@gmail.com>
2024-12-13 15:07:21 +0000

    Update ProjectJamsList.tsx

--- components/list/ProjectJamsList.tsx
+++ components/list/ProjectJamsList.tsx
@@ -16,66 +16,93 @@ import BoxView from "../view/BoxView";
 import MediaManager from "@/manager/MediaManager";
 
 type Props = {
-  title?: any,
-  idArray?: any,
-  addButton?: boolean,
-  allButton?: boolean,
-  onAddButtonPress?: () => void,
+  title?: any;
+  idArray?: any;
+  addButton?: boolean;
+  allButton?: boolean;
+  onAddButtonPress?: () => void;
 };
 
-const ProjectJamsList = ({ title, idArray, addButton, allButton, onAddButtonPress }: Props) => {
+const ProjectJamsList = ({
+  title,
+  idArray,
+  addButton,
+  allButton,
+  onAddButtonPress,
+}: Props) => {
   const numColumns = 3;
   const router = useRouter();
   const [profileJams, setProfileJams] = useState<any>([]);
+  const [selectedJams, setSelectedJams] = useState<any>([]);
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+  const findItemIndex = (row: any) =>
+    selectedJams.findIndex((id: any) => id == row.item.id);
+
+  const toggleItem = (row: any) => {
+    let selectedJamsList: any = [...selectedJams];
+    let index: number = findItemIndex(row);
+
+    if (index === -1) selectedJamsList.push(row.item.id);
+    else delete selectedJamsList[index];
+
+    selectedJamsList = selectedJamsList.filter((n: any) => n);
+    setSelectedJams(selectedJamsList);
+  };
+
+  const deleteItem = (row: any) => {
+    let selectedJamsList = [...selectedJams];
+    let index: number = findItemIndex(row);
+    if (index !== -1) delete selectedJamsList[index];
+
+    setSelectedJams(selectedJamsList.filter((n) => n));
+  };
 
   const renderItem = (row: any) => {
     let imageSize = MediaManager.getThumbnailSize();
     let output = null;
 
     if (row?.item?.id == "addItem") {
-      output = <AddItemButton
-        label={i18n.t('Add')}
-        width={imageSize.width}
-        height={imageSize.height}
-        onPress={onAddButtonPress}
-      />;
-    }
-    else if (!row?.item?.medias?.[0]?.url) {
-      output = <NoImageView 
-        width={imageSize.width} 
-        height={imageSize.height} 
-        rounded={true}
-      />;
-    }
-    else {
-      output = <View style={styles.item}>
-        <ImageView
-          uri={MediaManager.getImageUrl(row.item.medias[0].url)}
+      output = (
+        <AddItemButton
+          label={i18n.t("Add")}
           width={imageSize.width}
           height={imageSize.height}
-          resizeMode="cover"
-          style={[styles.image, ScreenManager.getGridCellSize(numColumns)]}
+          onPress={onAddButtonPress}
         />
-      </View>
+      );
+    } else if (!row?.item?.medias?.[0]?.url) {
+      output = (
+        <NoImageView
+          width={imageSize.width}
+          height={imageSize.height}
+          rounded={true}
+        />
+      );
+    } else {
+      output = (
+        <View style={styles.item}>
+          <ImageView
+            uri={MediaManager.getImageUrl(row.item.medias[0].url)}
+            width={imageSize.width}
+            height={imageSize.height}
+            resizeMode="cover"
+            style={[styles.image, ScreenManager.getGridCellSize(numColumns)]}
+          />
+        </View>
+      );
     }
 
     if (parseInt(row?.item?.id) > 0) {
-      output = <TouchableOpacity
-        key={row.item.id}
-        onPress={() =>
-          router.push({
-            pathname: "/jam",
-            params: { idArray: [row.item.id], title: title },
-          })
-        }
-      >
-        {output}
-      </TouchableOpacity>
+      output = (
+        <TouchableOpacity key={row.item.id} onPress={() => toggleItem(row)}>
+          {output}
+        </TouchableOpacity>
+      );
     }
 
     return output;
-  }
+  };
 
   if (!profileJams?.length && idArray?.length) {
     EntityManager.getJams({ items_ids: idArray }).then((data: any) => {
@@ -92,16 +119,18 @@ const ProjectJamsList = ({ title, idArray, addButton, allButton, onAddButtonPres
       <BoxView direction="row" align="center" justify="space-between">
         <TextView style={styles.title}>{title}</TextView>
 
-        { allButton && <TouchableOpacity
-          onPress={() =>
-            router.push({
-              pathname: "/jam",
-              params: { idArray: idArray, title: title },
-            })
-          }
-        >
-          <TextView style={styles.link}>{i18n.t("View all")}</TextView>
-        </TouchableOpacity> }
+        {allButton && (
+          <TouchableOpacity
+            onPress={() =>
+              router.push({
+                pathname: "/jam",
+                params: { idArray: idArray, title: title },
+              })
+            }
+          >
+            <TextView style={styles.link}>{i18n.t("View all")}</TextView>
+          </TouchableOpacity>
+        )}
       </BoxView>
 
       {profileJams?.length > 0 && (
