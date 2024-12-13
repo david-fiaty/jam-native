0caa0d5ec4c5464071eac9e53d045558f96f011c
david-fiaty <dfiaty@gmail.com>
2024-12-13 16:50:23 +0000

    Update SearchView.tsx

--- components/view/SearchView.tsx
+++ components/view/SearchView.tsx
@@ -1,39 +1,51 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { View, TouchableOpacity } from "react-native";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import { setSearchFilter } from "@/redux/slices/SearchSlice";
 import { Layout } from "@/constants/Layout";
 import { Colors } from "@/constants/Colors";
 import ImageView from "./ImageView";
 import BoxView from "./BoxView";
 import TextView from "./TextView";
-import ScreenManager from '@/manager/ScreenManager';
+import ScreenManager from "@/manager/ScreenManager";
 import i18n from "@/translation/i18n";
 import ListView from "./ListView";
 import DataManager from "@/manager/DataManager";
-import SpinnerView from './SpinnerView';
-import MediaManager from '@/manager/MediaManager';
-import StaticData from '@/constants/StaticData';
+import SpinnerView from "./SpinnerView";
+import MediaManager from "@/manager/MediaManager";
+import StaticData from "@/constants/StaticData";
+import EntityManager from "@/manager/EntityManager";
 
 const SearchView = () => {
   const dispatch = useDispatch();
   const searchState = useSelector((state: any) => state.search);
-  const [data, setData] = useState([]);
+  const [jamsData, setJamsData] = useState([]);
   const numColumns = 3;
 
   const renderTab = (row: any) => (
     <TouchableOpacity onPress={() => dispatch(setSearchFilter(row.item.id))}>
       <View style={styles.tabItem}>
-        <TextView style={searchState.filter == row.item.id ? {fontWeight: 'bold'} : {}}>
+        <TextView
+          style={
+            searchState.filter == row.item.id ? { fontWeight: "bold" } : {}
+          }
+        >
           {row.item.label}
         </TextView>
       </View>
     </TouchableOpacity>
   );
 
-  if (!data) return <SpinnerView />
+  if (!jamsData?.length) {
+    EntityManager.listJams().then((data: any) => {
+      setJamsData(data);
 
-  console.log('search');
+      console.log(jamsData?.length);
+    });
+  }
+
+
+  console.log("search");
 
   return (
     <BoxView
@@ -52,6 +64,15 @@ const SearchView = () => {
       />
 
       {/* Results */}
+      <ListView
+        data={jamsData}
+        numColumns={numColumns}
+        contentContainerStyle={{ gap: Layout.space.base }}
+        columnWrapperStyle={{ gap: Layout.space.base }}
+        scrollEnabled={false}
+        renderItem={(row: any) => console.log(row)}
+      />
+
       {/*
       <ListView
         data={data} 
@@ -76,7 +97,6 @@ const SearchView = () => {
       />
       */}
     </BoxView>
-    
   );
 };
 
@@ -90,7 +110,7 @@ const styles = {
     borderBottomColor: Colors.primary,
   },
   item: {
-    flexDirection: 'column',
+    flexDirection: "column",
     gap: Layout.space.small,
   },
   image: {
