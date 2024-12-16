d10651526f9a7a2be7bf57facabf43f24ff8a386
david-fiaty <dfiaty@gmail.com>
2024-12-16 18:36:02 +0000

    Update ProfileProjectsList.tsx

--- components/list/ProfileProjectsList.tsx
+++ components/list/ProfileProjectsList.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, View, TouchableOpacity } from "react-native";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "expo-router";
 import { Layout } from "@/constants/Layout";
 import { Colors } from "@/constants/Colors";
@@ -79,23 +79,25 @@ const ProfileProjectsList = ({ title, idArray, addButton, allButton, onAddButton
     return output;
   }
 
-  if (!profileProjects?.length && idArray?.length) {
-    EntityManager.getProjects({ items_ids: idArray }).then((data: any) => {
-      if (addButton === true) data.push({ id: "addItem" });
-      setProfileProjects(data);
-      setIsLoaded(true);
-    });
-  }
+  useEffect(() => {
+    if (!profileProjects?.length && idArray?.length) {
+      EntityManager.getProjects({ items_ids: idArray }).then((data: any) => {
+        if (addButton === true) data.push({ id: "addItem" });
+        setProfileProjects(data);
+        setIsLoaded(true);
+      });
+    }
 
-  if (profileProjects?.length > 0 ) {
-    profileProjects.map((item: any) => {
-      if (!Object.keys(projectImages).includes(item.id)) {
-        EntityManager.getProjectImageUrl(item).then((value: any) => {
-          if (value) addProjectImage({ ...projectImages, ...{[item.id]: value} });
-        });
-      }
-    });  
-  }
+    if (profileProjects?.length > 0 ) {
+      profileProjects.map((item: any) => {
+        if (!Object.keys(projectImages).includes(item.id)) {
+          EntityManager.getProjectImageUrl(item).then((value: any) => {
+            if (value) addProjectImage({ ...projectImages, ...{[item.id]: value} });
+          });
+        }
+      });  
+    }
+  });
 
   if (!isLoaded) return <SpinnerView />;
 
