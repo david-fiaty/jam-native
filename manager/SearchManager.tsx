0c5666277bc83bfbdd706dc8d9fcfe7751667c8a
david-fiaty <dfiaty@gmail.com>
2024-12-16 13:25:51 +0000

    Update SearchManager.tsx

--- manager/SearchManager.tsx
+++ manager/SearchManager.tsx
@@ -3,19 +3,42 @@ import EntityManager from "./EntityManager";
 class SearchManager {
 
   async loadData() {
-    const [jams, profiles, projects] = await Promise.all([
-      EntityManager.listJams(), 
-      EntityManager.listProfiles(),
-      EntityManager.listProjects(),
-    ]);
+    const [jams, profiles, projects] = await this.sendRequest();
     
+    return this.buildResponse({
+      jams: jams, 
+      profiles: profiles, 
+      projects: projects
+    });
+  }
+
+  async searchData(searchValue: string) {
+    const options = searchValue?.length ? { query_text: searchValue } : {};
+    const [jams, profiles, projects] = await this.sendRequest(options);
+
+    return this.buildResponse({
+      jams: jams, 
+      profiles: profiles, 
+      projects: projects
+    });
+  }
+
+  async sendRequest(options?: any) {
+    return await Promise.all([
+      EntityManager.listJams(options), 
+      EntityManager.listProfiles(options),
+      EntityManager.listProjects(options),
+    ]);
+  }
+
+  buildResponse(data?: any) {
     return {
-      jam: jams,
-      jammer: profiles,
-      venue: profiles,
-      project: projects,
-      call: profiles.filter((o: any) => o?.type == 'call'),
-      event: profiles.filter((o: any) => o?.type == 'event'),
+      jam: data.jams,
+      jammer: data.profiles,
+      venue: data.profiles,
+      project: data.projects,
+      call: data.profiles.filter((o: any) => o?.type == 'call'),
+      event: data.profiles.filter((o: any) => o?.type == 'event'),
     };
   }
 };
