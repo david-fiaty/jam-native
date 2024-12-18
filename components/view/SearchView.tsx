70149a31016cf828b76cb27b58ca79452ae0a41e
david-fiaty <dfiaty@gmail.com>
2024-12-18 15:04:43 +0000

    Update SearchView.tsx

--- components/view/SearchView.tsx
+++ components/view/SearchView.tsx
@@ -61,7 +61,7 @@ const SearchView = () => {
       align="flex-start"
       justify="space-between"
       scroll={true}
-      style={[Layout.screenContent, styles.container]}
+      style={Layout.screenContent}
     >
       {/* Search filters */}
       <ListView
@@ -106,16 +106,11 @@ const SearchView = () => {
 };
 
 const styles = {
-  container: {
-    width: '100%',
-  },
   tabContainer: {
     backgroundColor: Colors.white,
-    width: '100%',
   },
   tabItem: {
-    paddingVertical: Layout.space.base,
-    paddingHorizontal: Layout.space.base,
+    padding: Layout.space.base,
     borderBottomWidth: 1,
     borderBottomColor: Colors.primary,
   },
