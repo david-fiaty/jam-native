e9bf0307d9ed6a4201f5be3a00a62f6d01c0e5a1
david-fiaty <dfiaty@gmail.com>
2024-12-18 16:36:43 +0000

    Update ProfileProjectsList.tsx

--- components/list/ProfileProjectsList.tsx
+++ components/list/ProfileProjectsList.tsx
@@ -96,21 +96,24 @@ const ProfileProjectsList = ({
       EntityManager.getProjects({ items_ids: idArray }).then((data: any) => {
         if (addButton === true) data.push({ id: "addItem" });
         
+        let projectImagesList: any = {};
         if (data?.length > 0) {
           data.map((item: any) => {
             EntityManager.getProjectImageUrl(item).then((value: any) => {
-              if (value && !projectImages?.[item?.id])
-                setProjectImages({ ...projectImages, ...{ [item?.id]: value } });
+              if (value && !projectImages?.[item?.id]) {
+                projectImagesList[item.id] = value;
+              }
             });
           });
         }
 
+        setProjectImages(projectImagesList);
         setProfileProjects(data);
 
         setIsLoaded(true);
       });
     }
-  }, [profileProjects, projectImages, idArray, addButton]);
+  });
 
   if (!isLoaded) return <SpinnerView />;
 
