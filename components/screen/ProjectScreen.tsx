ab4c878a83e93cd46b2c84e4bdb550433af3965e
david-fiaty <dfiaty@gmail.com>
2024-12-19 13:02:32 +0000

    Update ProjectScreen.tsx

--- components/screen/ProjectScreen.tsx
+++ components/screen/ProjectScreen.tsx
@@ -4,7 +4,8 @@ import { Layout } from '@/constants/Layout';
 import { BaseProps } from '@/constants/Types';
 import BoxView from "../view/BoxView";
 import BackButton from "../button/BackButton";
-import TextView from "../view/TextView";
+import ProjectsList from "../list/ProjectsList";
+import i18n from "@/translation/i18n";
 
 type Props = BaseProps & {
   idArray?: any,
@@ -13,15 +14,16 @@ type Props = BaseProps & {
 
 const ProjectScreen = ({ idArray, title }: Props) => {
   const router = useRouter();
+  const screenTitle = title?.length ? title : i18n.t('No title available');
 
   return (
     <BoxView align="flex-start" justify="flex-start" scroll={false} style={[Layout.screenContent, styles.container]}>
       <BackButton
-        title={title}
+        title={screenTitle}
         onPress={() => router.back()}
       />
       <BoxView style={Layout.mainContent}>
-        <TextView>PROJECTS LIST</TextView>
+        <ProjectsList idArray={idArray} showSpinner={true} />
       </BoxView>
     </BoxView>
   );
