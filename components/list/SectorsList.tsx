95be1f98cc4fee188d1caf677cfdcac203f06046
david-fiaty <dfiaty@gmail.com>
2025-01-05 18:54:45 +0000

    Update SectorsList.tsx

--- components/list/SectorsList.tsx
+++ components/list/SectorsList.tsx
@@ -19,10 +19,7 @@ const SectorsList = () => {
   const [sectorsData, setSectorsData] = useState<any>(null);
   const [selectedIds, setSelectedIds] = useState<any>([]);
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
-  const activeScreen: any = ScreenManager.getActiveScreen();
-  const stateData = useSelector((state: any) => state[activeScreen.params.reducer]);
-  
-  console.log(stateData);
+  const formData = useSelector((state: any) => state[ScreenManager.getActiveScreen().params.reducer]);
   
   const updateSelection = (item: any, subItem: any) => {
     let selection: any[] = [...selectedIds];
