ecdd869930f5a2d57f010834b91394bb54593bc7
david-fiaty <dfiaty@gmail.com>
2025-01-06 18:20:25 +0000

    Update LocationMapView.tsx

--- components/view/LocationMapView.tsx
+++ components/view/LocationMapView.tsx
@@ -42,16 +42,36 @@ const LocationMapView = ({ style, children }: BaseProps) => {
   useEffect(() => {
     (async () => {
         let deviceLocation: any = await DeviceManager.getLocation();
-        let coords: any = selectedLocation || {
+        let coords: any = {
           latitude: deviceLocation?.coords?.latitude,
           longitude: deviceLocation?.coords?.longitude,
         };
         
+        if (selectedLocation) {
+          coords = selectedLocation;
+        }
+        else if (activeScreen?.params?.fields) {
+          coords = {
+            latitude: activeScreen.params.fields['geolocation_latitude'].value,
+            longitude: activeScreen.params.fields['geolocation_longitude'].value,
+          };
+        }
+      
         setCurrentLocation(coords);
         setSelectedLocation(coords);
+
+        dispatch(setFormData<any>({ 
+          resource: resource,
+          key: null, 
+          value: {
+            [fieldNames.latitude]: coords.latitude,
+            [fieldNames.longitude]: coords.longitude,
+          }, 
+        }));
+
         setIsLoaded(true);
     })();
-  }, []);
+  }, [selectedLocation, fieldNames, activeScreen]);
 
   if (!isLoaded) return <SpinnerView />;
   
