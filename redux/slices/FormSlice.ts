b334a50e163b72dbf51ae79147f2ca2556b5bd5b
david-fiaty <dfiaty@gmail.com>
2025-01-06 13:40:51 +0000

    Update FormSlice.ts

--- redux/slices/FormSlice.ts
+++ redux/slices/FormSlice.ts
@@ -2,7 +2,12 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const FormSlice = createSlice({
   name: 'form',
-  initialState: {},
+  initialState: {
+    jam: {},
+    profile: {},
+    project: {},
+    signup: {},
+  },
   reducers: {
     setFormData: (state: any, action: any) => {
       state[action.payload.resource][action.payload.key] = action.payload.value;
