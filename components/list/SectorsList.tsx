c2c49c3e48ff8d511514c411dc0bca9486767397
david-fiaty <dfiaty@gmail.com>
2025-01-07 16:05:35 +0000

    Update SectorsList.tsx

--- components/list/SectorsList.tsx
+++ components/list/SectorsList.tsx
@@ -118,7 +118,7 @@ const SectorsList = () => {
     >
       <BackButton
         title={i18n.t('Add industries')}
-        onPress={() => ScreenManager.toggleScreen('AddJamForm')}
+        onPress={() => ScreenManager.toggleScreen('SectorsList')}
       />
 
       <View style={styles.container}>
