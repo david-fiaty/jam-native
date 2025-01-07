23d4bf3eb2a15ffbb22608d14f3f007998809a53
david-fiaty <dfiaty@gmail.com>
2025-01-07 21:07:58 +0000

    Update AddProjectForm.tsx

--- components/form/AddProjectForm.tsx
+++ components/form/AddProjectForm.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { StyleSheet, View } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
-import { setProjectData } from "@/redux/slices/ProjectFormSlice";
+import { setFormData } from "@/redux/slices/FormSlice";
 import { Layout } from "@/constants/Layout";
 import i18n from "@/translation/i18n";
 import BoxView from "../view/BoxView";
@@ -18,21 +18,25 @@ import AddItemButton from "../button/AddItemButton";
 import ProjectJamsList from "../list/ProjectJamsList";
 
 const AddProjectForm = () => {
+  const resource: string = 'project';
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [isProcessing, setIsProcessing] = useState<boolean>(false);
   const [profileId, setProfileId] = useState<number>(0);
-  const projectData = useSelector((state: any) => state.projectForm);
+  const formData = useSelector((state: any) => state.form[resource]);
 
   const updateField = (key: string, value: any) => {
-    dispatch(
-      setProjectData<any>({ key: key, value: value, profile_id: profileId })
-    );
+    dispatch(setFormData<any>({ 
+      resource: resource,
+      key: key, 
+      value: value, 
+      profile_id: profileId,
+    }));
   };
 
   // Todo - Fix delete project jam
   const deleteJam = (row: any) => {
-    let selectedJams: any = [...projectData.jams];
+    let selectedJams: any = [...formData.jams];
     let index: number = selectedJams.findIndex((id: number) => id == row.item.id);
     if (index !== -1) delete selectedJams[index]; 
     selectedJams = selectedJams.filter((n: any) => n);
@@ -41,7 +45,7 @@ const AddProjectForm = () => {
 
   const submitForm = async () => {
     /*
-    EntityManager.addProject(projectData).then((success: boolean) => {
+    EntityManager.addProject(formData).then((success: boolean) => {
       setIsProcessing(false);
       //success === true
       false
@@ -78,19 +82,19 @@ const AddProjectForm = () => {
       <View style={Layout.formContainer}>
         <InputTextField
           placeholder={i18n.t("Name")}
-          value={projectData?.title}
+          value={formData?.title}
           onChangeText={(value: string) => updateField("name", value)}
         />
 
         <InputTextareaField
           placeholder={i18n.t("Description")}
-          value={projectData?.caption}
+          value={formData?.caption}
           onChangeText={(value: string) => updateField("description", value)}
         />
 
         <DividerView />
 
-        {!projectData?.jams?.length && (
+        {!formData?.jams?.length && (
           <BoxView direction="column" align="center" justify="center">
             <AddItemButton
               label={i18n.t("Add Jams")}
@@ -99,11 +103,11 @@ const AddProjectForm = () => {
           </BoxView>
         )}
 
-        {projectData?.jams?.length && (
+        {formData?.jams?.length && (
           <BoxView direction="column" align="flex-start" justify="flex-start">
             <TextView style={styles.title}>{i18n.t("Selected Jams")}</TextView>
             <ProjectJamsList 
-              idArray={projectData.jams} 
+              idArray={formData.jams} 
               addButton={true} 
               onAddEvent={() => ScreenManager.toggleScreen("SelectJamsForm")}
               onDeleteEvent={(row) => deleteJam(row)}
