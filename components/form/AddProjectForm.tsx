f96aa1329c44b32dbb8760ba4737fe7648f2dc5d
david-fiaty <dfiaty@gmail.com>
2025-01-08 16:58:00 +0000

    Update AddProjectForm.tsx

--- components/form/AddProjectForm.tsx
+++ components/form/AddProjectForm.tsx
@@ -119,7 +119,7 @@ const AddProjectForm = () => {
           <BoxView direction="column" align="flex-start" justify="flex-start">
             <TextView style={styles.title}>{i18n.t("Selected Jams")}</TextView>
             <ProjectJamsList 
-              idArray={formData?.jams_ids}
+              selectedIds={formData?.jams_ids}
               addButton={true} 
               onAddEvent={() => ScreenManager.toggleScreen("SelectJamsForm", {
                 resource: resource,
