861f93f617662ea6bfe9b44dd9af4342b6a5d87b
david-fiaty <dfiaty@gmail.com>
2025-01-08 16:50:13 +0000

    Update SelectJamsForm.tsx

--- components/form/SelectJamsForm.tsx
+++ components/form/SelectJamsForm.tsx
@@ -43,8 +43,6 @@ const updateSelectionsForm = () => {
     selectedJamsList = selectedJamsList.filter((n: any) => n);
     setSelectedJams(selectedJamsList);
 
-    console.log(resource);
-
     dispatch(setFormData<any>({ 
       resource: resource,
       key: 'jams_ids', 
