d14c3101b228537baa45de4dff9863e25d515caa
david-fiaty <dfiaty@gmail.com>
2025-01-09 16:38:56 +0000

    Update AddProjectForm.tsx

--- components/form/AddProjectForm.tsx
+++ components/form/AddProjectForm.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { StyleSheet, View } from "react-native";
+import { View } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { setFormData } from "@/redux/slices/FormSlice";
 import { Layout } from "@/constants/Layout";
