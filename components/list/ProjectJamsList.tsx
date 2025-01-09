ad06d0a3ba77b4bb5e448d024f53f7aa4c777a56
david-fiaty <dfiaty@gmail.com>
2025-01-09 14:52:22 +0000

    Update ProjectJamsList.tsx

--- components/list/ProjectJamsList.tsx
+++ components/list/ProjectJamsList.tsx
@@ -43,8 +43,9 @@ const ProjectJamsList = ({
 
   // const profileJams: any = activeScreen.params?.profileJams;
 
-  const findItemIndex = (row: any) =>
-    selectedJams.findIndex((id: any) => id == row.item.id);
+  const findItemIndex = (row: any) => {
+    return selectedJams.findIndex((id: any) => id == row.item.id);
+  };
 
   const toggleItem = (row: any) => {
     let selectedJamsList: any = [...selectedJams];
@@ -151,7 +152,7 @@ const ProjectJamsList = ({
 
   if (!isLoaded) return <SpinnerView />;
 
-   console.log('-->', resource, activeScreen?.params);
+   //console.log('-->', resource, activeScreen?.params);
 
   return (
     <View style={styles.container}>
