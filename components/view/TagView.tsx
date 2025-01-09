369e6cddc7c4745b7b02e543a2f994d41f664d0d
david-fiaty <dfiaty@gmail.com>
2025-01-09 17:58:22 +0000

    Create TagView.tsx

---
+++ components/view/TagView.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text } from 'react-native';
+import { BaseProps } from '@/constants/Types';
+import { Colors } from '@/constants/Colors';
+import { Layout } from '@/constants/Layout';
+
+const TagView = ({style, children}: BaseProps) => {
+  return (
+    <Text style={[styles.content, style]}>
+      {children}
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  content: {
+    color: Colors.primary,
+    fontSize: Layout.fontSize.base,
+    backgroundColor: Colors.secondary,
+  },
+});
+
+export default TagView;
