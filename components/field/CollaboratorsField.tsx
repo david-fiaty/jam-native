16860059b8b4d5e008adeb5ed57be143787c6a13
david-fiaty <dfiaty@gmail.com>
2025-01-09 19:02:53 +0000

    Update CollaboratorsField.tsx

--- components/field/CollaboratorsField.tsx
+++ components/field/CollaboratorsField.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { BaseProps } from "@/constants/Types";
-import { Colors } from '@/constants/Colors';
 import { Layout } from '@/constants/Layout';
 import i18n from "@/translation/i18n";
 import BoxView from "../view/BoxView";
@@ -9,6 +8,7 @@ import TextView from "../view/TextView";
 import IconView from "../view/IconView";
 import SpinnerView from '../view/SpinnerView';
 import TagView from '../view/TagView';
+import EntityManager from '@/manager/EntityManager';
 
 type Props = BaseProps & {
   selectedIds?: any;
@@ -16,17 +16,21 @@ type Props = BaseProps & {
 };
 
 const CollaboratorsField = ({ selectedIds, onPressEvent }: Props) => {
-const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [selectedProfiles, setSelectedProfiles] = useState<any>([]);
 
   useEffect(() => {
     (async () => {
       if (!isLoaded) { 
+        if (selectedIds?.length) setSelectedProfiles(await EntityManager.getProfiles({items_ids: selectedIds}));
         setIsLoaded(true);
       }
     })();
-  }, [isLoaded]);
+  }, [isLoaded, selectedIds]);
 
   if (!isLoaded) return <SpinnerView size="small" />;
+
+  console.log(selectedProfiles);
 
   return (
     <View style={styles.container}>
@@ -40,20 +44,24 @@ const [isLoaded, setIsLoaded] = useState<boolean>(false);
       </BoxView>
 
       <View style={styles.preview}>
-        <TagView
-          onDeleteButtonPress={() => console.log('delete tag')}  
-        >
-          Some tag
-        </TagView>
+        { selectedProfiles?.length > 0 && (
+          selectedProfiles.map((item: any) => {
+            return (
+              <TagView
+                key={item.id}
+                onDeleteButtonPress={() => console.log('delete tag', item.id)}  
+              >
+                {item?.profile_name}
+              </TagView>
+            );
+          })
+        )}
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-
-  },
   preview: {
     paddingTop: Layout.space.base,
   },
