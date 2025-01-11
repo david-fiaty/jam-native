fe79faa61399a1ab92a75406c3802fb80c7c80a6
david-fiaty <dfiaty@gmail.com>
2025-01-11 18:29:40 +0000

    Update Config.ts

--- constants/Config.ts
+++ constants/Config.ts
@@ -13,6 +13,7 @@ export const Config = {
   paginationSize: 10,
   dateFormat: 'MMM Do YYYY',
   messageDisplayDuration: 3500,
+  showApiErrors: true,
   defaultLocation: {
     latitude: 8.6195,
     longitude: 0.8248,
