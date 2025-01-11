72e08892e2c7c0e1a567d4da651d5a7ad66714cf
david-fiaty <dfiaty@gmail.com>
2025-01-11 21:35:14 +0000

    Update Config.ts

--- constants/Config.ts
+++ constants/Config.ts
@@ -19,7 +19,7 @@ export const Config = {
     longitude: 0.8248,
   },
   forceLogin: {
-    enabled: true,
+    enabled: false,
     credentials: {
       email: 'mitsiomotu@yopmail.com',
       password: 'Password1234',
