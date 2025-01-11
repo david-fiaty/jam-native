710de71f9a1318d5e575dd519e30cd0a9eb9aa97
david-fiaty <dfiaty@gmail.com>
2025-01-11 16:05:50 +0000

    Update AddJamForm.tsx

--- components/form/AddJamForm.tsx
+++ components/form/AddJamForm.tsx
@@ -205,8 +205,6 @@ const AddJamForm = () => {
           const sectorsIds = [...formData?.sectors_ids || []];
           const index = sectorsIds.findIndex((v) => v === item.id);
           if (index !== -1) sectorsIds.splice(index, 1);
-
-          console.log(sectorsIds);
           updateField('sectors_ids', sectorsIds.filter(Boolean));
         }}
       />
