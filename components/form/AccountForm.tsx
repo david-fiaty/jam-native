7f1a3049e6b70ec97c86c30b39c99da982014ff5
david-fiaty <dfiaty@gmail.com>
2025-01-12 14:14:21 +0000

    Update AccountForm.tsx

--- components/form/AccountForm.tsx
+++ components/form/AccountForm.tsx
@@ -12,6 +12,7 @@ import ButtonView from '../view/ButtonView';
 import DividerView from '../view/DividerView';
 import UserManager from '@/manager/UserManager';
 import DataManager from '@/manager/DataManager';
+import FormManager from '@/manager/FormManager';
 
 const AccountForm = () => {
   const resource: string = 'account';
@@ -43,12 +44,7 @@ const AccountForm = () => {
         let userData: any = DataManager.extract(['username', 'email', 'phone'], await UserManager.getUserData());
 
         setProfileId(profileId);
-        dispatch(setFormData<any>({ 
-          resource: resource,
-          key: null, 
-          value: userData, 
-        }));
-        
+        FormManager.setData(resource, null, userData);    
         setIsLoaded(true);
       } 
     })();
