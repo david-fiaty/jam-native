74cc014243704f54e401e074139e8c2d99243cdf
david-fiaty <dfiaty@gmail.com>
2025-01-12 19:45:56 +0000

    Update ProfileForm.tsx

--- components/form/ProfileForm.tsx
+++ components/form/ProfileForm.tsx
@@ -228,6 +228,11 @@ const ProfileForm = () => {
               title={i18n.t("Saved Projects")} 
               allButton={formData?.saved_projects?.length > 0}
               idArray={formData?.saved_projects}
+              onAddButtonPress={() => ScreenManager.toggleScreen("AddProjectForm", {
+                profileId: profileId,
+                profileJams: [18, 20, 32, 33, 34],
+                //profileJams: formData?.profile_jams, // Todo - Enable this
+              })}
             />
           </>
         )}
