afcbce63bf5f2f1d34ea98e02b4e37b5fb50c6f0
david-fiaty <dfiaty@gmail.com>
2025-01-12 17:32:22 +0000

    Update CountriesList.tsx

--- components/list/CountriesList.tsx
+++ components/list/CountriesList.tsx
@@ -26,8 +26,6 @@ const CountriesList = () => {
 
   const updateSelection = (item: any) => {
     let selection: any[] = [...formData?.[fieldName] || []];
-
-    console.log(selection);
     let index: number = selection.findIndex((v: any) => v == item.code);
 
     if (index === -1) selection.push(item.code)
