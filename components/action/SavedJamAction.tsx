2f1b57132bd6fff417b0c3a52540cb75a9884fb5
david-fiaty <dfiaty@gmail.com>
2025-01-13 15:00:59 +0000

    Update SavedJamAction.tsx

--- components/action/SavedJamAction.tsx
+++ components/action/SavedJamAction.tsx
@@ -13,6 +13,7 @@ import DataManager from '@/manager/DataManager';
 const SavedJamAction = () => {
   const [entity, setEntity] = useState<any>(null);
   const [isSaved, setIsSaved] = useState<boolean>(false);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const entityId = ScreenManager.getScreenEntityId();
 
   const actions: any = [
@@ -36,12 +37,16 @@ const SavedJamAction = () => {
 
   useEffect(() => {
     (async () => {
-      if (!entity) setEntity(await EntityManager.getJams({items_ids: [entityId]}));
-      if (!isSaved) setIsSaved(await EntityManager.saveJam(entityId));
-    })();
-  });
+      if (!isLoaded) {
+        setEntity(await EntityManager.getJams({items_ids: [entityId]}));
+        setIsSaved(await EntityManager.saveJam(entityId));
+      }
 
-  if (!entity) return <SpinnerView />;
+      setIsLoaded(true);
+    })();
+  }, [isLoaded, entityId]);
+
+  if (!isLoaded) return <SpinnerView />;
 
   return (
     <BoxView align="flex-start" justify="flex-start" style={Layout.screenContent}>
