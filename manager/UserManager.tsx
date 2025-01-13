97386027b0f7368822235caa71d936f8b28a98e6
david-fiaty <dfiaty@gmail.com>
2025-01-13 18:17:41 +0000

    Update UserManager.tsx

--- manager/UserManager.tsx
+++ manager/UserManager.tsx
@@ -44,11 +44,10 @@ class UserManager {
     let profileId: number = await this.getProfileId();
     let defaults = {};
     let profileData = [];
+    let variables: any = { '[profile_id]': profileId };
 
     if (profileId > 0) {
-      profileData = await DataManager.get('getProfile', {...defaults, ...options}, 
-        {'[profile_id]': profileId},
-      );
+      profileData = await DataManager.get('getProfile', {...defaults, ...options}, variables);
     }
 
     return profileData || {};
