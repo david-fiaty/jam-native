dc752edea82abe6036501c8a05ca33055edde4fa
david-fiaty <dfiaty@gmail.com>
2025-01-14 14:51:02 +0000

    Update EntityManager.tsx

--- manager/EntityManager.tsx
+++ manager/EntityManager.tsx
@@ -23,6 +23,12 @@ class EntityManager {
     return response;
   }
 
+  async updateJam(entityId: number, options: any) {
+    let defaults: any = {};
+    let variables: any = { '[entity_id]': entityId };
+
+    return await DataManager.put('updateJam', {...defaults, ...options}, variables); 
+  }
 
   async updateProfile(options: any) {
     let defaults: any = {};
