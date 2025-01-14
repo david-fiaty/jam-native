78e00b7cf35dcba4945e52c10cbe16508dee2c23
david-fiaty <dfiaty@gmail.com>
2025-01-14 15:35:49 +0000

    Update BackButton.tsx

--- components/button/BackButton.tsx
+++ components/button/BackButton.tsx
@@ -12,7 +12,12 @@ const BackButton = ({title, onPress}: Props) => {
   const ButtonView = () => {
     return (        
       <BoxView direction="row" align="center">
-        <IconView name="previous" theme="clear" onPress={onPress} padding={0} />
+        <IconView 
+          name="previous" 
+          theme="clear" 
+          padding={0} 
+          onPress={onPress} 
+        />
         <TextView style={styles.text}>{title}</TextView>
       </BoxView>
     );
