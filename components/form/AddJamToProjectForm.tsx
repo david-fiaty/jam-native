a48a270dd993b38a415587dc68f72e34101f16b6
david-fiaty <dfiaty@gmail.com>
2025-01-14 18:06:45 +0000

    Update AddJamToProjectForm.tsx

--- components/form/AddJamToProjectForm.tsx
+++ components/form/AddJamToProjectForm.tsx
@@ -54,10 +54,7 @@ const AddJamToProjectForm = () => {
 
         <ProfileProjectsList
           title={i18n.t("Your Projects")} 
-          //addButton={true}
-          profileId={profileId}
           idArray={[14, 18, 19]}
-          //allButton={formData?.profile_projects?.length > 0}
           //idArray={formData?.profile_projects}
           /*
           onAddButtonPress={() => {
