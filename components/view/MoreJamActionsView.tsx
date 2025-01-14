26a0d8e4e4532bd1ae09f70b0465f4262b164fb7
david-fiaty <dfiaty@gmail.com>
2025-01-14 15:28:52 +0000

    Update MoreJamActionsView.tsx

--- components/view/MoreJamActionsView.tsx
+++ components/view/MoreJamActionsView.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { View, Alert } from "react-native";
 import { Layout } from "@/constants/Layout";
-import { useRouter } from "expo-router";
 import BackButton from "../button/BackButton";
 import i18n from "@/translation/i18n";
 import BoxView from '../view/BoxView';
@@ -12,7 +11,6 @@ import ActionListItem from '../list/ListItem/ActionListItem';
 import DataManager from '@/manager/DataManager';
 
 const MoreJamActionsView = () => {
-  const router = useRouter();
   const [entity, setEntity] = useState<any>(null);
   const entityId = ScreenManager.getScreenEntityId();
 
