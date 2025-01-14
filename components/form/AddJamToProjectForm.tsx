90599d30ca827fdff9ddd793ca8338301dbed840
david-fiaty <dfiaty@gmail.com>
2025-01-14 15:41:16 +0000

    Update AddJamToProjectForm.tsx

--- components/form/AddJamToProjectForm.tsx
+++ components/form/AddJamToProjectForm.tsx
@@ -14,14 +14,15 @@ const AddJamToProjectForm = () => {
   const activeScreen: any = ScreenManager.getActiveScreen();
   const entityId: number = activeScreen.params.entityId;
 
-  /*
-
-  {
-    "profile_id": 1,
-    "items_ids": [3, 4]
-}
-
-  */
+  const submitForm = () => {
+    // Todo - Implement submit form
+    /*
+    {
+      "profile_id": 1,
+      "items_ids": [3, 4]
+    }
+    */
+  };
 
   useEffect(() => {
     (async () => {
