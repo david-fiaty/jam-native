cbbc38fb0d369f0407fbaca0984f4a61d0e6d2a6
david-fiaty <dfiaty@gmail.com>
2025-01-14 15:28:50 +0000

    Update AddJamToProjectForm.tsx

--- components/form/AddJamToProjectForm.tsx
+++ components/form/AddJamToProjectForm.tsx
@@ -1,8 +1,29 @@
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text } from 'react-native';
 import { Colors } from '@/constants/Colors';
 import { Layout } from '@/constants/Layout';
+import ScreenManager from '@/manager/ScreenManager';
+import SpinnerView from '../view/SpinnerView';
 
 const AddJamToProjectForm = () => {
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const activeScreen: any = ScreenManager.getActiveScreen();
+  const entityId: number = activeScreen.params.entityId;
+
+  useEffect(() => {
+    (async () => {
+      if (!isLoaded) {
+
+        setIsLoaded(true);
+      }
+      
+    })();
+  }, [isLoaded]);
+
+  if (!isLoaded) return <SpinnerView />;
+
+  console.log(entityId);
+
   return (
     <Text>
       ADD JAM TO PROJECT
