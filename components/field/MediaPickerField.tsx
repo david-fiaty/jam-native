5f5564be90c08f063d330827f1f28b98d681785f
david-fiaty <dfiaty@gmail.com>
2025-01-16 20:16:07 +0000

    Update MediaPickerField.tsx

--- components/field/MediaPickerField.tsx
+++ components/field/MediaPickerField.tsx
@@ -138,10 +138,9 @@ const MediaPickerField = ({label, value, preview, onSelectItem, onDeleteItem}: P
   useEffect(() => {
     (async () => {
       if (!isLoaded) {
+        setIsLoaded(true);
         setSelectedMedia(await getSelectedMedia());
       }
-
-      setIsLoaded(true);
     })();
   }, [isLoaded]);
   
@@ -155,6 +154,7 @@ const MediaPickerField = ({label, value, preview, onSelectItem, onDeleteItem}: P
       { selectedMedia?.length > 0 && preview &&
         <BoxView direction="row" align="flex-start" justify="left" style={styles.previewContainer}>
           { selectedMedia.map((data: any) => {
+            console.log(data);
             if (data?.uri) return renderImagePreview(data);
           })}
         </BoxView>
