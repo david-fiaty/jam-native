a5b557a4503d8ae850b78dcc2634bd47ef052898
david-fiaty <dfiaty@gmail.com>
2025-01-16 16:50:58 +0000

    Update MediaManager.tsx

--- manager/MediaManager.tsx
+++ manager/MediaManager.tsx
@@ -40,10 +40,33 @@ class MediaManager {
       return DataManager.extract(['base64'], item);
     }); 
   };
+
+  fetchImageAsBase64 = (url: string) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const response = await fetch(url);
+        const blob = await response.blob();
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          resolve(reader.result); 
+        };
+        reader.onerror = reject; 
+        reader.readAsDataURL(blob);
+      } catch (error) {
+        reject(error); 
+      }
+    });
+  };
   
-  urlToBase64 (url: string) {
-    
-  } 
+  getImageBase64 = async (url: string) => {
+    try {
+      return await this.fetchImageAsBase64(url);
+    } catch (error) {
+      console.error('Error fetching image:', error);
+      return null;
+    }
+  };
+  
 }
 
 export default new MediaManager();
