3f7bebb95436c84b86c4f99b5ca221f5c10189d8
david-fiaty <dfiaty@gmail.com>
2025-01-16 19:40:47 +0000

    Update MediaPickerField.tsx

--- components/field/MediaPickerField.tsx
+++ components/field/MediaPickerField.tsx
@@ -81,7 +81,7 @@ const MediaPickerField = ({label, value, preview, onSelectItem, onDeleteItem}: P
       allowsEditing: false,
       aspect: [4, 3],
       quality: 1,
-      //base64: true,
+      base64: true,
     });
   };
 
@@ -107,8 +107,8 @@ const MediaPickerField = ({label, value, preview, onSelectItem, onDeleteItem}: P
 
     return {
       assetId: media?.id,
-      fileName: media?.url,
-      uri: media?.url,
+      fileName: mediaUrl,
+      uri: mediaUrl,
       fileSize: null,
       height: 0,
       width: 0,
