d00e07b7993b06e9eb52c3c0f567899a1f6ef3a4
david-fiaty <dfiaty@gmail.com>
2025-01-16 18:08:13 +0000

    Update MediaPickerBase.tsx

--- components/base/MediaPickerBase.tsx
+++ components/base/MediaPickerBase.tsx
@@ -97,43 +97,13 @@ const MediaPickerBase = ({label, value, preview, onSelectItem, onDeleteItem}: Pr
     }
   };
 
-  const getSelectedMedia = () => {
-    // Todo - Get selected media
-    let mediaList: any = [...selectedMedia || []];
-
-    console.log(mediaList);
-
-    /*
-    for (const url of (value || [])) {
-      console.log(url);
-    }
-*/
-    /*
-    let url: string = MediaManager.getImageUrl(value?.[0].url);
-
-    if (DataManager.isUrl(url)) {
-      MediaManager.getImageBase64(url)
-      .then((data: any) => {
-        return data;
-      })
-      .catch(error => {
-        console.error(error);
-        return [];
-      });
-    }
-*/
-    
-  }; 
-
   useEffect(() => {
     (async () => {
       if (!isLoaded) {
         setIsLoaded(true);
       }
     })();
-
-    //setSelectedMedia(getSelectedMedia());
-  }, [isLoaded, selectedMedia, value]);
+  }, [isLoaded]);
 
   return (
     <View style={styles.container}>
