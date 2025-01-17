ec739cfb0d18d965465c4c18eac3e68fdcf0613f
david-fiaty <dfiaty@gmail.com>
2025-01-17 12:23:02 +0000

    Update JamsList.tsx

--- components/list/JamsList.tsx
+++ components/list/JamsList.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { Layout } from "@/constants/Layout";
 import { BaseProps } from "@/constants/Types";
 import BoxView from "../view/BoxView";
@@ -6,6 +7,7 @@ import SpinnerView from "../view/SpinnerView";
 import ListView from "../view/ListView";
 import EntityManager from "@/manager/EntityManager";
 import ListItem from "./JamsList/ListItem";
+import SearchManager from "@/manager/SearchManager";
 
 type Props = BaseProps & {
   idArray?: any,
@@ -16,6 +18,20 @@ const JamsList = ({idArray, showSpinner}: Props) => {
   const [jamsData, setJamsData] = useState<any>([]);
   const [sectorsData, setSectorsData] = useState<any>([]);
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [searchResultsIds, setSearchResultsIds] = useState<any[]>([]);
+  const searchState = useSelector((state: any) => state.search);
+
+  const getSearchResultsIds = async () => {
+    let jamResultsIds: any = [];
+    let searchValue: string = searchState.value || '';
+
+    if (searchValue.length > 0) {
+      let searchResults: any = await SearchManager.getResult(searchValue); 
+      jamResultsIds = (searchResults?.jam || []).map((o: any) => o.id);
+    }
+
+    return jamResultsIds;
+  };
 
   useEffect(() => {
     (async () => {
@@ -25,6 +41,8 @@ const JamsList = ({idArray, showSpinner}: Props) => {
         setJamsData(await EntityManager.listJams())
 
         setIsLoaded(true);
+
+        //setSearchResultsIds(await getSearchResultsIds());
       }
     })();
   }, [idArray]);
