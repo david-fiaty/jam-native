d7c02e34235fe0261d29d61a96e2462ae110c8b6
david-fiaty <dfiaty@gmail.com>
2025-01-17 12:44:49 +0000

    Update JamsList.tsx

--- components/list/JamsList.tsx
+++ components/list/JamsList.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
 import { Layout } from "@/constants/Layout";
 import { BaseProps } from "@/constants/Types";
 import BoxView from "../view/BoxView";
@@ -7,7 +6,6 @@ import SpinnerView from "../view/SpinnerView";
 import ListView from "../view/ListView";
 import EntityManager from "@/manager/EntityManager";
 import ListItem from "./JamsList/ListItem";
-import SearchManager from "@/manager/SearchManager";
 
 type Props = BaseProps & {
   idArray?: any,
@@ -18,38 +16,18 @@ const JamsList = ({idArray, showSpinner}: Props) => {
   const [jamsData, setJamsData] = useState<any>([]);
   const [sectorsData, setSectorsData] = useState<any>([]);
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
-  const [searchResultsIds, setSearchResultsIds] = useState<any[]>([]);
-  const searchState = useSelector((state: any) => state.search);
-
-  const getSearchResultsIds = async () => {
-    let resultsIds: any = [];
-    let searchResults: any = await SearchManager.getResult(searchState.value); 
-    resultsIds = (searchResults?.jam || []).map((o: any) => o.id);
-
-    return resultsIds;
-  };
-
-  const getJamsData = async () => {
-    if (idArray?.length) {
-      return await EntityManager.getJams({items_ids: idArray});
-    }
-    else if (searchState.value?.length) {
-      return await getSearchResultsIds();
-    }
-    else {
-      return await EntityManager.listJams();
-    }
-  };
 
   useEffect(() => {
     (async () => {
       if (!isLoaded) {
         setSectorsData(await EntityManager.getSectors());
-        setJamsData(await getJamsData());
+        if (idArray?.length) setJamsData(await EntityManager.getJams({items_ids: idArray}))
+        else setJamsData(await EntityManager.listJams())
+
         setIsLoaded(true);
       }
     })();
-  }, [idArray]);
+  });
 
   if (!isLoaded) return <SpinnerView />;
 
