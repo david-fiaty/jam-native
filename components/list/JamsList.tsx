a3280b4547339b31e37206c676a33a73e803a60e
david-fiaty <dfiaty@gmail.com>
2025-01-17 12:20:15 +0000

    Update JamsList.tsx

--- components/list/JamsList.tsx
+++ components/list/JamsList.tsx
@@ -21,13 +21,13 @@ const JamsList = ({idArray, showSpinner}: Props) => {
     (async () => {
       if (!isLoaded) {
         setSectorsData(await EntityManager.getSectors());
-        if (idArray?.length) setJamsData(await EntityManager.getJams({items_ids: idArray}))
-        else setJamsData(await EntityManager.listJams())
+        //if (idArray?.length) setJamsData(await EntityManager.getJams({items_ids: idArray}))
+        setJamsData(await EntityManager.listJams())
 
         setIsLoaded(true);
       }
     })();
-  });
+  }, [idArray]);
 
   if (!isLoaded) return <SpinnerView />;
 
