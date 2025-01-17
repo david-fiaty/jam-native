22b3016b3667138c549a792a85934b28a2a96f6c
david-fiaty <dfiaty@gmail.com>
2025-01-17 14:00:45 +0000

    Update SearchManager.tsx

--- manager/SearchManager.tsx
+++ manager/SearchManager.tsx
@@ -5,33 +5,11 @@ class SearchManager {
   data?: any;
   result?: any;
 
-  async getData() {
-    if (!this.data?.length) {
-      this.data = await this.loadData();
+  async loadData(searchValue?: string) {
+    if (!searchValue || !searchValue?.length) {
+      searchValue = Store.getState().search.value;
     }
 
-    return this.data;
-  }
-
-  async getResult(searchValue: string) {
-    if (!this.result?.length) {
-      this.result = await this.loadResult(searchValue);
-    }
-
-    return this.result;
-  }
-
-  async loadData() {
-    const [jams, profiles, projects] = await this.sendRequest();
-    
-    return this.buildResponse({
-      jams: jams, 
-      profiles: profiles, 
-      projects: projects
-    });
-  }
-
-  async loadResult(searchValue: string) {
     const options = searchValue?.length ? { query_text: searchValue } : {};
     const [jams, profiles, projects] = await this.sendRequest(options);
 
