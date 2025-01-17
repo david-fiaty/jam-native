904a24e88df17c78da63ecf302dbc549fff9c19f
david-fiaty <dfiaty@gmail.com>
2025-01-17 15:13:40 +0000

    Update SearchManager.tsx

--- manager/SearchManager.tsx
+++ manager/SearchManager.tsx
@@ -19,25 +19,31 @@ class SearchManager {
       projects: projects
     });
 
-    Store.dispatch(setSearchResult(JSON.stringify(response)));
+    this.setSearchResult(response);
 
     return response;
   }
 
-  /*
-  getSearchResultsIds = async () => {
-    let jamResultsIds: any = [];
-    let searchValue: string = searchState.value || '';
-
-    if (searchValue.length > 0) {
-      let searchResults: any = await SearchManager.loadData(searchValue); 
-      jamResultsIds = (searchResults?.jam || []).map((o: any) => o.id);
+  setSearchResult (response: any) {
+    let results: any = {};
+    for (const [key, data] of Object.entries(response)) {
+      results[key] = (data || []).map((o: any) => o.id);
     }
 
-    return jamResultsIds;
-  };
-  */
+    Store.dispatch(setSearchResult(JSON.stringify(results)));
+  }
 
+  getSearchResult(key?: string) {
+    let searchState = Store.getState().search;
+    let searchResult: any = searchState.result || '{}';
+    let data: any = JSON.parse(searchResult);
+
+    return key ? data[key] : data;
+  }
+
+  isExpanded() {
+    return Store.getState().search.expanded === true;
+  }
 
   async sendRequest(options?: any) {
     return await Promise.all([
@@ -56,10 +62,6 @@ class SearchManager {
       call: data.jams.filter((o: any) => o?.type == 'call'),
       event: data.jams.filter((o: any) => o?.type == 'event'),
     };
-  }
-
-  isExpanded() {
-    return Store.getState().search.expanded === true;
   }
 };
 
