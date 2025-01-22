47b7f6ece8af3122025c1bbb4b7788920eaa5b1f
david-fiaty <dfiaty@gmail.com>
2025-01-22 18:43:26 +0000

    Update JamsMapView.tsx

--- components/view/JamsMapView.tsx
+++ components/view/JamsMapView.tsx
@@ -64,11 +64,14 @@ const JamsMapView = ({ idArray }: Props) => {
     let latitude = currentLocation?.coords?.latitude || Config.defaultLocation.latitude;
     let longitude = currentLocation?.coords?.longitude || Config.defaultLocation.longitude;
 
+    let latitudeDelta = 0.28;
+    let longitudeDelta = latitudeDelta * ( DeviceManager.window.width/ DeviceManager.window.height);
+
     return {
       latitude: latitude,
       longitude: longitude,
-      latitudeDelta: 5,
-      longitudeDelta: 5,
+      latitudeDelta: latitudeDelta,
+      longitudeDelta: longitudeDelta,
     };
   };
 
