51199760a83c7c383ddac398127823da40833f25
david-fiaty <dfiaty@gmail.com>
2025-01-25 21:40:21 +0000

    Code cleanup

--- components/view/SearchView.tsx
+++ components/view/SearchView.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { View, TouchableOpacity } from "react-native";
 import { useSelector } from "react-redux";
 import { Layout } from "@/constants/Layout";
@@ -12,9 +12,6 @@ import SearchProfilesList from "../list/SearchProfilesList";
 import SearchProjectsList from "../list/SearchProjectsList";
 import SearchManager from "@/manager/SearchManager";
 import SpinnerView from "./SpinnerView";
-import BackButton from "../button/BackButton";
-import i18n from "@/translation/i18n";
-import ScreenManager from "@/manager/ScreenManager";
 
 const SearchView = () => {
   const [activeTab, setActiveTab] = useState<any>(null);
@@ -53,11 +50,6 @@ const SearchView = () => {
       scroll={true}
       style={Layout.screenContent}
     >
-      <BackButton
-        title={i18n.t('Back')}
-        onPress={() => ScreenManager.toggleModal('SearchView')}
-      />
-
       {/* Search filters */}
       <ListView
         data={StaticData.searchTabs}
