2986570b9c795945db7fcaded42bc88845b9f29f
david-fiaty <dfiaty@gmail.com>
2025-01-25 14:59:43 +0000

    Update ModalSlice.ts

--- redux/slices/ModalSlice.ts
+++ redux/slices/ModalSlice.ts
@@ -1,29 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { Modals } from '@/constants/Modals';
 
-const getModals = () => {
-  return Modals.map(({component, ...rest}) => ({...rest}));
-};
-
 const ModalSlice = createSlice({
   name: 'modal',
   initialState: [],
   reducers: {
     setActiveModal: (state, action) => {
-      let modals: any = getModals();
-      let modalState: any = [...state];
-      let index: any = modalState.findIndex((o: any) => o.name == action.payload.name);
+      let screens: any = [...state];
+      let index: any = screens.findIndex((o: any) => o.name == action.payload.name);
 
       if (index === -1) {
-        let screen = modals.find((o: any) => o.name == action.payload.name);
-        modalState.push({ ...screen, ...{ params: action.payload.params }});
+        let screen = Modals.find((o: any) => o.name == action.payload.name);
+        screens.push({ ...screen, ...{ params: action.payload.params }});
       }
       else {
-        delete modalState[index];
-        modalState = modalState.filter((o: any) => o);
+        delete screens[index];
+        screens = screens.filter((o: any) => o);
       }
 
-      return modalState;
+      return screens;
     },
   },
 });
