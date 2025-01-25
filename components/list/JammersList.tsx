415dbb3b39b5f0fd43139e9f012b6b50f7c186f6
david-fiaty <dfiaty@gmail.com>
2025-01-25 21:06:01 +0000

    Update JammersList.tsx

--- components/list/JammersList.tsx
+++ components/list/JammersList.tsx
@@ -1,20 +1,22 @@
 import { useState, useEffect } from 'react';
 import { View } from "react-native";
 import { Layout } from "@/constants/Layout";
+import { BaseProps } from '@/constants/Types';
 import TextView from "../view/TextView";
-import BackButton from "../button/BackButton";
 import i18n from "@/translation/i18n";
 import BoxView from "../view/BoxView";
 import ListView from "../view/ListView";
 import SpinnerView from "../view/SpinnerView";
-import ScreenManager from "@/manager/ScreenManager";
 import EntityManager from '@/manager/EntityManager';
 import ProfileListItem from './ListItem/ProfileListItem';
 
-const JammersList = () => {
+type Props = BaseProps & {
+  entityId?: any;
+};
+
+const JammersList = ({ entityId }: Props) => {
   const [profiles, setProfiles] = useState<any>(null);
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
-  const entityId = ScreenManager.getModalEntityId();
 
   useEffect(() => {
     (async () => {
@@ -30,11 +32,6 @@ const JammersList = () => {
 
   return (
     <BoxView align="flex-start" justify="flex-start" style={Layout.screenContent}>
-      <BackButton
-        title={i18n.t('Jammers')}
-        onPress={() => ScreenManager.toggleModal('JammersList')}
-      />
-      
       <View style={Layout.borderedListContainer}>
         {profiles?.length > 0 &&
           <ListView
