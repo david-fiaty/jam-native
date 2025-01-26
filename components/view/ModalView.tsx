f829b01ca9955aee8d33227761f1e6de50015375
david-fiaty <dfiaty@gmail.com>
2025-01-26 18:41:16 +0000

    Update ModalView.tsx

--- components/view/ModalView.tsx
+++ components/view/ModalView.tsx
@@ -85,7 +85,7 @@ const ModalView = ({ visible, login, animation, trigger, triggerAlignSelf, conte
 const styles = StyleSheet.create({
   backdrop: {
     color: Colors.white,
-    opacity: 0.7,
+    opacity: 0.5,
     marginTop: 50,
   },
   container: {
