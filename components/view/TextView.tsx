71bf726c36bafa2d814f44323da198cda387c456
david-fiaty <dfiaty@gmail.com>
2025-01-26 16:32:13 +0000

    Update TextView.tsx

--- components/view/TextView.tsx
+++ components/view/TextView.tsx
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
   container: {
     color: Colors.primary,
     fontSize: Layout.fontSize.base,
-    lineHeight: 17,
+    lineHeight: Layout.lineHeight,
   },
   underline: {
     borderBottomWidth: Layout.borderWidth.base,
