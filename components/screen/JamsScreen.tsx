ef30c882542cfeaee48a1dfb4a98c7c926a1a7ed
david-fiaty <dfiaty@gmail.com>
2025-01-27 19:59:12 +0000

    Update JamsScreen.tsx

--- components/screen/JamsScreen.tsx
+++ components/screen/JamsScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { useDispatch } from 'react-redux';
 import { setModalConfig } from "@/redux/slices/ModalSlice";
@@ -9,13 +9,27 @@ import FooterNavigation from "../navigation/FooterNavigation";
 import JamsList from "../list/JamsList";
 import ScreenManager from "@/manager/ScreenManager";
 import HeaderNavigation from "../navigation/HeaderNavigation";
+import SearchManager from "@/manager/SearchManager";
+import EntityManager from "@/manager/EntityManager";
 
 const JamsScreen = React.memo(() => {
   const dispatch = useDispatch();
+  const [jamsIds, setJamsIds] = useState<any>([]);
   const contentStyle = ScreenManager.getModalSize();
   
+  const getJamsIds = async () => {
+    let idArray: any = SearchManager.getSearchResult('jam');
+
+  };
+
   useEffect(() => {
+    
     dispatch(setModalConfig(ModalConfig));
+
+    (async () => {
+      setJamsIds(await getJamsIds());
+    });
+
   }, [ModalConfig]);
 
   return (  
