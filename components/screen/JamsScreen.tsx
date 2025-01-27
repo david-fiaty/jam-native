0e06372be80944517b1d76990dab598668c8b01d
david-fiaty <dfiaty@gmail.com>
2025-01-27 19:11:07 +0000

    Update JamsScreen.tsx

--- components/screen/JamsScreen.tsx
+++ components/screen/JamsScreen.tsx
@@ -11,26 +11,16 @@ import ScreenManager from "@/manager/ScreenManager";
 import HeaderNavigation from "../navigation/HeaderNavigation";
 import SpinnerView from "../view/SpinnerView";
 import SearchManager from "@/manager/SearchManager";
-import EntityManager from "@/manager/EntityManager";
 
 const JamsScreen = React.memo(() => {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
-  const [jamsData, setJamsData] = useState<any>([]);
+  const [jamsIds, setJamsIds] = useState<any>([]);
   const contentStyle = ScreenManager.getModalSize();
 
-  const getJamsData = async () => {
-    let idArray: any = SearchManager.getSearchResult('jam');
-    if (idArray?.length > 0) setJamsData(await EntityManager.getJams({ items_ids: idArray })); 
-    else setJamsData(await EntityManager.listJams());
-  };
-  
   useEffect(() => {
     dispatch(setModalConfig(ModalConfig));
-    (async () => {
-      await getJamsData();
-    })();
-
+    setJamsIds(SearchManager.getSearchResult('jam'));
     setIsLoaded(true);
   }, [isLoaded, ModalConfig]);
 
@@ -41,7 +31,7 @@ const JamsScreen = React.memo(() => {
       <HeaderNavigation />
 
       <BoxView style={[styles.content, contentStyle]} direction="column" align="center">
-        <JamsList idArray={jamsData}/>
+        <JamsList idArray={jamsIds}/>
       </BoxView>
       
       <FooterNavigation />
