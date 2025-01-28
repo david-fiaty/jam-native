4b5c119a2253d9439695edb198081495d6996295
david-fiaty <dfiaty@gmail.com>
2025-01-28 00:33:22 +0000

    Update SearchSlice.ts

--- redux/slices/SearchSlice.ts
+++ redux/slices/SearchSlice.ts
@@ -4,17 +4,21 @@ const SearchSlice = createSlice({
   name: 'search',
   initialState: {
     value: '',
-    result: '{}',
+    current: '{}',
+    default: '{}',
   },
   reducers: {
     setSearchValue: (state, action) => {
       state.value = action.payload;
     },
-    setSearchResult: (state, action) => {
-      state.result = action.payload;
+    setCurrentResult: (state, action) => {
+      state.current = action.payload;
+    },
+    setDefaultResult: (state, action) => {
+      state.default = action.payload;
     },
   },
 });
 
-export const { setSearchValue, setSearchResult } = SearchSlice.actions;
+export const { setSearchValue, setDefaultResult, setCurrentResult } = SearchSlice.actions;
 export default SearchSlice.reducer;
