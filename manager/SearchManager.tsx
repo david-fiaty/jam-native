99bd68b7dcb33464084e3ab16c312737bdf178fb
david-fiaty <dfiaty@gmail.com>
2025-01-28 12:49:44 +0000

    Update SearchManager.tsx

--- manager/SearchManager.tsx
+++ manager/SearchManager.tsx
@@ -8,7 +8,7 @@ class SearchManager {
       return await this.getDefaultResult();
     } 
     else if (searchValue == this.getCurrentValue()) {
-      return this.getCurrentResult();
+      return await this.getCurrentResult();
     }
     else {
       let searchResult: any = await this.loadData(searchValue);
@@ -18,10 +18,10 @@ class SearchManager {
   }
 
   async getDefaultResult() {
-    let defaultResult: any = JSON.parse(Store.getState().search.default);
+    let defaultResult: any = await Store.getState().search.default;
 
     if (Object.keys(defaultResult)?.length > 0) {
-      return defaultResult;
+      return await this.unpackResult(defaultResult);
     }
     else {
       defaultResult = await this.loadData();
@@ -31,10 +31,65 @@ class SearchManager {
     return defaultResult;
   }
 
+  setDefaultResult(result: any) {
+    Store.dispatch(setDefaultResult(this.packResult(result)));
+  }
+
+  getCurrentValue() {
+    return Store.getState().search.value;
+  }
+
+  setCurrentValue(value: any) {
+    Store.dispatch(setSearchValue(value));
+  }
+
+  async getCurrentResult() {
+    return await this.unpackResult(Store.getState().search.current);
+  }
+
+  setCurrentResult(result: any) {
+    Store.dispatch(setCurrentResult(this.packResult(result)));
+  }
+
+  packResult(result: any) {
+    let obj: any = {};
+  
+    for (const key in result) {
+      obj[key] = result[key].map((item: any) => item.id);
+    }
+  
+    return JSON.stringify(obj);
+  }
+
+  async unpackResult(str: any) {
+    let result: any = JSON.parse(str);    
+    let request: any = [];
+
+    for (const key in result) {
+      switch (key) {
+        case 'jam':
+          request.push(EntityManager.getJams({ items_ids: result[key] }));
+          break;
+
+        case 'profile':
+          request.push(EntityManager.getProfiles({ items_ids: result[key] }));
+          break;
+
+        case 'project':
+          request.push(EntityManager.getProjects({ items_ids: result[key] }));
+          break;
+      }
+    }
+
+    const [jam, profile, project] = await Promise.all(request);
+
+    return {jam, profile, project};
+  }
+
   async loadData(searchValue?: string) {
     const options = searchValue?.length ? { query_text: searchValue } : {};
-    const [jams, profiles, projects] = await this.sendRequest(options);
-    const response = this.buildResponse({
+    const [jams, profiles, projects] = await this.sendSearchRequest(options);
+    const response = this.buildSearchResponse({
       jams: jams, 
       profiles: profiles, 
       projects: projects
@@ -43,27 +98,7 @@ class SearchManager {
     return response;
   }
 
-  getCurrentValue() {
-    return Store.getState().search.value;
-  }
-
-  getCurrentResult() {
-    return JSON.parse(Store.getState().search.current);
-  }
-
-  setCurrentValue(value: any) {
-    Store.dispatch(setSearchValue(value));
-  }
-
-  setCurrentResult(result: any) {
-    Store.dispatch(setCurrentResult(JSON.stringify(result)));
-  }
-
-  setDefaultResult(result: any) {
-    Store.dispatch(setDefaultResult(JSON.stringify(result)));
-  }
-
-  async sendRequest(options?: any) {
+  async sendSearchRequest(options?: any) {
     return await Promise.all([
       EntityManager.listJams(options), 
       EntityManager.listProfiles(options),
@@ -71,7 +106,7 @@ class SearchManager {
     ]);
   }
 
-  buildResponse(data?: any) {
+  buildSearchResponse(data?: any) {
     return {
       jam: data.jams,
       project: data.projects,
