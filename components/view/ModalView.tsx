1771e963244c84a65b241550ebf644fef8d3c1b3
david-fiaty <dfiaty@gmail.com>
2025-01-29 15:40:49 +0000

    Update ModalView.tsx

--- components/view/ModalView.tsx
+++ components/view/ModalView.tsx
@@ -1,12 +1,7 @@
-import React, { useState } from 'react';
 import { useRouter } from "expo-router";
 import { StyleSheet, TouchableOpacity } from 'react-native';
 import { Layout } from '@/constants/Layout';
-import { Colors } from '@/constants/Colors';
-import Modal from "react-native-modal";
 import UserManager from '@/manager/UserManager';
-import BackButton from '../button/BackButton';
-import BoxView from './BoxView';
 import ScreenManager from '@/manager/ScreenManager';
 import ScreenView from './ScreenView';
 
@@ -21,10 +16,7 @@ type Props = {
   onTriggerPress?: (active: boolean) => void;
 };
 
-const modalPosition: any = ScreenManager.getModalPosition();
-const modalSize: any = ScreenManager.getModalSize();
-
-const ModalView = ({ name, visible, login, trigger, triggerAlignSelf, content, backTitle, onTriggerPress }: Props) => {
+const ModalView = ({ name, login, trigger, triggerAlignSelf, onTriggerPress }: Props) => {
   const router = useRouter();
   const isLoggedIn: boolean = UserManager.isLoggedIn();
 
@@ -55,23 +47,6 @@ const ModalView = ({ name, visible, login, trigger, triggerAlignSelf, content, b
 };
 
 const styles = StyleSheet.create({
-  container: {
-    position: 'absolute',
-    top: modalPosition.y,
-    left: modalPosition.x,
-    width: modalSize.width,
-  },
-  wrapper: {
-    width: '100%',
-    marginTop: 0,
-    backgroundColor: Colors.white,
-    paddingTop: Layout.space.base*2,
-    height: modalSize.height,
-  },
-  content: {
-    width: '100%',
-    flex: 1,
-  },
   backButtonContainer: {
     width: '100%',
   },
