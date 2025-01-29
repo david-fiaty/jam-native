8389ed8dc8aadbd518f43c9a89d3ab5cd3af36c3
david-fiaty <dfiaty@gmail.com>
2025-01-29 13:16:47 +0000

    Update ScreenManager.tsx

--- manager/ScreenManager.tsx
+++ manager/ScreenManager.tsx
@@ -85,7 +85,7 @@ class ScreenManager {
   getModalPosition() {
     return {
       x: 0,
-      y: this.getHeaderSize().height,
+      y: this.getHeaderSize().height + Layout.space.base,
     };
   }
 
