27d460e376e2c5035a69cf8ba9a802ce20ff9c18
david-fiaty <dfiaty@gmail.com>
2025-01-29 19:19:01 +0000

    Update JamStatusButton.tsx

--- components/button/JamStatusButton.tsx
+++ components/button/JamStatusButton.tsx
@@ -43,10 +43,10 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     borderRadius: Layout.radius.round,
     color: Colors.white,
-    fontSize: Layout.fontSize.small,
+    fontSize: 11,
     backgroundColor: Colors.primary,
-    paddingTop: Layout.space.base/6,
-    paddingBottom: Layout.space.base/4,
+    paddingTop: 0,
+    paddingBottom: 2,
   },
   active: {
     backgroundColor: Colors.primary,
