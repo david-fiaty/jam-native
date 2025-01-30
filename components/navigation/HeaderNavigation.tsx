76e1df5405681d55116901a95a046d0826ebf289
david-fiaty <dfiaty@gmail.com>
2025-01-30 14:26:09 +0000

    Update HeaderNavigation.tsx

--- components/navigation/HeaderNavigation.tsx
+++ components/navigation/HeaderNavigation.tsx
@@ -34,7 +34,9 @@ const HeaderNavigation = ({ onSearchSubmit, onSearchClear }: Props) => {
 
   const canShowButtons = () => {
     return true;
-    return RouteConfig.isMainRoute(route.name) || activeModal?.headerNavigation;
+    
+
+    
   };
 
   const renderSearchField = () => {
