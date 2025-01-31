47ac0cdfeaf64b1a754340752e8a1e4ac75d84ba
david-fiaty <dfiaty@gmail.com>
2025-01-31 14:15:30 +0000

    Update SearchField.tsx

--- components/field/SearchField.tsx
+++ components/field/SearchField.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { useSelector } from 'react-redux';
 import { BaseProps } from '@/constants/Types';
+import * as Animatable from 'react-native-animatable';
 import IconView from "../view/IconView";
 import InputTextField from "../field/InputTextField";
 import i18n from '@/translation/i18n';
@@ -84,21 +85,34 @@ const SearchField = ({ onSearchEdit, onSearchSubmit, onSearchClear }: Props) => 
         /> 
       }
 
-      {isExpanded && 
-        <InputTextField 
-          value={currentSearchValue}
-          placeholder={i18n.t('Search...')}
-          onChangeText={onChangeText}
-          onSubmitEditing={onSubmitEditing}
-          rightIcon={renderRightIcon()}
-        /> 
-      }
+    
+        <Animatable.View 
+          style={[styles.animated, (isExpanded ? styles.expanded : {})]}
+          transition="width"
+          duration={300}
+        >
+          <InputTextField 
+            value={currentSearchValue}
+            placeholder={i18n.t('Search...')}
+            onChangeText={onChangeText}
+            onSubmitEditing={onSubmitEditing}
+            rightIcon={renderRightIcon()}
+          /> 
+        </Animatable.View>
+
     </BoxView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    width: '100%',
+  },
+  animated: {
+    overflow: 'hidden',
+    width: '0%',
+  },
+  expanded: {
     width: '100%',
   },
 });
