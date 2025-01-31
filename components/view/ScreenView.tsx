8758cea20e689c811d7db1063677e4d70abf654d
david-fiaty <dfiaty@gmail.com>
2025-01-31 15:10:02 +0000

    Update ScreenView.tsx

--- components/view/ScreenView.tsx
+++ components/view/ScreenView.tsx
@@ -2,11 +2,15 @@ import { Colors } from '@/constants/Colors';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { BaseProps } from '@/constants/Types';
+import HeaderNavigation from '../navigation/HeaderNavigation';
+import FooterNavigation from '../navigation/FooterNavigation';
 
 const ScreenView = ({style, children}: BaseProps) => {
   return (
     <SafeAreaView style={[styles.container, style]}>
+      <HeaderNavigation />
       {children}
+      <FooterNavigation />
     </SafeAreaView>
   );
 };
