d55e7603449d80630699a3ec2f8e00649441b2a3
david-fiaty <dfiaty@gmail.com>
2025-01-31 13:10:15 +0000

    Update MainScreen.tsx

--- components/screen/MainScreen.tsx
+++ components/screen/MainScreen.tsx
@@ -1,43 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
-import { useDispatch } from 'react-redux';
-import { setIsSearching } from "@/redux/slices/SearchSlice";
 import { Colors } from "@/constants/Colors";
 import BoxView from "../view/BoxView";
 import FooterNavigation from "../navigation/FooterNavigation";
 import JamsList from "../list/JamsList";
 import HeaderNavigation from "../navigation/HeaderNavigation";
-import SearchManager from "@/manager/SearchManager";
 import SpinnerView from "../view/SpinnerView";
 import ModalConfig from "@/constants/ModalConfig";
 
 const MainScreen = () => {
-  const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
-
-  const loadSearchResult = async (value?: any) => {
-    await SearchManager.getSearchResult(value);
-  };
-
-  const onSearchEdit = async (value: any) => {
-    await loadSearchResult(value);
-  };
-
-  const onSearchSubmit = async (value: any) => {
-    dispatch(setIsSearching(true));
-    await loadSearchResult(value);
-    dispatch(setIsSearching(false));
-  };
-
-  const onSearchClear = async () => {
-    dispatch(setIsSearching(true));
-    await SearchManager.clearSearch();
-    dispatch(setIsSearching(false));
-  };
-
+  
   useEffect(() => {
     (async () => {
-      await loadSearchResult();
       setIsLoaded(true);
     })();
   }, [isLoaded, ModalConfig]);
@@ -46,11 +21,7 @@ const MainScreen = () => {
 
   return (  
     <BoxView direction="column" align="center" style={styles.container}>
-      <HeaderNavigation 
-        onSearchEdit={async (value: any) => await onSearchEdit(value)} 
-        onSearchSubmit={async (value: any) => await onSearchSubmit(value)} 
-        onSearchClear={async () => await onSearchClear()}
-      />
+      <HeaderNavigation />
 
       <BoxView 
         direction="column" 
