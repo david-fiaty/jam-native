fbd3e4a9ccf33c3d56cc06b11a4e34e1c54c36ac
david-fiaty <dfiaty@gmail.com>
2025-01-31 15:02:15 +0000

    Update main.tsx

--- app/main.tsx
+++ app/main.tsx
@@ -2,12 +2,16 @@ import { ThemeProvider } from '@rneui/themed';
 import ScreenView from '@/components/view/ScreenView';
 import BaseTheme from "@/constants/BaseTheme";
 import MainScreen from '@/components/screen/MainScreen';
+import HeaderNavigation from '@/components/navigation/HeaderNavigation';
+import FooterNavigation from '@/components/navigation/FooterNavigation';
 
 export default () => {
   return ( 
     <ThemeProvider theme={BaseTheme}>
       <ScreenView>
+        <HeaderNavigation />
         <MainScreen />
+        <FooterNavigation />
       </ScreenView>
     </ThemeProvider>
   );
