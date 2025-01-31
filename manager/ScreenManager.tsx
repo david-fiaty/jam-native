f5a90e1d99e7eb0fa7e8107def4997e5d824a725
david-fiaty <dfiaty@gmail.com>
2025-01-31 18:44:15 +0000

    Update ScreenManager.tsx

--- manager/ScreenManager.tsx
+++ manager/ScreenManager.tsx
@@ -18,14 +18,20 @@ class ScreenManager {
     this.statusBar = this.getStatusBarSize();
   }
 
-  pushScreen(router: any, path: string) {
+  pushScreen(router: any, path: string, params?: any) {
     this.toggleModal(null);
-    router.push(path);
+    router.push({
+      pathname: path,
+      params: params,
+    });
   }
 
-  replaceScreen(router: any, path: string) {
+  replaceScreen(router: any, path: string, params?: any) {
     this.toggleModal(null);
-    router.replace(path);
+    router.replace({
+      pathname: path,
+      params: params,
+    });
   }
 
   getStatusBarSize() {
