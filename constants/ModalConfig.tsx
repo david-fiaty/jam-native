bd601eaaae02463acc9efa99771b17b54ff3d948
david-fiaty <dfiaty@gmail.com>
2025-02-01 15:07:23 +0000

    Update ModalConfig.tsx

--- constants/ModalConfig.tsx
+++ constants/ModalConfig.tsx
@@ -9,6 +9,7 @@ import JammersList from "@/components/list/JammersList";
 import SectorsList from "@/components/list/SectorsList";
 import NotificationsMenu from "@/components/menu/NotificationsMenu";
 import SettingsMenu from "@/components/menu/SettingsMenu";
+import JamScreen from "@/components/screen/JamScreen";
 import JamsMapView from "@/components/view/JamsMapView";
 import LocationMapView from "@/components/view/LocationMapView";
 import MoreJamActionsView from "@/components/view/MoreJamActionsView";
@@ -74,6 +75,10 @@ const modals: any = [
   {
     name: 'MoreJamActionsView', 
     component: <MoreJamActionsView />,
+  },
+  {
+    name: 'JamScreen', 
+    component: <JamScreen />,
   },
 ];
 
