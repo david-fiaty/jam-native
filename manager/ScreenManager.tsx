dc366d8d40cde6986e6ba98dd8a5ecb8d294b9bd
david-fiaty <dfiaty@gmail.com>
2025-02-01 14:41:28 +0000

    Update ScreenManager.tsx

--- manager/ScreenManager.tsx
+++ manager/ScreenManager.tsx
@@ -2,6 +2,7 @@ import { Dimensions, ScaledSize, StatusBar } from 'react-native';
 import { Layout } from '@/constants/Layout';
 import { setMessage } from '@/redux/slices/MessageSlice';
 import { setActiveModal } from '@/redux/slices/ModalSlice';
+import { setActiveRoute } from '@/redux/slices/RouteSlice';
 import { Config } from '@/constants/Config';
 import Store from '@/redux/Store';
 
@@ -26,6 +27,10 @@ class ScreenManager {
     }
 
     Store.dispatch(setActiveModal(activeModals));
+    Store.dispatch(setActiveRoute([{
+      path: path,
+      params: params,
+    }]));
 
     router.push({
       pathname: path,
