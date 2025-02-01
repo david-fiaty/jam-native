1d7d70092879ad9e39c921df0fe7714436f013d9
david-fiaty <dfiaty@gmail.com>
2025-02-01 13:14:29 +0000

    Update ModalView.tsx

--- components/view/ModalView.tsx
+++ components/view/ModalView.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 import { BaseProps } from '@/constants/Types';
@@ -18,13 +18,10 @@ const ModalView = ({ children }: Props) => {
   const modalState = useSelector((state: any) => state.modal);
   const activeModal: any = ScreenManager.getActiveModal();
   const modalConfig: any = ModalConfig.build();
+  const [modalEffects, setModalEffects] = useState<any>({});
 
   const getModalEffects = () => {
-    if (isModalHidden()) {
-      return { in: 'fadeIn', out: 'fadeOut' };
-    }
 
-    return modalConfig.find((o: any) => o.name == activeModal?.name)?.effects;
   };
 
   const getModalContainerStyle = (): any => {
@@ -85,12 +82,21 @@ const ModalView = ({ children }: Props) => {
     return modalConfig.find((o: any) => o.name == ScreenManager.getActiveModal()?.name)?.component;
   };
 
+  useEffect(() => {
+    if (isModalHidden()) {
+      setModalEffects({ in: 'fadeIn', out: 'fadeOut' });
+    }
+    else {
+      setModalEffects(modalConfig.find((o: any) => o.name == activeModal?.name)?.effects);
+    }
+  }, []);
+
   return (
     <Modal
       coverScreen={false}
       hasBackdrop={false}
-      animationIn={getModalEffects()?.in}
-      animationOut={getModalEffects()?.out}
+      animationIn={modalEffects?.in}
+      animationOut={modalEffects?.out}
       isVisible={isModalVisible()}
       style={getModalContainerStyle()}
       hideModalContentWhileAnimating={true}
