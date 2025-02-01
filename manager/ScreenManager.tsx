60b2e310d6d61206e096053d724dd438fd3c7c49
david-fiaty <dfiaty@gmail.com>
2025-02-01 11:29:27 +0000

    Route slice

--- manager/ScreenManager.tsx
+++ manager/ScreenManager.tsx
@@ -2,9 +2,9 @@ import { Dimensions, ScaledSize, StatusBar } from 'react-native';
 import { Layout } from '@/constants/Layout';
 import { setMessage } from '@/redux/slices/MessageSlice';
 import { setActiveModal } from '@/redux/slices/ModalSlice';
+import { setActiveRoute } from '@/redux/slices/RouteSlice';
 import { Config } from '@/constants/Config';
 import Store from '@/redux/Store';
-
 
 class ScreenManager {
   messageTimeout?: any;
