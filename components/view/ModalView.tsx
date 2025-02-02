100c55bd449a4476e82d819de0bae81f071817dd
david-fiaty <dfiaty@gmail.com>
2025-02-02 17:00:08 +0000

    Update ModalView.tsx

--- components/view/ModalView.tsx
+++ components/view/ModalView.tsx
@@ -67,7 +67,7 @@ const ModalView = ({ children }: Props) => {
         <BackButton
           title={modalTitle}
           onPress={() => ScreenManager.toggleModal(modalName)}
-          containerStyle={styles.modalTitle}
+          containerStyle={Layout.modalTitle}
         />
       );
     }
@@ -105,13 +105,5 @@ const ModalView = ({ children }: Props) => {
     </Modal>
   );
 };
-
-const styles = StyleSheet.create({
-  modalTitle: {
-    marginLeft: Layout.space.base*1.5,
-    marginBottom: Layout.space.base*1.5,
-    width: '100%',
-  },
-});
 
 export default ModalView;
