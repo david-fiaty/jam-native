c14fe6ec577ad9f7fee2af86547f251de8e378dc
david-fiaty <dfiaty@gmail.com>
2025-02-04 17:17:48 +0000

    Update HostsList.tsx

--- components/list/HostsList.tsx
+++ components/list/HostsList.tsx
@@ -19,9 +19,9 @@ const HostsList = () => {
     (async () => {
       let data: any = await EntityManager.getJams({items_ids: [entityId]});
       setProfiles(await EntityManager.getProfiles({items_ids: data?.[0]?.collaborators}));
+      setIsLoaded(true);
     })();
 
-    setIsLoaded(true);
   }, [isLoaded, entityId]);
 
   if (!isLoaded) return <SpinnerView />;
