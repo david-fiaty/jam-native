43c62c285d6dd50e906a300ae39afc4f96b2be65
david-fiaty <dfiaty@gmail.com>
2025-03-10 17:37:48 +0000

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,12 @@ import BaseTheme from "@/constants/BaseTheme";
 import ScreenView from '@/components/view/ScreenView';
 import WelcomeScreen from '@/components/screen/WelcomeScreen';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(() => console.log('Service Worker registered'))
+    .catch(err => console.log('Service Worker registration failed', err));
+}
+
 export default () => {
   return ( 
     <ThemeProvider theme={BaseTheme}>
