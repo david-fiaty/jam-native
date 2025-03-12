1942afde6357c11ea9ef35a4eeb16a837abc41d8
david-fiaty <dfiaty@gmail.com>
2025-03-12 18:17:19 +0000

    Update UserManager.tsx

--- manager/UserManager.tsx
+++ manager/UserManager.tsx
@@ -81,7 +81,7 @@ class UserManager {
   }
 
   async getProfileId() {
-    let profileId: number = Store.getState().user.profileId;
+    let profileId: number = Store.getState()?.user?.profileId;
     if (profileId === 0) {
       let userAccount: any = await DataManager.get('currentUser');
       profileId = parseInt(userAccount?.profiles?.[0]?.id || 0);
@@ -183,7 +183,7 @@ class UserManager {
   };
 
   async updateLikedJams(entityId: number) {
-    let likedJams: any[] = [...Store.getState().user.likedJams];
+    let likedJams: any[] = [...Store.getState()?.user?.likedJams];
     
     if (likedJams.includes(entityId)) likedJams = likedJams.filter((v: any) => v !== entityId)
     else likedJams.push(entityId);
@@ -192,7 +192,7 @@ class UserManager {
   }
 
   async updateSavedJams(entityId: number) {
-    let savedJams: any[] = [...Store.getState().user.savedJams];
+    let savedJams: any[] = [...Store.getState()?.user?.savedJams];
     
     if (savedJams.includes(entityId)) savedJams = savedJams.filter((v: any) => v !== entityId)
     else savedJams.push(entityId);
@@ -201,7 +201,7 @@ class UserManager {
   }
 
   async updateLikedProjects(entityId: number) {
-    let likedProjects: any[] = [...Store.getState().user.likedProjects];
+    let likedProjects: any[] = [...Store.getState()?.user?.likedProjects];
     
     if (likedProjects.includes(entityId)) likedProjects = likedProjects.filter((v: any) => v !== entityId)
     else likedProjects.push(entityId);
@@ -210,7 +210,7 @@ class UserManager {
   }
 
   async updateSavedProjects(entityId: number) {
-    let savedProjects: any[] = [...Store.getState().user.savedProjects];
+    let savedProjects: any[] = [...Store.getState()?.user?.savedProjects];
     
     if (savedProjects.includes(entityId)) savedProjects = savedProjects.filter((v: any) => v !== entityId)
     else savedProjects.push(entityId);
