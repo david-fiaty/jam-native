29e670a84ba4ccd317ffa7495ddab71543e82c01
david-fiaty <dfiaty@gmail.com>
2025-03-14 11:49:57 +0000

    Delete webpack.config.js

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync(env, argv);
-
-  config.resolve.alias['react-native-maps'] = '@teovilla/react-native-web-maps';
-
-  return config;
-};
