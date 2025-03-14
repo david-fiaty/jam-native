52fd230f4fa48e67b1e9a67cf5040160d9778c41
david-fiaty <dfiaty@gmail.com>
2025-03-14 11:48:09 +0000

    Web gmap

--- components/view/LocationMapView.web.tsx
+++ components/view/LocationMapView.web.tsx
@@ -1,140 +1,15 @@
-import { useCallback, useMemo, useRef, useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
-import { ClusterProps, MarkerClusterer } from '@teovilla/react-native-web-maps';
-import type { Region } from 'react-native-maps';
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { Config } from "@/constants/Config";
 
-function MyClusterComponent(props: ClusterProps<{ onPress(): void }>) {
-  return (
-    <Marker
-      onPress={props.onPress}
-      coordinate={props.coordinate}
-      anchor={{ x: 0.5, y: 0.5 }}
-    >
-      <View style={styles.cluster}>
-        <Text style={styles.clusterText}>{props.pointCountAbbreviated}</Text>
-      </View>
-    </Marker>
-  );
-}
+const containerStyle = { width: "100%", height: "400px" };
+const center = { lat: 37.7749, lng: -122.4194 };
 
 const LocationMapView = () => {
-  const [region, setRegion] = useState<Region | null>(null);
-
-  const mapRef = useRef<MapView>(null);
-
-  const loadingFallback = useMemo(() => {
-    return (
-      <View>
-        <Text>Loading</Text>
-      </View>
-    );
-  }, []);
-
   return (
-    <View style={styles.container}>
-      <MapView
-        ref={mapRef}
-        provider="google"
-        style={{ flex: 1 }}
-        onRegionChange={setRegion}
-        loadingFallback={loadingFallback}
-        googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY}
-      >
-        <MarkerClusterer
-          region={region}
-          renderCluster={(cluster) => (
-            <MyClusterComponent
-              {...cluster}
-              onPress={() =>
-                mapRef.current?.animateCamera({
-                  center: cluster.coordinate,
-                  zoom: cluster.expansionZoom + 3,
-                })
-              }
-            />
-          )}
-        >
-          <Marker
-            coordinate={{
-              latitude: 59.33956246905637,
-              longitude: 18.050015441134114,
-            }}
-          />
-          <Marker
-            coordinate={{
-              latitude: 59.3442016958775,
-              longitude: 18.038256636812825,
-            }}
-          />
-        </MarkerClusterer>
-        {/* {SHOW_LOCATION_BUBBLE && position && (
-            <>
-              <Marker
-                anchor={{ x: 0.5, y: 1 }}
-                coordinate={{
-                  latitude: position.coords.latitude,
-                  longitude: position.coords.longitude,
-                }}
-              >
-                <View
-                  style={{
-                    paddingHorizontal: 10,
-                    paddingVertical: 5,
-                    borderRadius: 999,
-                    backgroundColor: 'salmon',
-                    position: 'relative',
-                    zIndex: 19,
-                  }}
-                >
-                  <View
-                    style={{
-                      position: 'absolute',
-                      width: 10,
-                      height: 10,
-                      backgroundColor: 'salmon',
-                      zIndex: 20,
-                      bottom: -5,
-                      alignSelf: 'center',
-                      transform: [{ rotate: '45deg' }],
-                    }}
-                  ></View>
-                  <Text>You are here</Text>
-                </View>
-                {Platform.select({
-                  native: <View style={{ height: 7 }}></View>,
-                })}
-              </Marker>
-              <Circle
-                center={{
-                  latitude: position.coords.latitude,
-                  longitude: position.coords.longitude,
-                }}
-                fillColor="rgba(0,0,0,0.5)"
-                radius={100}
-              />
-            </>
-          )} */}
-      </MapView>
-    </View>
+    <LoadScript googleMapsApiKey={'AIzaSyB91uv-OuWw6iNMjVZmuHJsRVDHP9baCq0'}>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10} />
+    </LoadScript>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  cluster: {
-    backgroundColor: 'salmon',
-    width: 20,
-    height: 20,
-    borderRadius: 999,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  clusterText: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default LocationMapView;
