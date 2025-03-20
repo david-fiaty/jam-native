c82c7cc636c232f84c8d04923bfe582b091ac4f6
david-fiaty <dfiaty@gmail.com>
2025-03-20 13:38:39 +0000

    Update ShareOptionsList.web.tsx

--- components/list/ShareOptionsList.web.tsx
+++ components/list/ShareOptionsList.web.tsx
@@ -1,7 +1,14 @@
 import { useState, useEffect } from 'react';
 import { View } from "react-native";
 import { Layout } from "@/constants/Layout";
-import TextView from "../view/TextView";
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  WhatsappIcon,
+} from "react-share";
 import i18n from "@/translation/i18n";
 import BoxView from "../view/BoxView";
 import ListView from "../view/ListView";
@@ -14,6 +21,46 @@ const ShareOptionsList = () => {
   const [profiles, setProfiles] = useState<any>(null);
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const entityId: number = ScreenManager.getModalEntityId();
+  const iconSize: number = 40;
+
+  const shareOptions: any[] = [
+    {
+      key: 'facebook',
+      label: i18n.t('Facebook'),
+      url: 'http://www.facebook.com',
+      render: (params: any) => {
+        return (
+          <FacebookShareButton {...params}>
+            <FacebookIcon size={iconSize} round={true} />
+          </FacebookShareButton>
+        );
+      }, 
+    },
+    {
+      key: 'twitter',
+      label: i18n.t('Twitter'),
+      url: 'http://www.twitter.com',
+      render: (params: any) => {
+        return (
+          <TwitterShareButton {...params}>
+            <TwitterIcon size={iconSize} round={true} />
+          </TwitterShareButton>
+        );
+      }, 
+    },
+    {
+      key: 'whatsapp',
+      label: i18n.t('Whatsapp'),
+      url: 'http://www.whatsapp.com',
+      render: (params: any) => {
+        return (
+          <WhatsappShareButton {...params}>
+            <WhatsappIcon size={iconSize} round={true} />
+          </WhatsappShareButton>
+        );
+      }, 
+    },
+  ];
 
   useEffect(() => {
     (async () => {
