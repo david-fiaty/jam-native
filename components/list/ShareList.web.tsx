c65677c83bae43c563e56a9dee950271a0250514
david-fiaty <dfiaty@gmail.com>
2025-03-20 13:24:35 +0000

    Update ShareList.web.tsx

--- components/list/ShareList.web.tsx
+++ components/list/ShareList.web.tsx
@@ -1,13 +1,47 @@
-import { StyleSheet } from 'react-native';
+import { useState, useEffect } from 'react';
+import { View } from "react-native";
+import { Layout } from "@/constants/Layout";
+import TextView from "../view/TextView";
+import i18n from "@/translation/i18n";
+import BoxView from "../view/BoxView";
+import ListView from "../view/ListView";
+import SpinnerView from "../view/SpinnerView";
+import EntityManager from '@/manager/EntityManager';
+import ProfileListItem from './ListItem/ProfileListItem';
+import ScreenManager from '@/manager/ScreenManager';
 
 const ShareList = () => {
+  const [profiles, setProfiles] = useState<any>(null);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const entityId: number = ScreenManager.getModalEntityId();
+
+  useEffect(() => {
+    (async () => {
+      let data: any = await EntityManager.getJams({items_ids: [entityId]});
+      setProfiles(await EntityManager.getProfiles({items_ids: data?.[0]?.collaborators}));
+      setIsLoaded(true);
+    })();
+
+  }, [isLoaded, entityId]);
+
+  if (!isLoaded) return <SpinnerView />;
+
   return (
-    <></>
+    <BoxView align="flex-start" justify="flex-start" style={Layout.screenContent}>
+      <View style={Layout.borderedListContainer}>
+        {profiles?.length > 0 &&
+          <ListView
+            data={profiles}
+            renderItem={(row: any) => <ProfileListItem item={row.item} />}
+          />
+        }
+
+        {!profiles?.length && 
+          <TextView>{i18n.t('No hosts available for this Jam.')}</TextView>
+        }
+      </View>
+    </BoxView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {},
-});
 
 export default ShareList;
