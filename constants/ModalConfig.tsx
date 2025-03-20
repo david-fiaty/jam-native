085e697d70fc9ea343c6b867951c0574ad17741c
david-fiaty <dfiaty@gmail.com>
2025-03-20 13:22:58 +0000

    Update ModalConfig.tsx

--- constants/ModalConfig.tsx
+++ constants/ModalConfig.tsx
@@ -7,6 +7,7 @@ import CountriesList from "@/components/list/CountriesList";
 import HostsList from "@/components/list/HostsList";
 import JammersList from "@/components/list/JammersList";
 import SectorsList from "@/components/list/SectorsList";
+import ShareList from "@/components/list/ShareList";
 import NotificationsMenu from "@/components/menu/NotificationsMenu";
 import SettingsMenu from "@/components/menu/SettingsMenu";
 import JamsMapView from "@/components/view/JamsMapView";
@@ -74,6 +75,10 @@ const modals: any = [
   {
     name: 'MoreJamActionsView', 
     component: <MoreJamActionsView />,
+  },
+  {
+    name: 'ShareList', 
+    component: <ShareList />,
   },
 ];
 
