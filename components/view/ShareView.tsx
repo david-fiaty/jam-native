6802a2be9dbba71a76245a6c329c3b128505e434
david-fiaty <dfiaty@gmail.com>
2025-03-20 13:10:08 +0000

    Create ShareView.tsx

---
+++ components/view/ShareView.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+const ShareView = () => {
+  return (
+    <></>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default ShareView;
