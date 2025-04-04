8ec44bb755619026b0f9bb7d85ba1cb73efcd035
david-fiaty <dfiaty@gmail.com>
2025-04-04 13:50:48 +0000

    Update ProfileManager.tsx

--- manager/ProfileManager.tsx
+++ manager/ProfileManager.tsx
@@ -4,18 +4,29 @@ import { Config } from '@/constants/Config';
 class ProfileManager {
   getFields() {
     return {
-      profile_picture: {},
-      email: {},
       profile_name: {},
-      profile_type: {},
       profile_description: {},
-      sectors_ids: {},
+      upload_profile_picture: null,
+      upload_other_docs: null,
+      sectors_ids: [1, 12],
+      scope_country_code: {},
+      region: 2,
+      town_or_locality: 2,
+      other_town_or_locality: {},
+      address: {},
       geolocation_latitude: {},
       geolocation_longitude: {},
-      address: {},
-      city: {},
-      region: {},
-      country: {},
+      email: {},
+      whatsapp_number: {},
+      phone_number: {},
+      website_link: {},
+      instagram_id: {},
+      facebook_link: {},
+      linkedin_link: {},
+      profile_type: {},
+      profile_personal: {},
+      profile_organization: {},
+      profile_venue: {},      
     };
   }
 };
