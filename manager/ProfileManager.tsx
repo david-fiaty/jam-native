e2c7d0047a8397f8e796e57d78a452a3875c2559
david-fiaty <dfiaty@gmail.com>
2025-04-06 16:07:19 +0000

    Update ProfileManager.tsx

--- manager/ProfileManager.tsx
+++ manager/ProfileManager.tsx
@@ -28,36 +28,6 @@ class ProfileManager {
     Store.dispatch(setFormData(payload));
   }
 
-  getFormValue(key: string) {
-    let formData: any = this.getFormData();
-    let formValue: any = formData?.[key] || '';
-
-    return formValue;
-  }
-
-  getFormData() {
-    return {...Store.getState().form?.profile};
-  }
-
-  renderFields() {
-    let formData: any = this.getFormData();
-
-    return (
-      <BoxView
-        align="flex-start"
-        justify="flex-start"
-        scroll={true}
-        style={[Layout.screenContent, this.getContainerStyles()]}
-      >
-        <View style={Layout.formContainer}>
-          {this.getFields().map((item: any) => {
-            return this.renderField(item, formData);
-          })}
-        </View>
-      </BoxView>
-    );
-  }
-
   canRenderField(item: any, formData: any) {
     return item.enabled === true 
       && (item.group === 'all' || item.group === formData?.['profile_type']);
@@ -68,7 +38,7 @@ class ProfileManager {
       return (
         <View key={item.key}>
           <TextView>{i18n.t(item.label)} {item?.required === true ? '*' : ''}</TextView>
-          {item.render(item, {})}
+          {item.render(item, formData, {})}
         </View>
       );
     }
@@ -86,10 +56,10 @@ class ProfileManager {
         key: 'upload_profile_picture',
         label: i18n.t('Profile picture'),
         group: 'all',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -104,10 +74,10 @@ class ProfileManager {
         key: 'profile_type',
         label: i18n.t('Profile type'),
         group: 'all', 
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <ProfileTypeField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               onChangeValue={(option: any) => this.setFormData(item, option.value)}
             />
           );
@@ -121,10 +91,10 @@ class ProfileManager {
         key: 'profile_name',
         label: i18n.t('Profile name'),
         group: 'all',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -139,10 +109,10 @@ class ProfileManager {
         key: 'profile_description',
         label: i18n.t('About'),
         group: 'all',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextareaField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -157,10 +127,10 @@ class ProfileManager {
         key: 'upload_other_docs',
         label: i18n.t('Other documents'),
         group: 'all',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -175,10 +145,10 @@ class ProfileManager {
         key: 'sectors_ids',
         label: i18n.t('Sectors'),
         group: 'all',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -193,10 +163,10 @@ class ProfileManager {
         key: 'scope_country_code',
         label: i18n.t('Country'),
         group: 'all',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -211,10 +181,10 @@ class ProfileManager {
         key: 'region',
         label: i18n.t('Region'),
         group: 'all',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -229,10 +199,10 @@ class ProfileManager {
         key: 'town_or_locality',
         label: i18n.t('Locality'),
         group: 'all',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -247,10 +217,10 @@ class ProfileManager {
         key: 'other_town_or_locality',
         label: i18n.t('Other locality'),
         group: 'all',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -265,10 +235,10 @@ class ProfileManager {
         key: 'address',
         label: i18n.t('Address'),
         group: 'all',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -283,10 +253,10 @@ class ProfileManager {
         key: 'geolocation_latitude',
         label: i18n.t('Latitude'),
         group: 'all',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -301,10 +271,10 @@ class ProfileManager {
         key: 'geolocation_longitude',
         label: i18n.t('Longitude'),
         group: 'all',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -319,10 +289,10 @@ class ProfileManager {
         key: 'email',
         label: i18n.t('Email'),
         group: 'all',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -337,10 +307,10 @@ class ProfileManager {
         key: 'whatsapp_number',
         label: i18n.t('Whatsapp number'),
         group: 'all',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -355,10 +325,10 @@ class ProfileManager {
         key: 'phone_number',
         label: i18n.t('Phone number'),
         group: 'all',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -373,10 +343,10 @@ class ProfileManager {
         key: 'website_link',
         label: i18n.t('Website link'),
         group: 'all',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -391,10 +361,10 @@ class ProfileManager {
         key: 'instagram_id',
         label: i18n.t('Instagram ID'),
         group: 'all',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -409,10 +379,10 @@ class ProfileManager {
         key: 'facebook_link',
         label: i18n.t('Facebook link'),
         group: 'all',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -427,10 +397,10 @@ class ProfileManager {
         key: 'linkedin_link',
         label: i18n.t('Linkedin link'),
         group: 'all',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -445,10 +415,10 @@ class ProfileManager {
         key: 'profile_personal',
         label: i18n.t('Personal profile'),
         group: 'personal',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -463,10 +433,10 @@ class ProfileManager {
         key: 'profile_organization',
         label: i18n.t('Organization profile'),
         group: 'organization',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -481,10 +451,10 @@ class ProfileManager {
         key: 'profile_venue',
         label: i18n.t('Venue profile'),
         group: 'venue',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
@@ -499,10 +469,10 @@ class ProfileManager {
         key: 'password',
         label: i18n.t('Password'),
         group: 'all',
-        render: (item: any, params: any) => {
+        render: (item: any, data: any, params?: any) => {
           return (
             <InputTextField
-              value={this.getFormValue(item.key)}
+              value={data[item.key]}
               placeholder={item.label}
               onChangeText={(value: string) => this.setFormData(item, value)}
             />
