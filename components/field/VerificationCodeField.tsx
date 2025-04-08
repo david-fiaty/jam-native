7fcfea9add8b3fc9ed8a3639652e3d2e46e650c6
david-fiaty <dfiaty@gmail.com>
2025-04-08 13:45:05 +0000

    Update VerificationCodeField.tsx

--- components/field/VerificationCodeField.tsx
+++ components/field/VerificationCodeField.tsx
@@ -1,34 +1,26 @@
-import { StyleSheet } from "react-native";
-import { Input } from "@rneui/themed";
 import { BaseProps } from "@/constants/Types";
-import { Colors } from "@/constants/Colors";
-import BoxView from "../view/BoxView";
 import TextView from "../view/TextView";
 import InputTextField from "./InputTextField";
 import i18n from "@/translation/i18n";
 
 type Props = BaseProps & {
   value?: string;
+  disabled?: boolean;
   onChangeText: (value: any) => void;
 };
 
-const VerificationCodeField = ({ value, onChangeText }: Props) => {
+const VerificationCodeField = ({ value, disabled, onChangeText }: Props) => {
   return (
     <>
       <TextView>{i18n.t('Verification code sent, check your mailbox')}</TextView>
       <InputTextField
         placeholder={i18n.t('Enter verification code')}
         value={value || ''}
+        disabled={disabled}
         onChangeText={(value: any) => onChangeText(value) }
       />
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-  },
-});
 
 export default VerificationCodeField;
