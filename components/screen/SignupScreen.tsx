15c508412cd438d7b9e6d976ac780f4668be0369
david-fiaty <dfiaty@gmail.com>
2025-04-09 12:00:47 +0000

    Update SignupScreen.tsx

--- components/screen/SignupScreen.tsx
+++ components/screen/SignupScreen.tsx
@@ -116,98 +116,89 @@ const SignupScreen = () => {
       </TextView>
 
       <DividerView />
-    
-      <BoxView
-        align="flex-start"
-        justify="flex-start"
-        scroll={true}
-        style={[Layout.screenContent, ProfileManager.getStyles().container]}
-      >
-        <View style={Layout.formContainer}>
-          {(!isEmailStepValid || !isCodeStepValid) && (
-            <>
-              <TextView>{i18n.t('Email')}</TextView>
-              <InputTextField
-                value={formData?.email || ''}
-                disabled={isEmailStepValid}
-                placeholder={i18n.t('Enter your email address')}
-                onChangeText={(value: string) => updateField('email', value)}
-              />
-            </>
-          )}
 
-          {isEmailStepValid && !isCodeStepValid && (
-            <>
-              {ProfileManager.renderField('signup', findField('email'), formData, { disabled: true })}
+      {(!isEmailStepValid || !isCodeStepValid) && (
+        <>
+          <TextView>{i18n.t('Email')}</TextView>
+          <InputTextField
+            value={formData?.email || ''}
+            disabled={isEmailStepValid}
+            placeholder={i18n.t('Enter your email address')}
+            onChangeText={(value: string) => updateField('email', value)}
+          />
+        </>
+      )}
 
-              <VerificationCodeField
-                value={formData?.code || ''}
-                onChangeText={(value: any) => updateField('code', value)}
-                disabled={false}
-              />
-            </>
-          )}
+      {isEmailStepValid && !isCodeStepValid && (
+        <>
+          {ProfileManager.renderField('signup', findField('email'), formData, { disabled: true })}
 
-          {isEmailStepValid && isCodeStepValid && (
-            <>
-              <ProfileImageField 
-                value={formData?.profile_picture?.url}
-                onChangeValue={(mediaList: any) => updateField('profile_picture', {url: mediaList[0]?.uri})}
-              />
+          <VerificationCodeField
+            value={formData?.code || ''}
+            onChangeText={(value: any) => updateField('code', value)}
+            disabled={false}
+          />
+        </>
+      )}
 
-              <DividerView />
-              <ProfileTypeField
-                value={formData?.profile_type}
-                onChangeValue={(option: any) => updateField('profile_type', option.value)}
-              />
-            </>
-          )}
-
-          {isEmailStepValid && isCodeStepValid && formData?.profile_type?.length > 0 && profileFields.map((item: any) => {
-            return ProfileManager.renderField('signup', item, formData);
-          })}
-
-          <ButtonView
-            label={i18n.t('Continue')}
-            isProcessing={isProcessing}
-            disabled={isSubmitDisabled()}
-            onPress={submitForm}
+      {isEmailStepValid && isCodeStepValid && (
+        <>
+          <ProfileImageField
+            value={formData?.profile_picture?.url}
+            onChangeValue={(mediaList: any) => updateField('profile_picture', { url: mediaList[0]?.uri })}
           />
 
-          {isEmailStepValid && !isCodeStepValid && (
-            <BoxView
-              direction="row"
-              align="center"
-              justify="space-between"
-              style={{ width: "100%" }}
-            >
-              <BoxView direction="row" align="center" justify="flex-start">
-                <LinkView onPress={() => router.replace("/signup")}>
-                  {i18n.t("Didn't receive code?")}
-                </LinkView>
-              </BoxView>
-              <SkipButton onPress={async () => router.replace(Config.mainRoute)} />
-            </BoxView>
-          )}
+          <DividerView />
+          <ProfileTypeField
+            value={formData?.profile_type}
+            onChangeValue={(option: any) => updateField('profile_type', option.value)}
+          />
+        </>
+      )}
 
-          {!isEmailStepValid && (
-            <BoxView
-              direction="row"
-              align="center"
-              justify="space-between"
-              style={{ width: "100%" }}
-            >
-              <BoxView direction="row" align="center" justify="flex-start">
-                <TextView>{i18n.t("You have an account?")}</TextView>
-                <LinkView onPress={async () => router.replace("/login")}>
-                  {i18n.t("Sign in")}
-                </LinkView>
-              </BoxView>
-              <SkipButton onPress={async () => router.replace(Config.mainRoute)} />
-            </BoxView>
-          )}
-        </View>
-      </BoxView>
+      {isEmailStepValid && isCodeStepValid && formData?.profile_type?.length > 0 && profileFields.map((item: any) => {
+        return ProfileManager.renderField('signup', item, formData);
+      })}
+
+      <ButtonView
+        label={i18n.t('Continue')}
+        isProcessing={isProcessing}
+        disabled={isSubmitDisabled()}
+        onPress={submitForm}
+      />
+
+      {isEmailStepValid && !isCodeStepValid && (
+        <BoxView
+          direction="row"
+          align="center"
+          justify="space-between"
+          style={{ width: "100%" }}
+        >
+          <BoxView direction="row" align="center" justify="flex-start">
+            <LinkView onPress={() => router.replace("/signup")}>
+              {i18n.t("Didn't receive code?")}
+            </LinkView>
+          </BoxView>
+          <SkipButton onPress={async () => router.replace(Config.mainRoute)} />
+        </BoxView>
+      )}
+
+      {!isEmailStepValid && (
+        <BoxView
+          direction="row"
+          align="center"
+          justify="space-between"
+          style={{ width: "100%" }}
+        >
+          <BoxView direction="row" align="center" justify="flex-start">
+            <TextView>{i18n.t("You have an account?")}</TextView>
+            <LinkView onPress={async () => router.replace("/login")}>
+              {i18n.t("Sign in")}
+            </LinkView>
+          </BoxView>
+          <SkipButton onPress={async () => router.replace(Config.mainRoute)} />
+        </BoxView>
+      )}
     </BoxView>
   );
 };
