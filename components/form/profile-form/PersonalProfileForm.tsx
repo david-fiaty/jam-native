9cf1242be56cedc4fa46ca90905c951a5edd9c2a
david-fiaty <dfiaty@gmail.com>
2025-04-09 15:24:39 +0000

    Update PersonalProfileForm.tsx

--- components/form/profile-form/PersonalProfileForm.tsx
+++ components/form/profile-form/PersonalProfileForm.tsx
@@ -1,14 +1,42 @@
 import { StyleSheet } from 'react-native';
 import { BaseProps } from '@/constants/Types';
+import { useSelector, useDispatch } from "react-redux";
+import i18n from '@/translation/i18n';
+import InputTextField from '@/components/field/InputTextField';
+import TextView from '@/components/view/TextView';
 
 type Props = BaseProps & {
+  resource?: any;
   item?: any;
   data?: any;
   params?: any;
 };
 
-const PersonalProfileForm = ({item, data, params}: Props) => {
-  return (<></>);
+const PersonalProfileForm = ({resource, item, data, params}: Props) => {
+  const formData: any = useSelector((state: any) => state.form[resource]);
+
+  const fields: any = [
+    {
+      signup: true,
+      profile: true,
+      enabled: true,
+      required: false,
+      key: 'profile_name',
+      label: i18n.t('Profile name (with no spaces)'),
+      profileType: 'all',
+      render: (item: any, data: any, params?: any) => {
+        return (
+          <InputTextField
+            value={data[item.key]}
+            placeholder={i18n.t('Profile name')}
+            onChangeText={(value: string) => {}}
+          />
+        );
+      },
+    },
+  ];
+
+  return (<TextView>personal profile fields</TextView>);
 };
 
 const styles = StyleSheet.create({
