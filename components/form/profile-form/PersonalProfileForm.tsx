cb58f1518da60a151ad198fbbaff69ba9db98b2c
david-fiaty <dfiaty@gmail.com>
2025-04-10 19:43:18 +0000

    Update PersonalProfileForm.tsx

--- components/form/profile-form/PersonalProfileForm.tsx
+++ components/form/profile-form/PersonalProfileForm.tsx
@@ -92,11 +92,7 @@ const PersonalProfileForm = ({ resource, item, data, params, parentKey }: Props)
 
   return (
     <View style={styles.container}>
-      {fields.map((o: any) => {
-        if (o?.enabled === true) {
-          return ProfileManager.renderSubField(o, formData);
-        }
-      })}
+      {fields.map((o: any) => ProfileManager.renderSubField(o, formData))}
     </View>
   );
 }
