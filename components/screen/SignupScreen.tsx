fba945c10c3b2e0ea069d5c1fe2d16832fe42cf7
david-fiaty <dfiaty@gmail.com>
2025-04-14 17:30:48 +0000

    Update SignupScreen.tsx

--- components/screen/SignupScreen.tsx
+++ components/screen/SignupScreen.tsx
@@ -12,7 +12,12 @@ import SignupForm from '../form/SignupForm';
 const SignupScreen = () => {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const formData = useSelector((state: any) => state.form?.profile);
   
+  const isContainerScrollable = () => {
+    return formData?.profile_type?.length > 0;
+  };
+
   useEffect(() => {
     if (!isLoaded) {
       dispatch(setValue(null));
@@ -25,7 +30,8 @@ const SignupScreen = () => {
       direction="column" 
       align="center" 
       justify="center" 
-      style={Layout.screenContent}
+      style={[Layout.screenContent, isContainerScrollable() ? { paddingTop: Layout.space.base*4 } : {}]}
+      scroll={isContainerScrollable()}
     >
       <LogoView size={80} />    
       <TextView style={styles.slogan}>{i18n.t('Create your JAM account')}</TextView> 
