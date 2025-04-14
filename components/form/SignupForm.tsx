39b4135b2038e090daab7504ab8473b77bb94108
david-fiaty <dfiaty@gmail.com>
2025-04-14 14:50:04 +0000

    Update SignupForm.tsx

--- components/form/SignupForm.tsx
+++ components/form/SignupForm.tsx
@@ -1,11 +1,14 @@
+import { useSelector } from "react-redux";
 import SignupCodeForm from './signup-form/SignupCodeForm';
 import SignupEmailForm from './signup-form/SignupEmailForm';
 
 const SignupForm = () => {
+  const formData: any = useSelector((state: any) => state.signup);
+
   return (
     <>
       <SignupEmailForm />
-      <SignupCodeForm />
+      { formData?.session?.length > 0 && <SignupCodeForm /> }
     </>
   );
 };
