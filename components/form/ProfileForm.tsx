17e99ad501b2407b2f2d4d218a180640d7df985d
david-fiaty <dfiaty@gmail.com>
2025-04-14 16:47:08 +0000

    Update ProfileForm.tsx

--- components/form/ProfileForm.tsx
+++ components/form/ProfileForm.tsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import TextView from "../view/TextView";
 import ProfileManager from "@/manager/ProfileManager";
 
 const ProfileForm = () => {
+  const formData: any = useSelector((state: any) => state.signup);
   const profileFields: any = ProfileManager.getFields();
 
-  return profileFields.map((item: any) => {
-    return ProfileManager.renderField('signup', item, {});
-  });
+  return (
+    <>
+      {profileFields.map((item: any) => {
+        return ProfileManager.renderField('signup', item, {});
+      })}
+    </>
+  );
 };
 
 export default ProfileForm;
