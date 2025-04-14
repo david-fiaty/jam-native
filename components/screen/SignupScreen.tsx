cf0fcfbcca2e9d4cddb9eabee02d8eb5b5100bc8
david-fiaty <dfiaty@gmail.com>
2025-04-14 17:58:42 +0000

    Update SignupScreen.tsx

--- components/screen/SignupScreen.tsx
+++ components/screen/SignupScreen.tsx
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
   },
   scrollableContainer: {
     paddingTop: Layout.space.base*4,
-    paddingBottom: Layout.space.base*4,
+    paddingBottom: Layout.space.base*2,
   },
 });
 
