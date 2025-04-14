faf63f65384216fd359292baf9bc392ede436c3e
david-fiaty <dfiaty@gmail.com>
2025-04-14 15:17:53 +0000

    Update SignupEmailForm.tsx

--- components/form/signup-form/SignupEmailForm.tsx
+++ components/form/signup-form/SignupEmailForm.tsx
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet } from 'react-native';
+import { useRouter } from "expo-router";
 import { useSelector, useDispatch } from "react-redux";
 import { setValue } from '@/redux/slices/SignupSlice';
 import { Layout } from '@/constants/Layout';
+import { Config } from "@/constants/Config";
 import i18n from "@/translation/i18n";
 import InputTextField from '@/components/field/InputTextField';
 import TextView from '@/components/view/TextView';
 import ButtonView from '@/components/view/ButtonView';
 import UserManager from "@/manager/UserManager";
+import BoxView from "@/components/view/BoxView";
+import LinkView from "@/components/view/LinkView";
+import SkipButton from "@/components/button/SkipButton";
 
 const SignupEmailForm = () => {
   const dispatch = useDispatch();
+  const router = useRouter();
   const [isProcessing, setIsProcessing] = useState<boolean>(false);
   const formData: any = useSelector((state: any) => state.signup);
 
@@ -21,7 +27,7 @@ const SignupEmailForm = () => {
     }));
   };
 
-  const submitData = async () => {    
+  const submitData = async () => {
     setIsProcessing(true);
 
     let result: any = await UserManager.sendSignupCode({
@@ -35,14 +41,14 @@ const SignupEmailForm = () => {
     setIsProcessing(false);
   };
 
-  const isSubmitButtonDisabled = () => { 
+  const isSubmitButtonDisabled = () => {
     return !formData?.email?.length;
   };
 
-  const isEmailFieldDisabled = () => { 
+  const isEmailFieldDisabled = () => {
     return formData?.email?.length && formData?.session?.length;
   };
-  
+
   return (
     <>
       <TextView style={styles.label}>{i18n.t('Email')}</TextView>
@@ -56,14 +62,33 @@ const SignupEmailForm = () => {
       {!isEmailFieldDisabled() && (
         <ButtonView
           label={i18n.t('Continue')}
-          isProcessing={isProcessing} 
-          onPress={submitData} 
+          isProcessing={isProcessing}
+          onPress={submitData}
           disabled={isSubmitButtonDisabled()}
         />
       )}
+
+      {!isEmailFieldDisabled() && (
+        <>
+          <BoxView
+            direction="row"
+            align="center"
+            justify="space-between"
+            style={{ width: "100%" }}
+          >
+            <BoxView direction="row" align="center" justify="flex-start">
+              <TextView>{i18n.t("You have an account?")}</TextView>
+              <LinkView onPress={async () => router.replace("/login")}>
+                {i18n.t("Sign in")}
+              </LinkView>
+            </BoxView>
+            <SkipButton onPress={async () => router.replace(Config.mainRoute)} />
+          </BoxView>
+        </>
+      )}
     </>
   );
-};
+}
 
 const styles = StyleSheet.create({
   slogan: {
