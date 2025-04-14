5ef5e35b7c00b3d4978eb516162820f9e1765145
david-fiaty <dfiaty@gmail.com>
2025-04-14 13:00:54 +0000

    Update SignupForm.tsx

--- components/form/SignupForm.tsx
+++ components/form/SignupForm.tsx
@@ -1,43 +1,9 @@
-import { StyleSheet } from 'react-native';
-import { Layout } from '@/constants/Layout';
-import i18n from "@/translation/i18n";
-import InputTextField from '../field/InputTextField';
-import TextView from '../view/TextView';
-import ButtonView from '../view/ButtonView';
+import SignupEmailForm from './signup-form/SignupEmailForm';
 
 const SignupForm = () => {
   return (
-    <>
-      <TextView style={styles.label}>{i18n.t('Email')}</TextView>
-      <InputTextField
-        //value={formData?.email || ''}
-        //disabled={isEmailStepValid}
-        placeholder={i18n.t('Enter your email address')}
-      //onChangeText={(value: string) => updateField('email', value)}
-      />
-
-      <ButtonView
-        label={i18n.t('Continue')}
-        //isProcessing={isProcessing} 
-        /*
-        onPress={() => {
-          setIsProcessing(true);
-          submitForm();
-        }} 
-        */
-      />
-    </>
+    <SignupEmailForm />
   );
 };
-
-const styles = StyleSheet.create({
-  slogan: {
-    textTransform: 'uppercase',
-    fontSize: Layout.fontSize.base,
-  },
-  label: {
-    alignSelf: 'flex-start',
-  },
-});
 
 export default SignupForm;
