eed8aec3fa6e851a7aaf66c56f091f924ed041d1
david-fiaty <dfiaty@gmail.com>
2025-04-15 16:23:19 +0000

    Update ScreenManager.tsx

--- manager/ScreenManager.tsx
+++ manager/ScreenManager.tsx
@@ -175,12 +175,9 @@ class ScreenManager {
   }
 
   getFooterSize() {
-
-    let activeModals: any = [...Store.getState().modal.active];
-
-    console.log(activeModals);
+    let activeModal: any = this.getActiveModal();
     let factor: number = 16;
-    let height: number = this.window.height/factor;
+    let height: number = activeModal?.footer === true ? this.window.height/factor : 0;
 
     return {
       width: this.window.width,
