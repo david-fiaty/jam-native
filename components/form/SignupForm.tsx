bfb90374d3e046ef45783a63b429ed42658dc78d
david-fiaty <dfiaty@gmail.com>
2025-04-15 13:52:09 +0000

    Update SignupForm.tsx

--- components/form/SignupForm.tsx
+++ components/form/SignupForm.tsx
@@ -1,28 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { setValue } from "@/redux/slices/SignupSlice";
+import { useSelector } from "react-redux";
 import SignupCodeForm from './signup-form/SignupCodeForm';
 import SignupEmailForm from './signup-form/SignupEmailForm';
 import ProfileForm from "./ProfileForm";
-import DataManager from "@/manager/DataManager";
 
 const SignupForm = () => {
-  const dispatch = useDispatch();
-  const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const formData: any = useSelector((state: any) => state.signup);
 
-  useEffect(() => {
-    if (!isLoaded) {
-      dispatch(setValue(null));
-      setIsLoaded(true);
-    }
-  }, [isLoaded]);
-  
   return (
     <>
-      {formData?.success !== true && <SignupEmailForm key={DataManager.createUuid()} />}
-      {formData?.success !== true && formData?.session?.length > 0 && <SignupCodeForm key={DataManager.createUuid()} />}
-      {formData?.success === true && <ProfileForm key={DataManager.createUuid()} />}
+      {formData?.success !== true && <SignupEmailForm />}
+      {formData?.success !== true && formData?.session?.length > 0 && <SignupCodeForm />}
+      {formData?.success === true && <ProfileForm />}
     </>
   );
 };
