9c53818e31d934f74f4e2a12c757ba726ec5293d
david-fiaty <dfiaty@gmail.com>
2025-04-16 15:15:08 +0000

    Update [section].tsx

--- app/[section].tsx
+++ app/[section].tsx
@@ -1,8 +1,6 @@
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Link } from 'expo-router';
 import { Colors } from '@/constants/Colors';
-import { BaseProps } from '@/constants/Types';
 import BoxView from '@/components/view/BoxView';
 import TextView from '@/components/view/TextView';
 
