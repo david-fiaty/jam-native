1a14d029a9c4d729e95cc41a5c11c3d551568a45
david-fiaty <dfiaty@gmail.com>
2025-04-16 15:58:49 +0000

    Update SectionView.tsx

--- components/view/SectionView.tsx
+++ components/view/SectionView.tsx
@@ -1,9 +1,5 @@
 import { StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Link } from 'expo-router';
 import { Colors } from '@/constants/Colors';
-import { BaseProps } from '@/constants/Types';
-import BoxView from '@/components/view/BoxView';
 import TextView from './TextView';
 import WelcomeSection from '../section/WelcomeSection';
 
