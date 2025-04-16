ed27872ebf3dcff4712411b4609b42fe9b8a796e
david-fiaty <dfiaty@gmail.com>
2025-04-16 15:02:42 +0000

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,10 @@
 import { StyleSheet } from 'react-native';
-import { Stack, useSegments, useRouter, Link } from 'expo-router';
 import { useGlobalSearchParams } from 'expo-router';
 import { ThemeProvider } from '@rneui/themed';
 import BaseTheme from "@/constants/BaseTheme";
-import TextView from '@/components/view/TextView';
 import SectionView from '@/components/view/SectionView';
 
 export default () => {
-
-  const router = useRouter();
   const { section } = useGlobalSearchParams();
 
   return ( 
