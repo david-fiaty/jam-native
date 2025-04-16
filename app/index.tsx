9e49a4131951ff6e71487b71b005653cfc0b6dbe
david-fiaty <dfiaty@gmail.com>
2025-04-16 14:50:05 +0000

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,17 @@
 import { View, StyleSheet } from 'react-native';
+import { Stack, useSegments, useRouter, Link } from 'expo-router';
+import { useGlobalSearchParams } from 'expo-router';
 import { ThemeProvider } from '@rneui/themed';
 import BaseTheme from "@/constants/BaseTheme";
 import ScreenView from '@/components/view/ScreenView';
-import WelcomeScreen from '@/components/screen/WelcomeScreen';
 import TextView from '@/components/view/TextView';
 import BoxView from '@/components/view/BoxView';
 
 export default () => {
 
-  
+  const router = useRouter();
+  const { section } = useGlobalSearchParams();
+
   return ( 
     <ThemeProvider theme={BaseTheme}>
       <ScreenView>
