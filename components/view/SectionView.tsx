7d7bccd72ea2ec72f435962afe421e21619ee605
david-fiaty <dfiaty@gmail.com>
2025-04-16 16:12:05 +0000

    Update SectionView.tsx

--- components/view/SectionView.tsx
+++ components/view/SectionView.tsx
@@ -2,20 +2,26 @@ import { StyleSheet } from 'react-native';
 import { Colors } from '@/constants/Colors';
 import TextView from './TextView';
 import WelcomeSection from '../section/WelcomeSection';
+import LoginSection from '../section/LoginSection';
 
 type Props = {
   name?: any;
 };
 
 const SectionView = ({ name }: Props) => {
+  const sections: any = [
+    {
+      name: 'welcome',
+      render: () => <WelcomeSection />,
+    },
+    {
+      name: 'login',
+      render: () => <LoginSection />,
+    },
+  ];
 
-  if (!name) {
-    return <WelcomeSection />
-  }
-
-  return (
-    <TextView>SectionView</TextView>
-  );
+  
+  return sections.find((o: any) => o.name === (name || 'welcome'))?.render();
 };
 
 const styles = StyleSheet.create({
