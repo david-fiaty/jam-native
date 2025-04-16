2e1847b2a743f14921969db30edcabc4ea83c5da
david-fiaty <dfiaty@gmail.com>
2025-04-16 13:56:27 +0000

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -2,12 +2,23 @@ import { ThemeProvider } from '@rneui/themed';
 import BaseTheme from "@/constants/BaseTheme";
 import ScreenView from '@/components/view/ScreenView';
 import WelcomeScreen from '@/components/screen/WelcomeScreen';
+import { Stack, useSegments, useRouter, Link } from 'expo-router';
+import { Button } from '@rneui/base';
+
+
 
 export default () => {
+
+  const router = useRouter();
+
+  
   return ( 
     <ThemeProvider theme={BaseTheme}>
       <ScreenView>
         <WelcomeScreen />
+      
+        <Link href="/test/xooo">Login</Link>
+        <Button title={'test'} onPress={() => router.push('/test/xooo')} />
       </ScreenView>
     </ThemeProvider>
   );
