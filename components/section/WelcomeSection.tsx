6a2af74e8e8acd0fc55b00bb6ff3ed95e9d0df08
david-fiaty <dfiaty@gmail.com>
2025-04-16 15:32:59 +0000

    Create WelcomeSection.tsx

---
+++ components/section/WelcomeSection.tsx
@@ -0,0 +1,52 @@
+import { View, StyleSheet } from 'react-native';
+import BottomLinks from '@/components/navigation/BottomLinks';
+import LoginSignupButton from '../button/LoginSignupButton';
+import LogoView from '../view/LogoView';
+import BoxView from '../view/BoxView';
+import { Divider } from '@rneui/base';
+import TextSlideshow from '../slideshow/TextSlideshow';
+
+const textSlideshowData = [
+  {
+    id: 1,
+    title: 'Create better, together',
+    content: 'Welcome to the Jam app. Jam is a place to explore and experience artists and creatives from different backgrounds in West Africa.',
+    link: 'aaa link',
+  },
+  {
+    id: 2,
+    title: 'Everything you need',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    link: 'bbb link',
+  },
+  {
+    id: 3,
+    title: 'The place to excel',
+    content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.',
+    link: 'ccc link',
+  },
+];
+
+const WelcomeSection = () => {
+  return (
+    <BoxView direction="column" align="center" justify="center" style={styles.container}>
+      <LogoView size={110} />    
+    
+      <TextSlideshow data={textSlideshowData} />
+
+      <Divider />
+      <LoginSignupButton />
+      
+      <Divider /><Divider />
+      <BottomLinks />
+    </BoxView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+  },
+});
+
+export default WelcomeSection;
