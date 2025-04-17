9842f2b2962658b124792e3da9723e28b31c1e55
david-fiaty <dfiaty@gmail.com>
2025-04-17 18:07:45 +0000

    Update SectorsField.tsx

--- components/field/SectorsField.tsx
+++ components/field/SectorsField.tsx
@@ -45,8 +45,15 @@ const SectorsField = ({ resource, field, label, value, placeholder, onPressEvent
   };
 
   const deleteItem = (item: any) => {
-    let selectedIds: any[] = [...(formData?.[fieldName] || [])];
+    let selectedIds: any[] =  value?.length > 0 ? value : [];
     let deleteIndex: number = selectedIds.findIndex((id: any) => id == item.id);
+
+
+    console.log('item', item);
+    console.log('selected ids', selectedIds);
+    console.log('delete index', deleteIndex);
+
+    return ;
 
     if (deleteIndex !== -1) selectedIds.splice(deleteIndex, 1);
 
