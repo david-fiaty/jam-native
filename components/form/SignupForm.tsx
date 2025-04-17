b3bd96b3d6e98df45a15620d6288205ca4f10a95
david-fiaty <dfiaty@gmail.com>
2025-04-17 17:31:03 +0000

    Update SignupForm.tsx

--- components/form/SignupForm.tsx
+++ components/form/SignupForm.tsx
@@ -17,7 +17,7 @@ const SignupForm = () => {
   useEffect(() => {
     (async () => {
         if (!isLoaded) {
-          //resetForm(); Todo - Enable this
+          resetForm();
           setIsLoaded(true);
         }
     })();
