ecbc27632391b5be96f9e29922a75bcae22550bb
david-fiaty <dfiaty@gmail.com>
2025-04-18 14:44:17 +0000

    Update OrganizationProfileForm.tsx

--- components/form/profile-form/OrganizationProfileForm.tsx
+++ components/form/profile-form/OrganizationProfileForm.tsx
@@ -34,7 +34,7 @@ const OrganizationProfileForm = ({ resource, mode, item, data, params, parentKey
             key={item.key}
             value={data?.[parentKey]?.[item.key] || ''}
             placeholder={i18n.t('Enter your organization name')}
-            onChangeText={(value: string) => onChange(item.key, value)}
+            onChangeText={(value: string) => onChange(parentKey, value)}
           />
         );
       },
@@ -53,7 +53,7 @@ const OrganizationProfileForm = ({ resource, mode, item, data, params, parentKey
             key={item.key}
             value={data?.[parentKey]?.[item.key] || ''}
             placeholder={i18n.t('Enter the creation year')}
-            onChangeText={(value: string) => onChange(item.key, value)}
+            onChangeText={(value: string) => onChange(parentKey, value)}
           />
         );
       },
