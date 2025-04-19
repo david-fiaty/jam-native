92005a65e8c94aee14a80b2e78b9a96348e2ffeb
david-fiaty <dfiaty@gmail.com>
2025-04-19 17:06:56 +0000

    Update [section].tsx

--- app/[section].tsx
+++ app/[section].tsx
@@ -13,7 +13,9 @@ export default () => {
   return (
     <ThemeProvider theme={BaseTheme}>
       <SafeAreaView style={styles.container}>
+        
         <TextView>header b</TextView>
+
         <BoxView 
           direction="column" 
           align="center" 
@@ -22,7 +24,9 @@ export default () => {
         >
           <SectionView name={section} />
         </BoxView>
+
         <TextView>footer b</TextView>
+
       </SafeAreaView>
     </ThemeProvider>
   );
