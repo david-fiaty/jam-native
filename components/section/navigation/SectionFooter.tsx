3cc610f58238414885cc4a7ac1ec95d77bec2594
david-fiaty <dfiaty@gmail.com>
2025-04-19 17:14:08 +0000

    Create SectionFooter.tsx

---
+++ components/section/navigation/SectionFooter.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text } from 'react-native';
+
+const SectionFooter = () => {
+  return (
+    <Text>Section footer</Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+});
+
+export default SectionFooter;
