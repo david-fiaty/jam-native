b099e063dd7fa0d9408d378d2648b0a7767b045a
david-fiaty <dfiaty@gmail.com>
2025-04-19 17:14:09 +0000

    Create SectionHeader.tsx

---
+++ components/section/navigation/SectionHeader.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text } from 'react-native';
+
+const SectionHeader = () => {
+  return (
+    <Text>Section header</Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+});
+
+export default SectionHeader;
