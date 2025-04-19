a1dd70f1d9576ff84c7275145bdbce7dc916f3ea
david-fiaty <dfiaty@gmail.com>
2025-04-19 16:54:33 +0000

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -2,45 +2,13 @@ import { StyleSheet } from 'react-native';
 import { useGlobalSearchParams } from 'expo-router';
 import { ThemeProvider } from '@rneui/themed';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Colors } from '@/constants/Colors';
 import BaseTheme from "@/constants/BaseTheme";
+import SectionView from '@/components/view/SectionView';
 import BoxView from '@/components/view/BoxView';
 import TextView from '@/components/view/TextView';
-import WelcomeSection from '@/components/section/WelcomeSection';
-import LoginSection from '@/components/section/LoginSection';
-import AboutSection from '@/components/section/AboutSection';
-import LegalSection from '@/components/section/LegalSection';
-import JamsSection from '@/components/section/JamsSection';
 
 export default () => {
   const { section } = useGlobalSearchParams();
-
-  const sections: any = [
-    {
-      name: 'welcome',
-      render: () => <WelcomeSection />,
-    },
-    {
-      name: 'login',
-      render: () => <LoginSection />,
-    },
-    {
-      name: 'about',
-      render: () => <AboutSection />,
-    },
-    {
-      name: 'legal',
-      render: () => <LegalSection />,
-    },
-    {
-      name: 'jams',
-      render: () => <JamsSection />,
-    },
-  ];
-
-  const renderSection = () => {
-    return sections.find((o: any) => o.name === (section || 'welcome'))?.render();
-  };
 
   return (
     <ThemeProvider theme={BaseTheme}>
@@ -52,7 +20,7 @@ export default () => {
           justify="center" 
           style={styles.container}
         >
-          {renderSection()}
+          <SectionView name={section} />
         </BoxView>
         <TextView>footer</TextView>
       </SafeAreaView>
