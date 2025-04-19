8b59382dfbc0e8b0db4bdfc6b361fbc7381c7d89
david-fiaty <dfiaty@gmail.com>
2025-04-19 17:17:29 +0000

    Update SectionHeader.tsx

--- components/section/navigation/SectionHeader.tsx
+++ components/section/navigation/SectionHeader.tsx
@@ -1,14 +1,17 @@
+import BoxView from '@/components/view/BoxView';
 import { StyleSheet, Text } from 'react-native';
 
 const SectionHeader = () => {
   return (
-    <Text>Section header</Text>
+    <BoxView direction="row" style={styles.container}>
+      <Text>Section header</Text>
+    </BoxView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-
+    backgroundColor: 'red',
   },
 });
 
