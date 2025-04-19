6e8425e73b8131b628f9fe78f00c025a51f83c86
david-fiaty <dfiaty@gmail.com>
2025-04-19 17:17:27 +0000

    Update SectionFooter.tsx

--- components/section/navigation/SectionFooter.tsx
+++ components/section/navigation/SectionFooter.tsx
@@ -1,14 +1,17 @@
+import BoxView from '@/components/view/BoxView';
 import { StyleSheet, Text } from 'react-native';
 
 const SectionFooter = () => {
   return (
-    <Text>Section footer</Text>
+    <BoxView direction="row" style={styles.container}>
+      <Text>Section header</Text>
+    </BoxView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-
+    backgroundColor: 'blue',
   },
 });
 
