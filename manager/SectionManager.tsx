b9bc7d6426a244779b128e3f420c2c1cc5c26a34
david-fiaty <dfiaty@gmail.com>
2025-04-20 13:21:15 +0000

    Update SectionManager.tsx

--- manager/SectionManager.tsx
+++ manager/SectionManager.tsx
@@ -1,6 +1,14 @@
+import { setSectionId } from "@/redux/slices/SectionSlice";
+import Store from "@/redux/Store";
 
 class SectionManager {
-
+  setCurrentSection(sectionId: string) {
+    Store.dispatch(setSectionId(sectionId));
+  }
+  
+  getCurrentSection(sectionName: string) {
+    return Store.getState().section;
+  }
 }
 
 export default (new SectionManager());
