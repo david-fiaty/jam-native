d300360c7f61c7d0316866968e143328849b25b0
david-fiaty <dfiaty@gmail.com>
2025-04-20 13:24:29 +0000

    Update SectionManager.tsx

--- manager/SectionManager.tsx
+++ manager/SectionManager.tsx
@@ -1,13 +1,53 @@
+import AboutSection from "@/components/section/AboutSection";
+import JamsSection from "@/components/section/JamsSection";
+import LegalSection from "@/components/section/LegalSection";
+import LoginSection from "@/components/section/LoginSection";
+import WelcomeSection from "@/components/section/WelcomeSection";
 import { setSectionId } from "@/redux/slices/SectionSlice";
 import Store from "@/redux/Store";
 
 class SectionManager {
-  setCurrentSection(sectionId: string) {
+  setActiveSection(sectionId: string) {
     Store.dispatch(setSectionId(sectionId));
   }
-  
-  getCurrentSection(sectionName: string) {
+
+  getActiveSection(sectionName: string) {
     return Store.getState().section;
+  }
+
+  getSections() {
+    return [
+      {
+        id: 'welcome',
+        header: false,
+        footer: false,
+        render: () => <WelcomeSection />,
+      },
+      {
+        id: 'login',
+        header: false,
+        footer: false,
+        render: () => <LoginSection />,
+      },
+      {
+        id: 'about',
+        header: true,
+        footer: false,
+        render: () => <AboutSection />,
+      },
+      {
+        id: 'legal',
+        header: true,
+        footer: false,
+        render: () => <LegalSection />,
+      },
+      {
+        id: 'jams',
+        header: true,
+        footer: true,
+        render: () => <JamsSection />,
+      },
+    ];
   }
 }
 
