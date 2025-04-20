e0b099f81396606d26ab0f3b2f5bba832ee21dd7
david-fiaty <dfiaty@gmail.com>
2025-04-20 14:53:45 +0000

    Update ModalManager.tsx

--- manager/ModalManager.tsx
+++ manager/ModalManager.tsx
@@ -1,5 +1,21 @@
 import { setModalId } from "@/redux/slices/ModalSlice";
 import Store from "@/redux/Store";
+import AddJamToProjectForm from "@/components/form/AddJamToProjectForm";
+import AddProjectForm from "@/components/form/AddProjectForm";
+import JamForm from "@/components/form/JamForm";
+import SelectJamsForm from "@/components/form/SelectJamsForm";
+import CollaboratorsList from "@/components/list/CollaboratorsList";
+import CountriesList from "@/components/list/CountriesList";
+import HostsList from "@/components/list/HostsList";
+import JammersList from "@/components/list/JammersList";
+import SectorsList from "@/components/list/SectorsList";
+import ShareOptionsList from "@/components/list/ShareOptionsList";
+import NotificationsMenu from "@/components/menu/NotificationsMenu";
+import SettingsMenu from "@/components/menu/SettingsMenu";
+import JamsMapView from "@/components/view/JamsMapView";
+import LocationMapView from "@/components/view/LocationMapView";
+import MoreJamActionsView from "@/components/view/MoreJamActionsView";
+import SearchView from "@/components/view/SearchView";
 
 class ModalManager {
   setActiveModalId(modalId: string) {
@@ -24,34 +40,68 @@ class ModalManager {
   getModals() {
     return [
       {
-        id: 'welcome',
-        header: false,
-        footer: false,
-        render: () => <WelcomeSection />,
+        name: 'JamsMapView', 
+        render: () => <JamsMapView />,
       },
       {
-        id: 'login',
-        header: false,
-        footer: false,
-        render: () => <LoginSection />,
+        name: 'JamForm', 
+        render: () => <JamForm />,
       },
       {
-        id: 'about',
-        header: true,
-        footer: false,
-        render: () => <AboutSection />,
+        name: 'AddProjectForm', 
+        render: () => <AddProjectForm />,
       },
       {
-        id: 'legal',
-        header: true,
-        footer: false,
-        render: () => <LegalSection />,
+        name: 'SelectJamsForm', 
+        render: () => <SelectJamsForm />,
       },
       {
-        id: 'jams',
-        header: true,
-        footer: true,
-        render: () => <JamsSection />,
+        name: 'AddJamToProjectForm', 
+        render: () => <AddJamToProjectForm />,
+      },
+      {
+        name: 'SettingsMenu', 
+        render: () => <SettingsMenu />,
+      },
+      {
+        name: 'NotificationsMenu', 
+        render: () => <NotificationsMenu />,
+      },
+      {
+        name: 'SearchView', 
+        render: () => <SearchView />,
+      },
+      {
+        name: 'LocationMapView', 
+        render: () => <LocationMapView />,
+      },
+      {
+        name: 'JammersList',
+        render: () => <JammersList />,
+      },
+      {
+        name: 'CollaboratorsList', 
+        render: () => <CollaboratorsList />,
+      },
+      {
+        name: 'SectorsList', 
+        render: () => <SectorsList />,
+      },
+      {
+        name: 'CountriesList', 
+        render: () => <CountriesList />,
+      },
+      {
+        name: 'HostsList', 
+        render: () => <HostsList />,
+      },
+      {
+        name: 'MoreJamActionsView', 
+        render: () => <MoreJamActionsView />,
+      },
+      {
+        name: 'ShareOptionsList', 
+        render: () => <ShareOptionsList />,
       },
     ];
   }
