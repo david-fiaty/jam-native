7ad21862adf4f1eb1b209a871a74a63eedd7287c
david-fiaty <dfiaty@gmail.com>
2025-04-20 12:01:06 +0000

    Update SectionHeader.tsx

--- components/section/navigation/SectionHeader.tsx
+++ components/section/navigation/SectionHeader.tsx
@@ -1,11 +1,21 @@
 import BoxView from '@/components/view/BoxView';
+import LogoView from '@/components/view/LogoView';
 import { Layout } from '@/constants/Layout';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import ScreenManager from '@/manager/ScreenManager';
 
 const SectionHeader = () => {
   return (
     <BoxView direction="row" style={styles.container}>
-      <Text>Section header</Text>
+      <BoxView direction="row" style={styles.headerLeft}>
+        <TouchableOpacity onPress={() => ScreenManager.toggleModal(null)}>
+          <LogoView size={Layout.logo.size} />
+        </TouchableOpacity>
+      </BoxView>
+  
+      <BoxView direction="row" style={styles.headerRight}>
+        
+      </BoxView>
     </BoxView>
   );
 };
@@ -14,6 +24,14 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: 'red',
     padding: Layout.space.base,
+  },
+  headerLeft: {
+    backgroundColor: 'yellow',
+    width: '50%',
+  },
+  headerRight: {
+    backgroundColor: 'yellow',
+    width: '50%'
   },
 });
 
