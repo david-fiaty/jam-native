49ccd9b222c6d222e14f5f05eb65020d36bb9f2b
david-fiaty <dfiaty@gmail.com>
2025-04-21 14:03:27 +0000

    Update ModalManager.tsx

--- manager/ModalManager.tsx
+++ manager/ModalManager.tsx
@@ -6,6 +6,7 @@ import i18n from "@/translation/i18n";
 import JamsMapView from "@/components/view/JamsMapView";
 import SearchView from "@/components/view/SearchView";
 import SettingsMenu from "@/components/menu/SettingsMenu";
+import NotificationsMenu from "@/components/menu/NotificationsMenu";
 
 class ModalManager {
   toggleModal(modalId: any) {
@@ -55,6 +56,11 @@ class ModalManager {
         id: 'SettingsMenu',
         title: i18n.t('Settings'),
         render: () => <SettingsMenu />,
+      },
+      {
+        id: 'NotificationsMenu',
+        title: i18n.t('Notifications'),
+        render: () => <NotificationsMenu />,
       },
     ].map((o: any) => {
       return {
