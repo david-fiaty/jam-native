5f65bb0d35c6dca9be460c5d45a7eed2f6abdba0
david-fiaty <dfiaty@gmail.com>
2025-04-21 16:33:43 +0000

    Update SignupSection.tsx

--- components/section/SignupSection.tsx
+++ components/section/SignupSection.tsx
@@ -1,18 +1,49 @@
+import { StyleSheet } from 'react-native';
 import { useSelector } from "react-redux";
 import SignupCodeForm from "../form/signup-form/SignupCodeForm";
 import SignupEmailForm from "../form/signup-form/SignupEmailForm";
 import ProfileForm from "../form/ProfileForm";
+import BoxView from "../view/BoxView";
+import { Layout } from "@/constants/Layout";
+import LogoView from "../view/LogoView";
+import TextView from "../view/TextView";
+import i18n from "@/translation/i18n";
 
 const SignupSection = () => {
-  const formData: any = useSelector((state: any) => state.signup);
+  const formData = useSelector((state: any) => state.form?.profile);
+  
+  const isContainerScrollable = () => {
+    return formData?.profile_type?.length > 0;
+  };
 
   return (
-    <>
+    <BoxView 
+      direction="column" 
+      align="center" 
+      justify="center" 
+      style={[Layout.screenContent, isContainerScrollable() ? styles.scrollableContainer : {}]}
+      scroll={isContainerScrollable()}
+    >
+      <LogoView size={80} />    
+      <TextView style={styles.slogan}>{i18n.t('Create your JAM account')}</TextView> 
+      
       {formData?.success !== true && <SignupEmailForm />}
       {formData?.success !== true && formData?.session?.length > 0 && <SignupCodeForm />}
       {formData?.success === true && <ProfileForm />}
-    </>
+
+    </BoxView>
   );
 };
+
+const styles = StyleSheet.create({
+  slogan: {
+    textTransform: 'uppercase',
+    fontSize: Layout.fontSize.base,
+  },
+  scrollableContainer: {
+    paddingTop: Layout.space.base*4,
+    paddingBottom: Layout.space.base*2,
+  },
+});
 
 export default SignupSection;
