073e2f256da6d6da50f74cbfc84d3fee599d1218
david-fiaty <dfiaty@gmail.com>
2025-04-22 14:15:09 +0000

    Revert "Update SectionManager.tsx"
    This reverts commit ccb3a7a1c16875a7bb51e97ad5ca8a8f013bd841.

--- manager/SectionManager.tsx
+++ manager/SectionManager.tsx
@@ -1,4 +1,4 @@
-import { setActiveSections } from "@/redux/slices/SectionSlice";
+import { setSectionId } from "@/redux/slices/SectionSlice";
 import Store from "@/redux/Store";
 import AboutSection from "@/components/section/AboutSection";
 import JamsSection from "@/components/section/JamsSection";
@@ -9,22 +9,19 @@ import i18n from "@/translation/i18n";
 import SignupSection from "@/components/section/SignupSection";
 
 class SectionManager {
-
-  setActiveSection(sectionId: string) {
-    let activeSections: any[] = [...Store.getState().section.active];
-    let targetSection: any = this.getSection(sectionId, false);
-
-    activeSections.push(targetSection);
-
-    Store.dispatch(setActiveSections(activeSections));
+  setActiveSectionId(sectionId: any) {
+    Store.dispatch(setSectionId(sectionId));
   }
 
   getActiveSection() {
-    let activeSections: any[] = [...Store.getState().section.active];
+    let sectionId: any = this.getActiveSectionId();
+    let section: any = this.getSection(sectionId);
 
-    if (activeSections.length > 0) {
-      return activeSections.pop();
-    }
+    return section;
+  }
+
+  getActiveSectionId() {
+    return Store.getState().section.sectionId;
   }
 
   getSection(modalId: any, renderer: boolean = true) {
