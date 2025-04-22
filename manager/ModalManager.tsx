492f6aa0eaa3f4cbb1a8cb24411d6c9419793976
david-fiaty <dfiaty@gmail.com>
2025-04-22 12:55:58 +0000

    Update ModalManager.tsx

--- manager/ModalManager.tsx
+++ manager/ModalManager.tsx
@@ -1,4 +1,4 @@
-import { setActiveModal } from "@/redux/slices/ModalSlice";
+import { setActiveModals } from "@/redux/slices/ModalSlice";
 import Store from "@/redux/Store";
 import TestModal from "@/components/modal/TestModal";
 import JamForm from "@/components/form/JamForm";
@@ -12,6 +12,39 @@ import MoreJamActionsView from "@/components/view/MoreJamActionsView";
 import JammersList from "@/components/list/JammersList";
 
 class ModalManager {
+
+  setActiveModal(modalId: string, params?: any) {
+    let activeModals: any[] = {...Store.getState().modal.active};
+
+    if (activeModals.length > 0 && activeModals[activeModals.length - 1].id === modalId) {
+      activeModals.pop();
+    } 
+    else {
+      activeModals.push({
+        ...this.getModal(modalId, false), 
+        ...{ params: params },
+      });
+    }
+
+    Store.dispatch(setActiveModals(activeModals));
+  }
+
+  getActiveModal() {
+    let activeModals: any[] = {...Store.getState().modal.active};
+
+    if (activeModals.length > 0) {
+      return activeModals.pop();
+    }
+
+    return null;
+  }
+
+
+
+
+
+
+
   toggleModal(modalId: any) {
     let activeModalId: any = this.getActiveModalId();
     let currentModalId: any = modalId === activeModalId ? null : modalId;
@@ -19,29 +52,14 @@ class ModalManager {
     this.setActiveModalId(currentModalId);
   } 
 
-  setActiveModalId(modalId: any) {
-    Store.dispatch(setModalId(modalId));
+  getModal(modalId: any, renderer: boolean = true) {
+    return this.getModals(renderer).find((o: any) => o.id === modalId);
   }
 
-  getActiveModal() {
-    let modalId: any = this.getActiveModalId();
-    let section: any = this.getModal(modalId);
-
-    return section;
-  }
-
-  getActiveModalId() {
-    return Store.getState().modal.modalId;
-  }
-
-  getModal(modalId: any) {
-    return this.getModals().find((o: any) => o.id === modalId);
-  }
-
-  getModals(withMethods: boolean = true) {
+  getModals(renderer: boolean = true) {
     let config: any[] = this.getConfig();
 
-    if (!withMethods) {
+    if (!renderer) {
       config = config.map(({ render, ...rest }) => rest);
     }
 
