9c08994ffc61f0da34ffb38b9d821925bc14f4fa
david-fiaty <dfiaty@gmail.com>
2025-04-22 14:21:04 +0000

    Update ListItemHeader.tsx

--- components/list/JamsList/ListItemHeader.tsx
+++ components/list/JamsList/ListItemHeader.tsx
@@ -17,7 +17,7 @@ const ListItemHeader = ({ row }: Props) => {
 
   const renderHosts = () => {
     return (
-      <TouchableOpacity onPress={() => ModalManager.toggleModal('HostsList')}>
+      <TouchableOpacity onPress={() => ModalManager.toggleModal('HostsList', { jamId: row?.item?.id })}>
         <TextView>
           @{i18n.t("host")} +{parseInt(row?.item?.collaborators?.length)}
         </TextView>
