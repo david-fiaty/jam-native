9f8fd7bc8837bb3f610f62e26ed5a510ef37bc9f
david-fiaty <dfiaty@gmail.com>
2025-04-22 13:50:53 +0000

    Update SectionSlice.ts

--- redux/slices/SectionSlice.ts
+++ redux/slices/SectionSlice.ts
@@ -3,14 +3,14 @@ import { createSlice } from '@reduxjs/toolkit';
 const SectionSlice = createSlice({
   name: 'section',
   initialState: {
-    sectionId: '',
+    active: [],
   },
   reducers: {
-    setSectionId: (state, action) => {
-      state.sectionId = action.payload;
+    setActiveSections: (state, action) => {
+      state.active = action.payload;
     },
   },
 });
 
-export const { setSectionId } = SectionSlice.actions;
+export const { setActiveSections } = SectionSlice.actions;
 export default SectionSlice.reducer;
