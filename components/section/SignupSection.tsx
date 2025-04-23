e098b799ca5d444345466b61452ea066963949e3
david-fiaty <dfiaty@gmail.com>
2025-04-23 13:12:18 +0000

    Update SignupSection.tsx

--- components/section/SignupSection.tsx
+++ components/section/SignupSection.tsx
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
 import { useSelector, useDispatch } from "react-redux";
 import { setValue } from '@/redux/slices/SignupSlice';
+import { Layout } from '@/constants/Layout';
 import SignupCodeForm from '../form/signup-form/SignupCodeForm';
 import SignupEmailForm from '../form/signup-form/SignupEmailForm';
 import ProfileForm from '../form/ProfileForm';
+import LogoView from '../view/LogoView';
+import TextView from '../view/TextView';
+import i18n from '@/translation/i18n';
 
 const SignupSection = () => {
   const dispatch = useDispatch();
@@ -24,12 +29,26 @@ const SignupSection = () => {
   }, [isLoaded]);
 
   return (
-    <>
+    <>    
+      <LogoView size={80} />    
+      <TextView style={styles.slogan}>{i18n.t('Create your JAM account')}</TextView> 
       {formData?.success !== true && <SignupEmailForm />}
       {formData?.success !== true && formData?.session?.length > 0 && <SignupCodeForm />}
       {formData?.success === true && <ProfileForm />}
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  slogan: {
+    textTransform: 'uppercase',
+    fontSize: Layout.fontSize.base,
+  },
+  scrollableContainer: {
+    paddingTop: Layout.space.base*4,
+    paddingBottom: Layout.space.base*10,
+    height: '100%',
+  },
+});
 
 export default SignupSection;
