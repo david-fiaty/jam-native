5bd6f1633f663518e5ede15255f6c763f22d51bd
david-fiaty <dfiaty@gmail.com>
2025-04-24 14:42:38 +0000

    Update SectorsList.tsx

--- components/list/SectorsList.tsx
+++ components/list/SectorsList.tsx
@@ -5,34 +5,127 @@ import { setFormData } from "@/redux/slices/FormSlice";
 import { Layout } from "@/constants/Layout";
 import { Colors } from "@/constants/Colors";
 import TextView from "../view/TextView";
-import BackButton from "../button/BackButton";
-import i18n from "@/translation/i18n";
 import BoxView from "../view/BoxView";
 import IconView from "../view/IconView";
 import ListView from "../view/ListView";
 import SpinnerView from "../view/SpinnerView";
-import ScreenManager from "@/manager/ScreenManager";
 import EntityManager from "@/manager/EntityManager";
 import CollapsibleView from "../view/CollapsibleView";
 
 type Props = {
   resource: string;
   field?: any;
-  value?: any;
 };
 
-const SectorsList = ({ resource, field, value }: Props) => {
+const SectorsList = ({ resource, field }: Props) => {
+  const dispatch = useDispatch();
+  const [sectorsData, setSectorsData] = useState<any>(null);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const formData: any = useSelector((state: any) => state.form[resource]);
 
-  console.log('resource --->', resource);
-  console.log('field --->', field);
-  console.log('value --->', value);
+  const updateSelection = (item: any, subItem: any) => {
+    let selection: any[] = [...formData?.[field] || []];
+    let itemIndex: number = selection.findIndex((id: any) => id == item.id);
+    let subItemIndex: number = selection.findIndex((id: any) => id == subItem.id);
 
-  console.log('formData', formData);
+    // Update ID pairs
+    if (itemIndex === -1 && subItemIndex === -1) {
+      selection.push(item.id, subItem.id);
+    }
+    else if (itemIndex !== -1 && subItemIndex === -1) {
+      selection.push(subItem.id);
+    }
+    else if (itemIndex !== -1 && subItemIndex !== -1) {
+      selection.splice(subItemIndex, 1);
+    }
+
+    // Remove parents without sub selection
+    let itemChildIds: any = item?.sub_sectors?.map((o: any) => o?.id);
+    let deleteItem: boolean = !selection.some((id: any) => itemChildIds.includes(id));
+    if (deleteItem) {
+      let index: number = selection.findIndex((id: any) => id == item.id);
+      selection.splice(index, 1);
+    }
+
+    // Update selection state
+    dispatch(setFormData<any>({ 
+      resource: resource,
+      key: field, 
+      value: selection,
+    }));
+  };
+
+  const renderSubItem = (item: any, subItem: any) => {
+    let isSelected: boolean = formData?.[field]?.includes(subItem.id);
+
+    return (
+      <TouchableOpacity 
+        key={subItem?.id}
+        onPress={() => updateSelection(item, subItem)} 
+      >
+        <BoxView direction="row" align="center" justify="space-around">
+          <IconView name="arrow" theme="clear" size={10} />
+          <TextView key={subItem?.id} style={styles.listSubItem}>
+            {subItem?.name}
+          </TextView>
+          
+          {isSelected && <IconView name="checkmark" theme="clear" size={15} /> }
+        </BoxView>
+      </TouchableOpacity>
+    );
+  };
+
+  const renderItem = (row: any) => {
+    return (
+      <BoxView style={styles.listItemCollapsible}>
+        <CollapsibleView
+          label={<TextView>{row?.item?.name}</TextView>}
+          openedLabel={<TextView>{row?.item?.name}</TextView>}
+          headerStyle={styles.itemHeader}
+          content={
+            <BoxView
+              direction="column"
+              align="flex-start"
+              style={styles.listItemDetails}
+            >
+              {row?.item?.sub_sectors?.length > 0 &&
+                row?.item?.sub_sectors?.map((subItem: any) => renderSubItem(row?.item, subItem))
+              }
+            </BoxView>
+          }
+        />
+      </BoxView>
+    );
+  };
+
+  useEffect(() => {
+    (async () => {
+      if (!isLoaded) {
+        setSectorsData(await EntityManager.getSectors());
+        setIsLoaded(true);
+      }
+    })();
+    
+  }, [isLoaded, sectorsData]);
+
+  if (!isLoaded) return <SpinnerView />;
+
   return (
-    <></>
+    <BoxView
+      align="flex-start"
+      justify="flex-start"
+      style={Layout.screenContent}
+    >
+      <View style={styles.container}>
+        {sectorsData?.length > 0 && (
+          <ListView
+            data={sectorsData}
+            renderItem={(row: any) => renderItem(row)}
+          />
+        )}
+      </View>
+    </BoxView>
   );
-  
 };
 
 const styles = StyleSheet.create({
