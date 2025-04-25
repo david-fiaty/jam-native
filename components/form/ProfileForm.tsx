f10f52697421b987cccfadaa08995b0250c83c20
david-fiaty <dfiaty@gmail.com>
2025-04-25 23:34:36 +0000

    Update ProfileForm.tsx

--- components/form/ProfileForm.tsx
+++ components/form/ProfileForm.tsx
@@ -13,6 +13,8 @@ import i18n from "@/translation/i18n";
 import UserManager from "@/manager/UserManager";
 import ScreenManager from "@/manager/ScreenManager";
 import SpinnerView from "../view/SpinnerView";
+import TextView from "../view/TextView";
+import InputTextField from "../field/InputTextField";
 
 type Props = {
   resource?: any;
@@ -35,7 +37,7 @@ const ProfileForm = ({ resource }: Props) => {
       value: value,
     }));
   };
- 
+
   const canRenderForm = () => {
     return formData?.profile_type?.length > 0
       || resource == 'profile';
@@ -104,10 +106,10 @@ const ProfileForm = ({ resource }: Props) => {
         setProfileId(await UserManager.getProfileId());
 
         if (resource == 'profile') {
-          dispatch(setFormData<any>({ 
+          dispatch(setFormData<any>({
             resource: resource,
-            key: null, 
-            value: await UserManager.getProfileData(), 
+            key: null,
+            value: await UserManager.getProfileData(),
           }));
         }
         setIsLoaded(true);
@@ -116,9 +118,9 @@ const ProfileForm = ({ resource }: Props) => {
   }, [isLoaded, formData, resource]);
 
   if (!isLoaded) return <SpinnerView />;
-  
+
   console.log('profile_form', formData);
-  
+
   return (
     <View style={[Layout.formContainer, styles.container]}>
       {resource == 'signup' && (
@@ -135,7 +137,35 @@ const ProfileForm = ({ resource }: Props) => {
         </>
       )}
 
-      {canRenderForm() === true && profileFields.map((item: any) => {
+      {formData?.profile_type == 'personal' && (
+        <>
+          <TextView>
+            {i18n.t('First name')}
+          </TextView>
+          <InputTextField
+            value={formData?.profile_personal?.first_name}
+            placeholder={i18n.t('Enter your first name')}
+            onChangeText={(value: string) => updateField('profile_personal', {
+              ...(formData?.profile_personal?.first_name || {}),
+              ...{ first_name: value },
+            })}
+          />
+
+          <TextView>
+            {i18n.t('Last name')}
+          </TextView>
+          <InputTextField
+            value={formData?.profile_personal?.last_name}
+            placeholder={i18n.t('Enter your last name')}
+            onChangeText={(value: string) => updateField('profile_personal', {
+              ...(formData?.profile_personal?.last_name || {}),
+              ...{ last_name: value },
+            })}
+          />
+        </>
+      )}
+
+      { /*canRenderForm() === true && profileFields.map((item: any) => {
         if (ProfileManager.canRenderField(resource, item, formData)) {
           return (
             <View key={item.key} style={styles.fieldContainer}>
@@ -143,7 +173,7 @@ const ProfileForm = ({ resource }: Props) => {
             </View>
           );
         }
-      })}
+      }) */}
 
       <View style={styles.submitButtonContainer}>
         <ButtonView
