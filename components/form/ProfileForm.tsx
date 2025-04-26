2e76fcd1959bbd9b97607884ecf33cd468661220
david-fiaty <dfiaty@gmail.com>
2025-04-26 05:09:31 +0000

    Update ProfileForm.tsx

--- components/form/ProfileForm.tsx
+++ components/form/ProfileForm.tsx
@@ -124,8 +124,6 @@ const ProfileForm = ({ resource }: Props) => {
 
   if (!isLoaded) return <SpinnerView />;
 
-  console.log('profile_form', formData);
-
   return (
     <View style={[Layout.formContainer, styles.container]}>
       {resource == 'signup' && (
