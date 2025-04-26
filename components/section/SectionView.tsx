186453055c04b32b44817de08341d76c7f54b6b3
david-fiaty <dfiaty@gmail.com>
2025-04-26 03:19:54 +0000

    Revert "Update SectionView.tsx"
    This reverts commit fa240988d94f5aa48f8040836a7b7f473b860f73.

--- components/section/SectionView.tsx
+++ components/section/SectionView.tsx
@@ -42,7 +42,7 @@ const SectionView = () => {
       >
         {currentSection?.render()}
   
-        <ModalView style={styles.modal} />
+        <ModalView currentSection={currentSection} style={styles.modal} />
       </BoxView>
       
       {currentSection?.showFooter === true && <SectionFooter style={styles.footer} />}
