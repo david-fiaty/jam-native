7be3bab654f7e305a065ca9804b8430fdd7e61db
david-fiaty <dfiaty@gmail.com>
2025-04-26 05:39:04 +0000

    Update LocationMapView.web.tsx

--- components/view/LocationMapView.web.tsx
+++ components/view/LocationMapView.web.tsx
@@ -4,7 +4,13 @@ import { Config } from "@/constants/Config";
 const containerStyle = { width: "100%", height: "400px" };
 const center = { lat: 37.7749, lng: -122.4194 };
 
-const LocationMapView = () => {
+type Props = {
+  resource: string,
+  latitude?: any;
+  longitude?: any;
+};
+
+const LocationMapView = ({ resource, latitude, longitude }: Props) => {
   return (
     <LoadScript googleMapsApiKey={Config.mapApiKey}>
       <GoogleMap 
