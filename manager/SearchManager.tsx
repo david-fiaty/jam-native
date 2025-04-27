0ecd3e9e5b5f4a83a5dfc521912ce5cd391bdf7b
david-fiaty <dfiaty@gmail.com>
2025-04-27 15:13:20 +0000

    Update SearchManager.tsx

--- manager/SearchManager.tsx
+++ manager/SearchManager.tsx
@@ -3,8 +3,15 @@ import EntityManager from "./EntityManager";
 import Store from '@/redux/Store';
 
 class SearchManager {
-  async getSearchResults(idArray: any []) {
-    return await EntityManager.getJams({ items_ids: idArray});
+  async getSearchResults(idArray: any [] = []) {
+    let searchState: any = Store.getState().search;
+    let itemsIds = [];
+
+    if (idArray?.length > 0) itemsIds = idArray
+    else if (searchState.resultIndex.length > 0) itemsIds = searchState.resultIndex
+    else itemsIds = searchState.defaultIndex;
+        
+    return await EntityManager.getJams({ items_ids: itemsIds});
   }
 
   async loadSearchResults(searchValue?: string, filter?: string) {
