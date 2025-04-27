44bd1c42f509c977ea08d4a71b247b4d7b9947f3
david-fiaty <dfiaty@gmail.com>
2025-04-27 13:40:24 +0000

    Update SearchManager.tsx

--- manager/SearchManager.tsx
+++ manager/SearchManager.tsx
@@ -7,18 +7,18 @@ class SearchManager {
     let searchState: any = Store.getState().search;
     let results: any[] = [];
 
-    if (searchState.searchValue.length && searchState.resultIndex.length && searchState.searchValue == searchValue) {
-      results = await EntityManager.getJams({ items_ids: searchState.resultIndex });
-    }
-    else if (searchState.searchValue.length && searchState.resultIndex.length && searchState.searchValue != searchValue) {
+    if (searchValue?.length && searchState.searchValue != searchValue) {
       results = await EntityManager.listJams({ query_text: searchValue });
       Store.dispatch(setSearchValue(searchValue));
       Store.dispatch(setResultIndex(results.map((o: any) => o.id)));
     }
+    if (searchValue?.length && searchState.searchValue == searchValue) {
+      results = await EntityManager.getJams({ items_ids: searchState.resultIndex });
+    }
     else if (searchState.defaultIndex.length) {
       results = await EntityManager.getJams({ items_ids: searchState.defaultIndex });
     }
-    else if (!searchState.defaultIndex.length) {
+    else {
       results = await this.getDefaultResults();
     }
 
