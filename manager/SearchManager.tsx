dc4d4378230e54b0bee4137c0629e4aea4437089
david-fiaty <dfiaty@gmail.com>
2025-04-27 13:32:43 +0000

    Update SearchManager.tsx

--- manager/SearchManager.tsx
+++ manager/SearchManager.tsx
@@ -1,6 +1,4 @@
 import { setSearchValue, setDefaultIndex, setResultIndex } from "@/redux/slices/SearchSlice";
-import { Config } from "@/constants/Config";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import EntityManager from "./EntityManager";
 import Store from '@/redux/Store';
 
@@ -17,12 +15,11 @@ class SearchManager {
       Store.dispatch(setSearchValue(searchValue));
       Store.dispatch(setResultIndex(results.map((o: any) => o.id)));
     }
-    else if (!searchState.defaultIndex.length) {
-      results = await EntityManager.listJams();
-      Store.dispatch(setDefaultIndex(results.map((o: any) => o.id)));
-    }
     else if (searchState.defaultIndex.length) {
       results = await EntityManager.getJams({ items_ids: searchState.defaultIndex });
+    }
+    else if (!searchState.defaultIndex.length) {
+      results = await this.getDefaultResults();
     }
 
     return results;
@@ -30,68 +27,18 @@ class SearchManager {
 
   async clearSearch() {
     Store.dispatch(setSearchValue(''));
+    let results: any = await this.getDefaultResults(); 
+
+    return results;
   }
 
-  async loadData(searchValue?: string) {
-    const options = searchValue?.length ? { query_text: searchValue } : {};
-    let data: any = [];
+  async getDefaultResults() {
+    let results = await EntityManager.listJams();
+    Store.dispatch(setDefaultIndex(results.map((o: any) => o.id)));
 
-    if (Config.dataCacheEnabled === true && searchValue?.length) {
-      let cacheKey = this.getCacheKey(searchValue);
-      data = await AsyncStorage.getItem(cacheKey);
-
-      if (data === null) {
-        data = await this.sendRequest(options);
-        await AsyncStorage.setItem(cacheKey, JSON.stringify(data));
-      }  
-      else {
-        data = JSON.parse(data);
-      }
-    }
-    else {
-      data = await this.sendRequest(options);
-    }
-
-    const [jams, profiles, projects] = data;
-    
-    const result = this.buildResponse({
-      jams: jams, 
-      profiles: profiles, 
-      projects: projects
-    });
-
-    this.setCurrentValue(searchValue);
-    this.setDefaultResult(result);
-    this.setCurrentResult(result);
-    
-    return result;
+    return results;
   }
 
-  async sendRequest(options?: any) {
-    return await Promise.all([
-      EntityManager.listJams(options), 
-      EntityManager.listProfiles(options),
-      EntityManager.listProjects(options),
-    ]);
-  }
-
-  buildResponse(data?: any) {
-    return {
-      jam: data.jams,
-      project: data.projects,
-      jammer: data.profiles,
-      venue: data.profiles?.filter((o: any) => o?.profile_type == 'venue'),
-      // Todo - Add categories
-      //personal: data.profiles.filter((o: any) => o?.profile_type == 'personal'),
-      //organization: data.profiles.filter((o: any) => o?.profile_type == 'organization'),
-      call: data.jams?.filter((o: any) => o?.type == 'call'),
-      event: data.jams?.filter((o: any) => o?.type == 'event'),
-    };
-  }
-
-  getCacheKey(searchValue: string) {
-    return searchValue.replace(/\W/g, '');
-  }
 };
 
 export default (new SearchManager());
