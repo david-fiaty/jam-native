bac7766f2006f92b0177c4206a55515d6c606416
david-fiaty <dfiaty@gmail.com>
2025-04-28 17:41:24 +0000

    Update SignupForm.tsx

--- components/form/SignupForm.tsx
+++ components/form/SignupForm.tsx
@@ -138,7 +138,7 @@ const SignupForm = () => {
             {i18n.t('Creation year')}
           </TextView>
           <InputTextField
-            keboardType="number-pad"
+            keyboardType="number-pad"
             value={formData?.profile_organization?.creation_year}
             placeholder={i18n.t('Enter the creation year')}
             onChangeText={(value: string) => updateField('profile_organization', {
