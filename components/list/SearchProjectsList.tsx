47fd46cf23f26e4fcb84d01e43af82c7ba5f74a5
david-fiaty <dfiaty@gmail.com>
2025-04-28 16:19:06 +0000

    Update SearchProjectsList.tsx

--- components/list/SearchProjectsList.tsx
+++ components/list/SearchProjectsList.tsx
@@ -18,6 +18,7 @@ const SearchProjectsList = ({ data, filter }: Props) => {
   const numColumns = 3;
   const router = useRouter();
   const [projectsImages, setProjectsImages] = useState<any>({});
+  const [currentData, setCurrentData] = useState<any[]>([]);
 
   const onItemPress = (row: any) => {
     ScreenManager.pushScreen(router, '/project', { idArray: [row.item.id], title: row.item.title });
@@ -31,30 +32,30 @@ const SearchProjectsList = ({ data, filter }: Props) => {
             setProjectsImages({ ...projectsImages, ...{ [item?.id]: value } });
         });
       });
+
+      setCurrentData(data);
     }
-  });
+  }, [data, filter]);
 
   return (
     <View>
-      {data?.length > 0 && (
-        <View style={styles.container}>
-          <ListView
-            data={data}
-            numColumns={numColumns}
-            contentContainerStyle={{ gap: Layout.space.base }}
-            columnWrapperStyle={{ gap: Layout.space.base }}
-            scrollEnabled={false}
-            emptyMessage={<TextView>{i18n.t("No results found for this search.")}</TextView>}
-            renderItem={(row: any) => (
-              <ProjectListItem 
-                row={row}
-                images={projectsImages}
-                onListItemPress={(row: any) => onItemPress(row)}
-              />
-            )}
-          />
-        </View>
-      )}
+      <View style={styles.container}>
+        <ListView
+          data={currentData}
+          numColumns={numColumns}
+          contentContainerStyle={{ gap: Layout.space.base }}
+          columnWrapperStyle={{ gap: Layout.space.base }}
+          scrollEnabled={false}
+          emptyMessage={<TextView>{i18n.t("No results found for this search.")}</TextView>}
+          renderItem={(row: any) => (
+            <ProjectListItem
+              row={row}
+              images={projectsImages}
+              onListItemPress={(row: any) => onItemPress(row)}
+            />
+          )}
+        />
+      </View>
     </View>
   );
 };
