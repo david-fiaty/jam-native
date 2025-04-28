308ee36f749416392aa6baf732ed7ae9ef7aee00
david-fiaty <dfiaty@gmail.com>
2025-04-28 17:54:51 +0000

    Update SignupForm.tsx

--- components/form/SignupForm.tsx
+++ components/form/SignupForm.tsx
@@ -39,7 +39,7 @@ const SignupForm = () => {
   };
 
   const submitForm = async () => {
-    let { password, ...profileData } = formData;
+    let { password, password_confirmation, ...profileData } = formData;
 
     let payload: any = {
       ...{ profile: profileData },
