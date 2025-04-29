5a39f8c5a1ef1a9a95c0a09b1818bbfa3fe32966
david-fiaty <dfiaty@gmail.com>
2025-04-29 10:28:37 +0000

    Update LegalSection.tsx

--- components/section/LegalSection.tsx
+++ components/section/LegalSection.tsx
@@ -1,8 +1,8 @@
+import { StyleSheet } from 'react-native';
 import { useRouter } from 'expo-router';
 import { Layout } from '@/constants/Layout';
 import TextView from '../view/TextView';
 import BoxView from '../view/BoxView';
-import StaticData from '@/constants/StaticData';
 import BottomLinks from './navigation/BottomLinks';
 
 const LegalSection = () => {
@@ -15,5 +15,11 @@ const LegalSection = () => {
     </BoxView>
   );
 };
+
+const styles = StyleSheet.create({
+  title: {
+    fontWeight: 'bold',
+  },
+});
 
 export default LegalSection;
