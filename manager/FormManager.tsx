286e2e1aef1c15d73a1cd9487d633b74b81b44a6
david-fiaty <dfiaty@gmail.com>
2025-04-29 16:00:49 +0000

    Update FormManager.tsx

--- manager/FormManager.tsx
+++ manager/FormManager.tsx
@@ -1,7 +1,88 @@
 import i18n from "@/translation/i18n";
 
 class FormManager {
+  validateFied(key: string, value: any, rules: any[]) {
+    let fieldRules: any = this.getValidationRules();
+    let errors: any = [];
 
+    for (const rule of rules) {
+      if (!fieldRules[rule].run(value)) {
+        errors.push({
+          field: key,
+          value: value,
+          message: fieldRules[rule].error(),
+        });
+      }
+    }
+
+    return errors;
+  }
+
+  getValidationRules() {
+    return {
+      required: {
+        run: (value: any) => {
+          return value && typeof value == 'string' && value.trim().length > 0;
+        },
+        error: () => {
+          return i18n.t('Invalid empty value');
+        },
+      },
+      number: {
+        run: (value: any) => {
+          let pattern: any = /^\d+$/;
+          return value && pattern.test(value);
+        },
+        error: () => {
+          return i18n.t('Invalid number value');
+        },
+      },
+      email: {
+        run: (value: any) => {
+          let pattern: any = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+          return value && pattern.test(value);
+        },
+        error: () => {
+          return i18n.t('Invalid email value');
+        },
+      },
+      date: {
+        run: (value: any) => {
+          try {
+            new Date(value);
+            return true;
+          } catch (error) {
+            return false;
+          }
+        },
+        error: () => {
+          return i18n.t('Invalid date value');
+        },
+      },
+      url: {
+        run: (value: any) => {
+          try {
+            new URL(value);
+            return true;
+          } catch (error) {
+            return false;
+          }
+        },
+        error: () => {
+          return i18n.t('Invalid URL value');
+        },
+      },
+      domain: {
+        run: (value: any) => {
+          let pattern: any = /^((?!-)[A-Za-z0-9-]{1, 63}(?<!-)\\.)+[A-Za-z]{2, 6}$/;
+          return value && pattern.test(value);
+        },
+        error: () => {
+          return i18n.t('Invalid domain value');
+        },
+      },
+    };
+  }
 };
 
 export default (new FormManager());
