629bfc5a7554ab36e618f2df05e8e1d28f16e235
david-fiaty <dfiaty@gmail.com>
2025-04-29 14:19:02 +0000

    Update JamItemSection.tsx

--- components/section/JamItemSection.tsx
+++ components/section/JamItemSection.tsx
@@ -1,7 +1,6 @@
 import { StyleSheet } from 'react-native';
 import { Layout } from '@/constants/Layout';
 import JamsList from '../list/JamsList';
-import TextView from '../view/TextView';
 
 type Props = {
   jamId: any;
