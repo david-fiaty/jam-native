73cef4ca428d68f7b8ab62199287244b68fd0eb1
david-fiaty <dfiaty@gmail.com>
2025-04-30 12:45:02 +0000

    Update FieldErrorView.tsx

--- components/view/FieldErrorView.tsx
+++ components/view/FieldErrorView.tsx
@@ -2,13 +2,13 @@ import { StyleSheet } from "react-native";
 import TextView from "./TextView";
 
 type Props = {
-  fieldKey: any;
+  message: any;
 };
 
-const FIeldErrorView = ({ fieldKey }: Props) => {
+const FIeldErrorView = ({ message }: Props) => {
   return (
     <TextView style={styles.container}>
-      {fieldKey}: Error message
+      {message}
     </TextView>
   );
 };
