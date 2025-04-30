8c03259f9dacf5e043e99b5ae92949773f24910e
david-fiaty <dfiaty@gmail.com>
2025-04-30 15:15:00 +0000

    Update ListItemDescription.tsx

--- components/list/JamsList/ListItemDescription.tsx
+++ components/list/JamsList/ListItemDescription.tsx
@@ -12,6 +12,9 @@ const ListItemDescription = ({ row }: Props) => {
   return (
     row?.item?.caption?.length > 0 && (
       <BoxView style={styles.container}>
+        {row?.item?.title?.length > 0 && (
+          <TextView style={styles.title}>{row.item.title}</TextView>
+        )}
         <TextView>{row?.item?.caption}</TextView>
       </BoxView>
     )
@@ -22,6 +25,9 @@ const styles = StyleSheet.create({
   container: {
     paddingHorizontal: Layout.space.base*1.2,
   },
+  title: {
+    fontWeight: 'bold',
+  }
 });
 
 export default ListItemDescription;
