bdfe4d300b394ed221c3eca530580c39a587d804
david-fiaty <dfiaty@gmail.com>
2025-05-01 14:13:04 +0000

    Update ProfileItemView.tsx

--- components/view/ProfileItemView.tsx
+++ components/view/ProfileItemView.tsx
@@ -108,7 +108,7 @@ const styles = StyleSheet.create({
     width: profileImageSize,
   },
   profileHeaderRight: {
-    paddingHorizontal: Layout.space.base*1.5,
+    paddingHorizontal: Layout.space.base,
     paddingTop: Layout.space.base*2, // Todo - Vertical align middle
     flex: 1,
     height: '100%',
