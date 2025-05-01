9727e94d02beabbba3479e33a12323e765443a91
david-fiaty <dfiaty@gmail.com>
2025-05-01 14:57:10 +0000

    Update ProfileItemView.tsx

--- components/view/ProfileItemView.tsx
+++ components/view/ProfileItemView.tsx
@@ -83,13 +83,12 @@ const ProfileItemView = ({ profileId }: Props) => {
 
       <DividerView />
 
-      <TextView style={styles.sectionTitle}>{i18n.t('Jams')}({profileItem?.number_of_jams || 0})</TextView>
+      <TextView style={styles.sectionTitle}>{i18n.t('Jams')} ({profileItem?.number_of_jams || 0})</TextView>
       <BoxView direction="row" align="center" justify="flex-start" style={styles.profileJams}>
         <ProfileJamsList 
           idArray={[18, 20, 32, 33, 37]} 
           // Todo - Remove this when sub routing working, or implement
           onListItemPress={(row: any) => console.log(row)}
-          
           //onAddButtonPress={() => ScreenManager.toggleModal("JamForm")}
         />   
 
