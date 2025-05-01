26b8f2a1c5d12a43f9938bd43156bd7e2c076723
david-fiaty <dfiaty@gmail.com>
2025-05-01 15:25:37 +0000

    Update ProfileItemView.tsx

--- components/view/ProfileItemView.tsx
+++ components/view/ProfileItemView.tsx
@@ -22,7 +22,6 @@ const profileImageSize: number = 100;
 const ProfileItemView = ({ profileId }: Props) => {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [profileItem, setProfileItem] = useState<any>(null);
-  const [profileSectors, setProfileSectors] = useState<any>([]);
   const [sectorsData, setSectorsData] = useState<any>([]);
 
   const renderProfileSectors = (sectorsIds?: any)  => {
@@ -83,7 +82,7 @@ const ProfileItemView = ({ profileId }: Props) => {
 
       <DividerView />
 
-      <TextView style={styles.sectionTitle}>{i18n.t('Jams')} ({profileItem?.number_of_jams || 0})</TextView>
+      <TextView style={styles.sectionTitle}>{i18n.t('Profile Jams')} ({profileItem?.number_of_jams || 0})</TextView>
       <BoxView direction="row" align="center" justify="flex-start" style={styles.profileJams}>
         <ProfileJamsList 
           idArray={[18, 20, 32, 33, 37]} 
