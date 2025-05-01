d34acde7b7ded4e043cb3bb56b5e326f76aeb47c
david-fiaty <dfiaty@gmail.com>
2025-05-01 11:11:35 +0000

    Update ProfileItemView.tsx

--- components/view/ProfileItemView.tsx
+++ components/view/ProfileItemView.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { StyleSheet } from 'react-native';
 import { BaseProps } from '@/constants/Types';
 import TextView from './TextView';
+import EntityManager from "@/manager/EntityManager";
 
 type Props = BaseProps & {
   profileId: any;
@@ -9,14 +10,19 @@ type Props = BaseProps & {
 
 const ProfileItemView = ({ profileId }: Props) => {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [profileItem, setProfileItem] = useState<any>(null);
 
   useEffect(() => {
     (async () => {
       if (!isLoaded) {
+        setProfileItem((await EntityManager.getProfiles({ items_ids: [profileId] }))?.[0]);
         setIsLoaded(true);
       }  
     })();
-  }, [isLoaded]);
+  }, [isLoaded, profileId]);
+
+  
+  console.log(profileItem)
 
   return (
     <TextView>{profileId}</TextView>
