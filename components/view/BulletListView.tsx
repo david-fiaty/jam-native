5a49c494d762f89b2a76953032e9a41950f1e66e
david-fiaty <dfiaty@gmail.com>
2025-05-03 12:23:07 +0000

    Update BulletListView.tsx

--- components/view/BulletListView.tsx
+++ components/view/BulletListView.tsx
@@ -14,7 +14,7 @@ const BulletListView = ({ data, bulletHidden }: Props) => {
   return (
     <View style={styles.container}>
       {data.map((row: any) => (
-        <BoxView direction="row" align="center" justify="flex-start">
+        <BoxView direction="row" align="center" style={styles.item}>
           {bulletHidden !== true && <View style={styles.bullet}></View>}
           <TextView>{row}</TextView>
         </BoxView>
@@ -25,8 +25,13 @@ const BulletListView = ({ data, bulletHidden }: Props) => {
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
     margin: 0,
     padding: 0,
+  },
+  item: {
+    flexWrap: 'wrap',
   },
   bullet: {
     width: bulletSize,
