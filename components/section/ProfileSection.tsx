1dbccb450eec5289abe6c7107e2805a039e4c440
david-fiaty <dfiaty@gmail.com>
2025-05-03 16:42:24 +0000

    Update ProfileSection.tsx

--- components/section/ProfileSection.tsx
+++ components/section/ProfileSection.tsx
@@ -1,9 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { Layout } from "@/constants/Layout";
 import ProfileForm from "../form/ProfileForm";
 import BoxView from "../view/BoxView";
+import ProfileProjectsList from "../list/ProfileProjectsList";
+import i18n from "@/translation/i18n";
+import UserManager from "@/manager/UserManager";
 
+const resource: string = 'profile';
 
 const ProfileSection = () => {
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [profileId, setProfileId] = useState<number>(0);
+  const formData = useSelector((state: any) => state.form?.[resource]);
+
+  useEffect(() => {
+    (async () => {
+      if (!isLoaded) {
+        setProfileId(await UserManager.getProfileId());
+        setIsLoaded(true);
+      }
+    })();
+  }, [isLoaded, formData]);
+
   return (
     <BoxView
       direction="column"
@@ -13,12 +32,36 @@ const ProfileSection = () => {
       scroll={true}
     >
 
+      <ProfileProjectsList
+        title={i18n.t("Your Projects")}
+        addButton={true}
+        allButton={formData?.profile_projects?.length > 0}
+        idArray={formData?.profile_projects}
+      /*
+      onAddButtonPress={() => {
+        ScreenManager.toggleModal("AddProjectForm", {
+          profileId: profileId,
+          profileJams: formData?.profile_jams || [],
+        });
+      }}
+      */
+      />
 
+      {formData?.saved_projects?.length > 0 && (
+        <ProfileProjectsList
+          title={i18n.t("Saved Projects")}
+          allButton={formData?.saved_projects?.length > 0}
+          idArray={formData?.saved_projects}
+        />
+      )}
 
-
-
-      
-      <ProfileForm />
+      {formData?.liked_projects?.length > 0 && (
+        <ProfileProjectsList
+          title={i18n.t("Liked Projects")}
+          allButton={formData?.liked_projects?.length > 0}
+          idArray={formData?.liked_projects}
+        />
+      )}
     </BoxView>
   );
 };
