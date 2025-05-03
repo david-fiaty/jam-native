41aac8c7847db3b0d3495ccd882180da22435f24
david-fiaty <dfiaty@gmail.com>
2025-05-03 14:44:07 +0000

    Update SectionManager.tsx

--- manager/SectionManager.tsx
+++ manager/SectionManager.tsx
@@ -20,6 +20,10 @@ class SectionManager {
     let activeSections: any = [...Store.getState().section.active];
     let previousSectionIndex: number = 0;
 
+    console.log('----------------------------------------');
+    console.log(activeSections)
+
+
     if (activeSections.length > 1) previousSectionIndex = activeSections.length - 2;
     else if (activeSections.length > 0) previousSectionIndex = activeSections.length - 1;
 
