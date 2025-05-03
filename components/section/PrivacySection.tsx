c1bc5baa07ac79cf6644a99e0082c2cf09f7ab84
david-fiaty <dfiaty@gmail.com>
2025-05-03 12:15:45 +0000

    Update PrivacySection.tsx

--- components/section/PrivacySection.tsx
+++ components/section/PrivacySection.tsx
@@ -1,5 +1,4 @@
 import { StyleSheet, View } from 'react-native';
-import { useRouter } from 'expo-router';
 import { Layout } from '@/constants/Layout';
 import TextView from '../view/TextView';
 import BoxView from '../view/BoxView';
@@ -8,8 +7,6 @@ import BulletListView from '../view/BulletListView';
 import i18n from '@/translation/i18n';
 
 const PrivacySection = () => {
-  const router = useRouter();
-
   return (
     <BoxView
       direction="column"
@@ -20,9 +17,7 @@ const PrivacySection = () => {
     >
       <TextView style={styles.title}>{i18n.t('Introduction')}</TextView>
       <TextView>
-        Jamm is a web and mobile platform that allows artists to share their projects, events, and achievements.
-        We are committed to protecting the privacy and security of our users' personal data.
-        This privacy policy describes how we collect, use, and protect your personal information.
+        {i18n.t('Jamm is a web and mobile platform that allows artists to share their projects, events, and achievements. We are committed to protecting the privacy and security of our users personal data. This privacy policy describes how we collect, use, and protect your personal information.')}
       </TextView>
 
       <TextView style={styles.title}>{i18n.t('Data Collection')}</TextView>
@@ -40,58 +35,67 @@ const PrivacySection = () => {
       </View>
 
       <TextView style={styles.title}>{i18n.t('Use of Data')}</TextView>
-      <TextView>
-        We use your data for:
-        - Personalizing your experience on Jamm
-        - Managing events and interactions between users
-        - Offering targeted advertising
-        - Continuously improving the user experience
-      </TextView>
+      <View>
+        <TextView>{i18n.t('We use your data for:')}</TextView>
+        <BulletListView data={[
+          i18n.t('Personalizing your experience on Jamm'),
+          i18n.t('Managing events and interactions between users'),
+          i18n.t('Offering targeted advertising'),
+          i18n.t('Continuously improving the user experience'),
+        ]} />
+        
+      </View>
 
       <TextView style={styles.title}>{i18n.t('Data Sharing')}</TextView>
-      <TextView>
-        We share your data with:
-        - Technical providers (hosting services, analytics tools)
-        - Business partners
-        - Public administrations, if required by law
-      </TextView>
+      <View>
+        <TextView>{i18n.t('We share your data with:')}</TextView>
+        <BulletListView data={[
+          i18n.t('Technical providers (hosting services, analytics tools)'),
+          i18n.t('Business partners'),
+          i18n.t('Public administrations, if required by law'),
+        ]} />
+      </View>
 
       <TextView style={styles.title}>{i18n.t('User Rights')}</TextView>
-      <TextView>
-        As a user, you have the right to:
-        - Access your data
-        - Rectify your data
-        - Request the erasure (deletion) of your data
-        - Request data portability
-        - Object to the processing of your data
-      </TextView>
+      <View>
+        <TextView>{i18n.t('As a user, you have the right to:')}</TextView>
+        <BulletListView data={[
+          i18n.t('Access your data'),
+          i18n.t('Rectify your data'),
+          i18n.t('Request the erasure (deletion) of your data'),
+          i18n.t('Request data portability'),
+          i18n.t('Object to the processing of your data'),
+        ]} />
+      </View>
 
       <TextView style={styles.title}>{i18n.t('Exercising Your Rights')}</TextView>
-      <TextView>
-        To exercise your rights, please contact us at the following email address: contact@jammm.app.
-      </TextView>
+      <TextView>{i18n.t('To exercise your rights, please contact us at the following email address: contact@jammm.app.')}</TextView>
 
       <TextView style={styles.title}>{i18n.t('Data Security')}</TextView>
-      <TextView>
-        We implement several measures to protect your data:
-        - Data encryption
-        - Firewalls
-        - Regular backups
-      </TextView>
+      <View>
+        <TextView style={styles.title}>{i18n.t('We implement several measures to protect your data:')}</TextView>
+        <BulletListView data={[
+          i18n.t('Data encryption'),
+          i18n.t('Firewalls'),
+          i18n.t('Regular backups'),
+        ]} />
+      </View>
 
       <TextView style={styles.title}>{i18n.t('International Data Transfers')}</TextView>
-      <TextView>
-        We comply with the GDPR for all data transfers outside the EU/EEA.
-        We use adequacy decisions for countries recognized by the European Commission and Standard Contractual Clauses for other countries.
-        We apply enhanced security measures, such as data encryption.
-      </TextView>
+      <View>
+      <BulletListView data={[
+          i18n.t('We comply with the GDPR for all data transfers outside the EU/EEA.'),
+          i18n.t('We use adequacy decisions for countries recognized by the European Commission and Standard Contractual Clauses for other countries.'),
+          i18n.t('We apply enhanced security measures, such as data encryption.'),
+      ]} />
+      </View>
 
       <TextView style={styles.title}>{i18n.t('Changes to the Policy')}</TextView>
       <TextView>
         {i18n.t('We reserve the right to modify this privacy policy at any time. In case of significant changes, we will notify you by email.')}
       </TextView>
 
-      <TextView style={styles.title}>{i18n.t('How to request deletion of your data on Jam')}</TextView>
+      <TextView style={styles.title}>{i18n.t('How to request deletion of your data on Jamm')}</TextView>
       <TextView>
         {i18n.t('If you want to delete your data associated with the Jammm app, please contact us at contact@jammm.app . We will remove all data within 7 days.')}
       </TextView>
