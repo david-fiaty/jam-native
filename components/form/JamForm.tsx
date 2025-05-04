71ddaf72706e468980d92ca0ed02faea4c045459
david-fiaty <dfiaty@gmail.com>
2025-05-04 18:51:42 +0000

    Update JamForm.tsx

--- components/form/JamForm.tsx
+++ components/form/JamForm.tsx
@@ -218,6 +218,8 @@ const JamForm = ({ jamId }: Props) => {
         <CollaboratorsField
           resource={resource}
           field="collaborators_ids"
+          placeholder={i18n.t('Select collaborators')}
+          value={formData?.collaborators_ids}
           onChangeValue={(value: string) => FormManager.updateField(resource, 'collaborators_ids', value)}
           onPress={() => ModalManager.toggleModal('CollaboratorsList', {
             resource: resource,
