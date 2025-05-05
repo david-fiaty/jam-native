331af7552aded393b31c50cb124b74bdd2bf0e07
david-fiaty <dfiaty@gmail.com>
2025-05-05 12:33:27 +0000

    Update SectionView.tsx

--- components/section/SectionView.tsx
+++ components/section/SectionView.tsx
@@ -12,8 +12,23 @@ import SectionHeader from '../section/navigation/SectionHeader';
 import SectionFooter from '../section/navigation/SectionFooter';
 import ModalView from "../modal/ModalView";
 import SectionBackButton from "./navigation/SectionBackButton";
-import SectionManager from "@/manager/SectionManager";
 import MessageView from "../view/MessageView";
+import i18n from "@/translation/i18n";
+import AboutSection from "@/components/section/AboutSection";
+import JamsSection from "@/components/section/JamsSection";
+import LegalSection from "@/components/section/LegalSection";
+import LoginSection from "@/components/section/LoginSection";
+import WelcomeSection from "@/components/section/WelcomeSection";
+import SignupSection from "@/components/section/SignupSection";
+import ProfileSection from "@/components/section/ProfileSection";
+import AccountForm from "@/components/form/AccountForm";
+import PasswordForm from "@/components/form/PasswordForm";
+import PrivacySection from "@/components/section/PrivacySection";
+import JamItemSection from "@/components/section/JamItemSection";
+import ProfileItemSection from "@/components/section/ProfileItemSection";
+import ProjectItemSection from "@/components/section/ProjectItemSection";
+import ProfileFormSection from "@/components/section/ProfileFormSection";
+import AddProjectForm from "@/components/form/AddProjectForm";
 
 const SectionView = () => { 
   const path = usePathname();
@@ -26,7 +41,7 @@ const SectionView = () => {
   const sectionId: any = path.split('/').pop();
 
   const getSectionStack = () => {
-    let section: any = SectionManager.getSection(sectionId, false);
+    let section: any = getSection(sectionId, false);
     let isStacked: any = sectionState.active.find((o: any) => o.id === sectionId);
     
     if (section && isStacked) {
@@ -44,7 +59,7 @@ const SectionView = () => {
 
   const getCurrentSection = () => {
     return {
-      ...SectionManager.getSection(sectionId || Config.defaultSection),
+      ...getSection(sectionId || Config.defaultSection),
       ...{ params: params },
     };
   };
@@ -58,6 +73,134 @@ const SectionView = () => {
       && currentSection?.showBackButton === true 
       && (!modalState.active.length || !isModalTitleVisible());
   };
+
+  const getSection = (sectionId: any, renderer: boolean = true) => {
+    return getSections(renderer).find((o: any) => o.id === sectionId);
+  }
+
+  const getSections = (renderer: boolean = true) => {
+    let config: any[] = getConfig();
+
+    if (!renderer) {
+      config = config.map(({ render, ...rest }) => rest);
+    }
+
+    return config;
+  }
+
+  const getConfig = () => {
+    return [
+      {
+        id: 'welcome',
+        title: i18n.t('Welcome'),
+        showTitle: false,
+        showHeader: false,
+        showFooter: false,
+        showBackButton: false,
+        render: (params: any) => <WelcomeSection {...params} />,
+      },
+      {
+        id: 'login',
+        title: i18n.t('Login'),
+        showTitle: false,
+        showHeader: false,
+        showFooter: false,
+        showBackButton: false,
+        render: (params: any) => <LoginSection {...params} />,
+      },
+      {
+        id: 'signup',
+        title: i18n.t('Signup'),
+        showTitle: false,
+        showHeader: false,
+        showFooter: false,
+        showBackButton: false,
+        render: (params: any) => <SignupSection {...params} />,
+      },
+      {
+        id: 'about',
+        title: i18n.t('About'),
+        showFooter: false,
+        backButtonRoute: '/',
+        render: (params: any) => <AboutSection {...params} />,
+      },
+      {
+        id: 'legal',
+        title: i18n.t('Legal'),
+        showFooter: false,
+        backButtonRoute: '/',
+        render: (params: any) => <LegalSection {...params} />,
+      },
+      {
+        id: 'privacy',
+        title: i18n.t('Privacy'),
+        showFooter: false,
+        backButtonRoute: '/',
+        render: (params: any) => <PrivacySection {...params} />,
+      },
+      {
+        id: 'jams',
+        title: i18n.t('Jams'),
+        showBackButton: false,
+        render: (params: any) => <JamsSection {...params} />,
+      },
+      {
+        id: 'jam-item',
+        title: i18n.t('Jam'),
+        showBackButton: true,
+        render: (params: any) => <JamItemSection {...params} />,
+      },
+      {
+        id: 'profile-item',
+        title: i18n.t('Profile'),
+        showBackButton: true,
+        render: (params: any) => <ProfileItemSection {...params} />,
+      },
+      {
+        id: 'project-item',
+        title: i18n.t('Project'),
+        showBackButton: true,
+        render: (params: any) => <ProjectItemSection {...params} />,
+      },
+      {
+        id: 'profile',
+        title: i18n.t('Your profile'),
+        render: (params: any) => <ProfileSection {...params} />,
+      },
+      {
+        id: 'profile-form',
+        title: i18n.t('Edit Profile'),
+        render: (params: any) => <ProfileFormSection {...params} />,
+      },
+      {
+        id: 'account',
+        title: i18n.t('Account'),
+        render: (params: any) => <AccountForm {...params} />,
+      },
+      {
+        id: 'password',
+        title: i18n.t('Password'),
+        render: (params: any) => <PasswordForm {...params} />,
+      },
+      {
+        id: 'add-project',
+        title: i18n.t('Add project'),
+        render: (params: any) => <AddProjectForm {...params} />,
+      },
+    ].map((o: any) => {
+      return {
+        ...{
+          showTitle: true,
+          showHeader: true,
+          showFooter: true,
+          showBackButton: true,
+          backButtonRoute: null,
+          params: {},
+        },
+        ...o,
+      };
+    });
+  }
 
   useEffect(() => {
     let activeSections: any [] = getSectionStack();
