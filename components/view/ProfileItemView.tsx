86b35a3632be4435a0677fdc2381d1c367ffce16
david-fiaty <dfiaty@gmail.com>
2025-05-08 14:19:25 +0000

    Update profile jams view

--- components/view/ProfileItemView.tsx
+++ components/view/ProfileItemView.tsx
@@ -72,7 +72,7 @@ const ProfileItemView = ({ profileId }: Props) => {
           allButton={true}
           title={<TextView style={styles.sectionTitle}>{i18n.t('Jams')} ({profileItem?.number_of_jams || 0})</TextView>}
           idArray={profileItem?.profile_jams?.map((o: any) => o.id)} // Todo - API should send ids, not full objects
-          onListItemPress={(row: any) => SectionManager.push(router, 'jam-item', { jamId: row?.item?.id, title: row?.item?.title })}
+          onListItemPress={(row: any) => SectionManager.push(router, 'jam-item', { jamId: JSON.stringify([row?.item?.id]), title: row?.item?.title })}
         />   
 
       </BoxView>
