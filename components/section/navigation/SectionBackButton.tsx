002bf11f81f0e4d806f3be2b407380ad45402d47
david-fiaty <dfiaty@gmail.com>
2025-05-09 12:09:17 +0000

    Update SectionBackButton.tsx

--- components/section/navigation/SectionBackButton.tsx
+++ components/section/navigation/SectionBackButton.tsx
@@ -21,6 +21,9 @@ const SectionBackButton = () => {
   const getCurrentSection = () => {
     let activeSections: any = sectionState.active;
     let targetSection: any = activeSections[activeSections.length - 1];
+
+
+    console.log(targetSection);
     
     return targetSection;
   };
