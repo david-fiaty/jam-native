75e92540a93c815730b5ebba14fc874f3dd15041
david-fiaty <dfiaty@gmail.com>
2025-05-10 12:45:11 +0000

    Update JamForm.tsx

--- components/form/JamForm.tsx
+++ components/form/JamForm.tsx
@@ -56,10 +56,11 @@ const JamForm = ({ jamId }: Props) => {
     };
 
     let result: any = await EntityManager.addJam(formData);
-    if (result.success === false) message.content = i18n.t('Invalid data submission.')
+    if (result.success === false) {
+      message.content = i18n.t('Invalid data submission.');
+      FormManager.addServerErrors(resource, result.response);
+    }
  
-      console.log(result);
-
     ScreenManager.showMessage(message);
     setIsProcessing(false);
   };
