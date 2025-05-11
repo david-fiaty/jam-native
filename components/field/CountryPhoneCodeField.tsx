3bafaecb29ffeb2ffa9d643e28005babd633543d
david-fiaty <dfiaty@gmail.com>
2025-05-11 13:31:23 +0000

    Create CountryPhoneCodeField.tsx

---
+++ components/field/CountryPhoneCodeField.tsx
@@ -0,0 +1,44 @@
+import { StyleSheet } from 'react-native';
+import BoxView from '../view/BoxView';
+import SelectListBase from '../base/SelectListBase';
+import StaticData from '@/constants/StaticData';
+import i18n from '@/translation/i18n';
+
+type Props = {
+  value?: any;
+  disabled?: any;
+  onChangeValue?: (option: any) => void;
+};
+
+const CountryPhoneCodeField = ({value, disabled, onChangeValue}: Props) => {
+  const countryCodes = StaticData.countryPhoneCodes;
+
+  const buildOptions = (optionsData: any) => {    
+    return [...(optionsData || [])].map((item: any) => {
+      return {
+        value: item.code,
+        label: `${item.name} (+${item.prefix})`,
+      }
+    });
+  };
+  
+  return (
+    <BoxView direction="column" align="left" style={styles.container}>
+      <SelectListBase 
+        placeholder={i18n.t('Select a phone code')}
+        value={value}
+        data={buildOptions(countryCodes)}  
+        onChangeValue={onChangeValue}
+        disabled={disabled}
+      />
+    </BoxView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+  },
+});
+
+export default CountryPhoneCodeField;
