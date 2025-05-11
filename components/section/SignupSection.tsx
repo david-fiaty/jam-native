f3f127e388563a368f0a92d76e364702f0250a5c
david-fiaty <dfiaty@gmail.com>
2025-05-11 13:00:54 +0000

    Update SignupSection.tsx

--- components/section/SignupSection.tsx
+++ components/section/SignupSection.tsx
@@ -32,7 +32,7 @@ const SignupSection = () => {
     (async () => {
         if (!isLoaded) {
           //resetForm(); Todo - Enable this and fix reset issue
-          setCurrentTab(StaticData.authTabs.find((o: any) => o?.default === true));
+          setCurrentTab((StaticData.authTabs.find((o: any) => o?.default === true))?.id);
           setIsLoaded(true);
         }
     })();
