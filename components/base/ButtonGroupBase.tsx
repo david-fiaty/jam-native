5047d8ae681a42398a90cd90f8c14379fde82921
david-fiaty <dfiaty@gmail.com>
2025-05-12 13:45:51 +0000

    Update ButtonGroupBase.tsx

--- components/base/ButtonGroupBase.tsx
+++ components/base/ButtonGroupBase.tsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { ButtonGroup } from "@rneui/themed";
+import { Layout } from "@/constants/Layout";
+import { Colors } from "@/constants/Colors";
 
 type Props = {
   value?: any;
   data?: object;
   disabled?: boolean;
+  containerStyle?: any
   onChangeValue?: (option: any) => void;
 };
 
-const ButtonGroupBase = ({value, data, disabled, onChangeValue}: Props) => {
+const ButtonGroupBase = ({value, data, disabled, containerStyle, onChangeValue}: Props) => {
   const [selectedIndex, setSelectedIndex] = useState<number>(0);
 
   const buildOptions = (optionsData: any) => {
@@ -35,6 +38,8 @@ const ButtonGroupBase = ({value, data, disabled, onChangeValue}: Props) => {
         selectedIndex={selectedIndex}
         buttons={buildOptions(data)}
         disabled={disabled}
+        containerStyle={[styles.groupContainer, containerStyle]}
+        buttonContainerStyle={styles.buttonContainer}
       />
     </View>
   );
@@ -42,7 +47,20 @@ const ButtonGroupBase = ({value, data, disabled, onChangeValue}: Props) => {
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
+    backgroundColor: Colors.secondary,
+    borderWidth: Layout.borderWidth.base, 
+    borderColor: Colors.secondary, 
+    borderRadius: Layout.radius.round,
+  },
+  groupContainer: {
+    backgroundColor: 'gray',
+    padding: 0,
+    margin: 0,
+  },
+  buttonContainer: {
+    backgroundColor: 'green',
+    padding: 0,
+    margin: 0,
   },
 });
 
