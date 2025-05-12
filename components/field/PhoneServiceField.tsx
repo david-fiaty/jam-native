711c171d7b66c4547a7cf424a0b108b26d7d0faa
david-fiaty <dfiaty@gmail.com>
2025-05-12 15:07:11 +0000

    Update PhoneServiceField.tsx

--- components/field/PhoneServiceField.tsx
+++ components/field/PhoneServiceField.tsx
@@ -13,41 +13,15 @@ type Props = {
 
 const PhoneServiceField = ({value, disabled, onChangeValue}: Props) => {
   const phoneServices = StaticData.phoneServices;
-
-  console.log(phoneServices);
-
-  const buildOptions = (optionsData: any) => {    
-    return [...(optionsData || [])].map((item: any) => {
-      return {
-        value: item?.id,
-        label: item?.label,
-      }
-    });
-  };
   
-  // Todo - Implement phone service component
-  
-  return (
-    <BoxView direction="column" align="left" style={styles.container}>
-      <ButtonGroupBase
-        data={phoneServices}
-      />
-      
-      { /*<SelectListBase 
-        placeholder={i18n.t('Select a phone service')}
-        value={value}
-        data={buildOptions(phoneServices)}  
-        onChangeValue={onChangeValue}
-        disabled={disabled}
-       /> */ }
-    </BoxView>
+  return (    
+    <ButtonGroupBase
+      value={value}
+      data={phoneServices}
+      onChangeValue={onChangeValue}
+      disabled={disabled}
+    />  
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-  },
-});
 
 export default PhoneServiceField;
