bb255a533ee58bdb865954db46795186e142c67e
david-fiaty <dfiaty@gmail.com>
2025-05-15 14:33:36 +0000

    Update SectionManager.tsx

--- manager/SectionManager.tsx
+++ manager/SectionManager.tsx
@@ -50,6 +50,8 @@ class SectionManager {
       let targetSection: any = sectionConfig.find((o: any) => o.id === activeSections[activeSections.length - 1].id);
 
       if (targetSection.backButtonRoute !== null) {
+        Store.dispatch(setActiveSections([]));
+        
         router.dismissTo({
           pathname: targetSection.backButtonRoute,
           params: targetSection?.params || {},
