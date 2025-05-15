e295bbbf357ecc8444eb295d94309837fd73bacb
david-fiaty <dfiaty@gmail.com>
2025-05-15 17:08:48 +0000

    Update AddProjectForm.tsx

--- components/form/AddProjectForm.tsx
+++ components/form/AddProjectForm.tsx
@@ -28,6 +28,7 @@ const AddProjectForm = () => {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [isProcessing, setIsProcessing] = useState<boolean>(false);
   const [profileId, setProfileId] = useState<number>(0);
+  const [profileItem, setProfileItem] = useState<any>(null);
   const formData: any = useSelector((state: any) => state.form[resource]);
 
   const updateField = (key: any, value: any) => {
@@ -55,8 +56,9 @@ const AddProjectForm = () => {
 
   useEffect(() => {
     (async () => {
-      if (!isLoaded) {
+      if (!isLoaded) { 
         setProfileId(await UserManager.getProfileId());
+        setProfileItem(await UserManager.getProfileData());
         
         dispatch(setFormData<any>({ 
           resource: resource,
@@ -73,6 +75,8 @@ const AddProjectForm = () => {
   }, [isLoaded, resource, profileId, formData]);
 
   if (!isLoaded) return <SpinnerView />;
+
+  console.log(profileItem?.profile_jams)
   
   return (
     <BoxView
@@ -155,7 +159,7 @@ const AddProjectForm = () => {
           isDeletable={true}
           multiSelect={true}
           title={<TextView>{i18n.t('Project Jams')}</TextView>}
-          idArray={formData?.profile_jams}
+          idArray={profileItem?.profile_jams}
           onAddButtonPress={() => ModalManager.toggleModal("SelectJamsForm", {
             field: 'jams_ids',
             resource: resource,
