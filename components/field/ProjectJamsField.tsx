6c1c3195b936d18977c2fc092178dab56eff64f7
david-fiaty <dfiaty@gmail.com>
2025-05-18 14:16:40 +0000

    Update ProjectJamsField.tsx

--- components/field/ProjectJamsField.tsx
+++ components/field/ProjectJamsField.tsx
@@ -1,14 +1,51 @@
-import { StyleSheet } from 'react-native';
-import TextView from '../view/TextView';
+import { useState, useEffect } from 'react';
+import { StyleSheet } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import SpinnerView from '../view/SpinnerView';
+import ProjectJamsList from '../list/ProjectJamsList';
+import ModalManager from '@/manager/ModalManager';
+import UserManager from '@/manager/UserManager';
 
 type Props = {
-
+  resource: string;
+  field: string;
+  value?: any;
+  placeholder?: any;
+  onPress?: () => void;
 };
 
-const ProjectJamsField = ({  }: Props) => {
+const ProjectJamsField = ({ resource, field, value, placeholder, onPress }: Props) => {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [currentValue, setCurrentValue] = useState<any>([]);
+  const [profileData, setProfileData] = useState<any>(null);
+  const formData: any = useSelector((state: any) => state.form[resource]);
+
+  useEffect(() => {
+    (async () => {
+      if (!isLoaded) {
+        setProfileData(await UserManager.getProfileData());
+        setIsLoaded(true);
+      }
+
+      //setCurrentValue(getSelectedCountries());        
+    })();    
+  }, [isLoaded, formData, field]);
+
+  if (!isLoaded) return <SpinnerView size="small" />;
 
   return (
-    <TextView>Project jams field</TextView>
+    <ProjectJamsList
+      resource="project"
+      idArray={formData?.jams_ids}
+      addButton={true}
+      onAddButtonPress={() => ModalManager.toggleModal("SelectJamsForm", {
+        field: 'jams_ids',
+        idArray: JSON.stringify(profileData?.profile_jams || []),
+        multiSelect: true,
+        resource: resource,
+      })}
+    />
   );
 };
 
