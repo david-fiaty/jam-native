2a578ff46cd50b247cc3ba8db9b29bc0fb1f0ef5
david-fiaty <dfiaty@gmail.com>
2025-05-19 15:39:51 +0000

    Update LanguageForm.tsx

--- components/form/LanguageForm.tsx
+++ components/form/LanguageForm.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Layout } from '@/constants/Layout';
 import i18n from "@/translation/i18n";
 import i18next from 'i18next';
@@ -7,12 +8,20 @@ import StaticData from '@/constants/StaticData';
 import UserManager from '@/manager/UserManager';
 
 const LanguageForm = () => {
+  const [currentlLanguage, setCurrentLanguage] = useState<any[]>([]);
   const data = StaticData.languages;
 
   const changeLanguage = (language: any) => {
     i18next.changeLanguage(language.value);
     UserManager.setLanguage(language.value);
   };
+
+  useEffect(() => {
+    (async () => {
+      setCurrentLanguage(await UserManager.getLanguage());
+    })();
+
+  }, []);
 
   return (
     <BoxView align="flex-start" justify="flex-start" scroll={true} style={Layout.formContainer}>
