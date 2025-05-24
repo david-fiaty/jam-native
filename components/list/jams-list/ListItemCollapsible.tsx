1e2067740cdb417fa9522b5e718167f04347708e
david-fiaty <dfiaty@gmail.com>
2025-05-24 14:13:05 +0000

    Update ListItemCollapsible.tsx

--- components/list/jams-list/ListItemCollapsible.tsx
+++ components/list/jams-list/ListItemCollapsible.tsx
@@ -122,7 +122,7 @@ const ListItemCollapsible = ({ row, sectorsData }: Props) => {
       <CollapsibleView
         label={<TextView>{i18n.t("View more.")}</TextView>}
         openedLabel={<TextView>{i18n.t("View less.")}</TextView>}
-        preview={true}
+        preview={'xxx'}
         content={
           <BoxView
             direction="column"
