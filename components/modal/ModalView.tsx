fdf8160f42bdb4c1357adf7610edbb96584cebf3
david-fiaty <dfiaty@gmail.com>
2025-05-25 00:45:53 +0000

    Update ModalView.tsx

--- components/modal/ModalView.tsx
+++ components/modal/ModalView.tsx
@@ -34,7 +34,8 @@ const ModalView = ({ currentSection, style }: Props) => {
     let top: number = 0;
 
     if (sectionState.active.length > 4 && modalState.active.length > 1) {
-      //top = -Layout.space.base*3.6;
+      // Todo - Enable or remove
+      //top = -Layout.space.base*3.6; 
     }
 
     return {
