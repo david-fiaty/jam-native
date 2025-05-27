4770b57f4d7c91268a64cb793661e78709c1a56e
david-fiaty <dfiaty@gmail.com>
2025-05-27 12:51:57 +0000

    Create SlideshowDots.tsx

---
+++ components/slideshow/SlideshowDots.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+
+type Props = {
+  index: number;
+  count?: number;
+};
+
+const SlideshowDots = ({ index, count }: Props) => {
+
+  return (<></>);
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default SlideshowDots;
