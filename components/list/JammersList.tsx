cbbbab9ed5dc54c89bcadd914f210cf974ec43ac
david-fiaty <dfiaty@gmail.com>
2025-05-28 13:18:48 +0000

    Update JammersList.tsx

--- components/list/JammersList.tsx
+++ components/list/JammersList.tsx
@@ -44,6 +44,15 @@ const JammersList = ({ jamId, jammersIds }: Props) => {
 
   if (!isLoaded) return <SpinnerView />;
 
+  console.log('---', jamId)
+  console.log(typeof jammersIds)
+console.log(jammersIds.join(','))
+  /*
+  if (jammersIds?.length) {
+    console.log(jammersIds.length.join(','))
+  }
+    */
+
   return (
     <View style={Layout.borderedListContainer}>
       {profiles?.length > 0 &&
