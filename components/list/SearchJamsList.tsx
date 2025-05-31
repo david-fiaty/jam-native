6b2f8f964fe32ef715bee9e67903f1b36fcb2d05
david-fiaty <dfiaty@gmail.com>
2025-05-31 15:52:13 +0000

    Update SearchJamsList.tsx

--- components/list/SearchJamsList.tsx
+++ components/list/SearchJamsList.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Platform } from "react-native";
 import { useRouter } from "expo-router";
 import { Layout } from "@/constants/Layout";
 import ListView from "../view/ListView";
@@ -58,6 +58,7 @@ const SearchJamsList = ({ data, filter }: Props) => {
       direction="column"
       align="flex-start"
       justify="flex-start"
+      scroll={Platform.OS === 'web' ? true : false}
       style={styles.container} 
     >
       <ListView
