ce76b8dde48ef16f28ebc87074c381ce0e6f28a9
david-fiaty <dfiaty@gmail.com>
2025-05-31 15:39:09 +0000

    Update SearchJamsList.tsx

--- components/list/SearchJamsList.tsx
+++ components/list/SearchJamsList.tsx
@@ -66,7 +66,6 @@ const SearchJamsList = ({ data, filter }: Props) => {
         numColumns={numColumns}
         contentContainerStyle={{ gap: Layout.space.base }}
         columnWrapperStyle={{ gap: Layout.space.base }}
-        scrollEnabled={false}
         renderItem={(row: any) => renderItem(row)}
       />
     </BoxView>
