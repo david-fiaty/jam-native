c5acfb8089262e2aa53787faa0d25972c6642e31
david-fiaty <dfiaty@gmail.com>
2025-06-02 16:26:32 +0000

    Update JamCommentsList.tsx

--- components/list/JamCommentsList.tsx
+++ components/list/JamCommentsList.tsx
@@ -99,6 +99,7 @@ const JamCommentsList = ({ entityId, entityType }: Props) => {
               </TouchableWithoutFeedback>
             )}
             openedLabel={<></>}
+            isExpanded={isFormExpanded}
             onLabelPress={() => setIsFormExpanded(true)}
             content={(
               <>
