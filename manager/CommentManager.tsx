207500a133ef4333055fa9107d3ed116b525fc39
david-fiaty <dfiaty@gmail.com>
2025-06-04 16:52:37 +0000

    Update CommentManager.tsx

--- manager/CommentManager.tsx
+++ manager/CommentManager.tsx
@@ -112,7 +112,7 @@ class CommentManager {
   }
 
   renderCommentForm(row?: any) {
-    let activeComment: any = Store.getState().form.comment;
+    let activeComment: any = Store.getState().comment;
     let commentId: any = activeComment?.id || null;
     let isEditing: boolean = false;
 
@@ -175,7 +175,7 @@ class CommentManager {
   }
 
   toggleCommentForm(row?: any) {
-    let activeComment: any = {...Store.getState().form.comment};
+    let activeComment: any = {...Store.getState().comment};
     let commentId: number = row?.item?.id || 0;
 
     console.log(activeComment);
@@ -198,7 +198,7 @@ class CommentManager {
   }
 
   renderCommentFormFields(row?: any) {
-    let commentState: any = Store.getState().form.comment;
+    let commentState: any = Store.getState().comment;
 
     return (
       <>
