a19107d42844239e67b7f1c631874dba827daffd
david-fiaty <dfiaty@gmail.com>
2025-06-06 16:04:38 +0000

    Update CommentItem.tsx

--- components/comment/CommentItem.tsx
+++ components/comment/CommentItem.tsx
@@ -67,7 +67,7 @@ const CommentItem = ({ commentData, profileImage, globalStyles }: Props) => {
           direction="row"
           align="center"
           justify="flex-start"
-          style={componentStyles.commentActionsContainer}
+          style={componentStyles.commentContainerRight}
         >
           {true && ( // Todo - Show only if there are replies
             <CollapsibleView
