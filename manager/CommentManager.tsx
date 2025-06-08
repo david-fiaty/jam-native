633def62aa87251806d144916678985ce3969d43
david-fiaty <dfiaty@gmail.com>
2025-06-08 17:56:23 +0000

    Update CommentManager.tsx

--- manager/CommentManager.tsx
+++ manager/CommentManager.tsx
@@ -78,12 +78,20 @@ class CommentManager {
         globalStyles={styles}
         profileImage={this.renderProfileImage(row)}
         // Todo - Render comment replies
-        commentReplies={
-          <TextView>
-            this is the comment replies list component for a comment
-          </TextView>
-        } 
+        commentReplies={this.renderCommentReplies(row)} 
       />
+    );
+  }
+
+  renderCommentReplies(row: any) {
+    let repliesIds: any = (this.entityComments.find((o: any) => o.id === row.item.id)).sub_ids;
+    
+    console.log('replies ids', repliesIds);
+
+    return (
+      <TextView>
+        this is the comment replies list component for a comment
+      </TextView>
     );
   }
 
