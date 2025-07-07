9516b30e9b96d30cbb8af0a85a7f4dc284b3f8db
david-fiaty <dfiaty@gmail.com>
2025-07-07 17:08:47 +0000

    Update TextSlideshow.tsx

--- components/slideshow/TextSlideshow.tsx
+++ components/slideshow/TextSlideshow.tsx
@@ -35,7 +35,7 @@ const styles = StyleSheet.create({
   wrapper: {
   },
   slide: {
-    flex: 1,
+    //flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#9DD6EB',
