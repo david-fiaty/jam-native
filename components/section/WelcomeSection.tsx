0850cf7cbc2e64e0c8550ef7ec822bf8fd38c2d8
david-fiaty <dfiaty@gmail.com>
2025-07-07 17:15:01 +0000

    Update WelcomeSection.tsx

--- components/section/WelcomeSection.tsx
+++ components/section/WelcomeSection.tsx
@@ -4,14 +4,16 @@ import LogoView from '../view/LogoView';
 import TextSlideshow from '../slideshow/TextSlideshow';
 import StaticData from '@/constants/StaticData';
 import BottomLinks from './navigation/BottomLinks';
+import BoxView from '../view/BoxView';
 
 const WelcomeSection = () => {
   return (
     <>
       <LogoView size={110} />
 
-      <TextSlideshow data={StaticData.welcomeSlideshow} />
-
+      <BoxView direction="column" align="center" justify="center" style={{ flex: 1, height: '100%' }}>
+        <TextSlideshow data={StaticData.welcomeSlideshow} />
+      </BoxView>
       <Divider />
       <LoginSignupButton />
 
