fc9e616ea3552e5a3a2122cf9ef80c79a15d6207
david-fiaty <dfiaty@gmail.com>
2025-07-11 15:23:59 +0000

    Update ProfileListItem.tsx

--- components/list/list-item/ProfileListItem.tsx
+++ components/list/list-item/ProfileListItem.tsx
@@ -5,6 +5,7 @@ import BoxView from '@/components/view/BoxView';
 import IconView from '@/components/view/IconView';
 import ImageView from "@/components/view/ImageView";
 import MediaManager from "@/manager/MediaManager";
+import UserManager from "@/manager/UserManager";
 
 const profileImageSize: number = 34;
 
@@ -19,22 +20,6 @@ const ProfileListItem = ({ row, selected, onListItemPress }: Props) => {
     if (onListItemPress) {
       onListItemPress(row);
     }
-  };
-
-  const getDisplayName = (item: any) => {
-    let displayName: string = '';
-
-    if (item?.profile_type == 'personal') {
-      displayName = `${item?.profile_personal?.first_name || ''} ${item?.profile_personal?.last_name || ''}`
-    }
-    else if (item?.profile_type == 'venue') {
-      displayName = item?.profile_venue?.venue_name;
-    }
-    else if (item?.profile_type == 'organization') {
-      displayName = item?.profile_organization?.organization_name;
-    }
-
-    return displayName.trim().length > 0 ? displayName : item?.profile_name;
   };
 
   return (
@@ -67,7 +52,7 @@ const ProfileListItem = ({ row, selected, onListItemPress }: Props) => {
           />
         )}
 
-        <TextView>{getDisplayName(row?.item)}</TextView>
+        <TextView>{UserManager.getProfileDisplayName(row?.item)}</TextView>
 
         {selected &&
           <IconView
