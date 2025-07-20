7136958152ccba457cb1dcf14a0189fdd5d3abe0
david-fiaty <dfiaty@gmail.com>
2025-07-20 20:35:41 +0000

    Update LanguageForm.tsx

--- components/form/LanguageForm.tsx
+++ components/form/LanguageForm.tsx
@@ -14,6 +14,19 @@ const LanguageForm = () => {
   const [currentlLanguage, setCurrentLanguage] = useState<string>('');
   const data = StaticData.languages;
 
+  const getLanguages = () => {
+    return [
+      {
+        label: i18n.t('French'),
+        value: 'fr',
+      },
+      {
+        label: i18n.t('English'),
+        value: 'en',
+      },
+    ];
+  };
+
   const changeLanguage = async (languageCode: string) => {
     setCurrentLanguage(languageCode);
     await setLanguage(languageCode);
@@ -40,8 +53,8 @@ const LanguageForm = () => {
     <BoxView align="flex-start" justify="flex-start" scroll={true} style={Layout.formContainer}>
       <SelectListBase
         value={currentlLanguage}
-        data={data} 
-        placeholder={i18n.t('Select a language')} 
+        data={getLanguages()}
+        placeholder={i18n.t('Select a language')}
         onChangeValue={async (option: any) => await changeLanguage(option.value)}
       />
     </BoxView>
