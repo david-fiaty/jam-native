10845db4927e67110572b783ffd724cebdd40556
david-fiaty <dfiaty@gmail.com>
2025-07-20 18:24:51 +0000

    Update SectionHeader.tsx

--- components/section/navigation/SectionHeader.tsx
+++ components/section/navigation/SectionHeader.tsx
@@ -48,7 +48,7 @@ const SectionHeader = ({ style } : Props) => {
       </BoxView>
 
       <BoxView direction="row" align="center" justify="flex-end" style={styles.headerRight}>      
-        {isLoggedIn && <SearchField />}
+        <SearchField />
 
         {isLoggedIn && (
           <IconView
