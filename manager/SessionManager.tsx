deb23c26c521db536a75f9720c8884fd7dcf031a
david-fiaty <dfiaty@gmail.com>
2025-07-21 14:59:09 +0000

    Update SessionManager.tsx

--- manager/SessionManager.tsx
+++ manager/SessionManager.tsx
@@ -1,24 +1,11 @@
+import { setTokenData } from '@/redux/slices/UserSlice';
 import { Config } from '@/constants/Config';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Store from "@/redux/Store";
 
 class SessionManager {
-  async isTokenValid() {
-    let data: any = await this.getTokenData();
-    let exists: boolean = data?.access_token?.length > 0;
-    let valid: boolean = true;
-    // Todo - Check expiry date sent by server
-    //let valid: boolean = data?.access_token_exp && data.access_token_exp > Date.now();
-
-    return exists && valid;
-  }
-
   async setTokenData(data: any) {
-    try {
-      let json: string = JSON.stringify(data);
-      await AsyncStorage.setItem(Config.storageKeys.tokenData, json);
-    } catch (error) {
-      console.log(error);
-    }
+    Store.dispatch(setTokenData(data));
   }
 
   async getTokenData() {
