36b791029434e24fefa8808f9e5ca75e65fa8719
david-fiaty <dfiaty@gmail.com>
2025-07-21 15:03:03 +0000

    Update UserManager.tsx

--- manager/UserManager.tsx
+++ manager/UserManager.tsx
@@ -35,7 +35,7 @@ class UserManager {
     let success = response?.tokens?.access_token?.length > 0;
 
     if (success) {
-      await SessionManager.setTokenData(response.tokens);
+      SessionManager.setTokenData(response.tokens);
     }
 
     return {
@@ -50,7 +50,7 @@ class UserManager {
     let message: string = '';
 
     if (success) {
-      await SessionManager.setTokenData(response.tokens);
+      SessionManager.setTokenData(response.tokens);
     }
     else if (response?.non_field_errors?.length > 0) {
       message = response.non_field_errors[0];
