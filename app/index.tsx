5c4ab2c938108e227a58a70629ac40b6b66f510a
david-fiaty <dfiaty@gmail.com>
2025-07-21 16:43:20 +0000

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,31 @@
 import { useEffect, useRef } from 'react';
 import { useRouter, useRootNavigationState } from 'expo-router';
+import { Config } from '@/constants/Config';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import UserManager from '@/manager/UserManager';
 import i18next from 'i18next';
+import ScreenManager from '@/manager/ScreenManager';
 
 export default () => {
   const router = useRouter();
   const rootNavigationState = useRootNavigationState();
   const hasRedirected = useRef<boolean>(false);
+
+  const getLanguage = async () => {
+    Config
+    try {
+      let language = ScreenManager.isWeb()
+        ? localStorage.getItem(Config.storageKeys.currentLanguage)
+        : await AsyncStorage.getItem(Config.storageKeys.currentLanguage);
+
+      return language || Config.fallbackLanguage;
+    }
+    catch (error) {
+      console.log(error);
+
+      return Config.fallbackLanguage;
+    }
+  };
 
   useEffect(() => {
     if (rootNavigationState?.key && !hasRedirected.current) {
@@ -14,7 +33,7 @@ export default () => {
       router.push('/welcome');
     }
 
-    UserManager.getLanguage().then((code: string) => {
+    getLanguage().then((code: string) => {
       i18next.changeLanguage(code);
     })
   }, [rootNavigationState]);
