3d4008c437b01f8a2890d88e457035ef14878fd9
david-fiaty <dfiaty@gmail.com>
2025-07-22 18:36:45 +0000

    Update SectorsField.tsx

--- components/field/SectorsField.tsx
+++ components/field/SectorsField.tsx
@@ -120,7 +120,7 @@ const SectorsField = ({ resource, field, value, placeholder, onPress }: Props) =
             );
           })}
 
-          <IconView name="plus" theme="transparent" onPress={onPress} />
+          <IconView name="down" theme="transparent" onPress={onPress} />
         </View>
       )}
     </>
