7a3ecd8afe6b16c029b9cca03d33767d385ecbf6
david-fiaty <dfiaty@gmail.com>
2025-07-29 17:51:25 +0000

    Update SearchView.tsx

--- components/view/SearchView.tsx
+++ components/view/SearchView.tsx
@@ -1,8 +1,7 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setCurrentTab } from "@/redux/slices/SearchSlice";
 import { Layout } from "@/constants/Layout";
-import SpinnerView from "./SpinnerView";
 import SearchManager from "@/manager/SearchManager";
 import SearchJamsList from "../list/SearchJamsList";
 import SearchProfilesList from "../list/SearchProfilesList";
@@ -13,10 +12,8 @@ import i18n from "@/translation/i18n";
 
 const SearchView = () => {
   const dispatch = useDispatch();
-  const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [searchData, setSearchData] = useState<any>([]);
   const searchState: any = useSelector((state: any) => state.search);
-  const prevSearchState: any = useRef(searchState);
   
   const searchTabs: any[] = [
     {
@@ -72,24 +69,12 @@ const SearchView = () => {
   };
 
   useEffect(() => {
-    (async () => {
-      if (!isLoaded) {
-        loadSearchData();
-        setIsLoaded(true);
-      }
-
-      if (prevSearchState.current.searchValue != searchState.searchValue) {
-        setSearchData(await SearchManager.getResults()); 
-        prevSearchState.current = searchState;
-      }
-    })();
-
+    loadSearchData();
+    
     if (!searchState.currentTab) {
       dispatch(setCurrentTab((searchTabs.find((o: any) => o?.default === true))?.id));
     }
-  }, [isLoaded, searchState, searchTabs]);
-
-  if (!isLoaded) return <SpinnerView />;
+  }, [searchState, searchTabs]);
   
   return (
     <BoxView
