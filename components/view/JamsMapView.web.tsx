882f56ef73aed4fb52f133d8b0fa94b032737671
david-fiaty <dfiaty@gmail.com>
2025-07-30 17:52:03 +0000

    Update JamsMapView.web.tsx

--- components/view/JamsMapView.web.tsx
+++ components/view/JamsMapView.web.tsx
@@ -87,7 +87,7 @@ const JamsMapView = ({ idArray }: Props) => {
               disableDefaultUI: true,
             }}
           >
-            {(JSON.parse(searchState.currentResults) || [])?.map((item: any) => renderJamMarker(item))}
+            {(JSON.parse(searchState.currentResults) || [])?.jam?.map((item: any) => renderJamMarker(item))}
           </GoogleMap>
         </View>
       </TouchableWithoutFeedback>
