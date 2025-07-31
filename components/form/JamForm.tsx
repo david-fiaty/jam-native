cdd923a0b46e152d3054ac1427d4183d7a2ad8bf
david-fiaty <dfiaty@gmail.com>
2025-07-31 17:41:55 +0000

    Update JamForm.tsx

--- components/form/JamForm.tsx
+++ components/form/JamForm.tsx
@@ -26,6 +26,7 @@ import MediaManager from "@/manager/MediaManager";
 import ModalManager from "@/manager/ModalManager";
 import FormManager from "@/manager/FormManager";
 import CountriesField from "../field/CountriesField";
+import CountryField from "../field/CountryField";
 
 const resource: string = 'jam';
 
@@ -217,6 +218,14 @@ const JamForm = () => {
         {FormManager.renderError('period')}
 
         <TextView>{i18n.t('Country')}</TextView>
+
+    
+        <CountryField
+          value={formData?.countries}
+          onChangeValue={(option: any) => FormManager.updateField(resource, 'country', option.value)}
+        />
+
+
         <CountriesField
           resource={resource}
           field="country"
