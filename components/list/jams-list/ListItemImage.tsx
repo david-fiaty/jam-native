a6a35297759c2d4ca1e8ae4eae196f3260fad3e9
david-fiaty <dfiaty@gmail.com>
2025-08-01 17:11:27 +0000

    Update ListItemImage.tsx

--- components/list/jams-list/ListItemImage.tsx
+++ components/list/jams-list/ListItemImage.tsx
@@ -37,7 +37,7 @@ const styles = StyleSheet.create({
     //alignItems: 'center',
     height: slideHeight,
     width: slideWidth,
-    marginTop: Layout.space.base*5,
+    marginTop: Layout.space.base,
   },
 });
 
