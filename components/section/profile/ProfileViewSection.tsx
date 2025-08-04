507f1aa4d944a592c9263159cb0f79a8e3ebe9eb
david-fiaty <dfiaty@gmail.com>
2025-08-04 15:59:15 +0000

    Update ProfileViewSection.tsx

--- components/section/profile/ProfileViewSection.tsx
+++ components/section/profile/ProfileViewSection.tsx
@@ -114,7 +114,6 @@ const ProfileSection = () => {
 
       <ProfileViewField label={i18n.t('Industries')}>
         <TextView>
-          {false || i18n.t('Unavailable')}
           {[20, 13, 6, 36, 31].map((id: any) => {
             return (
               <TextView key={id}>{(sectorsData.find((o: any) => o.id == id))?.name}</TextView>
