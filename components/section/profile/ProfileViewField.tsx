bc60bc97859ce66737f44fad919c6ecaff2aabba
david-fiaty <dfiaty@gmail.com>
2025-08-04 17:36:50 +0000

    Update ProfileViewField.tsx

--- components/section/profile/ProfileViewField.tsx
+++ components/section/profile/ProfileViewField.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 const ProfileViewField = ({ label, children }: Props) => {
   return (
-    <BoxView style={[styles.container]} gap={10}>
+    <BoxView style={[styles.container]}>
       {label && (
         <TextView style={styles.label}>{label}</TextView>
       )}
