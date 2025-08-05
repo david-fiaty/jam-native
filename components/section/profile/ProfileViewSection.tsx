9d51345f1ba8673ae233645a7ef393d95c183335
david-fiaty <dfiaty@gmail.com>
2025-08-05 16:07:01 +0000

    Update ProfileViewSection.tsx

--- components/section/profile/ProfileViewSection.tsx
+++ components/section/profile/ProfileViewSection.tsx
@@ -64,9 +64,9 @@ const ProfileSection = () => {
 
           <ProfileViewField>
             <BoxView direction="row" align="center" justify="flex-start">
-              <IconView name="instagram" theme="transparent" size={18} padding={0} />
-              <IconView name="facebook" theme="transparent" size={18} padding={0} />
-              <IconView name="linkedin" theme="transparent" size={18} padding={0} />
+              <IconView name="instagram" theme="transparent" size={18} padding={0} onPress={() => console.log('icon pressed')} />
+              <IconView name="facebook" theme="transparent" size={18} padding={0} onPress={() => console.log('icon pressed')} />
+              <IconView name="linkedin" theme="transparent" size={18} padding={0} onPress={() => console.log('icon pressed')} />
             </BoxView>
           </ProfileViewField>
 
