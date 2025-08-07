5dd17f3054d3a8b663f1c87ff5ebd7b543a37e1a
david-fiaty <dfiaty@gmail.com>
2025-08-07 21:04:26 +0000

    Update ProfileProjectsList.tsx

--- components/list/ProfileProjectsList.tsx
+++ components/list/ProfileProjectsList.tsx
@@ -80,6 +80,8 @@ const ProfileProjectsList = ({
         let projects: any = [];
         let images: any = {};
 
+        console.log(idArray)
+
         if (idArray.length) {
           projects = await EntityManager.getProjects(idArray);
         }
