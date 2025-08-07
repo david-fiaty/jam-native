12a226a5b7b5c24eb8d714cf62f52401bdec4862
david-fiaty <dfiaty@gmail.com>
2025-08-07 22:31:29 +0000

    Update ProfileJamsField.tsx

--- components/field/ProfileJamsField.tsx
+++ components/field/ProfileJamsField.tsx
@@ -1,111 +1,84 @@
 import { StyleSheet, View, TouchableOpacity } from "react-native";
 import { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
 import { useRouter } from "expo-router";
-import { setFormData } from '@/redux/slices/FormSlice';
 import { Layout } from "@/constants/Layout";
 import TextView from "../view/TextView";
 import i18n from "@/translation/i18n";
 import ListView from "../view/ListView";
 import EntityManager from "@/manager/EntityManager";
 import SpinnerView from "../view/SpinnerView";
-import BoxView from "../view/BoxView";
-import JamListItem from "./list-item/JamListItem";
 import SectionManager from "@/manager/SectionManager";
+import MediaManager from "@/manager/MediaManager";
+import NoImageView from "../view/NoImageView";
+import ImageView from "../view/ImageView";
+import ScreenManager from "@/manager/ScreenManager";
 
 type Props = {
-  resource?: string;
-  field?: string;
-  title?: any;
   idArray?: any;
-  addButton?: boolean;
-  allButton?: boolean;
-  isAddable?: boolean;
-  isDeletable?: boolean;
-  multiSelect?: boolean;
-  emptyMessage?: any;
-  onAddButtonPress?: () => void,
-  onListItemPress?: (row: any) => void;
+  isPublic?: boolean;
 };
 
-const ProfileJamsField = ({ resource, field, title, idArray, addButton, allButton, isAddable, isDeletable, multiSelect, emptyMessage, onAddButtonPress, onListItemPress }: Props) => {
+const ProfileJamsField = ({ idArray, isPublic }: Props) => {
   const numColumns = 3;
   const router = useRouter();
-  const dispatch = useDispatch();
   const [profileJams, setProfileJams] = useState<any[]>([]);
-  const [selectedIds, setSelectedIds] = useState<any>([]);
 
   const onItemPress = (row: any) => {
-    if (onListItemPress) {
-      onListItemPress(row);
-    }
-    else if (isDeletable !== true) {
-      SectionManager.push(router, 'jam-item', { 
-        jamId: JSON.stringify([row?.item?.id]), 
-        title: row?.item?.title,
-        disableInfiniteScroll: true,
-      });
-    }
-    else {
-      toggleItem(row);
-    }
+    SectionManager.push(router, 'jam-item', {
+      jamId: JSON.stringify([row?.item?.id]),
+      title: row?.item?.title,
+      disableInfiniteScroll: true,
+    });
   };
 
-  const toggleItem = (row: any) => {
-    if (multiSelect === true) {
-      let selectedIdsList = [...selectedIds];
-      let index: number = selectedIdsList.findIndex((id: any) => id == row.item.id);
+  const renderItem = (row: any) => {
+    let imageSize = MediaManager.getThumbnailSize();
+    let output = null;
 
-      if (index === -1) selectedIdsList.push(row.item.id);
-      else selectedIdsList.splice(index, 1);
-
-      setSelectedIds(selectedIdsList);
+    if (!row?.item?.medias?.[0]?.url || row?.item?.medias?.[0]?.url == 'undefined') {
+      output = (
+        <View style={styles.item}>
+          <NoImageView 
+            width={imageSize.width} 
+            height={imageSize.height} 
+            rounded={true}
+          />
+        </View>
+      );
     }
     else {
-      setSelectedIds([row.item.id]);
+      output = (
+        <View style={styles.item}>
+          <ImageView
+            uri={MediaManager.getImageUrl(row.item.medias[0].url)}
+            width={imageSize.width}
+            height={imageSize.height}
+            resizeMode="cover"
+            style={[styles.image, ScreenManager.getGridCellSize(numColumns)]}
+          />
+        </View>
+      );
     }
-  };
 
-  const deleteItem = (row: any) => {
-    let itemIds: any[] = [...selectedIds].filter((n: number) => n !== row.item.id);
-
-    setSelectedIds(itemIds);
-    
-    dispatch(setFormData<any>({ 
-      resource: resource,
-      key: field, 
-      value: itemIds, 
-    }));
+    return (
+      <TouchableOpacity onPress={() => onItemPress(row)}>
+        {output}
+      </TouchableOpacity>
+    );
   };
 
   useEffect(() => {
     (async () => {
       if (!profileJams?.length && Array.isArray(idArray) && idArray?.length > 0) {
-        let jams: any = await EntityManager.getJams(idArray);
-
-        if (addButton === true) {
-          jams.push({ id: "addItem" });
-        }
-
-        setProfileJams(jams);
+        setProfileJams(await EntityManager.getJams(idArray));
       }
     })();
-  }, [idArray, profileJams, addButton]);
+  }, [idArray, profileJams, isPublic]);
 
-  if (!profileJams) return <SpinnerView size="small" />; 
+  if (!profileJams) return <SpinnerView size="small" />;
 
   return (
     <View style={styles.container}>
-      <BoxView direction="row" align="center" justify="space-between" style={styles.title}>
-        { title && <TextView>{title}</TextView> }
-
-        { profileJams?.length > 0 && allButton && (
-          <TouchableOpacity onPress={() => SectionManager.push(router, 'jams', { idArray: idArray }, i18n.t('Your Jams'))}>
-            <TextView underline={true}>{i18n.t("View all")}</TextView>
-          </TouchableOpacity> 
-        )}
-      </BoxView>
-    
       <ListView
         data={profileJams}
         numColumns={numColumns}
@@ -113,18 +86,7 @@ const ProfileJamsField = ({ resource, field, title, idArray, addButton, allButto
         columnWrapperStyle={{ gap: Layout.space.base }}
         scrollEnabled={false}
         emptyMessage={<TextView>{i18n.t('No data available.')}</TextView>}
-        renderItem={(row: any) => (
-          <JamListItem 
-            row={row} 
-            isAddable={isAddable}
-            isDeletable={isDeletable}
-            multiSelect={multiSelect}
-            onAddButtonPress={onAddButtonPress}
-            onListItemPress={(row: any) => onItemPress(row)}
-            onDeleteItemPress={deleteItem}
-            isSelected={(selectedIds.findIndex((id: any) => id == row.item.id)) !== -1}
-          />
-        )}
+        renderItem={(row: any) => renderItem(row)}
       />
     </View>
   );
