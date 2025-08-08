68751bb4e7178398247fb029bede683d2242149f
david-fiaty <dfiaty@gmail.com>
2025-08-08 19:56:29 +0000

    Update ProfileView.tsx

--- components/view/ProfileView.tsx
+++ components/view/ProfileView.tsx
@@ -321,7 +321,7 @@ const ProfileView = ({ profileId, profileData, isOwner, isPublic }: Props) => {
 
           <TouchableOpacity onPress={() => {
             SectionManager.push(router, 'profile-jams', {
-              jamId: JSON.stringify(profileItem?.saved_jams  || []),
+              jamId: JSON.stringify(profileItem?.saved_jams || []),
               title: groupTitle,
               disableInfiniteScroll: true,
             });
