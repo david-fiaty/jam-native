f99e2f77cf720847e8890b335e0531569d6380ef
david-fiaty <dfiaty@gmail.com>
2025-08-09 17:12:01 +0000

    Update ProjectJamsField.tsx

--- components/field/ProjectJamsField.tsx
+++ components/field/ProjectJamsField.tsx
@@ -1,22 +1,75 @@
 import { useState, useEffect } from 'react';
+import { StyleSheet, View, TouchableOpacity } from "react-native";
 import { useSelector } from "react-redux";
+import { useRouter } from "expo-router";
+import { Layout } from '@/constants/Layout';
 import ProjectJamsList from '../list/ProjectJamsList';
 import ModalManager from '@/manager/ModalManager';
 import UserManager from '@/manager/UserManager';
+import ListView from '../view/ListView';
+import TextView from '../view/TextView';
+import SectionManager from '@/manager/SectionManager';
+import MediaManager from '@/manager/MediaManager';
+import AddItemButton from '../button/AddItemButton';
+import i18n from '@/translation/i18n';
+
+const numColumns = 3;
 
 type Props = {
   resource: string;
   field: string;
   value?: any;
   placeholder?: any;
+  emptyMessage?: any;
   onPress?: () => void;
 };
 
-const ProjectJamsField = ({ resource, field, value, placeholder, onPress }: Props) => {
+const ProjectJamsField = ({ resource, field, value, placeholder, emptyMessage, onPress }: Props) => {
+  const router = useRouter();
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [currentValue, setCurrentValue] = useState<any>([]);
-  const [profileData, setProfileData] = useState<any>(null);
   const formData: any = useSelector((state: any) => state.form[resource]);
+  const imageSize = MediaManager.getThumbnailSize();
+
+  const onItemPress = (row: any) => {
+    SectionManager.push(router, 'profile-jams', {
+      jamId: JSON.stringify([row?.item?.id]),
+      title: row?.item?.title,
+      disableInfiniteScroll: true,
+    });
+  };
+
+  const renderAddButton = () => {
+    return (
+      <AddItemButton
+        label={i18n.t('Add')}
+        width={imageSize.width}
+        height={imageSize.height}
+        onPress={() => SectionManager.push(router, 'add-jam')}
+      />
+    );
+  };
+
+  const renderItem = (row: any) => {
+    let output: any = null;
+    let imageUrl: any = row?.item?.medias?.[0]?.url;
+
+    if (row?.item?.id == "addItem") {
+      output = renderAddButton();
+    }
+    else {
+      output = MediaManager.renderImage(imageUrl, {
+        numColumns: numColumns,
+        imageSize: imageSize,
+      });
+    }
+
+    return (
+      <TouchableOpacity onPress={() => onItemPress(row)}>
+        {output}
+      </TouchableOpacity>
+    );
+  };
 
   const getCurrentValue = () => {
     return formData?.[field] || [];
@@ -25,7 +78,6 @@ const ProjectJamsField = ({ resource, field, value, placeholder, onPress }: Prop
   useEffect(() => {
     (async () => {
       if (!isLoaded) {
-        setProfileData(await UserManager.getProfileData());
         setIsLoaded(true);
       }
 
@@ -34,20 +86,33 @@ const ProjectJamsField = ({ resource, field, value, placeholder, onPress }: Prop
   }, [isLoaded, formData, field]);
 
   return (
-    <ProjectJamsList
-      resource={resource}
-      field={field}
-      idArray={currentValue}
-      addButton={true}
-      isDeletable={true}
-      onAddButtonPress={() => ModalManager.toggleModal("SelectJamsForm", {
-        field: 'jams_ids',
-        idArray: JSON.stringify(profileData?.profile_jams || []),
-        multiSelect: true,
-        resource: resource,
-      })}
-    />
+    <View style={styles.container}>
+      {currentValue?.length > 0 && (
+        <ListView
+          data={currentValue}
+          numColumns={numColumns}
+          contentContainerStyle={{ gap: Layout.space.base }}
+          columnWrapperStyle={{ gap: Layout.space.base }}
+          scrollEnabled={false}
+          emptyMessage={<TextView>{emptyMessage}</TextView>}
+          renderItem={(row: any) => renderItem(row)}
+        />
+      )}
+
+      {!currentValue?.length && (renderAddButton())}
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+  },
+  title: {
+    fontWeight: "bold",
+    marginBottom: Layout.space.base,
+    flex: 1,
+  },
+});
 
 export default ProjectJamsField;
