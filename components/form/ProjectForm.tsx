9f7fe4baebb486d2ce0ce9352b8d69d100804d4f
david-fiaty <dfiaty@gmail.com>
2025-08-10 19:03:30 +0000

    Update ProjectForm.tsx

--- components/form/ProjectForm.tsx
+++ components/form/ProjectForm.tsx
@@ -1,173 +1,88 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, View } from "react-native";
-import { useDispatch, useSelector } from "react-redux";
-import { setFormData } from "@/redux/slices/FormSlice";
+import { useState, useEffect } from "react";
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import { useRouter } from 'expo-router';
 import { Layout } from "@/constants/Layout";
-import i18n from "@/translation/i18n";
-import BoxView from "../view/BoxView";
-import SpinnerView from "../view/SpinnerView";
-import ScreenManager from "@/manager/ScreenManager";
-import ButtonView from "../view/ButtonView";
-import InputTextField from "../field/InputTextField";
-import InputTextareaField from "../field/InputTextareaField";
 import TextView from "../view/TextView";
 import EntityManager from "@/manager/EntityManager";
-import SectorsField from "../field/SectorsField";
-import PrivacyStatusField from "../field/PrivacyStatusField";
-import DatePickerField from "../field/DatePickerField";
-import CountriesField from "../field/CountriesField";
-import DataManager from "@/manager/DataManager";
-import ModalManager from "@/manager/ModalManager";
-import UserManager from "@/manager/UserManager";
-import FormManager from "@/manager/FormManager";
+import BoxView from "../view/BoxView";
+import i18n from "@/translation/i18n";
+import SectorsTagsView from "../view/SectorsTagsView";
+import ProjectViewField from "../field/ProjectViewField";
 import ProjectJamsField from "../field/ProjectJamsField";
+import UserManager from "@/manager/UserManager";
+import SectionManager from "@/manager/SectionManager";
 
-const resource: string = 'project';
+type Props = {
+  projectId?: any;
+};
 
-const ProjectForm = () => {
-  const dispatch = useDispatch();
+const ProjectView = ({ projectId }: Props) => {
+  const router = useRouter();
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
-  const [isProcessing, setIsProcessing] = useState<boolean>(false);
-  const [profileId, setProfileId] = useState<number>(0);
-  const formData: any = useSelector((state: any) => state.form[resource]);
+  const [projectItem, setProjectItem] = useState<any>(null);
 
-  const updateField = (key: any, value: any) => {
-    dispatch(setFormData<any>({
-      resource: resource,
-      key: key,
-      value: value,
-    }));
-  };
+  const renderProjectJams = () => {
+    return (
+      <>
+        <BoxView direction="row" align="center" justify="space-between" style={styles.groupTitleContainer}>
+          <TextView style={styles.groupTitle}>
+            {i18n.t('Jams')} ({projectItem?.jams?.length || 0})
+          </TextView>
 
-  const submitForm = async () => {
-    setIsProcessing(true);
-    let result: any = await EntityManager.addProject(formData);
-    let message: any = {
-      title: i18n.t('Create project'),
-      content: i18n.t('The project was successfully created.'),
-    };
+          <TouchableOpacity onPress={() => {
+            SectionManager.push(router, 'project-jams', {
+              jamId: JSON.stringify(projectItem?.jams || []),
+              title: i18n.t('Project Jams'),
+              disableInfiniteScroll: true,
+            });
+          }}>
+            <TextView underline={true}>{i18n.t("View all")}</TextView>
+          </TouchableOpacity>
+        </BoxView>
 
-    if (result.success === false) {
-      message.content = i18n.t('Invalid data submission');
-    }
-
-    ScreenManager.showMessage(message);
-
-    setIsProcessing(false);
+        <ProjectJamsField
+          idArray={projectItem?.jams || []}
+          emptyMessage={i18n.t('No data available.')}
+          isPublic={isPublic}
+          addable={!isPublic}
+          deletable={!isPublic}
+        />
+      </>
+    );
   };
 
   useEffect(() => {
     (async () => {
       if (!isLoaded) {
-        //FormManager.resetForm(resource); // Todo - Fix form reset on web
-        setProfileId(await UserManager.getProfileId());
-
-        dispatch(setFormData<any>({
-          resource: resource,
-          key: null,
-          value: {
-            ...formData,
-            ...{ profile_id: profileId },
-          },
-        }));
-
+        setProjectItem((await EntityManager.getProjects([projectId]))?.[0]);
         setIsLoaded(true);
       }
     })();
-  }, [isLoaded, resource, profileId, formData]);
+  }, [isLoaded, projectId]);
 
-  if (!isLoaded) return <SpinnerView />;
-
-  return (
-    <TextView>PROJECT FORM</TextView>
-  )
+  console.log(projectId)
 
   return (
     <BoxView
+      direction="column"
       align="flex-start"
       justify="flex-start"
       scroll={true}
-      style={[Layout.formContainer, styles.container]}
+      style={styles.container}
     >
-      <BoxView direction="column" style={[Layout.formContainer, styles.formContainer]}>
-        <TextView>{i18n.t("Name")}*</TextView>
-        <InputTextField
-          value={formData?.name}
-          onChangeText={(value: string) => FormManager.updateField(resource, 'name', value, ['string'])}
-        />
-        {FormManager.renderError('name')}
+      <ProjectViewField label={i18n.t('Name')}>
+        <TextView>{projectItem?.name || i18n.t('Unavailable')}</TextView>
+      </ProjectViewField>
 
-        <TextView>{i18n.t("Description")}*</TextView>
-        <InputTextareaField
-          value={formData?.description}
-          onChangeText={(value: string) => FormManager.updateField(resource, 'description', value, ['string'])}
-        />
-        {FormManager.renderError('description')}
+      <ProjectViewField label={i18n.t('Description')}>
+        <TextView>{projectItem?.description || i18n.t('Unavailable')}</TextView>
+      </ProjectViewField>
 
-        <TextView>{i18n.t("Privacy status")}*</TextView>
-        <PrivacyStatusField
-          value={formData?.privacy_status}
-          onChangeValue={(option: any) => FormManager.updateField(resource, 'privacy_status', option.value, ['string'])}
-        />
-        {FormManager.renderError('privacy_status')}
+      {renderProjectJams()}
 
-        <TextView>{i18n.t('Start date')}</TextView>
-        <DatePickerField
-          value={formData?.period?.start_datetime}
-          onChangeValue={(value: any) =>
-            updateField('period', {
-              ...(formData?.period || {}),
-              ...{ start_datetime: DataManager.formatDate(value) },
-            })
-          }
-        />
-
-        <TextView>{i18n.t('End date')}</TextView>
-        <DatePickerField
-          value={formData?.period?.end_datetime}
-          onChangeValue={(value: any) =>
-            updateField('period', {
-              ...(formData?.period || {}),
-              ...{ end_datetime: DataManager.formatDate(value) },
-            })
-          }
-        />
-
-        <TextView>{i18n.t('Select countries')}</TextView>
-        <CountriesField
-          multiple={true}
-          resource={resource}
-          field="scope_countries_codes"
-          placeholder={i18n.t('Select countries')}
-          value={formData?.scope_countries_codes}
-          onPress={() => ModalManager.toggleModal('CountriesList', {
-            resource: resource,
-            field: 'scope_countries_codes',
-            multiple: true,
-          })}
-        />
-
-        <SectorsField
-          resource={resource}
-          field="sectors_ids"
-          value={formData?.sectors_ids}
-        />
-
-        <TextView>{i18n.t('Project Jams')}</TextView>
-        <ProjectJamsField
-          idArray={formData?.jams_ids || []}
-          emptyMessage={i18n.t('No data available.')}
-          isPublic={false}
-          addable={true}
-        />
-
-        <View style={styles.subtmitButtoncontainer}>
-          <ButtonView
-            label={i18n.t("Submit")}
-            isProcessing={isProcessing}
-            onPress={submitForm}
-          />
-        </View>
+      <TextView style={styles.groupTitle}>{i18n.t('Sectors')}</TextView>
+      <BoxView direction="row" align="center" justify="flex-start" style={styles.projectSectors}>
+        <SectorsTagsView idArray={projectItem?.sectors} />
       </BoxView>
     </BoxView>
   );
@@ -175,22 +90,32 @@ const ProjectForm = () => {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     width: '100%',
-    height: '100%',
+    paddingBottom: Layout.space.base * 3,
   },
-  formContainer: {
-    maxWidth: '100%',
-    flexShrink: 1,
-    paddingTop: Layout.space.base,
-  },
-  addButtonContainer: {
+  groupTitleContainer: {
     width: '100%',
+    marginTop: Layout.space.base * 1.5,
   },
-  subtmitButtoncontainer: {
+  groupTitle: {
+    fontWeight: 'bold',
+    fontSize: 13,
+    marginTop: Layout.space.base / 1.5,
+  },
+  projectTitle: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    textTransform: 'capitalize',
+  },
+  projectDescription: {
     width: '100%',
-    marginTop: Layout.space.base,
-    marginBottom: Layout.space.base * 2,
+    marginTop: Layout.space.base / 1.5,
+  },
+  projectSectors: {
+    width: '100%',
+    flexWrap: 'wrap',
   },
 });
 
-export default ProjectForm;
+export default ProjectView;
