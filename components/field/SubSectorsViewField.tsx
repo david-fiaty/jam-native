88e7e70d02bb678565e3f1ef1746f05cc7cb2197
david-fiaty <dfiaty@gmail.com>
2025-08-11 16:05:17 +0000

    Update SubSectorsViewField.tsx

--- components/field/SubSectorsViewField.tsx
+++ components/field/SubSectorsViewField.tsx
@@ -1,10 +1,19 @@
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import TextView from "../view/TextView";
 
 type Props = {
-  
+  idArray: any;
 };
 
-const SubSectorsViewField = ({ }: Props) => {
+const SubSectorsViewField = ({ idArray }: Props) => {
+  const [sectorsData, setSectorsData] = useState<any[]>([]);
+  const appState = useSelector((state: any) => state.app);
+
+  useEffect(() => {
+    setSectorsData(appState.sectorsData);
+  }, [appState]);
+
   return (
     <TextView>SUB SECTORS VIEW FIELD</TextView>
   );
