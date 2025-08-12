97fe9478896dcef64616455d1bb88c39533ee7e8
david-fiaty <dfiaty@gmail.com>
2025-08-12 17:44:58 +0000

    Update ProjectForm.tsx

--- components/form/ProjectForm.tsx
+++ components/form/ProjectForm.tsx
@@ -35,7 +35,6 @@ const ProjectForm = ({ projectId }: Props) => {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [isProcessing, setIsProcessing] = useState<boolean>(false);
   const [profileId, setProfileId] = useState<number>(0);
-  const [projectItem, setProjectItem] = useState<any>(null);
   const formData: any = useSelector((state: any) => state.form[resource]);
 
   const updateField = (key: any, value: any) => {
@@ -96,9 +95,8 @@ const ProjectForm = ({ projectId }: Props) => {
   useEffect(() => {
     (async () => {
       if (!isLoaded) {
-        //setProjectItem((await EntityManager.getProjects([projectId]))?.[0]);
-        let projectData: any = (await EntityManager.getProjects([projectId]))?.[0] || {};
         //FormManager.resetForm(resource); // Todo - Fix form reset on web
+        let projectData: any = (await EntityManager.getProjects([projectId]))?.[0] || {};
         setProfileId(await UserManager.getProfileId());
 
         dispatch(setFormData<any>({
