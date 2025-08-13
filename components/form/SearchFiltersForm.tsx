9e5be32e0e6652b468d31ddb6ca01f8a90266cd8
david-fiaty <dfiaty@gmail.com>
2025-08-13 19:52:38 +0000

    Update SearchFiltersForm.tsx

--- components/form/SearchFiltersForm.tsx
+++ components/form/SearchFiltersForm.tsx
@@ -170,7 +170,7 @@ return (
     <BoxView direction="column" style={[Layout.formContainer, styles.formContainer]}>
       {renderCountriesFilters()}
       {renderSectorsFilter()}
-      {currentFilters?.sectors?.length > 0 && renderSubSectorsFilter()}
+      {!!currentFilters?.sectors?.length && renderSubSectorsFilter()}
     </BoxView>
   </BoxView>
 );
