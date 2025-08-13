60250e92d4046b50338c702eb47b0fe037d2ffc8
david-fiaty <dfiaty@gmail.com>
2025-08-13 16:22:22 +0000

    Update SearchFiltersForm.tsx

--- components/form/SearchFiltersForm.tsx
+++ components/form/SearchFiltersForm.tsx
@@ -80,7 +80,7 @@ const styles = StyleSheet.create({
   },
   filterTitle: {
     color: Layout.colors.black,
-    fontSize: 15,
+    fontSize: 14,
   },
   filterTagDisabled: {
     backgroundColor: Layout.colors.gray,
