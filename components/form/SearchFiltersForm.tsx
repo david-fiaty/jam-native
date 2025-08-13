d3b57f617294daf20dfe9150d613a9d91af1e773
david-fiaty <dfiaty@gmail.com>
2025-08-13 21:03:04 +0000

    Update SearchFiltersForm.tsx

--- components/form/SearchFiltersForm.tsx
+++ components/form/SearchFiltersForm.tsx
@@ -23,6 +23,7 @@ const SearchFiltersForm = ({ }: Props) => {
     return {
       countries: appState.countriesData,
       sectors: appState.sectorsData,
+      //subSectors: [...appState.sectorsData].map((sector: any) => sector.sub_sectors),
     };
   };
 
@@ -30,7 +31,7 @@ const SearchFiltersForm = ({ }: Props) => {
     let searchFilters: any = { ...currentFilters };
 
     if (!searchFilters?.[key]?.length || searchFilters[key].length < filtersConfig[key].length) {
-      searchFilters[key] = [...filtersConfig[key]].map((o: any) => o.id);
+      searchFilters[key] = [...(filtersConfig?.[key] || [])].map((o: any) => o.id);
     }
     else {
       searchFilters[key] = [];
