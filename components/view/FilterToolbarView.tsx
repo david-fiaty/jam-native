f600c9978d0ca37ee249b11e8bd18c8e97d896a2
david-fiaty <dfiaty@gmail.com>
2025-08-16 18:57:42 +0000

    Create FilterToolbarView.tsx

---
+++ components/view/FilterToolbarView.tsx
@@ -0,0 +1,39 @@
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import { useRouter } from 'expo-router';
+import { Layout } from '@/constants/Layout';
+import IconView from './IconView';
+import BoxView from './BoxView';
+import TextView from './TextView';
+import SectionManager from '@/manager/SectionManager';
+import i18n from '@/translation/i18n';
+
+const FilterToolbarView = () => {
+  const router = useRouter();
+
+  return (
+    <TouchableOpacity onPress={() => SectionManager.push(router, 'search-filters')}>
+      <BoxView
+        direction="row"
+        align="center"
+        justify="space-between"
+        style={styles.searchFilters}
+      >
+        <TextView>{i18n.t('Filter results')}</TextView>
+        <IconView
+          name="filter"
+          theme="transparent"
+          padding={0}
+          size={16}
+        />
+      </BoxView>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  searchFilters: {
+    width: '100%',
+  },
+});
+
+export default FilterToolbarView;
