0dd00cfa9ca37b3386b4c83b27779f6408f01638
david-fiaty <dfiaty@gmail.com>
2025-08-20 15:46:33 +0000

    Update SearchView.tsx

--- components/view/SearchView.tsx
+++ components/view/SearchView.tsx
@@ -11,61 +11,14 @@ import BoxView from "./BoxView";
 import i18n from "@/translation/i18n";
 import SpinnerView from "./SpinnerView";
 import FilterToolbarView from "./FilterToolbarView";
+import SearchManager from "@/manager/SearchManager";
 
 const SearchView = () => {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [currentResults, setCurrentResults] = useState<any>({});
   const searchState: any = useSelector((state: any) => state.search);
-
-  const searchTabs: any[] = [
-    {
-      id: 'jam',
-      label: i18n.t('Jams'),
-      numColumns: 2,
-      default: true,
-    },
-    {
-      id: 'looking',
-      label: i18n.t('Lookings'),
-      numColumns: 2,
-    },
-    {
-      id: 'call',
-      label: i18n.t('Calls'),
-      numColumns: 2,
-    },
-    {
-      id: 'event',
-      label: i18n.t('Events'),
-      numColumns: 2,
-    },
-    {
-      id: 'jammer',
-      label: i18n.t('Jammers'),
-      numColumns: 1,
-    },
-    {
-      id: 'personal',
-      label: i18n.t('Artists'),
-      numColumns: 1,
-    },
-    {
-      id: 'organization',
-      label: i18n.t('Organization'),
-      numColumns: 1,
-    },
-    {
-      id: 'venue',
-      label: i18n.t('Venues'),
-      numColumns: 2,
-    },
-    {
-      id: 'project',
-      label: i18n.t('Projects'),
-      numColumns: 2,
-    },
-  ];
+  const searchTabs: any[] = SearchManager.getSearchTabs();
 
   const getCurrentResults = () => {
     let data: any = JSON.parse(searchState.currentResults) || {};
