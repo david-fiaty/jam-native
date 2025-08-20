6c5fd186067e5d3599016ebcf76c9c84c2696ce0
david-fiaty <dfiaty@gmail.com>
2025-08-20 15:59:02 +0000

    Update SearchManager.tsx

--- manager/SearchManager.tsx
+++ manager/SearchManager.tsx
@@ -98,6 +98,20 @@ class SearchManager {
     return searchResults;
   }
 
+  getTabResults(key: string, currentTab: string, currentResults: any) {
+    let tab: string = currentTab;
+    let data: any = {...currentResults};
+    
+    if (key == 'jam' && tab && tab != 'jam') { 
+      data[key] = data[key].filter((o: any) => o.type == tab);
+    }
+    else if (key == 'profile' && tab && tab != 'jammer') {
+      data[key] = data[key].filter((o: any) => o.profile_type == tab);
+    }
+
+    return data[key];
+  }
+
   getSearchTabs() {
     return [
       {
