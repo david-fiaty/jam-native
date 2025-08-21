868bfb84b07fbef7f2bf4f155ead4f4e72ad78e0
david-fiaty <dfiaty@gmail.com>
2025-08-21 18:53:02 +0000

    Update NotificationsMenu.tsx

--- components/menu/NotificationsMenu.tsx
+++ components/menu/NotificationsMenu.tsx
@@ -22,9 +22,9 @@ const NotificationsMenu = () => {
   const onItemPress = async (row: any) => {
     await setStorageId(row.item.id);
 
-    SectionManager.push(router, 'notification-item', { 
-      notificationId: JSON.stringify([row.item.id]), 
-      title: row.item?.content?.content_data?.title 
+    SectionManager.push(router, 'notification-item', {
+      notificationId: JSON.stringify([row.item.id]),
+      title: row.item?.content?.content_data?.title
     });
   };
 
@@ -90,7 +90,7 @@ const NotificationsMenu = () => {
     return () => clearInterval(intervalId);
   }, []);
 
-  if (!isLoaded) return <SpinnerView />; 
+  if (!isLoaded) return <SpinnerView />;
 
   return (
     <BoxView
@@ -98,11 +98,16 @@ const NotificationsMenu = () => {
       justify="flex-start"
       style={Layout.menuContainer}
     >
-      <ListView
-        data={notifications}
-        renderItem={(row: any) => renderItem(row)}
-        emptyMessage={<TextView>{i18n.t('No notifications available.')}</TextView>}
-      />
+      {!!notifications?.length && (
+        <ListView
+          data={notifications}
+          renderItem={(row: any) => renderItem(row)}
+        />
+      )}
+
+      {!notifications?.length && (
+        <TextView>{i18n.t('No notifications available.')}</TextView>
+      )}
     </BoxView>
   );
 };
