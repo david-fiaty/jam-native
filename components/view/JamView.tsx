5202b3b93011999e7589041b664b58af66136d35
david-fiaty <dfiaty@gmail.com>
2025-08-25 17:00:58 +0000

    Update JamView.tsx

--- components/view/JamView.tsx
+++ components/view/JamView.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { StyleSheet, View } from "react-native";
-import { useRouter } from 'expo-router';
 import { useSelector } from 'react-redux';
 import { Layout } from "@/constants/Layout";
 import JamViewHeader from "./jam-view/JamViewHeader";
@@ -16,7 +15,6 @@ type Props = {
 };
 
 const JamView = ({ jamId, isPublic, onListItemAction }: Props) => {
-  const router = useRouter();
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [jamData, setJamData] = useState<any>(null);
   const appState = useSelector((state: any) => state.app);
