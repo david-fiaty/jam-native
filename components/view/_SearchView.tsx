fad6adae66a5380d8aabe8dc5576ba211a2de8ad
david-fiaty <dfiaty@gmail.com>
2025-09-03 17:02:59 +0000

    Create _SearchView.tsx

---
+++ components/view/_SearchView.tsx
@@ -0,0 +1,83 @@
+import { useState, useEffect } from "react";
+import { StyleSheet } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { setCurrentTab } from "@/redux/slices/SearchSlice";
+import { Layout } from "@/constants/Layout";
+import SearchJamsList from "../list/SearchJamsList";
+import SearchProfilesList from "../list/SearchProfilesList";
+import SearchProjectsList from "../list/SearchProjectsList";
+import TabsView from "./TabsView";
+import BoxView from "./BoxView";
+import SpinnerView from "./SpinnerView";
+import SearchFiltersView from "./SearchFiltersView";
+import SearchManager from "@/manager/SearchManager";
+
+const SearchView = () => {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const searchState: any = useSelector((state: any) => state.search);
+  const searchTabs: any[] = SearchManager.getSearchTabs();
+
+  const getTabResults = (key: string) => {
+    let data: any = JSON.parse(searchState.currentResults) || {};
+    let results: any[] = SearchManager.getTabResults(key, searchState.currentTab, data);
+
+    return results || [];
+  };
+
+  useEffect(() => {
+    if (!isLoaded) {
+      if (!searchState.currentTab) {
+        dispatch(setCurrentTab((searchTabs.find((o: any) => o?.default === true))?.id));
+      }
+
+      setIsLoaded(true);
+    }
+  }, [searchState, searchTabs, isLoaded]);
+
+  if (!isLoaded) return <SpinnerView />;
+
+  return (
+    <BoxView
+      direction="column"
+      align="center"
+      justify="flex-start"
+      style={[styles.container, Layout.screenContent]}
+    >
+      <TabsView
+        tabs={searchTabs}
+        currentTab={searchState.currentTab}
+        onItemPress={(tabId: string) => dispatch(setCurrentTab(tabId))}
+      />
+
+      <SearchFiltersView />
+
+      {SearchManager.isJamTab(searchState.currentTab) && (
+        <SearchJamsList
+          data={getTabResults('jam')}
+        />
+      )}
+
+      {SearchManager.isProfileTab(searchState.currentTab) &&
+        <SearchProfilesList
+          data={getTabResults('profile')}
+        />
+      }
+
+      {SearchManager.isProjectTab(searchState.currentTab) &&
+        <SearchProjectsList
+          data={getTabResults('project')}
+        />
+      }
+    </BoxView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+  },
+});
+
+export default SearchView;
