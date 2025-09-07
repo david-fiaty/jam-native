254fb6ebd16c081b062a160e00058cf895e17175
david-fiaty <dfiaty@gmail.com>
2025-09-07 17:17:59 +0000

    Update SearchProfilesList.tsx

--- components/list/SearchProfilesList.tsx
+++ components/list/SearchProfilesList.tsx
@@ -1,3 +1,4 @@
+import React, { memo } from "react";
 import { StyleSheet } from "react-native";
 import { useRouter } from "expo-router";
 import { Layout } from "@/constants/Layout";
@@ -80,4 +81,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default SearchProfilesList;
+export default memo(SearchProfilesList);
