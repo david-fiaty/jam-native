9941c094f0c7c4ff7c7185b256e2653b4401381f
david-fiaty <dfiaty@gmail.com>
2025-09-08 17:37:24 +0000

    Create MapManager.tsx

---
+++ manager/MapManager.tsx
@@ -0,0 +1,41 @@
+import { Marker } from "react-native-maps";
+import MarkerView from "@/components/view/MarkerView";
+import i18n from "@/translation/i18n";
+
+class MapManager {
+  renderMarker = (item: any) => {
+    if (item?.geolocation_longitude && item?.geolocation_latitude) {
+      return (
+        <Marker
+          key={item.id}
+          coordinate={this.getMarkerCoordinate(item)}
+        >
+          <MarkerView
+            title={this.getMarkerTitle(item)}
+            description={this.getMarkerDescription(item)}
+          />
+        </Marker>
+      );
+    }
+
+    return null;
+  }
+
+  
+  getMarkerCoordinate(item: any) {
+    return {
+      latitude: parseFloat(item?.geolocation_latitude),
+      longitude: parseFloat(item?.geolocation_longitude),
+    };
+  }
+
+  getMarkerTitle(item: any) {
+    return item?.title || i18n.t('No title available');
+  };
+
+  getMarkerDescription = (item: any) => {
+    return item?.caption || '';
+  };
+}
+
+export default (new MapManager());
