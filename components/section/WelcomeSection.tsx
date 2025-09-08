5338fe76eaee6323b3aec716cc80c3993ebd4e53
david-fiaty <dfiaty@gmail.com>
2025-09-08 14:54:06 +0000

    Update WelcomeSection.tsx

--- components/section/WelcomeSection.tsx
+++ components/section/WelcomeSection.tsx
@@ -35,7 +35,7 @@ const WelcomeSection = () => {
       style={styles.container}
     >
 
-      <MarkerView />
+      <MarkerView iconName="user" />
       <LogoView size={110} />
 
       <TextSlideshow data={slideshowData} />
