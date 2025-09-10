e8e1b3a06742f9a699ac46b0c9dfcca9763ef891
david-fiaty <dfiaty@gmail.com>
2025-09-10 17:07:50 +0000

    Update MapLegendView.tsx

--- components/view/MapLegendView.tsx
+++ components/view/MapLegendView.tsx
@@ -76,7 +76,7 @@ const styles = StyleSheet.create({
   legendWrapper: {
     position: 'absolute',
     bottom: 0,
-    height: 150,
+    height: 180,
     justifyContent: 'center',
     alignItems: 'center',
     overflow: 'hidden',
