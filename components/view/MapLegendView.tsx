70e9dfbce80be3a8303ed91071118d104c4256c8
david-fiaty <dfiaty@gmail.com>
2025-09-10 16:41:46 +0000

    Update MapLegendView.tsx

--- components/view/MapLegendView.tsx
+++ components/view/MapLegendView.tsx
@@ -70,6 +70,8 @@ const styles = StyleSheet.create({
 
   },
   legendContainer: {
+    position: 'absolute',
+    bottom: Layout.space.base * 5,
     height: 100,
     backgroundColor: 'red',
     justifyContent: 'center',
