b82fc8dab0bebe7133fc93a7995950610a5a6097
david-fiaty <dfiaty@gmail.com>
2025-09-10 16:24:36 +0000

    Update MapLegendView.tsx

--- components/view/MapLegendView.tsx
+++ components/view/MapLegendView.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 const MapLegendView = ({ }: Props) => {
   const [isVisible, setIsVisible] = useState(false);
-  const widthAnim = useRef(new Animated.Value(0)).current; 
+  const widthAnim = useRef(new Animated.Value(0)).current;
 
   const toggleLegend = () => {
     Animated.timing(widthAnim, {
@@ -23,20 +23,31 @@ const MapLegendView = ({ }: Props) => {
   };
 
   return (
-    <TouchableOpacity
-      style={styles.legend}
-      onPress={toggleLegend}
-    >
-      <IconView
-        name={isVisible ? 'left' : 'right'}
-        theme="transparent"
-      />
+    <>
+      {!isVisible && (
+        <TouchableOpacity
+          style={styles.legend}
+          onPress={toggleLegend}
+        >
+          <IconView
+            name={isVisible ? 'left' : 'right'}
+            theme="transparent"
+          />
+        </TouchableOpacity>
+      )}
 
-      <Animated.View style={[styles.box, { width: widthAnim }]}>
-        <Text>Expanding Box</Text>
-      </Animated.View>
+      {isVisible && (
+        <TouchableOpacity
+          style={styles.legend}
+          onPress={toggleLegend}
+        >
+          <Animated.View style={[styles.box, { width: widthAnim }]}>
+            <Text>Expanding Box</Text>
+          </Animated.View>
 
-    </TouchableOpacity>
+        </TouchableOpacity>
+      )}
+    </>
   );
 };
 
@@ -44,6 +55,13 @@ const styles = StyleSheet.create({
   container: {
     gap: Layout.space.base,
   },
+  openButton: {
+
+  },
+  closeButton: {
+
+  },
+
   legend: {
     position: 'absolute',
     bottom: Layout.space.base * 5,
