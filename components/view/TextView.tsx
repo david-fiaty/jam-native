e42c56d35133907f456fca5f764b80fed334850b
david-fiaty <dfiaty@gmail.com>
2025-09-11 13:32:24 +0000

    Update TextView.tsx

--- components/view/TextView.tsx
+++ components/view/TextView.tsx
@@ -4,19 +4,21 @@ import { Layout } from '@/constants/Layout';
 type Props = {
   underline?: boolean;
   color?: string;
+  backgroundColor?: string;
   size?: any;
   bold?: boolean;
   style?: any;
   children?: any;
 };
 
-const TextView = ({underline, color, size, bold, style, children}: Props) => {
+const TextView = ({underline, color, backgroundColor, size, bold, style, children}: Props) => {
   const containerStyle: any = {
     ...(underline ? styles.underline : {}),
     ...{
       color: color || Layout.colors.primary,
       fontSize: size || Layout.fontSize.base,
       fontWeight: bold ? 'bold' : 'normal',
+      backgroundColor: backgroundColor || Layout.colors.white,
     }
   };
 
