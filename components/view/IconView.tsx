c33ddc226b8c37c33ce58c90d0ea13e48f53b902
david-fiaty <dfiaty@gmail.com>
2025-09-11 17:01:18 +0000

    Update IconView.tsx

--- components/view/IconView.tsx
+++ components/view/IconView.tsx
@@ -42,7 +42,7 @@ const IconView = ({
       ...styles[theme],
       ...{
         borderRadius: Layout.radius[radius],
-        padding: padding >= 0 ? padding : defaultPadding,
+        padding: padding || defaultPadding,
       },
     };
 
@@ -72,7 +72,12 @@ const IconView = ({
 
   const textIcon = (
     <View style={styles.containerStyle}>
-      <TextView style={[getIconStyle(), { fontSize: size }]}>
+      <TextView 
+        style={getIconStyle()} 
+        size={size} 
+        paddingHorizontal={padding} 
+        paddingVertical={padding}
+      >
         {label}
       </TextView>
     </View>
