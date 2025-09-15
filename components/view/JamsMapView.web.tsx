dce109a5aa9431da2a4cb26e4c3726fe34a63fad
david-fiaty <dfiaty@gmail.com>
2025-09-15 10:49:23 +0000

    Update JamsMapView.web.tsx

--- components/view/JamsMapView.web.tsx
+++ components/view/JamsMapView.web.tsx
@@ -1,151 +1,73 @@
-import { GoogleMap, LoadScript, OverlayView, OverlayViewF } from "@react-google-maps/api";
-import { useState, useEffect, useRef } from "react";
-import { StyleSheet, View, TouchableWithoutFeedback } from "react-native";
-import { useDispatch, useSelector, shallowEqual } from "react-redux";
-import { setCurrentTab } from "@/redux/slices/SearchSlice";
-import { Layout } from "@/constants/Layout";
+// JamsMapView.web.tsx
+import React, { useState } from "react";
+import { GoogleMap, useJsApiLoader, OverlayView, InfoWindow } from "@react-google-maps/api";
 import { Config } from "@/constants/Config";
-import UserManager from "@/manager/UserManager";
-import SearchManager from "@/manager/SearchManager";
-import TabsView from "./TabsView";
-import SearchFiltersView from "./SearchFiltersView";
-import SpinnerView from "./SpinnerView";
-import MapManager from "@/manager/MapManager";
-import MapLegendView from "./MapLegendView";
 
-const JamsMapView = () => {
-  const dispatch = useDispatch();
-  const [currentLocation, setCurrentLocation] = useState<any>(null);
-  const [isLoaded, setIsLoaded] = useState<boolean>(false);
-  const [searchResults, setSearchResults] = useState<any>({});
-  const searchState: any = useSelector((state: any) => state.search, shallowEqual);
-  const prevSearchState: any = useRef(null);
-  const searchTabs: any[] = SearchManager.getSearchTabs();
-
-  const getInitialRegion = () => {
-    let latitude: any = Config.defaultLocation.latitude;
-    let longitude: any = Config.defaultLocation.longitude;
-    let latitudeDelta: any = 0.2;
-    let longitudeDelta: any = 0.2;
-
-    if (currentLocation?.latitude && currentLocation?.longitude) {
-      latitude = currentLocation.latitude;
-      longitude = currentLocation.longitude;
-    }
-
-    return {
-      lat: latitude,
-      lng: longitude,
-      //latitudeDelta: latitudeDelta,
-      //longitudeDelta: longitudeDelta,
-    };
-  };
-
-  const getTabResults = (key: string) => {
-    let results: any = {
-      ...searchResults,
-      ...{
-        [key]: SearchManager.getTabResults(key, searchState.currentTab, searchResults)
-      },
-    };
-
-    return results[key];
-  };
-
-  const getMarkerCoordinate = (item: any) => {
-    const lat = parseFloat(item?.geolocation_latitude);
-    const lng = parseFloat(item?.geolocation_longitude);
-
-    return { lat, lng };
-  };
-
-  const renderMarker = (item: any) => {
-    if (item?.geolocation_longitude && item?.geolocation_latitude) {
-      return (
-        <OverlayViewF
-          key={item.id}
-          position={getMarkerCoordinate(item)}
-          mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
-          getPixelPositionOffset={(width, height) => ({
-            x: -(width / 2),  
-            y: -height,
-          })}
-        >
-          {MapManager.renderMarker(item)}
-        </OverlayViewF>
-      );
-    }
-  };
-
-  useEffect(() => {
-    (async () => {
-      if (!isLoaded) {
-        if (!searchState.currentTab) {
-          dispatch(setCurrentTab((searchTabs.find((o: any) => o?.default === true))?.id));
-        }
-
-        setIsLoaded(true);
-      }
-
-      setCurrentLocation(await UserManager.getLocation());
-    })();
-  }, [searchState, searchTabs, isLoaded]);
-
-  useEffect(() => {
-    if (prevSearchState.current?.currentResults !== searchState.currentResults) {
-      setSearchResults(JSON.parse(searchState.currentResults) || {});
-
-      prevSearchState.current = searchState;
-    }
-  }, [searchState]);
-
-  if (!isLoaded) return <SpinnerView />;
-
-  return (
-    <LoadScript googleMapsApiKey={Config.mapApiKey}>
-      <TouchableWithoutFeedback>
-        <View style={[Layout.screenContent, styles.container]}>
-
-          <TabsView
-            tabs={searchTabs}
-            currentTab={searchState.currentTab}
-            onItemPress={(tabId: string) => dispatch(setCurrentTab(tabId))}
-          />
-
-          <SearchFiltersView />
-
-          <GoogleMap
-            mapContainerStyle={styles.map}
-            center={getInitialRegion()}
-            zoom={7}
-            options={{
-              styles: Layout.mapStyle,
-              disableDefaultUI: true,
-            }}
-          >
-            {SearchManager.isJamTab(searchState.currentTab) && getTabResults('jam').map((item: any) => renderMarker(item))}
-            {SearchManager.isProfileTab(searchState.currentTab) && getTabResults('profile').map((item: any) => renderMarker(item))}
-            {SearchManager.isProjectTab(searchState.currentTab) && getTabResults('project').map((item: any) => renderMarker(item))}
-          </GoogleMap>
-
-          <MapLegendView />
-        </View>
-      </TouchableWithoutFeedback>
-    </LoadScript>
-  );
+const containerStyle = {
+  width: "100%",
+  height: "100%",
 };
 
-const styles = StyleSheet.create({
-  container: {
-    padding: 0,
-    gap: Layout.space.base,
-    width: '100%',
-    flexGrow: 1,
-    backgroundColor: Layout.colors.white,
-  },
-  map: {
-    flex: 1,
-  },
-});
+const center = {
+  lat: 37.7749,
+  lng: -122.4194,
+};
 
-export default JamsMapView;
+const places = [
+  { id: 1, name: "Marker One", position: { lat: 37.7749, lng: -122.4194 } },
+  { id: 2, name: "Marker Two", position: { lat: 37.7849, lng: -122.4094 } },
+];
+
+export default function JamsMapView() {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: Config.mapApiKey,
+  });
+
+  const [selectedPlace, setSelectedPlace] = useState<any>(null);
+
+  if (!isLoaded) return <div>Loading...</div>;
+
+  return (
+    <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={13}>
+      {places.map((place) => (
+        <React.Fragment key={place.id}>
+          {/* Custom Marker */}
+          <OverlayView
+            position={place.position}
+            mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+          >
+            <div
+              onClick={() => setSelectedPlace(place)}
+              style={{
+                background: "white",
+                border: "2px solid #333",
+                borderRadius: "50%",
+                width: "24px",
+                height: "24px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                cursor: "pointer",
+              }}
+            >
+              📍
+            </div>
+          </OverlayView>
+
+          {/* Callout / InfoWindow */}
+          {selectedPlace?.id === place.id && (
+            <InfoWindow
+              position={place.position}
+              onCloseClick={() => setSelectedPlace(null)}
+            >
+              <div>
+                <h4>{place.name}</h4>
+                <p>Custom info here</p>
+              </div>
+            </InfoWindow>
+          )}
+        </React.Fragment>
+      ))}
+    </GoogleMap>
+  );
+}
