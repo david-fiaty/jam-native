dc50968b78fed8a7915b08fd4f884456bab8d211
david-fiaty <dfiaty@gmail.com>
2025-09-15 14:48:44 +0000

    Update ProfileCalloutView.tsx

--- components/view/marker-view/ProfileCalloutView.tsx
+++ components/view/marker-view/ProfileCalloutView.tsx
@@ -1,26 +1,41 @@
 import React, { ReactNode } from "react";
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { useRouter } from "expo-router";
 import { Layout } from '@/constants/Layout';
 import TextView from "../TextView";
 import i18n from "@/translation/i18n";
 import UserManager from "@/manager/UserManager";
 import DataManager from "@/manager/DataManager";
+import SectionManager from "@/manager/SectionManager";
 
 type Props = {
   item: any;
 };
 
 const ProfileCalloutView = ({ item }: Props) => {
+  const router = useRouter();
+
+  const onViewMorePress = (row: any) => {
+    let path: string = 'public-jam';
+    let params: any = {
+      jamId: item?.id,
+      title: i18n.t('Jam'),
+      itemData: JSON.stringify(item),
+      disableInfiniteScroll: true,
+    };
+
+    SectionManager.push(router, path, params);
+  };
 
   return (
     <View>
-      <div>{UserManager.getProfileDisplayName(item)}</div>
-      <div>{item?.profile_name}</div>
-      <div>{UserManager.getProfileTypeLabel(item?.profile_type)}</div>
-      <div>{DataManager.truncateText(item?.profile_description, 50)}</div>
-      <button>
+      <TextView>{UserManager.getProfileDisplayName(item)}</TextView>
+      <TextView>{item?.profile_name}</TextView>
+      <TextView>{UserManager.getProfileTypeLabel(item?.profile_type)}</TextView>
+      <TextView>{DataManager.truncateText(item?.profile_description, 50)}</TextView>
+      <TouchableOpacity onPress={() => onViewMorePress(item)}>
         {i18n.t('Show more')}
-      </button>
+      </TouchableOpacity>
     </View>
   );
 };
