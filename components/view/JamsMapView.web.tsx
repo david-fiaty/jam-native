8806fb878d7cdf3dc958019bcb0d0cec3b3a1649
david-fiaty <dfiaty@gmail.com>
2025-09-15 14:23:26 +0000

    Update JamsMapView.web.tsx

--- components/view/JamsMapView.web.tsx
+++ components/view/JamsMapView.web.tsx
@@ -114,7 +114,7 @@ const JamsMapView = () => {
                 <div>{UserManager.getProfileDisplayName(item)}</div>
                 <div>{item?.profile_name}</div>
                 <div>{UserManager.getProfileTypeLabel(item?.profile_type)}</div>
-                <div>{DataManager.truncateText(item?.profile_description || '', 55)}</div>
+                <div>{DataManager.truncateText(item?.profile_description, 55)}</div>
                 <button>
                   {i18n.t('Show more')}
                 </button>
