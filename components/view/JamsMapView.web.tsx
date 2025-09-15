a4b5ededb0eddbb0789c90e69cf661d83e87f40e
david-fiaty <dfiaty@gmail.com>
2025-09-15 11:15:17 +0000

    Update JamsMapView.web.tsx

--- components/view/JamsMapView.web.tsx
+++ components/view/JamsMapView.web.tsx
@@ -1,7 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { GoogleMap, useJsApiLoader, OverlayView, InfoWindow, OverlayViewF } from "@react-google-maps/api";
+import { StyleSheet, View, TouchableWithoutFeedback } from "react-native";
+import { useDispatch, useSelector, shallowEqual } from "react-redux";
+import { setCurrentTab } from "@/redux/slices/SearchSlice";
+import { Layout } from "@/constants/Layout";
 import { Config } from "@/constants/Config";
+import UserManager from "@/manager/UserManager";
+import SearchManager from "@/manager/SearchManager";
+import TabsView from "./TabsView";
+import SearchFiltersView from "./SearchFiltersView";
 import SpinnerView from "./SpinnerView";
+import MapManager from "@/manager/MapManager";
+import MapLegendView from "./MapLegendView";
 
 const containerStyle = {
   width: "100%",
@@ -19,16 +29,79 @@ const places = [
 ];
 
 const JamsMapView = () => {
+  const dispatch = useDispatch();
+  //const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [searchResults, setSearchResults] = useState<any>({});
+  const searchState: any = useSelector((state: any) => state.search, shallowEqual);
+  const prevSearchState: any = useRef(null);
+  const searchTabs: any[] = SearchManager.getSearchTabs();
+  const [selectedPlace, setSelectedPlace] = useState<any>(null);
+  const [currentLocation, setCurrentLocation] = useState<any>(null);
+
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: Config.mapApiKey,
   });
 
-  const [selectedPlace, setSelectedPlace] = useState<any>(null);
+  const getInitialRegion = () => {
+    let latitude: any = Config.defaultLocation.latitude;
+    let longitude: any = Config.defaultLocation.longitude;
+    let latitudeDelta: any = 0.2;
+    let longitudeDelta: any = 0.2;
+
+    if (currentLocation?.latitude && currentLocation?.longitude) {
+      latitude = currentLocation.latitude;
+      longitude = currentLocation.longitude;
+    }
+
+    return {
+      lat: latitude,
+      lng: longitude,
+      //latitudeDelta: latitudeDelta,
+      //longitudeDelta: longitudeDelta,
+    };
+  };
+
+  const getTabResults = (key: string) => {
+    let results: any = {
+      ...searchResults,
+      ...{
+        [key]: SearchManager.getTabResults(key, searchState.currentTab, searchResults)
+      },
+    };
+
+    return results[key];
+  };
+
+  useEffect(() => {
+    (async () => {
+      if (!isLoaded) {
+        if (!searchState.currentTab) {
+          dispatch(setCurrentTab((searchTabs.find((o: any) => o?.default === true))?.id));
+        }
+
+        //setIsLoaded(true);
+      }
+
+      setCurrentLocation(await UserManager.getLocation());
+    })();
+  }, [searchState, searchTabs, isLoaded]);
+
+  useEffect(() => {
+    if (prevSearchState.current?.currentResults !== searchState.currentResults) {
+      setSearchResults(JSON.parse(searchState.currentResults) || {});
+
+      prevSearchState.current = searchState;
+    }
+  }, [searchState]);
 
   if (!isLoaded) return <SpinnerView />;
 
   return (
-    <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={13}>
+    <GoogleMap 
+      mapContainerStyle={containerStyle} 
+      //center={center} 
+      center={getInitialRegion()}
+      zoom={7}>
       {places.map((place) => (
         <React.Fragment key={place.id}>
           <OverlayViewF
