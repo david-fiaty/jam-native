b9bdb51a7bcf59df76628c1909797d349c285cc8
david-fiaty <dfiaty@gmail.com>
2025-09-16 15:07:08 +0000

    Update JamsMapView.tsx

--- components/view/JamsMapView.tsx
+++ components/view/JamsMapView.tsx
@@ -73,11 +73,11 @@ const JamsMapView = () => {
         >
           {MapManager.renderMarker(item, zoomLevel)}
 
-          {/*<Callout>
-            <View>
+          <Callout>
+            <View style={{width: 150, height: 150}}>
               <TextView>Test tooltip callout</TextView>
             </View>
-          </Callout> */}
+          </Callout>
         </Marker>
       );
     }
