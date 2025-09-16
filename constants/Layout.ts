8196eaba0e6ddd3d2debbc768272964f930cd6aa
david-fiaty <dfiaty@gmail.com>
2025-09-16 17:03:22 +0000

    Update Layout.ts

--- constants/Layout.ts
+++ constants/Layout.ts
@@ -59,14 +59,22 @@ const logo: any = {
 
 const mapStyle: any = [
   {
-    featureType: "poi", 
-    elementType: "labels",
-    stylers: [{ visibility: "off" }],
+    "featureType": "poi", 
+    "elementType": "labels",
+    "stylers": [
+      { 
+        "visibility": "off" 
+      }
+    ],
   },
   {
-    featureType: "transit",
-    elementType: "labels",
-    stylers: [{ visibility: "off" }],
+    "featureType": "transit",
+    "elementType": "labels",
+    "stylers": [
+      { 
+        "visibility": "off" 
+      }
+    ],
   },
   {
     "elementType": "geometry",
