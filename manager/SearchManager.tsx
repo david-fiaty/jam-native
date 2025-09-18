9b9276eb1e47ab96f12a1f34eb4ec81e1bea355a
david-fiaty <dfiaty@gmail.com>
2025-09-18 15:51:21 +0000

    Update SearchManager.tsx

--- manager/SearchManager.tsx
+++ manager/SearchManager.tsx
@@ -33,13 +33,22 @@ class SearchManager {
     }
   }
 
+  async loadMoreResults(key: string, currentPage: number) {
+    let searchState: any = Store.getState().search;
+  }
+
   async sendRequest(searchValue?: string) {
-    let payload: any = {};
+    let payload: any = {
+      page: 5, 
+    };
 
     if (searchValue?.length) {
       payload = {
-        query_text: searchValue,
-        query_title: searchValue,
+        ...payload,
+        ...{
+          query_text: searchValue,
+          query_title: searchValue,
+        },
       };
     }
 
