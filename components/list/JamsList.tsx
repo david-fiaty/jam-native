021afb66644c70a382fb03b542e32ad422f78167
david-fiaty <dfiaty@gmail.com>
2025-09-18 16:53:30 +0000

    Update JamsList.tsx

--- components/list/JamsList.tsx
+++ components/list/JamsList.tsx
@@ -44,9 +44,11 @@ const JamsList = ({ idArray }: Props) => {
   };
 
   const onEndReached = async () => {
+    setIsFetching(true);
     let moreResults: any[] = await SearchManager.loadMoreResults('jam', currentPage);
     setListData((prevData) => [...(prevData || []), ...(moreResults || [])]);
     setCurrentPage((prevPage: number) => prevPage + 1);
+    setIsFetching(false);
   };
 
   useEffect(() => {
