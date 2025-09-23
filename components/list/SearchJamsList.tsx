42781b28e425385546fee1c8613786dee794d289
david-fiaty <dfiaty@gmail.com>
2025-09-23 17:32:27 +0000

    Update SearchJamsList.tsx

--- components/list/SearchJamsList.tsx
+++ components/list/SearchJamsList.tsx
@@ -13,9 +13,13 @@ import SearchManager from "@/manager/SearchManager";
 import LoadingMoreView from "../view/LoadingMoreView";
 import ModalManager from "@/manager/ModalManager";
 
+type Props = {
+  data?: any;
+};
+
 const numColumns = 2;
 
-const SearchJamsList = () => {
+const SearchJamsList = ({ data }: Props) => {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [isFetching, setIsFetching] = useState<boolean>(false);
   const [currentPage, setCurrentPage] = useState(1);
