2cb5d2b8bc7179732a2659468dc5a9fdbe64ea50
david-fiaty <dfiaty@gmail.com>
2025-09-23 13:29:39 +0000

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,7 @@ import { setCurrentLanguage } from '@/redux/slices/UserSlice';
 import { Config } from '@/constants/Config';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import i18next from 'i18next';
-import ScreenManager from '@/manager/ScreenManager';
+import ScreenManager from '@/manager/ScreenManager'; 
 import EntityManager from '@/manager/EntityManager';
 
 export default () => {
