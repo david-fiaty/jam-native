c8ef23df4a7ad26dfdba194a5d35e7ee8b0515e0
david-fiaty <dfiaty@gmail.com>
2025-09-24 14:07:57 +0000

    Update SearchProjectsList.tsx

--- components/list/SearchProjectsList.tsx
+++ components/list/SearchProjectsList.tsx
@@ -71,18 +71,6 @@ const SearchProjectsList = () => {
     if (!isLoaded) setIsLoaded(true);
   }, [isLoaded]);
 
-  /*
-  useEffect(() => {
-    if (prevSearchState.current !== searchState || !isLoaded) {
-      setListData([]);
-      setCurrentPage(1);
-      fetchListData();
-      prevSearchState.current = searchState;
-      setIsLoaded(true);
-    }
-  }, [searchState, isLoaded]);
-  */
-
   if (!isLoaded) return <SpinnerView />;
 
   return (
