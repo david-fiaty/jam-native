e8d94400ac87fed6fcca9304f4022189d17177ba
david-fiaty <dfiaty@gmail.com>
2025-09-25 17:37:34 +0000

    Update SearchFiltersView.tsx

--- components/view/SearchFiltersView.tsx
+++ components/view/SearchFiltersView.tsx
@@ -10,10 +10,7 @@ import ModalManager from "@/manager/ModalManager";
 
 const SearchFiltersView = () => {
   const searchState: any = useSelector((state: any) => state.search, shallowEqual);
-  const resultCount: any = searchState.tabResults[searchState.currentTab];
-
-
-  console.log(resultCount);
+  const resultCount: any = searchState.tabResults?.[searchState.currentTab];
   
   return (
     <TouchableOpacity 
