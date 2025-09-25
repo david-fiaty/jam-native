c9cb0a04056d58ae5355cf8ef589d94d2c3a3001
david-fiaty <dfiaty@gmail.com>
2025-09-25 12:04:08 +0000

    Code cleanup

--- manager/DataManager.tsx
+++ manager/DataManager.tsx
@@ -1,11 +1,15 @@
+import { Config } from '@/constants/Config';
 import Endpoints from '@/constants/Endpoints';
 import ApiManager from './ApiManager';
 import moment from "moment";
-import { Config } from '@/constants/Config';
 
 class DataManager {
   async get(key: keyof typeof Endpoints, options?: any, variables?: any) {
-    let data: any = await ApiManager.get(key, options, variables);
+    let data: any = await ApiManager.get(key, options, variables); 
+
+    if (Endpoints[key]?.countKey) {
+      console.log(key, data[Endpoints[key]?.countKey])
+    }
 
     if (Endpoints[key]?.dataKey !== null) {
       return data?.[Endpoints[key].dataKey];
@@ -24,15 +28,6 @@ class DataManager {
 
   async put(key: keyof typeof Endpoints, data: any, variables?: any) {
     return await ApiManager.put(key, data, variables);
-  }
-
-  async find(key: keyof typeof Endpoints, idField: string, idValues: any) {
-    idValues = Array.isArray(idValues) ? idValues : [idValues];
-    let data: any = await ApiManager.get(key);
-    let haystack: any = data?.[Endpoints[key]?.dataKey];
-    let result: any = haystack.find((item: any) => idValues.includes(item[idField]));
-
-    return result || {};
   }
 
   createUuid() { 
