df696087a874ec4f3c8efc7a6f69808612b33907
david-fiaty <dfiaty@gmail.com>
2025-09-28 16:27:07 +0000

    Update SearchJamsList.tsx

--- components/list/search/SearchJamsList.tsx
+++ components/list/search/SearchJamsList.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useState, useEffect } from "react";
+import React, { memo, useState, useEffect, useRef } from "react";
 import { StyleSheet, TouchableOpacity } from "react-native";
 import { useSelector, shallowEqual } from "react-redux";
 import { Layout } from "@/constants/Layout";
@@ -22,6 +22,7 @@ const SearchJamsList = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [currentData, setCurrentData] = useState<any[]>([]);
   const searchState: any = useSelector((state: any) => state.search, shallowEqual);
+  const prevSearchState: any = useRef(null);
   const imageSize = MediaManager.getThumbnailSize(numColumns);
 
   const onItemPress = (row: any) => {
@@ -80,6 +81,13 @@ const SearchJamsList = () => {
       }
     })();
   }, [isLoaded]);
+
+  useEffect(() => {
+    if (prevSearchState.current !== searchState) {
+      prevSearchState.current = searchState;  
+    }
+  }, [searchState]);
+
 
   if (!isLoaded) return <SpinnerView />;
 
