4d92ff93c2dc84eee5a4070f69eec16671ae381b
david-fiaty <dfiaty@gmail.com>
2025-09-29 13:09:22 +0000

    Update SearchProfilesList.tsx

--- components/list/search/SearchProfilesList.tsx
+++ components/list/search/SearchProfilesList.tsx
@@ -14,7 +14,11 @@ import LoadingMoreView from "@/components/view/LoadingMoreView";
 import ModalManager from "@/manager/ModalManager";
 import SpinnerView from "@/components/view/SpinnerView";
 
-const SearchProfilesList = () => {
+type Props = {
+  state?: any;
+};
+
+const SearchProfilesList = ({ state }: Props) => {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [isFetching, setIsFetching] = useState<boolean>(false);
   const [listData, setListData] = useState<any[]>([]);
