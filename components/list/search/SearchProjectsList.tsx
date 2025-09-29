e059096a4c926c1e3f540ad8372565e2a154361f
david-fiaty <dfiaty@gmail.com>
2025-09-29 13:09:23 +0000

    Update SearchProjectsList.tsx

--- components/list/search/SearchProjectsList.tsx
+++ components/list/search/SearchProjectsList.tsx
@@ -14,9 +14,13 @@ import SearchManager from "@/manager/SearchManager";
 import LoadingMoreView from "@/components/view/LoadingMoreView";
 import ModalManager from "@/manager/ModalManager";
 
+type Props = {
+  state?: any;
+};
+
 const numColumns = 2;
 
-const SearchProjectsList = () => {
+const SearchProjectsList = ({ state }: Props) => {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [isFetching, setIsFetching] = useState<boolean>(false);
   const [listData, setListData] = useState<any[]>([]);
