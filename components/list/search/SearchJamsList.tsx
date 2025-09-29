794fd13d7254207f854585c3d0b7a9ffcd5e3811
david-fiaty <dfiaty@gmail.com>
2025-09-29 14:59:03 +0000

    Update SearchJamsList.tsx

--- components/list/search/SearchJamsList.tsx
+++ components/list/search/SearchJamsList.tsx
@@ -54,7 +54,7 @@ const SearchJamsList = () => {
     let moreResults: any[] = await SearchManager.loadResults(searchState.currentTab, currentPage);
 
     if (moreResults?.length > 0) {
-      //setListData((prevData) => [...(prevData || []), ...moreResults]);
+      setListData((prevData) => [...(prevData || []), ...moreResults]);
       setCurrentPage((prevPage: number) => prevPage + 1);
     }
 
@@ -89,9 +89,9 @@ const SearchJamsList = () => {
         scroll={ScreenManager.isWeb() ? true : false}
         style={styles.container}
       >
-        { !!searchState.tabResults[searchState.currentTab]?.listData?.length && (
+        {!!listData?.length && (
           <ListView
-            data={searchState.tabResults[searchState.currentTab]?.listData || []}
+            data={listData}
             numColumns={numColumns}
             contentContainerStyle={styles.contentContainerStyle}
             columnWrapperStyle={styles.columnWrapperStyle}
@@ -102,12 +102,12 @@ const SearchJamsList = () => {
           />
         )}
 
-        {isLoaded && !isFetching && !searchState.tabResults[searchState.currentTab]?.listData?.length && (
+        {isLoaded && !isFetching && !listData?.length && (
           <TextView>{i18n.t('No results available')}</TextView>
         )}
       </BoxView>
 
-      {isLoaded && isFetching && !!searchState.tabResults[searchState.currentTab]?.listData?.length && <LoadingMoreView bottomSpace={Layout.space.base * 2} />}
+      {isLoaded && isFetching && !!listData?.length && <LoadingMoreView bottomSpace={Layout.space.base * 2} />}
     </>
   );
 };
