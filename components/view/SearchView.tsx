5544076448b7fb1b32f99ab4e8e07b078714aae7
david-fiaty <dfiaty@gmail.com>
2025-09-29 12:10:45 +0000

    Update SearchView.tsx

--- components/view/SearchView.tsx
+++ components/view/SearchView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { StyleSheet } from "react-native";
 import { useDispatch, useSelector, shallowEqual } from "react-redux";
 import { setCurrentTab } from "@/redux/slices/SearchSlice";
@@ -15,6 +15,7 @@ const SearchView = () => {
   const dispatch = useDispatch();
   const searchState: any = useSelector((state: any) => state.search, shallowEqual);
   const searchTabs: any[] = SearchManager.getSearchTabs();
+  const prevSearchState: any = useRef(null);
 
   const onTabPress = (tabId: string) => {
     dispatch(setCurrentTab(tabId));
@@ -37,6 +38,14 @@ const SearchView = () => {
       dispatch(setCurrentTab((searchTabs.find((o: any) => o?.default === true))?.id));
     }
   }, [searchState, searchTabs]);
+
+  useEffect(() => {
+    if (SearchManager.shouldRefreshResults(prevSearchState.current, searchState)) {
+      // Todo - Finish check implementation
+      console.log(prevSearchState.current, searchState)
+    }
+  }, [searchState]);
+
 
   return (
     <BoxView
