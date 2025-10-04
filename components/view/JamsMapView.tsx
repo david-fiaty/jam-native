e3685a271b4fcc73336a67d2b73327c43327e09f
david-fiaty <dfiaty@gmail.com>
2025-10-04 17:37:35 +0000

    Update JamsMapView.tsx

--- components/view/JamsMapView.tsx
+++ components/view/JamsMapView.tsx
@@ -1,6 +1,6 @@
 import MapView, { Callout, Marker, PROVIDER_DEFAULT, PROVIDER_GOOGLE } from "react-native-maps";
 import { useState, useEffect, useRef } from "react";
-import { StyleSheet, TouchableWithoutFeedback, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { useDispatch, useSelector, shallowEqual } from "react-redux";
 import { setCurrentTab } from "@/redux/slices/SearchSlice";
 import { Layout } from "@/constants/Layout";
@@ -12,7 +12,7 @@ import TabsView from "./TabsView";
 import MapManager from "@/manager/MapManager";
 import MapLegendView from "./MapLegendView";
 import LoadingMoreView from "./LoadingMoreView";
-
+    
 const JamsMapView = () => {
   const dispatch = useDispatch();
   const [currentLocation, setCurrentLocation] = useState<any>(null);
@@ -110,14 +110,13 @@ const JamsMapView = () => {
   if (!currentLocation?.latitude || !currentLocation?.longitude || !isLoaded) return <SpinnerView />;
 
   return (
-    <View style={styles.container}>
-      <TabsView
-        tabs={searchTabs}
-        currentTab={searchState.currentTab}
-        onItemPress={(tabId: string) => dispatch(setCurrentTab(tabId))}
-      />
+      <View style={styles.container}>
+        <TabsView
+          tabs={searchTabs}
+          currentTab={searchState.currentTab}
+          onItemPress={(tabId: string) => dispatch(setCurrentTab(tabId))}
+        />
 
-      <TouchableWithoutFeedback>
         <MapView
           style={styles.map}
           provider={PROVIDER_DEFAULT}
@@ -131,11 +130,10 @@ const JamsMapView = () => {
         </MapView>
 
         <MapLegendView />
-      </TouchableWithoutFeedback>
-
-      {isLoaded && isFetching && <LoadingMoreView />}
-    </View>
-
+        
+        {isLoaded && isFetching && <LoadingMoreView />}
+      </View>
+  
   );
 };
 
