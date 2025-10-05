ab6d5615e2eb7a96eebe2b19c8191dbe25ae9bc9
david-fiaty <dfiaty@gmail.com>
2025-10-05 12:30:05 +0000

    Update PublicProfileSection.tsx

--- components/section/profile/PublicProfileSection.tsx
+++ components/section/profile/PublicProfileSection.tsx
@@ -1,3 +1,5 @@
+import { StyleSheet, View } from "react-native";
+import BoxView from "@/components/view/BoxView";
 import ProfileView from "@/components/view/ProfileView";
 
 type Props = {
@@ -7,12 +9,27 @@ type Props = {
 
 const PublicProfileSection = ({ profileId, itemData }: Props) => {
   return (
-    <ProfileView 
-      profileId={profileId} 
-      isPublic={true}
+    <BoxView
+      direction="column"
+      align="center"
+      justify="flex-start"
+      style={styles.container}
+      scroll={true}
+    >
+      <ProfileView
+        profileId={profileId}
+        isPublic={true}
       //itemData={itemData}
-    />
+      />
+    </BoxView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'relative',
+    width: '100%',
+  },
+});
 
 export default PublicProfileSection;
