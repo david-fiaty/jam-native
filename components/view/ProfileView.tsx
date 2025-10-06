d8d74ecf096c674ff75a03e3114d7aac4fbf9ee4
david-fiaty <dfiaty@gmail.com>
2025-10-06 15:17:14 +0000

    Update ProfileView.tsx

--- components/view/ProfileView.tsx
+++ components/view/ProfileView.tsx
@@ -217,8 +217,7 @@ const ProfileView = ({ profileId, itemData, isOwner, isPublic }: Props) => {
             label={i18n.t('Country')}
             iconRight="location"
             onIconRightPress={() => ModalManager.toggleModal('ProfileLocationMapView', {
-              latitude: profileData?.geolocation_latitude,
-              longitude: profileData?.geolocation_longitude,
+              profileData: profileData,
             })}
           >
             <TextView>
