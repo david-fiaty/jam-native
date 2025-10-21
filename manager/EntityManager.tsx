f3c90cb9daaec1935495348c97b4c0c6fe1e7a00
david-fiaty <dfiaty@gmail.com>
2025-10-21 14:19:02 +0000

    Update EntityManager.tsx

--- manager/EntityManager.tsx
+++ manager/EntityManager.tsx
@@ -196,19 +196,8 @@ class EntityManager {
   }
 
   async addJam(entityData: any) {
-
-    console.log('1 - entityData ---->', entityData);
-
     let payload: any = FormManager.objectToFormData(entityData);
-
-    console.log('2 - payload ---->', entityData);
-
-
     let response: any = await DataManager.post('addJam', payload);
-
-    console.log('3 - response ---->', response);
-
-
     let success: boolean = false;
 
     if (response?.id > 0) {
