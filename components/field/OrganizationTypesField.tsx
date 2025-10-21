31dfaaff42be0da174905299ad280dbe5c614070
david-fiaty <dfiaty@gmail.com>
2025-10-21 16:33:24 +0000

    Update OrganizationTypesField.tsx

--- components/field/OrganizationTypesField.tsx
+++ components/field/OrganizationTypesField.tsx
@@ -5,7 +5,6 @@ import { setFormData } from '@/redux/slices/FormSlice';
 import { Layout } from '@/constants/Layout';
 import IconView from "../view/IconView";
 import TagView from '../view/TagView';
-import EntityManager from '@/manager/EntityManager';
 import InputTextField from './InputTextField';
 import SpinnerView from '../view/SpinnerView';
 
