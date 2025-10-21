28d64f410e00ef404b17d54d1a037bff7775b0bc
david-fiaty <dfiaty@gmail.com>
2025-10-21 16:23:49 +0000

    Update ProfileForm.tsx

--- components/form/ProfileForm.tsx
+++ components/form/ProfileForm.tsx
@@ -171,6 +171,9 @@ const ProfileForm = () => {
             />
             {FormManager.renderError('profile_organization.organization_name')}
 
+            <TextView>
+              {i18n.t('Organization types')}*
+            </TextView>
             <OrganizationTypesField
               resource={resource}
               field="organization_types"
