4d67cd05fa0b91fad3c35208a37fcbfafffd6ab3
david-fiaty <dfiaty@gmail.com>
2025-10-22 16:04:17 +0000

    Update ProfileForm.tsx

--- components/form/ProfileForm.tsx
+++ components/form/ProfileForm.tsx
@@ -45,6 +45,8 @@ const ProfileForm = () => {
 
     let result: any = await UserManager.updateProfile(data);
 
+    console.log(result)
+
     if (result?.success === false) {
       FormManager.addServerErrors(resource, result?.data?.meta);
 
