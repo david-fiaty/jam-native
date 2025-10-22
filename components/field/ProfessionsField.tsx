aaab7c4527b229bca6a256128d15de2c726d718e
david-fiaty <dfiaty@gmail.com>
2025-10-22 14:25:20 +0000

    Update ProfessionsField.tsx

--- components/field/ProfessionsField.tsx
+++ components/field/ProfessionsField.tsx
@@ -172,7 +172,7 @@ const ProfessionsField = ({ resource, field, value, placeholder }: Props) => {
             inside={getSelectedSubProfessions().length > 0}
             style={!getSelectedSubProfessions().length ? styles.element : styles.preview}
             iconStyle={getSelectedSubProfessions().length > 0 ? styles.iconRight : {}}
-            data={getSelectedSubProfessions()}
+            data={getSubProfessionsOptions()}
             renderItem={(o: any) => renderItem(o)}
             renderSelectedItem={(o, unSelect) => renderSelectedItem(o, unSelect)}
             onChange={(selectedIds: any) => updateSelection(selectedIds)}
