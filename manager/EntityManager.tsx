15f9dd6a70c6ef62943ee059f97e497059661a02
david-fiaty <dfiaty@gmail.com>
2025-10-23 14:10:27 +0000

    Update EntityManager.tsx

--- manager/EntityManager.tsx
+++ manager/EntityManager.tsx
@@ -42,7 +42,7 @@ class EntityManager {
       profile_id: profileId,
       page_size: Config.paginationSize,
       page: 1,
-    }; 
+    };
 
     return await DataManager.get('listProjects', { ...defaults, ...options }, variables, search);
   }
@@ -192,9 +192,20 @@ class EntityManager {
 
   async addJam(data: any) {
     let payload: any = FormManager.objectToFormData(data);
+
+    for (let pair of payload.entries()) {
+      if (pair[0] == 'upload_medias') {
+        console.log(pair[1]); 
+      }
+      
+    }
+
+
+
+    return
     let response: any = await DataManager.post('addJam', payload);
-    let success: boolean = false;  
- 
+    let success: boolean = false;
+
     if (response?.id > 0) {
       UserManager.updateProfileReference('profile_jams', response.id);
       success = true;
@@ -213,7 +224,7 @@ class EntityManager {
     if (response?.id > 0) {
       UserManager.updateProfileReference('profile_projects', response.id);
       success = true;
-    } 
+    }
 
     return {
       success: success,
@@ -334,9 +345,9 @@ class EntityManager {
   getJamOwnerName(item: any) {
     let ownerName: string = item?.profile?.profile_name;
 
-    if (ownerName?.length > Config.maxUserNameLength) {  
+    if (ownerName?.length > Config.maxUserNameLength) {
       ownerName = ownerName.substring(0, Config.maxUserNameLength) + '...';
-    } 
+    }
 
     return ownerName;
   }
@@ -348,7 +359,7 @@ class EntityManager {
       label = (this.getJamTypes().find((o: any) => o.id === jamType))?.name;
     }
 
-    return label?.length > 0 ? label: i18n.t('Unavailable');
+    return label?.length > 0 ? label : i18n.t('Unavailable');
   }
 
   getJamTypes() {
