393c41b63acdc90e92a8712455096df933b72f35
david-fiaty <dfiaty@gmail.com>
2025-10-24 13:21:04 +0000

    Create ProfileFormPersonal.tsx

---
+++ components/form/profile-form/ProfileFormPersonal.tsx
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { Layout } from "@/constants/Layout";
+import { setFormData } from "@/redux/slices/FormSlice";
+import ProfileImageField from "@/components/field/ProfileImageField";
+import ButtonView from "@/components/view/ButtonView";
+import i18n from "@/translation/i18n";
+import UserManager from "@/manager/UserManager";
+import ScreenManager from "@/manager/ScreenManager";
+import SpinnerView from "@/components/view/SpinnerView";
+import TextView from "@/components/view/TextView";
+import InputTextField from "@/components/field/InputTextField";
+import InputTextareaField from "@/components/field/InputTextareaField";
+import SectorsField from "@/components/field/SectorsField";
+import ModalManager from "@/manager/ModalManager";
+import LocationPickerField from "@/components/field/LocationPickerField";
+import ProfileTypeField from "@/components/field/ProfileTypeField";
+import FormManager from "@/manager/FormManager";
+import BoxView from "@/components/view/BoxView";
+import VenueTypesField from "@/components/field/VenueTypesField";
+import CountriesField from "@/components/field/CountriesField";
+import OrganizationTypesField from "../field/OrganizationTypesField";
+import CulturalActivityTypesField from "../field/CulturalActivityTypesField";
+import ProfessionsField from "../field/ProfessionsField";
+
+const resource: string = 'profile';
+
+const ProfileFormPersonal = () => {
+  const formData = useSelector((state: any) => state.form?.[resource]);
+
+  return (
+    <>
+      <TextView>
+        {i18n.t('First name')}*
+      </TextView>
+      <InputTextField
+        value={formData?.profile_personal?.first_name}
+        placeholder={i18n.t('Enter your first name')}
+        onChangeText={(value: string) => FormManager.updateField(resource, 'profile_personal', {
+          ...(formData?.profile_personal || {}),
+          ...{ first_name: value },
+        }, ['string'])}
+      />
+      {FormManager.renderError('profile_personal.first_name')}
+
+      <TextView>
+        {i18n.t('Last name')}
+      </TextView>
+      <InputTextField
+        value={formData?.profile_personal?.last_name}
+        placeholder={i18n.t('Enter your last name')}
+        onChangeText={(value: string) => FormManager.updateField(resource, 'profile_personal', {
+          ...(formData?.profile_personal || {}),
+          ...{ last_name: value },
+        }, ['string'])}
+      />
+      {FormManager.renderError('profile_personal.last_name')}
+
+      <ProfessionsField
+        resource={resource}
+        field="professions_ids"
+        value={formData?.professions_ids}
+      />
+
+      <TextView>
+        {i18n.t('Main cultural activities')}
+      </TextView>
+      <CulturalActivityTypesField
+        resource={resource}
+        field="main_cultural_activities"
+        parent="profile_personal"
+        placeholder={i18n.t('Select cultural activities')}
+        value={formData?.profile_personal?.main_cultural_activities}
+        onPress={() => ModalManager.toggleModal('CulturalActivityTypesList', {
+          resource: resource,
+          field: "main_cultural_activities",
+          parent: "profile_personal",
+        })}
+      />
+      {FormManager.renderError('profile_personal.main_cultural_activities')}
+    </>
+  );
+}
+
+export default ProfileFormPersonal;
