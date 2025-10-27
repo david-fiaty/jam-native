110e7453f0ad54c7f78dcd34cf5a216c877ec89f
david-fiaty <dfiaty@gmail.com>
2025-10-27 14:26:50 +0000

    Update ProfileFormVenue.tsx

--- components/form/profile-form/ProfileFormVenue.tsx
+++ components/form/profile-form/ProfileFormVenue.tsx
@@ -134,6 +134,19 @@ const ProfileFormVenue = ({ resource, formData }: Props) => {
       />
       {FormManager.renderError('profile_venue.is_open_24h')}
 
+      <TextView>
+        {i18n.t('Appointment only')}
+      </TextView>
+      <InputTextField
+        value={formData?.profile_venue?.is_by_appointment_only}
+        placeholder={i18n.t('Appointment only')}
+        onChangeText={(value: string) => FormManager.updateField(resource, 'profile_venue', {
+          ...(formData?.profile_venue || {}),
+          ...{ is_by_appointment_only: value },
+        }, ['string'])}
+      />
+      {FormManager.renderError('profile_venue.is_by_appointment_only')}
+
 
     </>
   );
