d791b04caa4183be9110bf6010380bace05d18d9
david-fiaty <dfiaty@gmail.com>
2025-10-28 15:08:52 +0000

    Update JamForm.tsx

--- components/form/JamForm.tsx
+++ components/form/JamForm.tsx
@@ -150,6 +150,7 @@ const JamForm = ({ jamId, isPublic }: Props) => {
 
         <TextView>{i18n.t('Select media')} *</TextView>
         <MediaPickerField
+          mediaTypes={['images']}
           preview={true}
           placeholder={i18n.t('Select media files')}
           value={formData?.upload_medias || []}
