a9aa3b76a3d02c67a1ca4314e675cc2855788ed1
david-fiaty <dfiaty@gmail.com>
2025-10-28 15:20:46 +0000

    Update DocumentPickerField.tsx

--- components/field/DocumentPickerField.tsx
+++ components/field/DocumentPickerField.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect, JSX } from "react";
 import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import { Layout } from '@/constants/Layout';
 import * as ImagePicker from 'expo-image-picker';
+import * as DocumentPicker from 'expo-document-picker';
 import ImageView from '../view/ImageView';
 import TextView from '../view/TextView';
 import BoxView from '../view/BoxView';
