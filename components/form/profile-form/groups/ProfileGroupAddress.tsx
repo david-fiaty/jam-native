5144606eb05c4d48c9ab726244cc7b2c4e762905
david-fiaty <dfiaty@gmail.com>
2025-10-30 12:50:40 +0000

    Create ProfileGroupAddress.tsx

---
+++ components/form/profile-form/groups/ProfileGroupAddress.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import i18n from "@/translation/i18n";
+import TextView from "@/components/view/TextView";
+import InputTextField from "@/components/field/InputTextField";
+import ModalManager from "@/manager/ModalManager";
+import FormManager from "@/manager/FormManager";
+import CulturalActivityTypesField from "@/components/field/CulturalActivityTypesField";
+import ProfessionsField from "@/components/field/ProfessionsField";
+
+type Props = {
+  resource: any;
+  formData: any;
+};
+
+const ProfileGroupAddress = ({ resource, formData }: Props) => {
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default ProfileGroupAddress;
