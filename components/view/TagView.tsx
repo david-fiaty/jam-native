560018b11a4f07174aac7b67a9b31603bfec8f05
david-fiaty <dfiaty@gmail.com>
2025-10-31 13:28:17 +0000

    Update TagView.tsx

--- components/view/TagView.tsx
+++ components/view/TagView.tsx
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
     borderRadius: Layout.radius.round,
     paddingVertical: Layout.space.base/2.5,
     paddingHorizontal: Layout.space.base/2,
-    paddingRight: Layout.space.base*2.8,
+    paddingRight: Layout.space.base*2.7,
     alignSelf: 'flex-start',
     maxWidth: '100%',
     position: 'relative',
