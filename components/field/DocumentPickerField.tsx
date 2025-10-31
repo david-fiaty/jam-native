6ea1e713c5db88f6bfdac22248628214d35f2bff
david-fiaty <dfiaty@gmail.com>
2025-10-31 13:24:49 +0000

    Update DocumentPickerField.tsx

--- components/field/DocumentPickerField.tsx
+++ components/field/DocumentPickerField.tsx
@@ -37,7 +37,7 @@ const DocumentPickerField = ({ value, placeholder, preview, multiple, mediaTypes
         containerStyle={styles.tagItem}
         onDeleteButtonPress={() => deleteMedia(data)}
       >
-        {data.name}
+        {DataManager.truncateText(data.name, 60)}
       </TagView>
     );
   };
