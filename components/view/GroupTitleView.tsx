a7d4fc5a5d7b77c08907bd4df6bd2ff63be53331
david-fiaty <dfiaty@gmail.com>
2025-10-31 11:41:25 +0000

    Create GroupTitleView.tsx

---
+++ components/view/GroupTitleView.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { Layout } from '@/constants/Layout';
+import TextView from './TextView';
+
+type Props = {
+  label?: any;
+};
+
+const GroupTitleView = ({ label }: Props) => {
+  return (
+    <TextView style={[styles.container]} bold={true}>
+      {label}
+    </TextView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Layout.space.base * 1.5,
+  },
+});
+
+export default GroupTitleView;
