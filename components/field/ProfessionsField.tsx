818218d7a1450cc92024f5740dfdb770cf924d84
david-fiaty <dfiaty@gmail.com>
2025-11-01 16:47:12 +0000

    Update ProfessionsField.tsx

--- components/field/ProfessionsField.tsx
+++ components/field/ProfessionsField.tsx
@@ -12,13 +12,27 @@ import i18n from '@/translation/i18n';
 import FormManager from '@/manager/FormManager';
 
 type Props = {
-  resource: string;
+  resource?: any;
+  fieldKey?: any;
+  parentKey?: any;
+  rules?: any;
+  //formData?: any;
+
   field: string;
   value?: any;
   placeholder?: any;
 };
 
-const ProfessionsField = ({ resource, field, value, placeholder }: Props) => {
+const ProfessionsField = ({   
+  resource,
+  fieldKey,
+  parentKey,
+  rules,
+  //formData, 
+  field, 
+  value, 
+  placeholder 
+}: Props) => {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [professionsData, setProfessionsData] = useState<any[]>([]);
