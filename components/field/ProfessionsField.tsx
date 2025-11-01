b0571564071ea73407eb553002244cef7b4aeeb4
david-fiaty <dfiaty@gmail.com>
2025-11-01 17:00:57 +0000

    Update ProfessionsField.tsx

--- components/field/ProfessionsField.tsx
+++ components/field/ProfessionsField.tsx
@@ -35,8 +35,8 @@ const ProfessionsField = ({
 }: Props) => {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
-  const [itemsData, setItemsData] = useState<any[]>([]);
-  const [itemsOptions, setItemsOptions] = useState<any[]>([]);
+  const [professionsData, setProfessionsData] = useState<any[]>([]);
+  const [professionsOptions, setProfessionsOptions] = useState<any[]>([]);
   const appState = useSelector((state: any) => state.app, shallowEqual);
 
   const updateSelection = (selectedIds: any[]) => {
@@ -67,7 +67,7 @@ const ProfessionsField = ({
       return listOptions;
     }
 
-    itemsData
+    professionsData
       .filter((o: any) => formData[field].includes(o.id))
       .map((x: any) => {
       (x?.sub_professions || []).map((y: any) => {
@@ -83,7 +83,7 @@ const ProfessionsField = ({
 
   const getSelectedOptions = () => {
     let selectedIds: any[] = formData?.[field] || [];
-    let optionsIds: any[] = itemsOptions.map((o: any) => o.value);
+    let optionsIds: any[] = professionsOptions.map((o: any) => o.value);
 
     return selectedIds.filter((id: any) => optionsIds.includes(id));
   };
@@ -143,7 +143,7 @@ const ProfessionsField = ({
   useEffect(() => {
     (async () => {
       if (!isLoaded) {
-        setItemsData(appState.professionsData);
+        setProfessionsData(appState.professionsData);
         setProfessionsOptions(getProfessionsOptions(appState.professionsData));
         setIsLoaded(true);
       }
@@ -165,7 +165,7 @@ const ProfessionsField = ({
           placeholderStyle={styles.placeholderStyle}
           iconColor={Layout.colors.primary}
           onChange={(selectedIds: any) => updateSelection(selectedIds)}
-          data={itemsOptions}
+          data={professionsOptions}
           renderItem={(o: any) => renderItem(o)}
           renderSelectedItem={(o, unSelect) => renderSelectedItem(o, unSelect)}
         />
