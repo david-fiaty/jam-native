2fbaa01bfa57e49155d380b7a67925e42b068631
david-fiaty <dfiaty@gmail.com>
2025-11-02 14:10:05 +0000

    Update ProfileFormVenue.tsx

--- components/form/profile-form/ProfileFormVenue.tsx
+++ components/form/profile-form/ProfileFormVenue.tsx
@@ -10,6 +10,7 @@ import DataManager from "@/manager/DataManager";
 import ProfileGroupDocuments from "./groups/ProfileGroupDocuments";
 import GroupTitleView from "@/components/view/GroupTitleView";
 import ProfileGroupActivities from "./groups/ProfileGroupActivities";
+import WeekDaysField from "@/components/field/WeekDaysField";
 
 type Props = {
   resource: any;
