d19fa388ed2a3c0db53afa672ac20c5f7d2ae8fe
david-fiaty <dfiaty@gmail.com>
2025-11-03 16:14:08 +0000

    Update ProfileGroupAddress.tsx

--- components/form/profile-form/groups/ProfileGroupAddress.tsx
+++ components/form/profile-form/groups/ProfileGroupAddress.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import i18n from "@/translation/i18n";
 import TextView from "@/components/view/TextView";
 import InputTextField from "@/components/field/InputTextField";
-import ModalManager from "@/manager/ModalManager";
 import FormManager from "@/manager/FormManager";
 import LocationPickerField from "@/components/field/LocationPickerField";
 import CountriesField from "@/components/field/CountriesField";
