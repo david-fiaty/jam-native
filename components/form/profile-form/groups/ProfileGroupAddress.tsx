30496d9f20ff8b1503b96015b18c7e74b1165dae
david-fiaty <dfiaty@gmail.com>
2025-11-03 13:09:53 +0000

    Update ProfileGroupAddress.tsx

--- components/form/profile-form/groups/ProfileGroupAddress.tsx
+++ components/form/profile-form/groups/ProfileGroupAddress.tsx
@@ -61,6 +61,7 @@ const ProfileGroupAddress = ({ resource, formData, parentKey }: Props) => {
         longitudeKey="geolocation_latitude"
         latitudeValue={formData?.geolocation_latitude}
         longitudeValue={formData?.geolocation_longitude}
+        rules={['required']}
         placeholder={i18n.t('Select your location')}
       />
     </>
