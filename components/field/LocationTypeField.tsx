492f30000fef9c1720df1caa75f3e8b1383ff67d
david-fiaty <dfiaty@gmail.com>
2025-11-04 15:13:34 +0000

    Update LocationTypeField.tsx

--- components/field/LocationTypeField.tsx
+++ components/field/LocationTypeField.tsx
@@ -4,14 +4,30 @@ import SelectListBase from '../base/SelectListBase';
 import StaticData from '@/constants/StaticData';
 import i18n from '@/translation/i18n';
 import EntityManager from '@/manager/EntityManager';
+import FormManager from '@/manager/FormManager';
 
 type Props = {
-  value?: any,
+  resource?: any;
+  fieldKey?: any;
+  parentKey?: any;
+  rules?: any;
+  value?: any;
+  label?: any;
+  placeholder?: any;
   onChangeValue?: (option: any) => void,
 };
 
-const LocationTypeField = ({value, onChangeValue}: Props) => {
-  const buildOptions = (optionsData: any) => {    
+const LocationTypeField = ({
+  resource,
+  fieldKey,
+  parentKey,
+  rules,
+  value,
+  label,
+  placeholder,
+  onChangeValue
+}: Props) => {
+  const buildOptions = (optionsData: any) => {
     return [...(optionsData || [])].map((item: any) => {
       return {
         value: item?.id,
@@ -19,16 +35,22 @@ const LocationTypeField = ({value, onChangeValue}: Props) => {
       }
     });
   };
-  
+
   return (
-    <BoxView direction="column" align="center" style={styles.container}>
-      <SelectListBase 
-        value={value}
-        data={buildOptions(EntityManager.getLocationTypes())} 
-        onChangeValue={onChangeValue}
-        placeholder={i18n.t('Select a location type')}
-      />
-    </BoxView>
+    <>
+      {FormManager.renderLabel(label, rules)}
+      
+      <BoxView direction="column" align="center" style={styles.container}>
+        <SelectListBase
+          value={value}
+          data={buildOptions(EntityManager.getLocationTypes())}
+          onChangeValue={onChangeValue}
+          placeholder={placeholder}
+        />
+      </BoxView>
+
+      {FormManager.renderError(fieldKey, parentKey)}
+    </>
   );
 };
 
