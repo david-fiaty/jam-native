c4e18715380bffff838cf4ace0eea833c548f6a2
david-fiaty <dfiaty@gmail.com>
2025-11-04 14:40:56 +0000

    Update ProfileFormPersonal.tsx

--- components/form/profile-form/ProfileFormPersonal.tsx
+++ components/form/profile-form/ProfileFormPersonal.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import i18n from "@/translation/i18n";
-import TextView from "@/components/view/TextView";
 import InputTextField from "@/components/field/InputTextField";
 import ProfessionsField from "@/components/field/ProfessionsField";
 import GroupTitleView from "@/components/view/GroupTitleView";
