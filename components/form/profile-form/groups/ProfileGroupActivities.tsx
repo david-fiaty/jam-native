9c3600a8d821c571e96f364eac939cb147803943
david-fiaty <dfiaty@gmail.com>
2025-11-04 14:27:42 +0000

    Update ProfileGroupActivities.tsx

--- components/form/profile-form/groups/ProfileGroupActivities.tsx
+++ components/form/profile-form/groups/ProfileGroupActivities.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import i18n from "@/translation/i18n";
-import TextView from "@/components/view/TextView";
 import InputTextField from "@/components/field/InputTextField";
 import GroupTitleView from "@/components/view/GroupTitleView";
 import SectorsField from "@/components/field/SectorsField";
@@ -27,15 +26,13 @@ const ProfileGroupActivities = ({ resource, formData, parentKey }: Props) => {
         value={formData?.[parentKey]?.main_cultural_activities || []}
       />
 
-      <TextView>
-        {i18n.t('Other cultural activities')}
-      </TextView>
       <InputTextField
         resource={resource}
         fieldKey="other_cultural_activities"
         parentKey={parentKey}
         rules={['string']}
         value={formData?.[parentKey]?.other_cultural_activities || ''}
+        label={i18n.t('Other cultural activities')}
         placeholder={i18n.t('Enter other cultural activities')}
       />   
 
