61d75d8da24919902b0466a858cf1a61b78220e1
david-fiaty <dfiaty@gmail.com>
2025-11-05 15:48:57 +0000

    Update ProjectForm.tsx

--- components/form/ProjectForm.tsx
+++ components/form/ProjectForm.tsx
@@ -129,7 +129,7 @@ const ProjectForm = ({ projectId, isPublic }: Props) => {
         <ProjectJamsField
           resource={resource}
           fieldKey="jams_ids"
-          value={formData?.jams_ids}
+          value={formData?.jams_ids || []}
           rules={['required', 'array']}
           label={`${i18n.t('Jams')} (${formData?.jams_ids?.length || 0})`}
           emptyMessage={i18n.t('No data available.')}
