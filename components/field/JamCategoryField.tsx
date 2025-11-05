13fed62db8aba65088ea9e82ab77f8edfc0a19d1
david-fiaty <dfiaty@gmail.com>
2025-11-05 13:28:40 +0000

    Create JamCategoryField.tsx

---
+++ components/field/JamCategoryField.tsx
@@ -0,0 +1,78 @@
+import { StyleSheet } from 'react-native';
+import BoxView from '../view/BoxView';
+import SelectListBase from '../base/SelectListBase';
+import StaticData from '@/constants/StaticData';
+import FormManager from '@/manager/FormManager';
+
+type Props = {
+  resource?: any;
+  fieldKey?: any;
+  parentKey?: any;
+  rules?: any;
+  value?: any;
+  label?: any;
+  placeholder?: any;
+  disabled?: any;
+  onChangeValue?: (option: any) => void;
+};
+
+const JamCategoryField = ({
+  resource,
+  fieldKey,
+  parentKey,
+  rules,
+  value,
+  label,
+  placeholder,
+  disabled,
+  onChangeValue
+}: Props) => {
+  const profileTypes = StaticData.profileTypes;
+
+  const buildOptions = (optionsData: any) => {
+    return [...(optionsData || [])].map((item: any) => {
+      return {
+        value: item?.id,
+        label: item?.label,
+      }
+    });
+  };
+
+  const onChangeEvent = (option: any) => {
+    if (onChangeValue) {
+      onChangeValue(option);
+    }
+    else if (resource && fieldKey && !parentKey) {
+      FormManager.updateField(resource, fieldKey, option.value, rules);
+    }
+    else if (resource && fieldKey && parentKey) {
+      FormManager.updateField(resource, `${parentKey}.${fieldKey}`, option.value, rules);
+    }
+  };
+
+  return (
+    <>
+      {FormManager.renderLabel(label, rules)}
+
+      <BoxView direction="column" align="left" style={styles.container}>
+        <SelectListBase
+          placeholder={placeholder}
+          value={value || ''}
+          data={buildOptions(profileTypes)}
+          onChangeValue={onChangeEvent}
+          disabled={disabled}
+        />
+
+        {FormManager.renderError(fieldKey, parentKey)}
+      </BoxView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+  },
+});
+
+export default JamCategoryField;
