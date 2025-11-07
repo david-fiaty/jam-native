2ec7c906b1f6464ed3cd168e868cec1b6fb99410
david-fiaty <dfiaty@gmail.com>
2025-11-07 17:40:33 +0000

    Update InputPhoneField.tsx

--- components/field/InputPhoneField.tsx
+++ components/field/InputPhoneField.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, View } from "react-native";
+import { AsYouType } from 'libphonenumber-js'
 import { Layout } from "@/constants/Layout";
 import { Config } from "@/constants/Config";
 import getUnicodeFlagIcon from 'country-flag-icons/unicode';
@@ -89,6 +90,9 @@ const InputPhoneField = ({
   };
 
   const onChangePhoneValue = (fieldValue: any) => {
+    fieldValue = new AsYouType().input(selectedCountry.prefix + fieldValue);
+    fieldValue = fieldValue.replace(`${selectedCountry.prefix} `, '');
+
     FormManager.updateField(resource, phoneNumberFieldKey, fieldValue, rules, parentKey, {
       countryCode: selectedCountry.code,
     });
