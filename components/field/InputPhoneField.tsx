6d20914cae01dc3d83081e29ac37981c5a25a753
david-fiaty <dfiaty@gmail.com>
2025-11-07 14:56:49 +0000

    Update InputPhoneField.tsx

--- components/field/InputPhoneField.tsx
+++ components/field/InputPhoneField.tsx
@@ -157,6 +157,7 @@ const styles = StyleSheet.create({
   },
   inputTextField: {
     backgroundColor: Layout.colors.white,
+    borderWidth: 0,
   },
   listItem: {
     paddingVertical: Layout.space.base,
