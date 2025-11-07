a29348c8547aacbb59f18572ddc0204c540011a6
david-fiaty <dfiaty@gmail.com>
2025-11-07 17:07:19 +0000

    Update InputPhoneField.tsx

--- components/field/InputPhoneField.tsx
+++ components/field/InputPhoneField.tsx
@@ -2,14 +2,11 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, View } from "react-native";
 import { Layout } from "@/constants/Layout";
 import { Config } from "@/constants/Config";
-import { isValidPhoneNumber } from 'libphonenumber-js';
-import parsePhoneNumber from 'libphonenumber-js'
 import getUnicodeFlagIcon from 'country-flag-icons/unicode';
 import BoxView from "../view/BoxView";
 import FormManager from "@/manager/FormManager";
 import TextView from "../view/TextView";
 import InputTextField from "./InputTextField";
-import i18n from "@/translation/i18n";
 import SelectListBase from "../base/SelectListBase";
 import StaticData from "@/constants/StaticData";
 
@@ -88,24 +85,13 @@ const InputPhoneField = ({
   const onChangeCodeValue = (item: any) => {
     let targetCountry: any = StaticData.countryPhoneCodes.find((o: any) => o.code == item.value);
     setSelectedCountry(targetCountry);
+    
     FormManager.updateField(resource, phonePrefixFieldKey, targetCountry.prefix, rules, parentKey);
   };
 
   const onChangePhoneValue = (fieldValue: any) => {
-    /*
-    if (selectedCountry?.code) {
-      let parsedNumber: any = parsePhoneNumber(fieldValue, selectedCountry.code.toUpperCase());
-
-      if (parsedNumber && parsedNumber.isValid()) {
-        // Todo - Update form data
-        console.log(phoneNumber)
-        console.log(selectedCountry)
-        //console.log(parsedNumber);
-      }
-    }
-      */
-
     setPhoneNumber(fieldValue);
+
     FormManager.updateField(resource, phoneNumberFieldKey, fieldValue, rules, parentKey, {
       countryCode: selectedCountry.code,
     });
