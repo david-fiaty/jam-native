30a2b9dc645c614d8bcff31e6317c6e91fb84e89
david-fiaty <dfiaty@gmail.com>
2025-11-08 10:55:26 +0000

    Delete BaseTheme.ts

--- constants/BaseTheme.ts
+++
@@ -1,9 +0,0 @@
-import { createTheme } from '@rneui/themed';
-import { Layout } from './Layout';
-
-const BaseTheme = createTheme({
-  lightColors: {},
-  components: {},
-});
-
-export default BaseTheme;
