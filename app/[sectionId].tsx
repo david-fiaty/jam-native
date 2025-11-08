869979949d510f2d970ac868a920a69e91acf8fb
david-fiaty <dfiaty@gmail.com>
2025-11-08 10:51:25 +0000

    Update [sectionId].tsx

--- app/[sectionId].tsx
+++ app/[sectionId].tsx
@@ -1,17 +1,13 @@
 import { StyleSheet } from 'react-native';
-import { ThemeProvider } from '@rneui/themed';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Layout } from '@/constants/Layout';
-import BaseTheme from "@/constants/BaseTheme";
 import SectionView from '@/components/view/SectionView';
 
 export default () => {
   return (
-    <ThemeProvider theme={BaseTheme}>
-      <SafeAreaView style={styles.container}>
-        <SectionView />
-      </SafeAreaView>
-    </ThemeProvider>
+    <SafeAreaView style={styles.container}>
+      <SectionView />
+    </SafeAreaView>
   );
 }
 
