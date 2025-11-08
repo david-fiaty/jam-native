8282ef1c4f20d1a53aeb30a6ff066b8b346cbc91
david-fiaty <dfiaty@gmail.com>
2025-11-08 10:50:08 +0000

    Update BaseTheme.ts

--- constants/BaseTheme.ts
+++ constants/BaseTheme.ts
@@ -2,21 +2,8 @@ import { createTheme } from '@rneui/themed';
 import { Layout } from './Layout';
 
 const BaseTheme = createTheme({
-  lightColors: {
-    primary: Layout.colors.primary,
-  },
-  components: {
-    Button: {
-      titleStyle: {
-        color: Layout.colors.white,
-        fontWeight: 'normal',
-      },
-      buttonStyle: {
-        backgroundColor: Layout.colors.primary,
-        borderRadius: Layout.radius.round,
-      },
-    },
-  },
+  lightColors: {},
+  components: {},
 });
 
 export default BaseTheme;
