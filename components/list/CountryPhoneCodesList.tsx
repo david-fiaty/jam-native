f0a806097e005f76d4efc5e40cbfde501aefb45a
david-fiaty <dfiaty@gmail.com>
2025-11-10 15:20:37 +0000

    Update CountryPhoneCodesList.tsx

--- components/list/CountryPhoneCodesList.tsx
+++ components/list/CountryPhoneCodesList.tsx
@@ -11,6 +11,7 @@ import FormManager from '@/manager/FormManager';
 import ContentManager from '@/manager/ContentManager';
 import InputTextField from '../field/InputTextField';
 import i18n from '@/translation/i18n';
+import DataManager from '@/manager/DataManager';
 
 type Props = {
   resource: string;
@@ -69,21 +70,10 @@ const CountryPhoneCodesList = ({ resource, fieldKey, parentKey, rules, value }: 
 
   const toggleItem = (entityId: number) => {
     let targetCountry: any = listData.find((o: any) => o.code == entityId);
-    let fieldValue: string = targetCountry.prefix + extractNumber(value);
+    let fieldValue: string = targetCountry.prefix + DataManager.extractPhoneNumber(value);
 
     setSelectedIds([entityId]);
     FormManager.updateField(resource, fieldKey, fieldValue, rules, parentKey);
-  };
-
-  const extractNumber = (fieldValue: any) => {
-    if (fieldValue) {
-      let prefixList: any[] = listData.map((o: any) => o.prefix);
-      let foundPrefix: any = prefixList.find((prefix: any) => fieldValue.startsWith(prefix));
-
-      return fieldValue.replace(foundPrefix, '').replaceAll(' ', '');
-    }
-
-    return '';
   };
 
   const renderItem = (row: any) => {
