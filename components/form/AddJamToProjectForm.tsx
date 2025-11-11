7a2a3bf20f917ebcc311cb1e52ea7374f62b6e00
david-fiaty <dfiaty@gmail.com>
2025-11-11 15:15:18 +0000

    Update AddJamToProjectForm.tsx

--- components/form/AddJamToProjectForm.tsx
+++ components/form/AddJamToProjectForm.tsx
@@ -57,6 +57,7 @@ const AddJamToProjectForm = () => {
 
       <DividerView />
       
+      <TextView>{i18n.t('Select projects from your profile')}:</TextView>
       <ProfileProjectsField
         idArray={profileData?.profile_projects || []}
         emptyMessage={i18n.t('No data available.')}
