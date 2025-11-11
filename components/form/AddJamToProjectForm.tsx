fff93d72eba911c99a818f428c5c9b9ca5a255d2
david-fiaty <dfiaty@gmail.com>
2025-11-11 14:35:26 +0000

    Update AddJamToProjectForm.tsx

--- components/form/AddJamToProjectForm.tsx
+++ components/form/AddJamToProjectForm.tsx
@@ -2,11 +2,11 @@ import { useState, useEffect } from 'react';
 import { Layout } from '@/constants/Layout';
 import ScreenManager from '@/manager/ScreenManager';
 import SpinnerView from '../view/SpinnerView';
-import BackButton from '../button/BackButton';
+//import BackButton from '../button/BackButton';
 import BoxView from '../view/BoxView';
 import i18n from '@/translation/i18n';
 import TextView from '../view/TextView';
-import ProfileProjectsList from '../list/ProfileProjectsList';
+//import ProfileProjectsList from '../list/ProfileProjectsList';
 import UserManager from '@/manager/UserManager';
 import EntityManager from '@/manager/EntityManager';
 import ButtonView from '../view/ButtonView';
@@ -16,7 +16,7 @@ import ModalManager from '@/manager/ModalManager';
 const AddJamToProjectForm = () => {
 
   // Todo - Implement component
-  return <></>;
+  return <TextView>Add jam to project form</TextView>;
   
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [isProcessing, setIsProcessing] = useState<boolean>(false);
