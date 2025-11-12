7e7b4938da882423730de6cf46e3b4f0e279da89
david-fiaty <dfiaty@gmail.com>
2025-11-12 13:54:48 +0000

    Update MoreJamActionsView.tsx

--- components/view/MoreJamActionsView.tsx
+++ components/view/MoreJamActionsView.tsx
@@ -132,7 +132,7 @@ const MoreJamActionsView = ({ jamId }: Props) => {
             {
               text: i18n.t('Yes'),
               onPress: async () => {
-                let result: any = await EntityManager.deleteJam(jamId);
+                let result: any = await UserManager.deleteJam(jamId);
                 let message: any = {
                   title: i18n.t('Delete'),
                   content: i18n.t('The JAM! was successfully deleted.'),
