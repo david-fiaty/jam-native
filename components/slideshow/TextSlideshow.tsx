f427bc2d389b97a641e1b632858dd68df8ef7c86
david-fiaty <dfiaty@gmail.com>
2025-11-14 15:57:07 +0000

    Update TextSlideshow.tsx

--- components/slideshow/TextSlideshow.tsx
+++ components/slideshow/TextSlideshow.tsx
@@ -51,6 +51,9 @@ const TextSlideshow = ({ data }: Props) => {
         windowSize={3}
         onProgressChange={progress}
         renderItem={({ index }) => renderItem(data[index], index)}
+        onConfigurePanGesture={gestureChain => (
+          gestureChain.activeOffsetX([-10, 10])
+        )}
       />
 
       <Pagination.Basic
