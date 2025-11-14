3d1eba3a9a08801ab330259a58a67518c619ed8a
david-fiaty <dfiaty@gmail.com>
2025-11-14 15:42:26 +0000

    Update ImageSlideshow.tsx

--- components/slideshow/ImageSlideshow.tsx
+++ components/slideshow/ImageSlideshow.tsx
@@ -52,9 +52,6 @@ const ImageSlideshow = ({ data }: Props) => {
         windowSize={3}
         onProgressChange={progress}
         renderItem={({ index }) => renderItem(data[index], index)}
-        onConfigurePanGesture={gestureChain => (
-          gestureChain.activeOffsetX([-10, 10])
-        )}
       />
 
       <Pagination.Basic
