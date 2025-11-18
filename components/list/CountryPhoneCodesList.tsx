90669218473d0e4eddff9d5af4e5bc957e5bed65
david-fiaty <dfiaty@gmail.com>
2025-11-18 10:52:51 +0000

    Update CountryPhoneCodesList.tsx

--- components/list/CountryPhoneCodesList.tsx
+++ components/list/CountryPhoneCodesList.tsx
@@ -98,13 +98,7 @@ const CountryPhoneCodesList = ({ resource, fieldKey, parentKey, rules, value }: 
 
   const toggleItem = (entityId: number) => {
     let targetCountry: any = listData.find((o: any) => o.code == entityId);
-    let phoneNumber: string = DataManager.extractPhoneNumber(value);
     let fieldValue: string = targetCountry.prefix;
-
-    /*
-    if (phoneNumber) {
-      fieldValue += phoneNumber;
-    }*/
 
     setSelectedCountry(targetCountry);
 
