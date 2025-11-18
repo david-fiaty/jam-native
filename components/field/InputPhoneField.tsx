ec622e879fa6d44bdf7b07e729c0a0dd67710ba5
david-fiaty <dfiaty@gmail.com>
2025-11-18 11:29:18 +0000

    Update InputPhoneField.tsx

--- components/field/InputPhoneField.tsx
+++ components/field/InputPhoneField.tsx
@@ -273,6 +273,7 @@ const InputPhoneField = ({
     }
   }, [isLoaded, countryOptions]);
 
+  console.log(DataManager.extractPhonePrefix('+2289088'));
   //console.log(new AsYouType().input('+22890880983'))
   
   return renderComponent();
