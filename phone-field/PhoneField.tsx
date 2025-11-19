0c84ed194c6f48f1cc4247f1c9539e3592d672cb
david-fiaty <dfiaty@gmail.com>
2025-11-19 15:01:58 +0000

    Update PhoneField.tsx

--- phone-field/PhoneField.tsx
+++ phone-field/PhoneField.tsx
@@ -1,6 +1,26 @@
+import { Text } from "react-native";
+import InlinePhoneField from "./InlinePhoneField";
+import SplitPhoneField from "./SplitPhoneField";
 
-const PhoneField = () => {
+interface PhoneFieldProps {
+  mode?: 'inline' | 'split';
+  value?: string;
+  onChange?: (data: any) => void;
+}
 
+const PhoneField = ({ mode, value, onChange }: PhoneFieldProps) => {
+  mode = mode || 'inline';
+
+  if (mode == 'inline') {
+    return (
+      <InlinePhoneField />
+    );
+  }
+  else if (mode == 'split') {
+    return (
+      <SplitPhoneField />
+    );
+  }
 };
 
 export default PhoneField;
