227e0d7affcc630d6a287cd1f9552ad9c2c237a9
david-fiaty <dfiaty@gmail.com>
2025-11-19 15:21:43 +0000

    Update PhoneField.tsx

--- phone-field/PhoneField.tsx
+++ phone-field/PhoneField.tsx
@@ -13,7 +13,10 @@ interface PhoneFieldProps {
   rules?: any[];
   formatValue?: boolean;
   showFlags?: boolean;
+  style?: any;
   containerStyle?: any;
+  inputTextStyle?: any;
+  selectListStyle?: any;
   onChangeValue?: (data: any) => void;
   onValidateValue?: (data: any) => void;
 }
@@ -30,7 +33,10 @@ const PhoneField = ({
   rules, 
   formatValue,
   showFlags,
+  style,
   containerStyle,
+  inputTextStyle,
+  selectListStyle,
   onChangeValue, 
   onValidateValue
 }: PhoneFieldProps) => {
