037936ebff1da3381a0df32d8d96368314669d33
david-fiaty <dfiaty@gmail.com>
2025-11-19 16:31:54 +0000

    Code cleanup

--- phone-field/PhoneField.tsx
+++ phone-field/PhoneField.tsx
@@ -4,7 +4,7 @@ import SplitWidget from "./widget/SplitWidget";
 interface PhoneFieldProps {
   id?: any;
   ref?: any;
-  mode?: 'inline' | 'split';
+  layout?: 'inline' | 'split';
   value?: any;
   defaultValue?: any;
   defaultCountry?: any;
@@ -21,14 +21,14 @@ interface PhoneFieldProps {
 }
 
 const PhoneField = ( props: PhoneFieldProps ) => {
-  const mode: string = props?.mode || 'inline';
+  const layout: string = props?.layout || 'inline';
 
-  if (mode == 'inline') {
+  if (layout == 'inline') {
     return (
       <InlineWidget {...props} />
     );
   }
-  else if (mode == 'split') {
+  else if (layout == 'split') {
     return (
       <SplitWidget {...props} />
     );
--- phone-field/widget/InlineWidget.tsx
+++ phone-field/widget/InlineWidget.tsx
@@ -3,7 +3,7 @@ import { Text } from "react-native";
 interface PhoneFieldProps {
   id?: any;
   ref?: any;
-  mode?: 'inline' | 'split';
+  layout?: 'inline' | 'split';
   value?: any;
   defaultValue?: any;
   defaultCountry?: any;
@@ -12,8 +12,8 @@ interface PhoneFieldProps {
   rules?: any[];
   formatValue?: boolean;
   showFlags?: boolean;
-  modalSelection: boolean;
-  searchEnabled: boolean;
+  modalSelection?: boolean;
+  searchEnabled?: boolean;
   containerStyle?: any;
   onChangeValue?: (data: any) => void;
   onValidateValue?: (data: any) => void;
