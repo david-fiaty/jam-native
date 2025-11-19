86724191cc5022d1f3dc11318608ea2541dd665c
david-fiaty <dfiaty@gmail.com>
2025-11-19 15:34:46 +0000

    Update InlinePhoneField.tsx

--- phone-field/InlinePhoneField.tsx
+++ phone-field/InlinePhoneField.tsx
@@ -1,6 +1,24 @@
 import { Text } from "react-native";
 
-const InlinePhoneField = () => {
+interface PhoneFieldProps {
+  id?: any;
+  ref?: any;
+  mode?: 'inline' | 'split';
+  value?: any;
+  defaultValue?: any;
+  defaultCountry?: any;
+  placeholder?: string;
+  disabled?: boolean;
+  rules?: any[];
+  formatValue?: boolean;
+  showFlags?: boolean;
+  modalSelection: boolean;
+  containerStyle?: any;
+  onChangeValue?: (data: any) => void;
+  onValidateValue?: (data: any) => void;
+}
+
+const InlinePhoneField = ( props: PhoneFieldProps) => {
   return (
     <Text>Inline phone field</Text>
   );
