a4b450ddf05a340d4e0a74957b001ae4f4c3a57c
david-fiaty <dfiaty@gmail.com>
2026-01-24 15:41:27 +0000

    Update UserManager.tsx

--- manager/UserManager.tsx
+++ manager/UserManager.tsx
@@ -76,11 +76,8 @@ class UserManager {
     let variables: any = { '[profile_id]': profileId };
     let success: boolean = false;
 
-    let payload: any = FormManager.objectToFormData({ ...defaults, ...data }); 
-    //let payload: any = { ...defaults, ...data }; 
+    let payload: any = FormManager.objectToFormData({ ...defaults, ...data });  
     let response: any = await DataManager.put('updateProfile', payload, variables);
-
-    console.log(response);
      
     if (response?.id > 0) success = true;
 
