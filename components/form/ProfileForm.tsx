e3927228fa05d1c694198090782a11550d0497ba
david-fiaty <dfiaty@gmail.com>
2026-01-25 13:52:58 +0000

    Update ProfileForm.tsx

--- components/form/ProfileForm.tsx
+++ components/form/ProfileForm.tsx
@@ -40,7 +40,7 @@ const ProfileForm = ({ resource, onSubmit }: Props) => {
     else {
 
       let data: any = { ...formData };
-
+      
       delete data.profile_picture;
       if (data.hasOwnProperty('upload_profile_picture') && data.upload_profile_picture === null) {
         delete data.upload_profile_picture;
